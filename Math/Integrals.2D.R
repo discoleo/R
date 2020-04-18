cb260c16df05b3a405e66f5461488a7d862f6832
discoleo <leo.mada@syonic.eu>
2020-04-18 21:33:29 +0300

    Add files via upload

--- Math/Integrals.2D.R
+++ Math/Integrals.2D.R
@@ -2,12 +2,14 @@
 ### Leonard Mada
 ###
 ### Double Integrals
+### draft 0.1
 
 
 # including:
 # - Gamma(1/n);
 # - Int[0, Inf] (e^(-x^n));
-# - double integrals;
+# - double integrals:
+#   genralization to various powers;
 
 
 ### helper functions
@@ -40,18 +42,42 @@ int.f = function(i.f, lower=0, upper=Inf, print=TRUE, ...) {
 
 ### Exponential functions
 
-e4.f = function(x) {
-	exp(-x^4)
+e.f = function(x, pow=4) {
+	exp(-x^pow)
 }
-e4.2D.f = function(x, y) {
-	exp(-x^4-y^4)
+e.2D.f = function(x, y, pow=4) {
+	exp( -x^pow - y^pow)
 }
-e4C.2D.f = function(c, d) {
-	exp(-2*d) / 2 / sqrt(d^2 - c^4)
+eC.2D.f = function(c, d, pow=4) {
+	exp(-2*d) / 2 / sqrt(d^2 - c^pow)
 }
-e4Inner.f = function(x) {
-	Inner.Integral(x, e4.2D.f, inner.limits=c(0, Inf))
+
+# generate Inner Integrals
+Inner.gen = function(pow=4) {
+	ePow.2D.f = function(x, y) {
+		exp( -x^pow - y^pow)
+	}
+	eInner.f = function(x) {
+		Inner.Integral(x, ePow.2D.f, inner.limits=c(0, Inf))
+	}
+	return(eInner.f)
 }
+Inner.L.gen = function(pow=4) {
+	eCPow.2D.f = function(c, d) {
+		exp(-2*d) / sqrt(d^2 - c^pow) * 2 / pow
+	}
+	eInner.f = function(d) {
+		Inner.L.Integral(d, eCPow.2D.f, inner.limits.f=function(lim) lim^(2/pow),
+			subdivisions=512, rel.tol=1E-10, stop.on.error=F)
+	}
+	return(eInner.f)
+}
+
+
+### Cases: Pow = 4
+e4Inner.f = Inner.gen(4)
+
+# e4C.In.f = Inner.L.gen(4)
 e4C.In.f = function(d) {
 	Inner.L.Integral(d, e4C.2D.f, inner.limits.f=sqrt,
 		subdivisions=512, rel.tol=1E-10, stop.on.error=F)
@@ -59,13 +85,19 @@ e4C.In.f = function(d) {
 
 ################
 
+### n = 4
+n = 4
+
 # direct Integral
-g4 = int.f(e4.f)
-g4 * 4 / gamma(1/4)
+g4 = int.f(e.f, pow=n)
+g4 * n / gamma(1/n)
 
 # a double integral
-int.f(e4Inner.f)
-(gamma(1/4) / 4)^2
+# int.f(e4Inner.f)
+int.f(Inner.gen(n))
+# check result
+(gamma(1/n) / n)^2
 
-# change of variables
-int.f(e4C.In.f, subdivisions=512)
+### change of variables
+# int.f(e4C.In.f, subdivisions=512)
+int.f(Inner.L.gen(n), subdivisions=512)
