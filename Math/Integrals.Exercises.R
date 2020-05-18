d3e4ca4c343d96ff79c0b55c6baca9accc3b3d85
discoleo <leo.mada@syonic.eu>
2020-05-18 21:52:17 +0300

    Integrals: Exercises

---
+++ Math/Integrals.Exercises.R
@@ -0,0 +1,25 @@
+
+### Leonard Mada
+###
+### Integrals: exercises
+### draft 0.1
+
+
+### Various Exercises
+
+#####################
+
+#########
+### Ex. 1
+### https://www.youtube.com/watch?v=V5fjlHh9pR8
+
+# partial generalization
+
+a = 7
+
+integrate(function(x) 1/(a*cos(x)^4 + a*sin(x)^4 - 1), lower=0, upper=pi/4)
+# Exact solution
+1/(a-1) / sqrt(2 - 2/(a-1)) * pi/2
+
+
+
