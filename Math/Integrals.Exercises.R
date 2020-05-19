3f31a01ddd911ef535f7389d3d82e882591d85a2
discoleo <leo.mada@syonic.eu>
2020-05-19 09:21:33 +0300

    Integrals: more Exercises

--- Math/Integrals.Exercises.R
+++ Math/Integrals.Exercises.R
@@ -2,7 +2,7 @@
 ### Leonard Mada
 ###
 ### Integrals: exercises
-### draft 0.1
+### draft 0.1b
 
 
 ### Various Exercises
@@ -13,7 +13,8 @@
 ### Ex. 1
 ### https://www.youtube.com/watch?v=V5fjlHh9pR8
 
-# partial generalization
+### 1.a.) partial generalization
+### 1/(a*cos(x)^4 + a*sin(x)^4 - 1)
 
 a = 7
 
@@ -22,4 +23,31 @@ integrate(function(x) 1/(a*cos(x)^4 + a*sin(x)^4 - 1), lower=0, upper=pi/4)
 1/(a-1) / sqrt(2 - 2/(a-1)) * pi/2
 
 
+### 1.b.) partial generalization
 
+a = 7
+
+integrate(function(x) sin(2*x)*cos(2*x)/(a*cos(x)^4 + a*sin(x)^4 - 1), lower=0, upper=pi/4)
+# Exact solution
+-1/(2*a) * log((a - 2)/(2*a - 2))
+1/(2*a) * log((2*a - 2)/(a - 2))
+
+### 1.c.) TODO: variants using results from Ex. 2;
+
+
+###############
+
+
+#########
+### Ex. 2
+
+m = complex(re=cos(2*pi/3), im=sin(2*pi/3))
+
+upper = Inf
+integrate(function(x) 1/(x^4 - x^2 + 1), lower=0, upper=upper)
+# Exact solution
+exact = function(x) 1/(m^2 - m) * (m*atan(x*m) - m^2*atan(x*m^2))
+exact(upper) - exact(0)
+# computed
+exact(Inf)
+pi/2
