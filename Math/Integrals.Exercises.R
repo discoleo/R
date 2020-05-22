ae5e47cf629d03c0904eb69f92fedde34e32f750
discoleo <leo.mada@syonic.eu>
2020-05-22 11:34:30 +0300

    Integral Exercises: higher powers

--- Math/Integrals.Exercises.R
+++ Math/Integrals.Exercises.R
@@ -2,7 +2,7 @@
 ### Leonard Mada
 ###
 ### Integrals: Exercises
-### draft 0.3c
+### draft 0.4a
 
 
 ### Various Exercises
@@ -252,6 +252,35 @@ integrate(function(x) 1/(sin(x) + a), lower=lower, upper=upper)
 exact(upper, a) - exact(lower, a)
 
 
+### 1/(sin(x) + a * cos(x) + b)
+exact = function(x, a, b) {
+	c2 = 1/sqrt(a^2 + 1)
+	# TODO: correct issues when a < 0!
+	alfa = acos(c2) # atan2(a*c2, c2) # sign(a) * acos(c2)
+	c1 = b * c2
+	a.sq = sqrt(complex(re = 1 - c1^2, im=0))
+	# print(a.sq)
+	#
+	# c2/2 * log((1 - cos(x + alfa))/(1 + cos(x + alfa)))
+	# with fractions; alternative: atan;
+	r1 = c2/2 * 1/a.sq * log((a.sq - cos(x + alfa))/(a.sq + cos(x + alfa)))
+	# use result from 1/(sin(x)^2 - a^2);
+	r2 = c2/2 * 1/a.sq * log((a.sq * tan(x + alfa) - c1)/(a.sq * tan(x + alfa) + c1))
+	r1 - r2
+	#
+	r = c2/2 * 1/a.sq * log(
+		(a.sq - cos(x + alfa)) / (a.sq + cos(x + alfa)) /
+		(a.sq * tan(x + alfa) - c1)* (a.sq * tan(x + alfa) + c1))
+}
+# TODO: correct formula when a < 0!
+a = 2
+b = 3
+lower = 0
+upper = pi/2
+integrate(function(x) 1/(sin(x) + a * cos(x) + b), lower=lower, upper=upper)
+exact(upper, a, b) - exact(lower, a, b)
+
+
 ### 1/(sin(x)^2 - a^2) & 1/(sin(x)^2 + a^2)
 a = 1/2
 lower = pi/5
@@ -277,34 +306,132 @@ exact(upper, a * 1i) - exact(lower, a * 1i)
 # see https://github.com/discoleo/R/blob/master/Math/Integrals.Fractions.Unity.R
 # [the complete theory - still TODO]
 
-### cos(x) / (sin(x)^3 + cos(x)^3)
-exact = function(x) {
-	n = 3
+unity = function(n, sum=TRUE) {
 	m = complex(re=cos(2*pi/n), im=sin(2*pi/n))
+	if(sum) {
+		half = (n-1) %/% 2
+		m.m = m^(1:half)
+		m.m = cbind(m.m, 1/m.m)
+		sum = m.m[,1] + m.m[,2]
+		return(list("m"=m.m, "sum"=sum))
+	}
+	return(m)
+}
+### ONLY n = ODD!
+exact = function(x, n=3) {
+	m.all = unity(n)
+	m = m.all$m
+	m.sum = m.all$sum
 	b0 = 1/n
-	m.sum = (m + m^2)
 	a = b0 * m.sum
 	m.shift = m.sum/2
 	D = -2*b0 + a*m.shift
 	m.sq = sqrt(1 - m.shift^2)
 	#
 	b0 * log(-tan(x) - 1 + 0i) +
-	(a/2*log((tan(x) + m)*(tan(x) + m^2))) +
-	(D / m.sq * atan(-(tan(x) + m.shift)/m.sq))
+	sum(a/2*log((tan(x) + m[,1])*(tan(x) + m[,2]))) +
+	sum(D / m.sq * atan(-(tan(x) + m.shift)/m.sq))
 }
+pseudo.exact = function(n, x.pow, lower, upper) {
+	# faking now: x^x.pow / (x^n + 1) dx
+	# for exact solutions:
+	# see https://github.com/discoleo/R/blob/master/Math/Integrals.Fractions.Unity.R
+	integrate(function(x) tan(x)^x.pow/cos(x)^2 / (tan(x)^n + 1), lower=lower, upper=upper)
+}
+log.exact = function(lower, upper, n) {
+	log((sin(upper)^n + cos(upper)^n) / (sin(lower)^n + cos(lower)^n)) / n
+}
+
+##########
+### cos(x) / (sin(x)^3 + cos(x)^3)
 #
+n = 3
 lower = pi/10
 upper = pi/2
 integrate(function(x) cos(x) / (sin(x)^3 + cos(x)^3), lower=lower, upper=upper)
-exact(upper) - exact(lower)
+exact(upper, n) - exact(lower, n)
 
 
 integrate(function(x) sin(x) / (sin(x)^3 + cos(x)^3), lower=lower, upper=upper)
-- exact(pi/2 - upper) + exact(pi/2 - lower)
+- exact(pi/2 - upper, n) + exact(pi/2 - lower, n)
 
-### TODO: power 5 (trivial)
+
+####################
+### Powers 5, 7, ...
+### (trivial)
+
+### n = 5
+n = 5
+lower = pi/10
+upper = pi/2
 integrate(function(x) cos(x)^3 / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+exact(upper, n) - exact(lower, n)
+
+
+
+### n = 7
+n = 7
+lower = pi/10
+upper = pi/2
 integrate(function(x) cos(x)^5 / (sin(x)^7 + cos(x)^7), lower=lower, upper=upper)
+exact(upper, n) - exact(lower, n)
+
+
+### Workout ALL subtypes
+### A[i, j] = sin(x)^i * cos(x)^j / (sin(x)^n + cos(x)^n)
+
+### n = 5
+n = 5
+lower = pi/10
+upper = pi/2
+
+### A[3,0] & A[0, 3]
+integrate(function(x) sin(x)^3 / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+pseudo.exact(n, 3, lower, upper) # see also different solution above
+#
+integrate(function(x) cos(x)^3 / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+exact(upper, n) - exact(lower, n)
+
+### A[1, 2] & A[2, 1]
+integrate(function(x) sin(x)*cos(x)^2 / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+pseudo.exact(n, 1, lower, upper)
+#
+integrate(function(x) sin(x)^2*cos(x) / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+pseudo.exact(n, 2, lower, upper)
+
+### A[1, 0] & A[0, 1] = (A[1, 2] + A[3, 0], A[2, 1] + A[0, 3])
+integrate(function(x) sin(x) / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+pseudo.exact(n, 1, lower, upper)$value + pseudo.exact(n, 3, lower, upper)$value
+#
+integrate(function(x) cos(x) / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+pseudo.exact(n, 2, lower, upper)$value + pseudo.exact(n, 0, lower, upper)$value
+
+
+### TODO:
+### A[2, 2] = (A[4,0] + A[0,4])
+
+
+### A[5, 0] + A[0, 5] = x
+# sin(x)^5 + cos(x)^5 =
+# = (sin(x) + cos(x))*(1 - 2*A[2,2] - A[1,1] + A[2,2])
+# = (sin(x) + cos(x))*(1 - A[2,2] - A[1,1])
+# = A[1,0] + A[0,1] - A[3,2] - A[2,3] - A[2,1] - A[1,2]
+# = (A[1,0] + A[0,1] - A[2,1] - A[1,2]) - A[3,2] - A[2,3]
+# Test
+integrate(function(x) (cos(x)*sin(x)^4 - sin(x)*cos(x)^4) / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+log.exact(lower, upper, n)
+# A[3, 2] & A[2, 3]
+# A[3, 2]
+integrate(function(x) sin(x)^3*cos(x)^2 / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+A32 = 1/2 * (log.exact(lower, upper, n) +
+		pseudo.exact(n, 1, lower, upper)$value - pseudo.exact(n, 2, lower, upper)$value -
+	(upper - lower - pseudo.exact(n, 3, lower, upper)$value - pseudo.exact(n, 0, lower, upper)$value) )
+A32
+# A[2, 3]
+integrate(function(x) sin(x)^2*cos(x)^3 / (sin(x)^5 + cos(x)^5), lower=lower, upper=upper)
+A23 = (A32 - log.exact(lower, upper, n) +
+	pseudo.exact(n, 2, lower, upper)$value - pseudo.exact(n, 1, lower, upper)$value)
+A23
 
 
 
@@ -316,5 +443,8 @@ lower = pi/10
 upper = pi/2
 integrate(function(x) cos(x) / ( sqrt(sin(x)^2 + 1) * (1 + sin(x)) ), lower=lower, upper=upper)
 exact = function(x) sqrt(2)/4 * log((1 - cos(atan(sin(x))+pi/4))/(1 + cos(atan(sin(x))+pi/4)))
+exact = function(x) sqrt(2)/4 * log(
+	(sqrt((sin(x)^2 +1)*2) - 1 + sin(x)) /
+	(sqrt((sin(x)^2 +1)*2) + 1 - sin(x)))
 exact(upper) - exact(lower)
 
