b9be96c9e7efeff36b8d24cf70898cb2bf036acb
discoleo <leo.mada@syonic.eu>
2020-05-29 03:44:47 +0300

    Problems: added description/comments

--- Math/Fractals.R
+++ Math/Fractals.R
@@ -2,7 +2,13 @@
 ### Leonard Mada
 ###
 ### Fractals
+### draft v 0.1b
 
+
+### TODO:
+# Rachel Skipper: Finiteness properties for simple groups
+# https://www.youtube.com/watch?v=AWgoI5jspYc
+# - is it meaningful to apply various formalisms?
 
 inner.poly = function(center, width, isH=TRUE, alpha=1/3) {
 	if(isH) {
@@ -28,8 +34,18 @@ plot.new()
 plot.window(xlim=c(0.5,5.5), ylim=c(.5,3.5))
 polygon(c(1,1,5,5), c(1,3,3,1), col="black")
 
-# the Fractal
+# Problems:
 # Q: What is the Fractal Dimension?
+# small axis:
+# - extension possible only for 1 generation;
+# - OR different scaling for elements on small axis: 1/4 x 1/2 of original scaling;
+
+
+# Fractal
+
+# TODO:
+# - correct some of the positions (on L2, L3);
+# - see also the problems with the small axis;
 
 # L1
 inner.poly(c(3, 2), 1)
@@ -47,6 +63,7 @@ inner.poly(c((2+2.5)/2, 3-0.5/2), 0.25, isH=F)
 inner.poly(c((3.5+4)/2, 3-0.5/2), 0.25, isH=F)
 inner.poly(c((2+2.5)/2, 1+0.5/2), 0.25, isH=F)
 inner.poly(c((3.5+4)/2, 1+0.5/2), 0.25, isH=F)
+# + another 4 pieces for L3;
 
 
 par(par.old)
