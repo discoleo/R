881906614e11d83ce68fcc7357767dcd0c605a73
discoleo <leo.mada@syonic.eu>
2020-08-02 20:49:52 +0300

    extended with another parameter: K + 3*x - 5*x^3 + 3*x^5 + x^6
    - solved all:
    K + 3*n^2*x + 3*n*(n-1)*x^2 - (6*n-1)*x^3 - 3*(n-1)*x^4 + 3*x^5 + x^6 = 0;

--- Math/Polynomials.Derived.P6.R
+++ Math/Polynomials.Derived.P6.R
@@ -7,7 +7,7 @@
 ### P6 Polynomials
 ### Derived from Special Factorizations
 ###
-### draft v.0.3e
+### draft v.0.3e-bis
 
 
 ### Factorization of the P6 Polynomials
@@ -54,10 +54,11 @@
 #####################
 
 ### History
-# draft v.0.3e:
+# draft v.0.3e+bis:
 # - solved:
 #   K + 3*x - 5*x^3 + 3*x^5 + x^6 = 0;
 #   K + 3*x + 6*x^2 + 7*x^3 + 6*x^4 + 3*x^5 + x^6 = 0;
+#   K + 3*n^2*x + 3*n*(n-1)*x^2 - (6*n-1)*x^3 - 3*(n-1)*x^4 + 3*x^5 + x^6 = 0;
 # draft v.0.3c-d(bis):
 # - solved: -1 + b1*x - b2*x^2 + b3*x^3 + b2*x^4 + b1*x^5 + x^6 = 0;
 # - technique to generate symmetrical P12 polynomials
@@ -901,6 +902,17 @@ x = sapply(k, function(k) roots(c(1,1i, k-1))) / 1i
 p = mult.p(c((k[1]-1), 1i, 1), c((k[2]-1), 1i, 1))
 p = round0(mult.p(p, c((k[3]-1), 1i, 1)))
 -K + 3*x + 6*x^2 + 7*x^3 + 6*x^4 + 3*x^5 + x^6
+
+
+### *** ALL K & n ***
+K = 3
+n = 2
+k = (K+n^3)^(1/3) * m3.all
+x = sapply(k, function(k) roots(c(1,1, k-n)))
+p = mult.p(c((k[1]-n), 1,1), c((k[2]-n), 1,1))
+p = round0(mult.p(p, c((k[3]-n), 1,1)))
+p
+K + 3*n^2*x + 3*n*(n-1)*x^2 - (6*n-1)*x^3 - 3*(n-1)*x^4 + 3*x^5 + x^6
 
 
 ###########################
