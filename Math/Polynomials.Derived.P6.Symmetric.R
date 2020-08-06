e4dd9d22c5e318668f33e445e4da93d916a176c6
discoleo <leo.mada@syonic.eu>
2020-08-07 02:39:14 +0300

    moved from Polynomials.Derived.P6.R
    - separate file;

---
+++ Math/Polynomials.Derived.P6.Symmetric.R
@@ -0,0 +1,641 @@
+
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### P6 Polynomials:
+### Symmetric Polynomials
+###
+### draft v.0.1a
+
+
+### Decomposition of Symmetric Polynomials of order 2n
+### into Polynomials of order n
+
+# - every *strictly* symmetric polynomial of order 2*n
+#   can be "decomposed" into polynomials of order n;
+# - strictly symmetric:
+#   1 + b1*x + b2*x^2 + ... + b2*x^(n-2) + b1*x^(n-1) + x^n;
+#   b[j] = b[n-j] ***and*** b0 == 1;
+# - "strictily" symmetric polynomials of order 2*n+1
+#   have the trivial root x = -1 and can be factored into:
+#   (x+1)*P[2n], where P[2n] is strictly symmetric;
+
+
+################
+
+### History
+# draft v.0.1a:
+# - new file, moved from Polynomials.Derived.P6.R
+#   (during the last subversion of v.0.3b);
+
+
+### TODO:
+# - rework/simplify the P8 as well;
+
+
+################
+
+library(polynom)
+library(pracma)
+
+### helper functions
+unity = function(n=3, all=TRUE) {
+	m = complex(re=cos(2*pi/n), im=sin(2*pi/n))
+	if(all) {
+		m = m^(0:(n-1))
+	}
+	return(m)
+}
+mult.p = function(p1, p2) {
+	p.m = outer(p1, p2)
+    p = as.vector(tapply(p.m, row(p.m) + col(p.m), sum))
+	return(p)
+}
+round0.p = function(p, tol=1E-7) {
+	p = round0(as.vector(p), tol=tol)
+	class(p) = "polynomial"
+	return(p)
+}
+round0 = function(m, tol=1E-7) {
+	m[abs(Re(m)) < tol & abs(Im(m)) < tol] = 0
+	isZero = (Re(m) != 0) & (abs(Re(m)) < tol)
+	if(sum(isZero) > 0) {
+		m[isZero] = complex(re=0, im=Im(m[isZero]))
+	}
+	isZero = (Im(m) != 0) & (abs(Im(m)) < tol)
+	if(sum(isZero) > 0) {
+		m[isZero] = Re(m[isZero])
+	}
+	return(m)
+}
+### Solver: simplified
+solve.p6sym = function(b, type="symmetric") {
+	# b = symmetric coefficients
+	# 1 + b1*x + b2*x^2 + b3*x^3 + b2*x^4 + b1*x^5 + x^6
+	if(length(b) == 3) {
+		if(type == "minus") {
+			r = roots(c(1, b[1], -b[2]+3, b[3] + 2*b[1]))
+			b0 = -1;
+		} else {
+			r = roots(c(1, b[1], b[2]-3, b[3] - 2*b[1]))
+			b0 = 1;
+		}
+		x = sapply(r, function(r) roots(c(1,-r,b0)))
+		p = round0.p(poly.calc(x))
+		b = c(b0, b, rev(b[-3]), 1)
+		if(type == "minus") {
+			b[5] = -b[5]; # b4
+		}
+		id = 0:6
+		err = sapply(x, function(x) round0(sum(b*x^id)))
+		return(list(x=x, p=p, err=err))
+	} else {
+		print("Not yet implemented!")
+	}
+}
+
+################
+
+### Examples:
+
+###
+b = c(-2, 0, -1)
+p = solve.p6sym(b)
+p
+1 - 2*x - x^3 - 2*x^5 + x^6
+
+###
+b = c(-2, 3, 2)
+p = solve.p6sym(b)
+p
+1 - 2*x + 3*x^2 + 2*x^3 + 3*x^4 - 2*x^5 + x^6
+
+###
+b = c(1, 3, 3)
+p = solve.p6sym(b)
+p
+1 + x + 3*x^2 + 3*x^3 + 3*x^4 + x^5 + x^6
+
+##############
+
+##############
+### Series ###
+
+### 1 + b1*x + b1*x^5 + x^6
+p = sapply(-5:5, function(b) print(solve.p6sym(c(b, 0, 0))$p))
+1 - 5*x - 5*x^5 + x^6 
+1 - 4*x - 4*x^5 + x^6 
+1 - 3*x - 3*x^5 + x^6 
+1 - 2*x - 2*x^5 + x^6 
+1 - x - x^5 + x^6 
+1 + x^6 
+1 + x + x^5 + x^6 
+1 + 2*x + 2*x^5 + x^6 
+1 + 3*x + 3*x^5 + x^6 
+1 + 4*x + 4*x^5 + x^6 
+1 + 5*x + 5*x^5 + x^6
+
+### e.g. - x^2 - x^4
+p = sapply(-5:5, function(b) print(solve.p6sym(c(-1, -1, b))$p))
+1 - x - x^2 - 5*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 - 4*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 - 3*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 - 2*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 - x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 - 0 - x^4 - x^5 + x^6 
+1 - x - x^2 + x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 + 2*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 + 3*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 + 4*x^3 - x^4 - x^5 + x^6 
+1 - x - x^2 + 5*x^3 - x^4 - x^5 + x^6
+### e.g. 0*(x^2 + x^4)
+p = sapply(-5:5, function(b) print(solve.p6sym(c(-1, 0, b))$p))
+1 - x - 5*x^3 - x^5 + x^6 
+1 - x - 4*x^3 - x^5 + x^6 
+1 - x - 3*x^3 - x^5 + x^6 
+1 - x - 2*x^3 - x^5 + x^6 
+1 - x - x^3 - x^5 + x^6 
+1 - x - x^5 + x^6 
+1 - x + x^3 - x^5 + x^6 
+1 - x + 2*x^3 - x^5 + x^6 
+1 - x + 3*x^3 - x^5 + x^6 
+1 - x + 4*x^3 - x^5 + x^6 
+1 - x + 5*x^3 - x^5 + x^6
+### e.g. +(x^2 + x^4)
+p = sapply(-5:5, function(b) print(solve.p6sym(c(-1, 1, b))$p))
+1 - x + x^2 - 5*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 - 4*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 - 3*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 - 2*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 - x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 - 0 + x^4 - x^5 + x^6 
+1 - x + x^2 + x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 + 2*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 + 3*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 + 4*x^3 + x^4 - x^5 + x^6 
+1 - x + x^2 + 5*x^3 + x^4 - x^5 + x^6
+
+
+### Minus derivation of *Strictly* Symmetric
+# -1 + b1*x + b2*x^2 + b3*x^3 - b2*x^4 + b1*x^5 + x^6
+
+# [in the old version] [deprecated]
+# b1 => b1*1i, b3 => b3*-1i; x = -1i * x;
+# p6sq.gen(c(1, b1 + 6, 4*b1 + 9 + b2, 2*b1 + 2 + 2*b2 + b3)
+
+
+### "Minus"-series: +b2*x^2 - b2*x^4 & b0 = -1;
+p = sapply(-5:5, function(b) print(solve.p6sym(c(-1, 1, b), type="minus")$p))
+-1 - x + x^2 - 5*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 - 4*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 - 3*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 - 2*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 - x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 - 0 - x^4 - x^5 + x^6 
+-1 - x + x^2 + x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 + 2*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 + 3*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 + 4*x^3 - x^4 - x^5 + x^6 
+-1 - x + x^2 + 5*x^3 - x^4 - x^5 + x^6
+
+
+###
+p = solve.p6sym(c(1,2,2), type="minus")
+x = p$x
+-1 + x + 2*x^2 + 2*x^3 - 2*x^4 + x^5 + x^6
+
+###
+p = solve.p6sym(c(1,-2,2), type="minus")
+x = p$x
+-1 + x - 2*x^2 + 2*x^3 + 2*x^4 + x^5 + x^6
+
+###
+p = solve.p6sym(c(1,3,3), type="minus")
+x = p$x
+-1 + x + 3*x^2 + 3*x^3 - 3*x^4 + x^5 + x^6
+
+###
+p = solve.p6sym(c(1,-3,3), type="minus")
+x = p$x
+-1 + x - 3*x^2 + 3*x^3 + 3*x^4 + x^5 + x^6
+
+###
+p = solve.p6sym(c(-2,-3,5), type="minus")
+x = p$x
+-1 - 2*x - 3*x^2 + 5*x^3 + 3*x^4 - 2*x^5 + x^6
+
+
+
+#################
+
+### for P8: [old code]
+# p6sq.gen(c(1, 8 + b1, 20 + 6*b1 + b2, 16 + 9*b1 + 4*b2 + b3, 2 + 2*b1 + 2*b2 + 2*b3 + b4))
+b1 = 1; b2 = 0; b3 = 0; b4 = 1;
+# set the function parameter as the coefficient to list
+sapply(-6:6, function(b2) print(p6sq.gen(c(1, 8 + b1, 20 + 6*b1 + b2, 16 + 9*b1 + 4*b2 + b3, 2 + 2*b1 + 2*b2 + 2*b3 + b4))$p))
+
+
+
+
+#########################
+#########################
+
+
+# experimental: power 3
+p6sq3.old.gen = function(p3.coeff, mult=1, b0=-1, asSq=TRUE) {
+	m = unity(3, all=F)
+	r = c(roots(p3.coeff))
+	r = ifelse(Im(r) == 0 & Re(r) < 0, - (-Re(r))^(1/3), r^(1/3))
+	x1 = c(r, m*r, m^2*r)
+	# x1 = c(sqrt(r+0i), -(sqrt(r+0i)))
+	x = sapply(x1, function(r) roots(c(1, mult*r, 0, b0)))
+	p = poly.calc(x)
+	for(i in 1:length(p)) p[[i]] = round0(p[[i]])
+	if(asSq) {
+		x = x[1,]^2 # the squares => P6
+		len = length(p3.coeff)*4 - 3
+		p = polynomial(p[seq(from=1, to=len, by=2)])
+	}
+	return(list(x=x, p=p))
+}
+# new version
+p6sq3.gen = function(p3.coeff, mult=1, b0=-1, asSq=T) {
+	# m = unity(3, all=F)
+	r = c(roots(p3.coeff))
+	x = sapply(r, function(r) roots(c(1, -3*b0 + mult*r, 3*b0^2 + mult*r, -b0^3)))
+	p = poly.calc(x)
+	for(i in 1:length(p)) p[[i]] = round0(p[[i]])
+	return(list(x=x, p=p))
+}
+
+# p6sq.gen(c(1, b1 + 6, 4*b1 + 9 + b2, 2*b1 + 2 + 2*b2 + b3)
+
+### Test
+p = p6sq3.gen(c(1,-1,1,-1), asSq=F)
+p
+
+p = p6sq3.gen(c(1,0,1,0,-1), asSq=F)
+polynomial(p$p) / polynomial(c(1,1))^4
+1 + 8*x + 29*x^2 + 60*x^3 + 75*x^4 + 60*x^5 + 29*x^6 + 8*x^7 + x^8
+
+###
+p = p6sq3.gen(c(1,-15,30))
+x = p$x
+1 + 21*x + 105*x^2 + 170*x^3 + 105*x^4 + 21*x^5 + x^6
+
+########################
+########################
+
+
+### Old
+### TODO: cleanup
+
+
+### Strictly Symmetric P6
+
+
+#######################
+### *Strictly* Symmetic
+# b0 == 1
+# 1 + b1*x + b2*x^2 + b3*x^3 + b2*x^4 + b1*x^5 + x^6
+# new simplified version: solve.p6sym(c(b1, b2, b3))
+# old variant:
+# p6sq.gen(c(1, b1 + 6, 4*b1 + 9 + b2, 2*b1 + 2 + 2*b2 + b3)
+
+
+### Examples
+
+p = sapply(-6:6, function(b) print(solve.p6sym(c(b, 0, 0))$p))
+# the values of the roots must be extracted explicitly from solve.p6sym();
+1 - 5*x - 5*x^5 + x^6
+1 - 4*x - 4*x^5 + x^6
+1 - 3*x - 3*x^5 + x^6
+1 - 2*x - 2*x^5 + x^6
+1 - x - x^5 + x^6
+1 + x^6
+1 + x + x^5 + x^6 # basically -x;
+1 + 2*x + 2*x^5 + x^6
+1 + 3*x + 3*x^5 + x^6
+1 + 4*x + 4*x^5 + x^6
+1 + 5*x + 5*x^5 + x^6
+p = sapply(-6:6, function(b) print(solve.p6sym(c(-1, 0, b))$p))
+1 - x - 6*x^3 - x^5 + x^6 
+1 - x - 5*x^3 - x^5 + x^6 
+1 - x - 4*x^3 - x^5 + x^6 
+1 - x - 3*x^3 - x^5 + x^6 
+1 - x - 2*x^3 - x^5 + x^6 
+1 - x - x^3 - x^5 + x^6 
+1 - x - x^5 + x^6 
+1 - x + x^3 - x^5 + x^6 
+1 - x + 2*x^3 - x^5 + x^6 
+1 - x + 3*x^3 - x^5 + x^6 
+1 - x + 4*x^3 - x^5 + x^6 
+1 - x + 5*x^3 - x^5 + x^6 
+1 - x + 6*x^3 - x^5 + x^6
+
+series.id = -5:5
+p = sapply(series.id, function(b) print(solve.p6sym(c(0,-1, b))$p))
+p = sapply(series.id, function(b) print(solve.p6sym(c(1, 1, b))$p))
+p = sapply(series.id, function(b) print(solve.p6sym(c(5, b, -2))$p))
+p = sapply(series.id, function(b) print(solve.p6sym(c(5, b, -3))$p))
+p = sapply(series.id, function(b) print(solve.p6sym(c(-2, -2, b))$p))
+p = sapply(series.id, function(b) print(solve.p6sym(c(-3, -3, b))$p))
+p = sapply(series.id, function(b) print(solve.p6sym(c(-3, -4, b))$p))
+
+
+### 1 - x - x^2 - x^4 - x^5 + x^6
+p = solve.p6sym(c(-1,-1, 0))
+p
+x = p$x
+1 - x - x^2 - x^4 - x^5 + x^6
+
+
+### [old workout]
+### 1 + x - x^2 - x^4 + x^5 + x^6 [same as previous]
+# the initial workout of the solution;
+x = solve(polynomial(c(1,1,-1,0,-1,1,1)))
+1 + x - x^2 - x^4 + x^5 + x^6
+
+poly.calc(x-1/x)
+x.r = x - 1/x
+-4 - 4*x.r^2 + 3*x.r^4 + x.r^6
+poly.calc((x-1/x)[c(1,3,5)]^2 + 1)
+2 - 7*x + x^3
+# Solution starts from: 2 - 7*x + x^3;
+
+
+# Analysis [old]
+p = poly.calc(x[c(3:6)])
+p
+# p has the coefficients as roots of the following polynomials:
+x.r = p[[2]]
+x.r^3 - 2*x.r^2 - 3*x.r + 2
+x.r = p[[3]]
+x.r^3 - 2*x.r^2 - 6*x.r + 8
+
+p = poly.calc(x[c(1:2)])
+p
+# p has the coefficient as roots of the following polynomial:
+x.r = p[[2]]
+x.r
+x.r^3 - x.r^2 - 4*x.r + 2
+2 - 4*x.r - x.r^2 + x.r^3
+
+
+#################
+
+
+### TODO: cleanup
+
+### 1 - 3*x - 3*x^5 + x^6
+r = roots(c(1,3,-3,-4))
+x1 = c(sqrt(r+0i), -(sqrt(r+0i)))
+x = sapply(x1, function(r) roots(c(1,r,-1)))
+poly.calc(x)
+1 - 3*x^2 - 3*x^10 + x^12
+x = x[1,]^2
+1 - 3*x - 3*x^5 + x^6
+
+
+### 1 - 3*x + 2*x^3 - 3*x^5 + x^6
+r = roots(c(1,3,-3,-2))
+x1 = c(sqrt(r+0i), -(sqrt(r+0i)))
+x = sapply(x1, function(r) roots(c(1,r,-1)))
+poly.calc(x)
+1 - 3*x^2 + 2*x^6 - 3*x^10 + x^12
+x = x[1,]^2
+1 - 3*x + 2*x^3 - 3*x^5 + x^6
+
+
+### 1 - 6*x^2 + 8*x^3 - 6*x^4 + x^6
+r = roots(c(1,6,+3,-2))
+x1 = c(sqrt(r+0i), -(sqrt(r+0i)))
+x = sapply(x1, function(r) roots(c(1,r,-1)))
+poly.calc(x)
+1 - 6*x^4 + 8*x^6 - 6*x^8 + x^12
+x = x[1,]^2
+1 - 6*x^2 + 8*x^3 - 6*x^4 + x^6
+
+
+### 1 - 10*x^2 + 16*x^3 - 10*x^4 + x^6
+r = roots(c(1,6,-1,-2))
+x1 = c(sqrt(r+0i), -(sqrt(r+0i)))
+x = sapply(x1, function(r) roots(c(1,r,-1)))
+poly.calc(x)
+1 - 10*x^4 + 16*x^6 - 10*x^8 + x^12
+x = x[1,]^2
+1 - 10*x^2 + 16*x^3 - 10*x^4 + x^6
+
+
+### 1 - 3*x + 4*x^2 - 6*x^3 + 4*x^4 - 3*x^5 + x^6
+r = roots(c(1,3,1,-2))
+x1 = c(sqrt(r+0i), -(sqrt(r+0i)))
+x = sapply(x1, function(r) roots(c(1,r,-1)))
+poly.calc(x)
+1 - 3*x^2 + 4*x^4 - 6*x^6 + 4*x^8 - 3*x^10 + x^12
+x = x[1,]^2
+1 - 3*x + 4*x^2 - 6*x^3 + 4*x^4 - 3*x^5 + x^6
+
+
+###
+sapply(-6:6, function(b) print(p6sq.gen(c(1, 5, 6, b))$p))
+
+sapply(-6:6, function(b) print(p6sq.gen(c(1,5, b,-2))$p))
+
+sapply(-10:10, function(b) print(p6sq.gen(c(1, 5, 8, b))$p))
+
+
+# the full series:
+sapply(-6:6, function(b) print(p6sq.gen(c(1,5, 5, b))$p))
+# 1 - x - b*x^3 - x^5 + x^6 # b from -6 to +6 (fully generalized);
+
+### 1 - x - x^3 - x^5 + x^6
+p = p6sq.gen(c(1,5, 5, -1))
+x = p$x[1,]^2
+1 - x - x^3 - x^5 + x^6
+
+### 1 - x + x^3 - x^5 + x^6
+# factorization: (x^2-x+1), but still part of a series;
+p = p6sq.gen(c(1,5, 5, 1))
+x = p$x[1,]^2
+1 - x + x^3 - x^5 + x^6
+
+
+### 1 - x - 4*x^3 - x^5 + x^6
+p = p6sq.gen(c(1,5, 5,-4))
+p
+x = p$x[1,]^2
+1 - x - 4*x^3 - x^5 + x^6
+
+### trivial, but still interesting
+
+### 1 - x - 3*x^3 - x^5 + x^6
+# factorization: (x^2+x+1)*...
+p = p6sq.gen(c(1,5, 5,-3))
+p
+x = p$x[1,]^2
+1 - x - 3*x^3 - x^5 + x^6
+
+### 1 - x - 2*x^3 - x^5 + x^6
+# roots: include +i, -i
+p = p6sq.gen(c(1,5, 5,-2))
+p
+x = p$x[1,]^2
+1 - x - 2*x^3 - x^5 + x^6
+
+
+###
+p6sq.gen(c(1,5,-1,-2))
+1 - x^2 - 6*x^4 + 10*x^6 - 6*x^8 - x^10 + x^12
+
+
+###########################
+###########################
+
+#######################
+### Symmetrical P12 ###
+
+m1 = complex(re=cos(pi/3), im=sin(pi/3)) # -1!
+m3 = complex(re=cos(2*pi/3), im=sin(2*pi/3))
+c2 = 2*cos(2*pi/5 * 1:2)
+c3 = 2*cos(2*pi/7 * 1:3)
+
+###
+r = roots(c(1,3, 0,0,0, 3,1))
+x = c(r * m1, r/m1)
+poly.calc(x)
+1 + 3*x + 9*x^2 + 3*x^5 - 7*x^6 + 3*x^7 + 9*x^10 + 3*x^11 + x^12
+
+###
+r1 = roots(c(1,3*m1, 0,0,0, 3*m1,1))
+r2 = roots(c(1,3/m1, 0,0,0, 3/m1,1))
+x = c(r1, r2)
+poly.calc(x)
+1 + 3*x + 9*x^2 + 3*x^5 + 20*x^6 + 3*x^7 + 9*x^10 + 3*x^11 + x^12
+
+
+### Mixing m1 with m-1
+r1 = roots(c(1,3*m1, 2*m3,0,2*m3, 3*m1,1))
+r2 = roots(c(1,3/m1, 2/m3,0,2/m3, 3/m1,1))
+x = c(r1, r2)
+poly.calc(x)
+1 + 3*x + 7*x^2 + 6*x^3 + 2*x^4 + 9*x^5 + 28*x^6 + 9*x^7 + 2*x^8 + 6*x^9 + 7*x^10 + 3*x^11 + x^12
+
+### Mixing m1 with m-1
+r1 = roots(c(1,3*m1, 2/m3,0,2/m3, 3*m1,1))
+r2 = roots(c(1,3/m1, 2*m3,0,2*m3, 3/m1,1))
+x = c(r1, r2)
+poly.calc(x)
+1 + 3*x + 7*x^2 - 12*x^3 + 2*x^4 - 9*x^5 + 28*x^6 - 9*x^7 + 2*x^8 - 12*x^9 + 7*x^10 + 3*x^11 + x^12
+
+### Only m-1
+r1 = roots(c(1,3*m1, 2/m1,0,2/m1, 3*m1,1))
+r2 = roots(c(1,3/m1, 2*m1,0,2*m1, 3/m1,1))
+x = c(r1, r2)
+poly.calc(x)
+1 + 3*x + 11*x^2 - 6*x^3 + 6*x^4 - 3*x^5 + 28*x^6 - 3*x^7 + 6*x^8 - 6*x^9 + 11*x^10 + 3*x^11 + x^12
+
+
+### Sqrt()
+r1 = roots(c(1, 3*sqrt(2), 2-sqrt(2),0,2-sqrt(2), 3*sqrt(2),1))
+r2 = roots(c(1,-3*sqrt(2), 2+sqrt(2),0,2+sqrt(2),-3*sqrt(2),1))
+x = c(r1, r2)
+poly.calc(x)
+1 - 14*x^2 + 12*x^3 + 6*x^4 + 12*x^5 - 30*x^6 + 12*x^7 + 6*x^8 + 12*x^9 - 14*x^10 + x^12
+
+
+### Sqrt()
+r1 = roots(c(1, 1i,-1i, 0,-1i, 1i,1))
+r2 = roots(c(1,-1i, 1i, 0, 1i,-1i,1))
+x = c(r1, r2)
+poly.calc(x)
+1 + x^2 - 2*x^3 + x^4 - 2*x^5 + 6*x^6 - 2*x^7 + x^8 - 2*x^9 + x^10 + x^12
+
+
+### cos(2*pi/5)
+r1 = roots(c(1, c2[1], 0,0,0, c2[1],1))
+r2 = roots(c(1, c2[2], 0,0,0, c2[2],1))
+x = c(r1, r2)
+poly.calc(x)
+1 - x - x^2 - x^5 - x^7 - x^10 - x^11 + x^12
+
+
+### cos(2*pi/7)
+r1 = roots(c(1, c3[1], 0,0,0, c3[1],1))
+r2 = roots(c(1, c3[2], 0,0,0, c3[2],1))
+r3 = roots(c(1, c3[3], 0,0,0, c3[3],1))
+x = c(r1, r2, r3)
+poly.calc(x)
+1 - x - 2*x^2 + x^3 - x^5 - x^6 + x^7 - 2*x^8 - 2*x^10 +  
++ x^11 - x^12 - x^13 + x^15 - 2*x^16 - x^17 + x^18
+
+
+### cos(2*pi/7)
+r1 = roots(c(1, c3[1]^2 - c3[1], 0,0,0, c3[1]^2 - c3[1],1))
+r2 = roots(c(1, c3[2]^2 - c3[2], 0,0,0, c3[2]^2 - c3[2],1))
+r3 = roots(c(1, c3[3]^2 - c3[3], 0,0,0, c3[3]^2 - c3[3],1))
+x = c(r1, r2, r3)
+poly.calc(x)
+1 + 6*x + 5*x^2 + x^3 + 6*x^5 + 13*x^6 + 15*x^7 + 5*x^8 + 5*x^10 +
++ 15*x^11 + 13*x^12 + 6*x^13 + x^15 + 5*x^16 + 6*x^17 + x^18
+
+
+### (...)^(1/3)
+k = 2^(1/3) * c(1, m3, m3^2)
+r1 = roots(c(1, k[1], 0,0,0, k[1],1))
+r2 = roots(c(1, k[2], 0,0,0, k[2],1))
+r3 = roots(c(1, k[3], 0,0,0, k[3],1))
+x = c(r1, r2, r3)
+poly.calc(x)
+1 + 2*x^3 + 3*x^6 + 6*x^7 + 6*x^11 + 3*x^12 + 2*x^15 + x^18
+#
+r1 = roots(c(1, k[1] * 1i, 0,0,0, k[1] * 1i,1)) / 1i
+r2 = roots(c(1, k[2] * 1i, 0,0,0, k[2] * 1i,1)) / 1i
+r3 = roots(c(1, k[3] * 1i, 0,0,0, k[3] * 1i,1)) / 1i
+x = c(r1, r2, r3)
+poly.calc(x)
+-1 + 2*x^3 + 3*x^6 + 6*x^7 + 6*x^11 - 3*x^12 + 2*x^15 + x^18
+
+
+### 2x entangled (...)^(1/3)
+m3.grid = expand.grid(c(1, m3, m3^2), c(1, m3, m3^2))
+k = 3^(1/3) * m3.grid[,1] - 2^(1/3) * m3.grid[,2]
+r = sapply(k, function(k) roots(c(k, 1, 0,0,0, 1, k)) )
+x = sort(r)
+poly.calc(x)
+1 + 165*x^3 + 12*x^6 + 495*x^7 + 991*x^9 + 18*x^10 + 495*x^11 + 45*x^12 + 2979*x^13 + 45*x^14 +
++ 2640*x^15 + 54*x^16 + 3006*x^17 + 153*x^18 + 7425*x^19 + 135*x^20 + 4374*x^21 + 99*x^22 + 7425*x^23 +
++ 309*x^24 + 10026*x^25 + 153*x^26 + 4950*x^27 + 153*x^28 + 10026*x^29 + 309*x^30 + 7425*x^31 +
++ 99*x^32 + 4374*x^33 + 135*x^34 + 7425*x^35 + 153*x^36 + 3006*x^37 + 54*x^38 + 2640*x^39 + 45*x^40 +
++ 2979*x^41 + 45*x^42 + 495*x^43 + 18*x^44 + 991*x^45 + 495*x^47 + 12*x^48 + 165*x^51 + x^54
+# predict(p, x[54]) # NOT better!
+#
+r = sapply(k, function(k) roots(c(1, 1/k, 0,0,0, 1/k, 1)) )
+x = sort(r)
+poly.calc(x)
+# the same polynomial
+
+
+###############
+
+m7 = unity(7, all=F)
+m7 = m7^(1:6) # without 1!
+
+###
+x = sapply( m7, function(m) roots(c(1, 1-m, m)) )
+round0.p(poly.calc(x))
+1 - 7*x + 20*x^2 - 28*x^3 + 15*x^4 + 7*x^5 - 8*x^6 - 7*x^7 + 15*x^8 + 28*x^9 + 20*x^10 + 7*x^11 + x^12
+
+### the same as with -m^1
+x = sapply( m7, function(m) roots(c(1, 1-m^2, -m^2)) )
+round0.p(poly.calc(x))
+1 + 7*x + 22*x^2 + 42*x^3 + 57*x^4 + 63*x^5 + 64*x^6 + 63*x^7 + 57*x^8 + 42*x^9 + 22*x^10 + 7*x^11 + x^12
+#
+x = sapply( m7, function(m) roots(c(1, 1-m^2, m^2)) )
+round0.p(poly.calc(x))
+1 - 7*x + 20*x^2 - 28*x^3 + 15*x^4 + 7*x^5 - 8*x^6 - 7*x^7 + 15*x^8 + 28*x^9 + 20*x^10 + 7*x^11 + x^12
+
+
+#################
