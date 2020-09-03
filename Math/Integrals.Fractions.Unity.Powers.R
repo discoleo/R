dcc586a69df89b6a046e1a3c2e8999ac4349ba2c
discoleo <leo.mada@syonic.eu>
2020-09-03 16:53:44 +0300

    added: Integral x/(x^n - 1)^p dx

--- Math/Integrals.Fractions.Unity.Powers.R
+++ Math/Integrals.Fractions.Unity.Powers.R
@@ -12,15 +12,22 @@
 ### - Polynomial fractions:
 ###   Integral( P(x) / (x^n - 1)^p )dx
 ###
-### draft v.0.1a-fix
+### draft v.0.1b
 
 
 
 ### History
 
+# v.0.1b:
+# - added:
+#   Integral x / (x^n - 1)^p dx;
+#   Integral x^(n+1) / (x^n - 1)^p dx;
 # v.0.1a-fix:
 # - bug fix: wrong sign in formulas;
-# v.01a:
+# - solved:
+#   Integral 1 / (x^n - 1)^p dx;
+#   Integral x^n / (x^n - 1)^p dx;
+# v.0.1a:
 # - initial release;
 
 
@@ -51,6 +58,7 @@
 
 ##################
 ### I(n, n, p + 1)
+### I(0, n, p + 1)
 
 # (x^n - 1 + 1) / (x^n - 1)^(p+1)
 # = 1/(x^n - 1)^p + 1/(x^n - 1)^(p+1)
@@ -94,4 +102,59 @@ lim = c(2, 4)
 integrate(F.f, lower=lim[1], upper=lim[2], k=0, n=n, p = p + 1)
 -1/(n*p) * (F.range(lim, 1, n, p) + (n*p - 1)*I.f(lim, 0, n, p)$value)
 
+
+### Test
+n = 5
+p = 3
+lim = c(1.1, 4)
+### I(0, n, 2)
+integrate(F.f, lower=lim[1], upper=lim[2], k=0, n=n, p = p + 1)
+-1/(n*p) * (F.range(lim, 1, n, p) + (n*p - 1)*I.f(lim, 0, n, p)$value)
+
+
 ########################
+
+
+##################
+### I(n, n, p + 1)
+### I(0, n, p + 1)
+
+# x*(x^n - 1 + 1) / (x^n - 1)^(p+1)
+# = x/(x^n - 1)^p + x/(x^n - 1)^(p+1)
+# =>
+# I(n+1, n, p+1) = I(1, n, p) + I(1, n, p+1)
+# I(n+1, n, p+1) - I(1, n, p+1) = I(1, n, p)
+
+# d/dx F(2, n, p)
+# = (2*x^(n+1) - 2*x - n*p*x^(n+1)) / (x^n - 1 )^(p+1)
+# = -(n*p - 2)*F(n+1, n, p+1) - 2*F(1, n, p+1)
+# =>
+# (n*p - 2)*I(n+1, n, p+1) + 2*I(1, n, p+1) = - F(2, n, p);
+# Note: Integral = Fraction!
+
+### I(n+1, n, p+1)
+# I(n+1, n, p+1) = 1/(n*p) * (2*I(1, n, p) - F(2, n, p))
+
+### I(1, n, p+1)
+# I(1, n, p+1) = -1/(n*p) * ((n*p - 2)*I(1, n, p) + F(2, n, p))
+
+
+### Test
+n = 5
+p = 3
+lim = c(1.1, 4)
+### I(n+1, n, 2)
+integrate(F.f, lower=lim[1], upper=lim[2], k= n+1, n=n, p = p + 1)
+-1/(n*p) * (F.range(lim, 2, n, p) - 2*I.f(lim, 1, n, p)$value)
+
+
+### Test
+n = 5
+p = 3
+lim = c(1.1, 4)
+### I(0, n, 2)
+integrate(F.f, lower=lim[1], upper=lim[2], k=1, n=n, p = p + 1)
+-1/(n*p) * (F.range(lim, 2, n, p) + (n*p - 2)*I.f(lim, 1, n, p)$value)
+
+
+
