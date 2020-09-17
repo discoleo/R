232a24fe7f481a6586371b398397e633d454c492
discoleo <leo.mada@syonic.eu>
2020-09-18 02:25:26 +0300

    Statistics of Supersonic Gases

---
+++ Physics & Chemistry/Gas.Supersonic.R
@@ -0,0 +1,19 @@
+
+### Supersonic Gases
+### Statistics
+###
+### Leonard Mada
+###
+### [initial ideas]
+
+
+### Supersonic Gases
+
+### Ideas
+# - model statistics of supersonic gases;
+# - model shockwaves;
+
+### Examples:
+# - sun as a hot supersonic gas;
+
+
