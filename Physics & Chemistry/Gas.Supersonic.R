4a024e8cd1485626b4d7d4896614cb9e78169f80
discoleo <leo.mada@syonic.eu>
2020-09-18 07:11:58 +0300

    Sun as a Supersonic gas [Ideas]

--- Physics & Chemistry/Gas.Supersonic.R
+++ Physics & Chemistry/Gas.Supersonic.R
@@ -10,10 +10,26 @@
 ### Supersonic Gases
 
 ### Ideas
-# - model statistics of supersonic gases;
-# - model shockwaves;
+# - develop the statistical tools for supersonic hot gases;
+# - model the statistics of supersonic hot gases;
+# - model the shockwaves in supersonic hot gases;
 
 ### Examples:
 # - sun as a hot supersonic gas;
+# - nuclear blast as a supersonic gas;
 
+#################
+### Use Cases ###
+
+### Extreme-Fission Reactors
+# - reactors based on extensive fission of nuclear fuels;
+# - supersonic gases will play a critical role in such reactors;
+#  -- unlike plasms that are externally heated, the extreme fission
+#     itself generates the actual heat, producing a supersonic gas;
+#  -- containment and heat transfer may be therefore far more challenging;
+# - Note:
+#   I claim that the sun also functions by the principle of
+#   extreme fission and *NOT* by fusion. Understanding the sun as
+#   a suoersonic hot gas may be important to develop
+#   specific mathematical tools.
 
