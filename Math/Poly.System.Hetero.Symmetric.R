de3b223de812dc6a4eb28da5286bcbb13fbc1bbb
discoleo <leo.mada@syonic.eu>
2020-09-23 23:31:18 +0300

    variants: x^3 + x^3*y & x^3 + x*y^3

--- Math/Poly.System.Hetero.Symmetric.R
+++ Math/Poly.System.Hetero.Symmetric.R
@@ -7,15 +7,21 @@
 ### Polynomial Systems:
 ### Heterogenous Symmetric
 ###
-### draft v.0.1m-ext
+### draft v.0.1n
 ### & branch v.0.2d
 
 
 ### Heterogenous Symmetric Polynomial Systems
 
 ### 2 Variables:
+### Simple System:
 # x^n + P(x, y) = R
 # y^n + P(y, x) = R
+
+### Generalised System:
+# P(x, y) = 0
+# P(y, x) = 0
+# where P(x, y) is any polynomial of 2 variables;
 
 
 ###############
@@ -51,6 +57,11 @@
 # M5.) x^2*y^2 + b3*x^2*y + b2*x^2 + b1*x; (TODO: P2 => P4)
 # M6.) TODO: x^2*y^2 + b4*x^2*y + b3*x*y^2 + b2*x^2 + b1*x
 # M7.) TODO: x^3*y^2 + b2*x^2 + b1*x = R;
+### Mixt: other
+# Mm3.1.) x^3 + a*x^3*y = R; (TODO: P3 => P6)
+# Mm3.2.) x^3 + a*x^3*y + b1*x*y = R;
+# Mm3.3.) x^3 + a*x^y^3 = R; (TODO: P5 => P10)
+#         https://www.youtube.com/watch?v=jRwiJTaAu4c
 ### Mixt: (x*y)^n
 # M7.) TODO: (x*y)^3 + ...:
 #      (x*y)^3 + x^3 = R;
@@ -101,6 +112,10 @@
 # - the simple cases are less rewarding;
 
 ### [branch v.0.1]
+### draft v.0.1n:
+# - solved:
+#   -- x^3 + b*x^3*y = R; (TODO: P3 => P6)
+#   -- x^3 + b*x*y^3 = R; (TODO: P5 => P10)
 ### draft v.0.1m - v.0.1.m-ext:
 # - various extensions:
 #   -- x^4*y^3 Series;
@@ -1572,7 +1587,7 @@ R^2 + (b[1]^2 - 2*R)*x^4 + x^8
 
 # Trivial solutions: x = y *OR* x = -y;
 
-# Diff =>
+### Diff =>
 # x^4 - y^4 = 0
 # (x - y)*(x^3 + y^3 + x*y*(x+y)) = 0
 # (x - y)*((x+y)^3 - 2*x*y*(x+y)) = 0
@@ -1628,7 +1643,7 @@ round0.p(poly.calc(sol[,1]))
 # x^2*y + b1*x = R
 # y^2*x + b1*y = R
 
-### Solution:
+### Solution: *NO*
 
 ### Diff =>
 # x*y*(x - y) + b1*(x-y) = 0
@@ -1642,6 +1657,7 @@ round0.p(poly.calc(sol[,1]))
 # but: x*y + b1 = 0
 # => NO solution (x != y);
 
+### NO Solution!
 
 
 ###############
@@ -1793,6 +1809,7 @@ y^2*x + b[3]*x*y + b[2]*y^2 + b[1]*y
 # b1*S^3 - R*S^2 + b1*b2*S + b1^2*b3 - b1^2 = 0
 
 solve.htx3y = function(b, R) {
+	if(R == 0 && b[1] == 0) return(list(sol=NA, p=NA))
 	r.sum = roots(c(b[1], - R, b[1]*b[2], b[1]^2*b[3] - b[1]^2))
 	r.sum = round0(r.sum)
 	r.sum = r.sum[ r.sum != 0 ] # avoid division by 0
@@ -1857,6 +1874,12 @@ sol = solve.htx3y(b, R)
 x = sol$sol[,1]; y = sol$sol[,2];
 sol
 -1 + 9*x - 19*x^2 - x^5 + x^6
+
+###
+p = sapply(-6:6, function(r) print(solve.htx3y(c(r, -3, 2), r)$p))
+#
+r = -3; sol = solve.htx3y(c(r, -3, 2), r); x = sol$sol[,1]
+-9 - 21*x - 15*x^3 - x^5 + x^6
 
 
 ###################
@@ -2291,7 +2314,7 @@ round0.p(poly.calc(sol[,1]))
 
 #################################
 ### x^2*y^2 + b4*x^2*y + b3*x*y^2
-### + b2*x^2 + b1*x
+###  + b2*x^2 + b1*x
 
 # x^2*y^2 + b4*x^2*y + b3*x*y^2 + b2*x^2 + b1*x = R
 # y^2*x^2 + b4*y^2*x + b3*y*x^2 + b2*y^2 + b1*y = R
@@ -2357,6 +2380,7 @@ round0.p(poly.calc(sol[,1]))
 
 
 ##########################
+##########################
 
 ######################
 ### (x*y)^3 Series ###
@@ -2399,7 +2423,155 @@ round0.p(poly.calc(sol[,1]))
 # P12 based on P4;
 
 
+
+##########################
+
+##########################
+### 2 Highest Terms:   ###
+###   Simple + Mixt    ###
+##########################
+
+###############
+### x^3 + x^3*y
+
+# x^3 + a*x^3*y = R
+# y^3 + a*y^3*x = R
+
+### Extension:
+# x^3 + a*x^3*y + b1*x*y = R
+
+### Solution:
+
+### Diff =>
+# (x-y)*(x^2 + y^2 + x*y + a*x*y*(x+y)) = 0
+# Case: x != 0 =>
+# x^2 + y^2 + x*y + a*x*y*(x+y) = 0
+# S^2 - x*y + a*x*y*S = 0
+# x*y = - S^2 / (a*S - 1)
+
+### Sum =>
+# x^3 + y^3 + a*x*y*(x^2 + y^2) - 2*R = 0
+# S^3 - 3*x*y*S + a*x*y*(S^2 - 2*x*y) - 2*R
+# S^3 + 3*S^3 / (a*S - 1) - a*S^2*(S^2 - 2*x*y) / (a*S - 1) - 2*R
+# a*S^4 - S^3 + 3*S^3 - a*S^2*(S^2 - 2*x*y) - 2*R*(a*S - 1)
+# 2*S^3 + 2*a*S^2*x*y - 2*R*a*S + 2*R
+# 2*S^3 - 2*a*S^4 / (a*S - 1) - 2*R*a*S + 2*R
+# 2*a*S^4 - 2*S^3 - 2*a*S^4 - 2*R*a^2*S^2 + 2*R*a*S + 2*a*R*S - 2*R
+# - 2*S^3 - 2*R*a^2*S^2 + 4*a*R*S - 2*R
+# S^3 + R*a^2*S^2 - 2*a*R*S + R
+
+### Extension: + b1*x*y
+# (b1*a+1)*S^3 + (a^2*R-b1)*S^2 - 2*a*R*S + R
+
+solve.x3y = function(a, R, b=0, type=1) {
+	# type = 1: simple;
+	if(type == 2) {
+		coeff = c(b[1]*a[1]+1, a[1]^2*R - b[1], - 2*a[1]*R, R)
+	} else if(type == 1) {
+		coeff = c(1, R*a[1]^2, - 2*a[1]*R, R)
+	} else if(type == 5) {
+		coeff = c(a[1]^2, - a[1], -1, - a[1]^2*R, - 2*a[1]*R, - R)
+	}
+	x.s = roots(coeff)
+	xy = if(type < 5) x.s^2 / (1 - a[1]*x.s) else x.s^2 / (a[1]*x.s + 1);
+	x.d = sqrt(x.s^2 - 4*xy + 0i)
+	x = (x.s + x.d)/2
+	y = (x.s - x.d)/2
+	sol = cbind(x, y)
+	sol = rbind(sol, sol[,2:1])
+	p = round0.p(poly.calc(sol[,1]))
+	return(list(sol=sol, p=p))
+}
+
+### Example
+a = 3
+R = 1
+#
+sol = solve.x3y(a, R)
+x = sol$sol[,1]; y = sol$sol[,2];
+sol
+
+### Test
+x^3 + a[1]*x^3*y
+y^3 + a[1]*y^3*x
+
+### Classic Polynomial
+round0.p(poly.calc(sol$sol[,1]))
+err = 1 + 3*x - 2*x^3 - 3*x^4 + 9*x^5 + x^6
+round0(err)
+
+###
+R = 1
+l = sapply(-6:6, function(a) print(solve.x3y(a, R)$p))
+
+### Extension
+
+### Example 2: Extension
+a = -3
+b = c(1/2)
+R = 2
+#
+sol = solve.x3y(a, R=R, b=b, type=2)
+x = sol$sol[,1]; y = sol$sol[,2];
+sol
+
+### Test
+x^3 + a[1]*x^3*y + b[1]*x*y
+y^3 + a[1]*y^3*x + b[1]*x*y
+
+
+
+################
+### Alternative:
+### x^3 + x*y^3
+
+# x^3 + a*x*y^3 = R
+# y^3 + a*x^3*y = R
+
+# Cheol-Hyun Cho:
+# Fukaya category for Landau-Ginzburg orbifolds and Berglund-Hübsch
+# homological mirror symmetry for curve singularities
+# [from 53:30] https://www.youtube.com/watch?v=jRwiJTaAu4c
+
+### TODO: add x*y & (x*y)^2 terms;
+
+### Diff =>
+# (x-y)*(x^2 + y^2 + x*y - a*x*y*(x+y)) = 0
+# Case: x != 0 =>
+# x^2 + y^2 + x*y - a*x*y*(x+y) = 0
+# S^2 - x*y - a*x*y*S = 0
+# x*y = S^2 / (a*S + 1)
+
+### Sum =>
+# x^3 + y^3 + a*x*y*(x^2 + y^2) - 2*R = 0
+# S^3 - 3*x*y*S + a*x*y*(S^2 - 2*x*y) - 2*R
+# S^3 - 3*S^3 / (a*S + 1) + a*S^2*(S^2 - 2*x*y) / (a*S + 1) - 2*R
+# a*S^4 + S^3 - 3*S^3 + a*S^2*(S^2 - 2*x*y) - 2*R*(a*S + 1)
+# a*S^4 - S^3 - a*S^2*x*y - a*R*S - R
+# a*S^4 - S^3 - a*S^4/(a*S + 1) - a*R*S - R
+# a^2*S^5 - a*S^4 - S^3 - a^2*R*S^2 - 2*a*R*S - R
+
+### Example
+a = 1
+R = 1
+#
+sol = solve.x3y(a, R, type=5)
+x = sol$sol[,1]; y = sol$sol[,2];
+sol
+
+### Test
+x^3 + a[1]*x*y^3
+y^3 + a[1]*y*x^3
+
+### Classic Polynomial
+round0.p(poly.calc(sol$sol[,1]))
+err = 1 - 3*x + 3*x^2 - 3*x^3 + 4*x^4 - 3*x^5 + 3*x^6 - 2*x^7 + x^8 - x^9 + x^10
+round0(err)
+
+
 ####################
+####################
+
 
 ####################
 ### Mixt Order 5 ###
