ba376f9e57164d134c501ff0a2145b3e1152abd2
discoleo <leo.mada@syonic.eu>
2020-10-15 20:02:31 +0300

    Problem: Cost-Sort

---
+++ Optimization/Sort.Cost_Sort.R
@@ -0,0 +1,23 @@
+##################
+###
+### Cost-Sort
+###
+### Leonard Mada
+###
+### draft v.0.1a
+
+
+### Problem: Optimize the Cost-Sort sort
+
+### Data
+# S = set of items X[i];
+### Properties of X:
+# X[i]: can be ranked/ordered;
+# X[i]: each x[i] has an associated cost;
+
+### Cost:
+# Swap(x[i], x[j]) = mean(Cost(x[i]), Cost(x[j]));
+
+### Task:
+# Minimize(Cost(Sort(S)));
+
