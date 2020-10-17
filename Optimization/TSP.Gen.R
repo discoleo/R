f150a121bdcc127784c7797ea1b7caa0188e7f44
discoleo <leo.mada@syonic.eu>
2020-10-17 08:09:36 +0300

    + starting city

--- Optimization/TSP.Gen.R
+++ Optimization/TSP.Gen.R
@@ -1,4 +1,12 @@
-
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### TSP Models
+### Data Generators
+###
+### draft v.0.1b
 
 
 ####################
@@ -21,10 +29,42 @@ watermelon.gen = function(n, m, l, random=TRUE, d=3) {
 	if(random) {
 		p.x = sapply(1:m, function(id) runif(n, 0, l))
 	} else {
+		# TODO: fix repeating (0, 0);
 		p.x = matrix(rep( (0:(n-1)) * l/n, m), ncol=m)
 	}
 	p.y = sapply(1:m, ellipse.f, x=p.x)
 	return(list("x"=p.x, "y"=p.y))
+}
+
+rnorm2d.gen = function(n1, n2=n1, sd1=1, sd2=1) {
+	len = n1*n2
+	p.x = rnorm(len, sd=sd1)
+	p.y = rnorm(len, sd=sd2)
+	return(list("x"=p.x, "y"=p.y))
+}
+
+runif2d.gen = function(n1, n2=n1, max=2) {
+	# max = 2 is more close to rnorm
+	# alternative: max = 4*sd
+	len = n1*n2
+	p.x = runif(len, max=max)
+	p.y = runif(len, max=max)
+	return(list("x"=p.x, "y"=p.y))
+}
+
+find.base = function(m, middle=FALSE) {
+	# m = matrix with coordinates of cities
+	# only with non-random
+	# TODO: more options
+	if( ! middle) {
+		isZero = m[,1] == 0 & m[,2] == 0
+	} else {
+		m0 = m[m[,2] == 0, 1]
+		mid = m0[rank(m0) == round(length(m0)/2)]
+		isZero = m[,2] == 0 & m[,1] == mid
+	}
+	id = match(TRUE, isZero)
+	return(id)
 }
 
 ###########################
@@ -34,7 +74,7 @@ watermelon.gen = function(n, m, l, random=TRUE, d=3) {
 library(TSP)
 
 
-setwd("/Math")
+setwd("C:/Users/Leo Mada/Desktop/DB/Math")
 
 
 ###########################
@@ -45,12 +85,16 @@ p = watermelon.gen(20, m=ell.n, 10, random=F)
 plot(p$x, p$y)
 
 cities = matrix(c(as.vector(p$x), as.vector(p$y)), ncol=2)
+id = find.base(cities, middle=T)
+id
 
+### TSP data
 etsp <- ETSP(cities)
 etsp
 
 ### calculate a tour
-tour <- solve_TSP(etsp, method = "nn")
+# tour <- solve_TSP(etsp, method = "nn"
+tour <- solve_TSP(etsp, method = "nn", control=list(start=id))
 tour
 
 tour_length(tour)
@@ -66,4 +110,24 @@ dev.off()
 
 # image(atsp, tour)
 
+
+###########################
+
+###
+p = rnorm2d.gen(10)
+# p = runif2d.gen(10)
+plot(p$x, p$y)
+
+
+cities = matrix(c(as.vector(p$x), as.vector(p$y)), ncol=2)
+
+etsp <- ETSP(cities)
+etsp
+
+### calculate a tour
+tour <- solve_TSP(etsp, method = "nn")
+tour
+
+tour_length(tour)
+plot(etsp, tour, tour_col = "red")
 
