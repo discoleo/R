2917cc38fd2397ae1adc91041558b6f9c3a36878
discoleo <leo.mada@syonic.eu>
2020-11-24 23:53:59 +0200

    Quasi-Gamma Correction + Homomorphic

--- Img/Img.Filter.Homomorphic.R
+++ Img/Img.Filter.Homomorphic.R
@@ -4,12 +4,14 @@
 ### Homomorphic Filtering
 ###
 ### Leonard Mada
-### draft v.0.1
+### draft v.0.2
 
 
 # based on:
 # 1.) https://en.wikipedia.org/wiki/Homomorphic_filtering
 # 2.) http://www.worldcolleges.info/sites/default/files/enggnotes/homomorphic-processing-and-its-application-to-image-enhancement-.pdf
+
+# Mix between quasi-Gamma correction & Homomorphic filter;
 
 
 ##################
@@ -26,9 +28,9 @@ setwd(".../Img")
 ###################
 
 ### Load image
-img.str = "....jpg"
+img.str = "..."
 # Tested on:
-# - 1st image (houses in London):
+# - 1st image (London, aerial view of houses):
 # https://www.dailymail.co.uk/news/article-2085752/Eye-sky-The-pretty-patterns-housing-developments-world.html
 
 
@@ -59,7 +61,7 @@ x.fft = fft(img.log)
 ### Filter ###
 
 ### generate Filter
-filter.f = function(dim, D0=40, c=0.5, H=2, L=0.5) {
+filter.gen = function(dim, D0=40, c=0.5, H=2, L=0.5) {
 	# c is redundant;
 	diff = H - L;
 	x0.d = dim[1] / 2; y0.d = dim[2] / 2;
@@ -88,11 +90,41 @@ filter.himg = function(img.fft, flt) {
 	}
 	img.flt;
 }
+fft.log = function(img) {
+	### Log-Transform
+	img.log = log(1 + img)
+	### FFT
+	return(fft(img.log))
+}
+filter.ret.img = function(img, img.fft, D0=10, dif=2, H=2, L=0.5, show.filter=FALSE) {
+	if(missing(img.fft)) img.fft = fft.log(img)
+	img.dim = dim(img.fft)
+	flt = filter.gen(img.dim[1:2], D0=10, H=H, L=L)
+	if(show.filter) {
+		display(flt)
+	}
+	### Apply Filter
+	img.flt = filter.himg(img.fft, flt)
+	### Convert back to Image
+	### Inverse FFT:
+	img2 = Re(fft(img.flt, inverse=1)/prod(img.dim[1:3]))
+	### Reverse Log-Transform
+	img2 = exp(img2) - 1
+	img2[img2 < 0] = 0
+	### corrected image
+	if(length(dif) == 1) {
+		img2 - dif*img;
+	} else {
+		lapply(dif, function(dif) img2 - dif*img)
+	}
+}
 
 #################
 
+### Test
+
 ### Create Filter
-flt = filter.f(img.dim[1:2], D0=10)
+flt = filter.gen(img.dim[1:2], D0=10)
 
 # display Filter:
 flt[1:10, 1:10]
@@ -123,16 +155,29 @@ img2 = img2 - img;
 # alternative:
 # img2 = img2 - L*img; # may be useful as well;
 
-
-###############
-### Display ###
-
 # img  = original;
 # img2 = Homomorphic filtered;
 # img2 - img = difference;
 # either img2 or the difference (img2 - img) can be useful;
+
+#########################
+#########################
+
+#########################
+### Filter: automatic ###
+
+D0 = 40; # 10, 40, 100
+img2 = filter.ret.img(img, x.fft, D0=D0, dif=2);
+
+
+###############
+### Display ###
+
+# variants:
+display(combine(img, img2, img-img2))
+#
 display(combine(img, img2, img2-img))
-# variant: tested with the houses image;
+# variant: tested on the image with houses (& manual filtering);
 display(combine(img, img2-img, img2))
 
 
@@ -142,7 +187,7 @@ display(combine(img, img2-img, img2))
 old.par = par(mfrow = c(3,1))
 plot(img)
 plot(img2)
-plot(img2 - img)
+plot(img - img2) # for the automatic filter variant
 
 par(old.par)
 
