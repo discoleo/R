e810ebed76ff0dcead7b845b9d981746370bbe98
discoleo <leo.mada@syonic.eu>
2020-12-06 18:52:39 +0200

    Extension 2
    Ext 1: x*y^2 + y*z^2 + z*x^2 + b1*(x+y+z) = R1;
    Ext 2: x*y + x*z + y*z + b2*(x+y+z) = R2;

--- Math/Poly.System.Hetero.Symmetric.S3.Mixt.R
+++ Math/Poly.System.Hetero.Symmetric.S3.Mixt.R
@@ -7,7 +7,7 @@
 ### Heterogenous Symmetric S3:
 ### Mixt Type
 ###
-### draft v.0.1a-ext1
+### draft v.0.1a-ext2
 
 
 ### Heterogenous Symmetric
@@ -18,9 +18,10 @@
 
 ### History
 
-### draft v.0.1a-ext1:
-# - Extension:
+### draft v.0.1a-ext1 - v.0.1a-ext2:
+# - Extensions:
 #   x*y^2 + y*z^2 + z*x^2 + b1*(x+y+z) = R1;
+#   x*y + x*z + y*z + b2*(x+y+z) = R2;
 ### draft v.0.1a:
 # - moved to new file
 #   from Poly.System.Hetero.Symmetric.S3.Mixt.R;
@@ -75,14 +76,19 @@ R3*S^3 - (R1+6*R3)*R2*S + R1^2 + R2^3 + 9*R3^2 + 3*R1*R3 # = 0
 
 ### Solution
 solve.ht3 = function(R, b=0) {
-	if(b == 0) {
+	if(length(b) == 1 && b[1] == 0) {
 		coeff = c(R[3], 0, - (R[1]+6*R[3])*R[2], R[1]^2 + R[2]^3 + 9*R[3]^2 + 3*R[1]*R[3])
 	} else {
 		coeff = c(R[3], (b[1]^2 + b[1]*R[2]), - (R[1]*R[2] + 3*b[1]*R[3] + 6*R[2]*R[3] + 2*b[1]*R[1]),
 			R[1]^2 + R[2]^3 + 9*R[3]^2 + 3*R[1]*R[3])
 	}
+	if(length(b) > 1) {
+		# Ext 2:
+		coeff = coeff - c(b[2]^3 + b[1]*b[2], -b[2]*(R[1] + 3*b[2]*R[2] + 6*R[3]), 3*b[2]*R[2]^2, 0)
+	}
 	S = roots(coeff)
-	x = sapply(S, function(x) roots(c(1,-x,R[2],-R[3])))
+	b2 = if(length(b) > 1) b[2] else 0; # Ext 2;
+	x = sapply(S, function(x) roots(c(1,-x, R[2] - b2*x, -R[3])))
 	S = matrix(S, ncol=3, nrow=3, byrow=T)
 	yz = R[3]/x
 	yz.s = S - x
@@ -100,7 +106,7 @@ test.ht3 = function(x, y, z, R, b=0) {
 	}
 	### Test
 	err1 = x*y^2 + y*z^2 + z*x^2 + b[1]*(x+y+z) # - R[1] # = 0
-	err2 = x*y + y*z + z*x # - R[2] # = 0
+	err2 = x*y + y*z + z*x + if(length(b) < 2) 0 else b[2]*(x+y+z) # - R[2] # = 0
 	err3 = x*y*z # - R[3] # = 0
 	err = rbind(err1, err2, err3)
 	if( ! missing(R)) {
@@ -142,7 +148,10 @@ round0(err)
 ###############
 ### Extensions:
 
+### Extension 1:
 ### x*y^2 + y*z^2 + z*x^2 + b1*(x+y+z) = R1
+### Extension 2:
+### x*y + x*z + y*z + b2*(x+y+z) = R2;
 
 ### * x*z^2 =>
 x^2*y^2*z^2 + x*y*z^4 + z^3*x^3 + b1*(x+y+z)*x*z^2 - R1*x*z^2 # = 0
@@ -194,4 +203,34 @@ poly.calc(x)
 err = 1 + 3*x + x^2 - 5*x^4 - 10*x^5 - 11*x^6 - 6*x^7 - 2*x^8 + x^9
 round0(err)
 
+
+###
+# R3 = (b[2]^2 + b[1]*b[2]) +/- 1;
+R = c(0, 1, 9); b = c(1, 2);
+sol = solve.ht3(R, b=b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+test.ht3(x, y, z, b=b)
+
+poly.calc(x)
+
+# numeric instability with 1st root!
+err = -729 - 19521*x - 8649*x^2 - 6428*x^3 - 4979*x^4 - 2234*x^5 - 653*x^6 - 154*x^7 - 122*x^8 + x^9
+round0(err)
+
+
+###
+# R3 = (b[2]^2 + b[1]*b[2]) +/- 1;
+R = c(0, 1, -2); b = c(2, -1);
+sol = solve.ht3(R, b=b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+test.ht3(x, y, z, b=b)
+
+poly.calc(x)
+
+err = 8 - 72*x + 60*x^2 - 42*x^3 + 60*x^4 - 12*x^5 + 31*x^6 + 9*x^7 + 21*x^8 + x^9
+round0(err)
 
