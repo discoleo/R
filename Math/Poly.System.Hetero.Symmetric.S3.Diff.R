fbcd2db6f17ecc7a3867593c3943f531df47fe4f
discoleo <leo.mada@syonic.eu>
2020-12-20 08:20:00 +0200

    solved: x^3 - y^3 + b1*x*y = R;

--- Math/Poly.System.Hetero.Symmetric.S3.Diff.R
+++ Math/Poly.System.Hetero.Symmetric.S3.Diff.R
@@ -7,7 +7,7 @@
 ### Polynomial Systems: S3
 ### Hetero-Symmetric Differences
 ###
-### draft v.0.1a-ext2
+### draft v.0.1b
 
 
 ### Hetero-Symmetric Differences
@@ -23,6 +23,8 @@ z^n - x^n + b*z*x = R
 ###############
 ### History ###
 
+### draft v.0.1b:
+# - solved: x^3 - y^3 + b1*x*y = R;
 ### draft v.0.1a-ext2:
 # - extension A1 power 2: + b[3]*(x+y+z)^2;
 ### draft v.0.1a:
@@ -76,7 +78,7 @@ b*E2 - 3*R # = 0
 E2 = 3*R[1] / b[1]
 
 ### Sum(z^2*...) =>
-# b*x*y*z*S = R*(x^2+y^2=z^2)
+# b*x*y*z*S = R*(x^2+y^2+z^2)
 b*E3*S - R*(S^2 - 2*E2) # = 0
 b*E3*S = R*(S^2 - 2*E2)
 b*E3*S = R*(S^2 - 6*R / b)
@@ -127,15 +129,14 @@ b^3*S^6 + R*(b^4 - 15*b^2)*S^4 + R^2*(27*b - 18*b^3)*S^2 + 81*R^3*(b^2 + 3)
 
 solve.Ht3Diff = function(R, b) {
 	if(length(b) == 1) {
-		S = sqrt( - R*(b^2 + 3) / b + 0i)
-		S = c(S, -S);
+		coeff = c(b[1], 0, R[1]*(b[1]^2 + 3))
 	} else if(length(b) == 2) {
-		det = sqrt(b[2]^2*(b[1]^2 + 3)^2 - 4*b[1]*R[1]*(b[1]^2 + 3) + 0i)
-		S = c(b[2]*(b[1]^2 + 3) + c(1,-1)*det) / 2 / b[1]
+		coeff = c(b[1], - b[2]*(b[1]^2 + 3), R[1]*(b[1]^2 + 3))
 	} else if(length(b) == 3) {
-		det = sqrt(b[2]^2*(b[1]^2 + 3)^2 - 4*R[1]*(b[1]^2 + 3)*(b[1] - b[3]*(b[1]^2 + 3)) + 0i)
-		S = c(b[2]*(b[1]^2 + 3) + c(1,-1)*det) / 2 / (b[1] - b[3]*(b[1]^2 + 3))
+		coeff = c((b[1] - b[3]*(b[1]^2 + 3)), - b[2]*(b[1]^2 + 3), R[1]*(b[1]^2 + 3))
 	}
+	det = sqrt(coeff[2]^2 - 4*coeff[1]*coeff[3] + 0i)
+	S = c( - coeff[2] + det, - coeff[2] - det) / 2 / coeff[1]
 	print(S)
 	#
 	b2 = if(length(b) > 1) b[2] else 0; # Ext A1: pow 1;
@@ -260,4 +261,136 @@ b*E2*S^2 - 2*b*E2^2 - b*E3*S - 2*R*S^2 + 4*R*E2 # = 0
 b*E2*S^2 - 2*b*E2^2 - R*(S^2 - 6*R / b) - 2*R*S^2 + 4*R*E2 # = 0
 b^2*E2*S^2 - 2*b^2*E2^2 - R*(b*S^2 - 6*R) - 2*b*R*S^2 + 4*R*b*E2 # = 0
 3*b*R*S^2 - 3*b*R*S^2 # = 0
+
+
+###########################
+
+###############
+### Order 3 ###
+###############
+
+# x^3 - y^3 + b*x*y = R
+# y^3 - z^3 + b*y*z = R
+# z^3 - x^3 + b*x*z = R
+
+### Solution:
+
+### Case 1:
+det = sqrt(R[1]/b[1])
+x = y = z = c(det, -det)
+
+### Case 2:
+# (x, y, z) NOT equal;
+
+### Sum =>
+b*E2 - 3*R # = 0
+E2 = 3*R[1] / b[1]
+
+### Sum(z^3*...) =>
+# b*x*y*z*(x^2 + y^2 + z^2) = R*(x^3 + y^3 + z^3)
+b*E3*(S^2 - 2*E2) - R*(S^3 - 3*E2*S + 3*E3) # = 0
+(b*S^2 - 2*b*E2 - 3*R)*E3 - R*(S^3 - 3*E2*S) # = 0
+(b*S^2 - 9*R)*E3 - R*(S^3 - 3*E2*S) # = 0
+b*(b*S^2 - 9*R)*E3 - R*(b*S^3 - 9*R*S) # = 0
+
+### Eq 3:
+# x^3 - y^3 = R - b[1]*x*y
+### sq =>
+x^6 + y^6 - 2*x^3*y^3 - (R^2 + b[1]^2*x^2*y^2 - 2*b[1]*R*x*y) # = 0
+### Sum() =>
+2*(x^6 + y^6 + z^6) - 2*(x^3*y^3 + x^3*z^3 + y^3*z^3) - b[1]^2*(x^2*y^2 + x^2*z^2 + y^2*z^2) + 2*b[1]*R*E2 - 3*R^2
+2*(- 2*E2^3 + 3*E3^2 - 12*E2*E3*S + 9*E2^2*S^2 + 6*E3*S^3 - 6*E2*S^4 + S^6) +
+	- 2*(E2^3 - 3*E3*(E2*S - E3)) - b[1]^2*(E2^2 - 2*E3*S) + 2*b[1]*R*E2 - 3*R^2
+- 6*E2^3 - b[1]^2*E2^2 - 18*E2*E3*S + 18*E2^2*S^2 + 12*E3*S^3 - 12*E2*S^4 + 2*S^6 + 2*b[1]^2*E3*S + 2*b[1]*R*E2 - 3*R^2
+- 6*b[1]^3*E2^3 - b[1]^5*E2^2 - 18*b[1]^3*E2*E3*S + 18*b[1]^3*E2^2*S^2 +
+	+ 12*b[1]^3*E3*S^3 - 12*b[1]^3*E2*S^4 + 2*b[1]^3*S^6 + 2*b[1]^5*E3*S + 2*b[1]^4*R*E2 - 3*b[1]^3*R^2
+(12*b[1]^3*S^3 + 2*b[1]^5*S - 54*b[1]^2*R*S)*E3 +
+	+ 2*b[1]^3*S^6 - 36*b[1]^2*R*S^4 + 2*81*b[1]*R^2*S^2 - 6*27*R^3 - 6*b[1]^3*R^2
+R*(12*b[1]^3*S^3 + 2*b[1]^5*S - 54*b[1]^2*R*S)*(b[1]*S^3 - 9*R*S) +
+	+ b[1]*(b[1]*S^2 - 9*R)*(2*b[1]^3*S^6 - 36*b[1]^2*R*S^4 + 2*81*b[1]*R^2*S^2 - 6*27*R^3 - 6*b[1]^3*R^2)
+
+### =>
+(27*R^3*b1^4 + 729*R^4*b1) +
+	+ (- 12*R^2*b1^5 - 567*R^3*b1^2)*S^2 +
+	+ (R*b1^6 + 162*R^2*b1^3)*S^4 +
+	+ (- 21*R*b1^4)*S^6 + b1^5*S^8
+(b[1]*S^2 - 9*R) * (b[1]^4*S^6 - 12*R*b[1]^3*S^4 + (R*b[1]^5 + 54*R^2*b[1]^2)*S^2 - (3*R^2*b[1]^4 + 81*R^3*b[1]))
+(b[1]*S^2 - 9*R) * (b[1]*S^2 - 3*R) * (b[1]^3*S^4 - 9*R*b[1]^2*S^2 + (R*b[1]^4 + 27*R^2*b[1]))
+
+
+coeff = c(b[1]^3, 0, - 9*R[1]*b[1]^2, 0, (R[1]*b[1]^4 + 27*R[1]^2*b[1]))
+
+
+### Solution
+solve.Ht3DiffP3 = function(R, b) {
+	if(b[1] == 0) stop("Cannot solve when b[1] == 0!")
+	if(length(b) == 1) {
+		coeff = c(b[1]^3, 0, - 9*R[1]*b[1]^2, 0, (R[1]*b[1]^4 + 27*R[1]^2*b[1]))
+	} else if(length(b) == 2) {
+		# TODO
+	} else if(length(b) == 3) {
+		# TODO
+	}
+	S = roots(coeff)
+	print(S)
+	#
+	b2 = if(length(b) > 1) b[2] else 0; # Ext A1: pow 1;
+	b3 = if(length(b) > 2) b[3] else 0; # Ext A3: pow 2;
+	R1 = R[1] - b2*S - b3*S^2
+	E2 = 3*R1 / b[1]
+	E3 = R1*(b[1]*S^3 - 9*R1*S) / b[1] / (b[1]*S^2 - 9*R1)
+	### x
+	x = sapply(seq_along(S), function(id) roots(c(1, -S[id], E2[id], - E3[id])))
+	len = length(S)
+	S  = rep(S,  each=3)
+	E3 = rep(E3, each=3)
+	R1 = rep(R1, each=3)
+	isZero = round0(x) == 0
+	# x = x[ ! isZero] # TODO: changes length of x => S;
+	yz = E3/x
+	yz.s = S - x
+	### robust: includes Ext A1: powers 1 & 2;
+	y3 = (yz.s^3 + R1 - (3*yz.s + b[1])*yz) / 2
+	y = (R1 - x^3 + y3) / b[1] / x
+	z = yz.s - y;
+	sol = cbind(x=as.vector(x), y=as.vector(y), z=as.vector(z))
+	### x = 0
+	if(any(isZero)) {
+		print("TODO: x == 0")
+	}
+	### x == y == z
+	if(length(b) < 2) {
+		x = y = z = c(1,-1) * sqrt(R[1] / b[1] + 0i)
+	} else if(length(b) == 2) {
+		x = y = z = roots(c(b[1], 3*b[2], -R[1]))
+	} else if(length(b) == 3) {
+		x = y = z = roots(c(b[1] + 9*b[3], 3*b[2], -R[1]))
+	}
+	sol = rbind(sol, cbind(x,y,z))
+	return(sol)
+}
+
+
+### Examples:
+R = 2
+b = 3
+#
+sol = solve.Ht3DiffP3(R, b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+x^3 - y^3 + b[1]*x*y # - R[1]
+y^3 - z^3 + b[1]*y*z # - R[1]
+z^3 - x^3 + b[1]*x*z # - R[1]
+
+
+### Debug
+x = 1.2583507026 + 1.1639791181i
+y = 0.2282819654 - 1.0195743279i
+z = 0.4163440587 + 0.6438339512i
+#
+S = x+y+z
+E3 = x*y*z
+E2 = x*y + x*z + y*z
+
 
