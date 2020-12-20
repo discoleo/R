325db9407d908b03ff38340ef6be2266c9fea902
discoleo <leo.mada@syonic.eu>
2020-12-20 18:48:17 +0200

    various power extensions (type A1): +(x+y+z)^k

--- Math/Poly.System.Hetero.Symmetric.S3.Diff.R
+++ Math/Poly.System.Hetero.Symmetric.S3.Diff.R
@@ -7,7 +7,7 @@
 ### Polynomial Systems: S3
 ### Hetero-Symmetric Differences
 ###
-### draft v.0.2a
+### draft v.0.2b
 
 
 ### Hetero-Symmetric Differences
@@ -26,6 +26,9 @@ z^n - x^n + b*z*x = R
 ###############
 ### History ###
 
+### draft v.0.2b:
+# - implemented various power extensions of type A1:
+#   powers 1, 2 & 3: system + b[k+1]*(x+y+z)^k;
 ### draft v.0.2a:
 # - moved derivation of formulas to:
 #   Poly.System.Hetero.Symmetric.S3.Diff.Derivation.R;
@@ -261,22 +264,50 @@ x^6 + y^6 - 2*x^3*y^3 - (R^2 + b[1]^2*x^2*y^2 - 2*b[1]*R*x*y) # = 0
 coeff = c(b[1]^3, 0, - 9*R[1]*b[1]^2, 0, (R[1]*b[1]^4 + 27*R[1]^2*b[1]))
 
 
+### Extension A1: power 1:
+(b[1]*S^2 + 9*b[2]*S - 9*R) *
+	(b[1]^3*S^4 + 9*b[1]^2*b[2]*S^3 +
+	+ (27*b[1]*b[2]^2 - 9*R*b[1]^2)*S^2  - (b[1]^4*b[2] + 54*b[1]*b[2]*R)*S + (R*b[1]^4 + 27*R^2*b[1]))
+### Extension A1: power 2:
+((b[1] + 9*b[3])*S^2 + 9*b[2]*S - 9*R) *
+	((b[1]^3 + 9*b[1]^2*b[3] + 27*b[1]*b[3]^2)*S^4 + 9*(b[1]^2*b[2] + 6*b[1]*b[2]*b[3])*S^3 +
+	+ (27*b[1]*b[2]^2 - 9*R*b[1]^2 + 54*b[1]*b[2]*b[3] - b[1]^4*b[3] + 54*b[1]*b[3]*R)*S^2  +
+	- (b[1]^4*b[2] + 54*b[1]*b[2]*R)*S + (R*b[1]^4 + 27*b[1]*R^2))
+### Extension A1: power 3:
+(9*b[4]*S^3 + (b[1] + 9*b[3])*S^2 + 9*b[2]*S - 9*R) *
+	(27*b[1]*b[4]^2*S^6 + 9*(b[1]^2*b[4] + 6*b[1]*b[3]*b[4])*S^5 +
+	+ (b[1]^3 + 9*b[1]^2*b[3] + 27*b[1]*b[3]^2 + 54*b[1]*b[2]*b[4])*S^4 +
+	+ (9*b[1]^2*b[2] + 54*b[1]*b[2]*b[3] - b[1]^4*b[4] - 54*b[1]*b[4]*R)*S^3 +
+	+ (27*b[1]*b[2]^2 - 9*R*b[1]^2 - b[1]^4*b[3] - 54*b[1]*b[3]*R)*S^2  +
+	- (b[1]^4*b[2] + 54*b[1]*b[2]*R)*S + (R*b[1]^4 + 27*b[1]*R^2))
+
+
 ### Solution
 solve.Ht3DiffP3 = function(R, b) {
-	if(b[1] == 0) stop("Cannot solve when b[1] == 0!")
+	if(b[1] == 0) stop("System undefined when b[1] == 0!")
 	if(length(b) == 1) {
 		coeff = c(b[1]^3, 0, - 9*R[1]*b[1]^2, 0, (R[1]*b[1]^4 + 27*R[1]^2*b[1]))
 	} else if(length(b) == 2) {
-		# TODO
+		coeff = c(b[1]^3, 9*b[1]^2*b[2], (27*b[1]*b[2]^2 - 9*R[1]*b[1]^2),
+			- (b[1]^4*b[2] + 54*b[1]*b[2]*R[1]), (R[1]*b[1]^4 + 27*R[1]^2*b[1]))
 	} else if(length(b) == 3) {
-		# TODO
+		coeff = c((b[1]^3 + 9*b[1]^2*b[3] + 27*b[1]*b[3]^2), 9*(b[1]^2*b[2] + 6*b[1]*b[2]*b[3]),
+			(27*b[1]*b[2]^2 - 9*R[1]*b[1]^2 + 54*b[1]*b[2]*b[3] - b[1]^4*b[3] + 54*b[1]*b[3]*R[1]),
+			- (b[1]^4*b[2] + 54*b[1]*b[2]*R[1]), (R*b[1]^4 + 27*R[1]^2*b[1]))
+	} else if(length(b) == 4) {
+		coeff = c(27*b[1]*b[4]^2, 9*(b[1]^2*b[4] + 6*b[1]*b[3]*b[4]),
+			(b[1]^3 + 9*b[1]^2*b[3] + 27*b[1]*b[3]^2 + 54*b[1]*b[2]*b[4]), # S^4
+			9*(b[1]^2*b[2] + 6*b[1]*b[2]*b[3] - 6*b[1]*b[4]*R[1]) - b[1]^4*b[4], # S^3
+			(27*b[1]*b[2]^2 - 9*R[1]*b[1]^2 - b[1]^4*b[3] - 54*b[1]*b[3]*R[1]), # S^2
+			- (b[1]^4*b[2] + 54*b[1]*b[2]*R[1]), (R[1]*b[1]^4 + 27*b[1]*R[1]^2))
 	}
 	S = roots(coeff)
 	print(S)
 	#
 	b2 = if(length(b) > 1) b[2] else 0; # Ext A1: pow 1;
-	b3 = if(length(b) > 2) b[3] else 0; # Ext A3: pow 2;
-	R1 = R[1] - b2*S - b3*S^2
+	b3 = if(length(b) > 2) b[3] else 0; # Ext A1: pow 2;
+	b4 = if(length(b) > 3) b[4] else 0; # Ext A1: pow 3;
+	R1 = R[1] - b2*S - b3*S^2 - b4*S^3
 	E2 = 3*R1 / b[1]
 	E3 = R1*(b[1]*S^3 - 9*R1*S) / b[1] / (b[1]*S^2 - 9*R1)
 	### x
@@ -316,6 +347,8 @@ solve.Ht3DiffP3 = function(R, b) {
 		x = y = z = roots(c(b[1], 3*b[2], -R[1]))
 	} else if(length(b) == 3) {
 		x = y = z = roots(c(b[1] + 9*b[3], 3*b[2], -R[1]))
+	} else if(length(b) == 4) {
+		x = y = z = roots(c(27*b[4], (b[1] + 9*b[3]), 3*b[2], - R[1]))
 	}
 	sol = rbind(sol, cbind(x,y,z))
 	return(sol)
@@ -346,4 +379,58 @@ x = sol[,1]; y = sol[,2]; z = sol[,3];
 x^3 - y^3 + b[1]*x*y # - R[1]
 y^3 - z^3 + b[1]*y*z # - R[1]
 z^3 - x^3 + b[1]*x*z # - R[1]
+
+
+###############
+### Extensions:
+
+### Ext. power 1:
+R = 1
+b = c(1, -2)
+#
+sol = solve.Ht3DiffP3(R, b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+x^3 - y^3 + b[1]*x*y + b[2]*(x+y+z) # - R
+y^3 - z^3 + b[1]*y*z + b[2]*(x+y+z) # - R
+z^3 - x^3 + b[1]*x*z + b[2]*(x+y+z) # - R
+
+round0.p(poly.calc(x[1:12]))
+
+err = 28 + 330*x + 1563*x^2 + 4526*x^3 + 20325*x^4 + 47980*x^5 + 34722*x^6 - 20524*x^7 + 7654*x^8 +
+	- 1510*x^9 + 219*x^10 - 18*x^11 + x^12
+round0(err)
+
+
+### Ext. power 2:
+R = 1
+b = c(1, -2, -2)
+#
+sol = solve.Ht3DiffP3(R, b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+x^3 - y^3 + b[1]*x*y + b[2]*(x+y+z) + b[3]*(x+y+z)^2 # - R
+y^3 - z^3 + b[1]*y*z + b[2]*(x+y+z) + b[3]*(x+y+z)^2 # - R
+z^3 - x^3 + b[1]*x*z + b[2]*(x+y+z) + b[3]*(x+y+z)^2 # - R
+
+round0.p(poly.calc(x[1:12])) ) * 7^3 * 13^3
+
+err = 28 + 330*x + 2217*x^2 + 10206*x^3 + 34283*x^4 + 83236*x^5 + 147578*x^6 + 217756*x^7 + 410766*x^8 +
+	+ 920194*x^9 + 1572389*x^10 + 1639638*x^11 + 753571*x^12
+round0(err)
+
+
+### Ext. power 3:
+R = 1
+b = c(1, -2, -2, 1)
+#
+sol = solve.Ht3DiffP3(R, b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+x^3 - y^3 + b[1]*x*y + b[2]*(x+y+z) + b[3]*(x+y+z)^2 + b[4]*(x+y+z)^3 # - R
+y^3 - z^3 + b[1]*y*z + b[2]*(x+y+z) + b[3]*(x+y+z)^2 + b[4]*(x+y+z)^3 # - R
+z^3 - x^3 + b[1]*x*z + b[2]*(x+y+z) + b[3]*(x+y+z)^2 + b[4]*(x+y+z)^3 # - R
 
