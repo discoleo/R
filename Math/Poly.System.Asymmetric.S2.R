543511658479d9c766d6c6b0be67c9c46c7a907c
discoleo <leo.mada@syonic.eu>
2020-12-28 06:22:35 +0200

    Order 3: x^3 + b*y = R1;

--- Math/Poly.System.Asymmetric.S2.R
+++ Math/Poly.System.Asymmetric.S2.R
@@ -7,7 +7,7 @@
 ### Asymmetric S2:
 ### Base Types
 ###
-### draft v.0.1a-sol
+### draft v.0.1b
 
 
 ### Asymmetric Polynomial Systems: 2 Variables
@@ -17,6 +17,21 @@
 # x^n + b*y = R1
 # y^n + b*x = R2
 # R1 != R2
+
+
+###############
+### History ###
+###############
+
+
+### draft v.0.1b:
+# - solved Order 3:
+#   x^3 + b*y = R1;
+### draft v.0.1a:
+# - solved Order 2:
+#   x^2 + b*y = R1;
+# - special cases: when b1[S] == 0;
+
 
 ####################
 ####################
@@ -206,3 +221,208 @@ S = x+y; R1 = R[1]; R2 = R[2];
 ### Test
 x^2 + b[1]*y
 y^2 + b[1]*x
+
+
+######################
+
+###############
+### Order 3 ###
+
+# x^3 + b*y = R1
+# y^3 + b*x = R2
+
+### Sum =>
+x^3 + y^3 + b*(x+y) - R1 - R2 # = 0
+S^3 - 3*x*y*S + b*S - R1 - R2
+# 3*x*y*S = S^3 + b*S - R1 - R2
+
+### Sum(x*...) =>
+x^4 + y^4 + 2*b*x*y - R1*x - R2*y # = 0
+S^4 - 4*x*y*S^2 + 2*(x*y)^2 + 2*b*x*y - R1*x - R2*y # = 0
+S^6 - 4*x*y*S^4 + 2*(x*y)^2*S^2 + 2*b*x*y*S^2 - R1*x*S^2 - R2*y*S^2 # = 0
+-3*S^6 - 6*b*S^4 + 6*(2*R1 + 2*R2)*S^3 + 2*(S^3 + b*S - R1 - R2)^2 +
+	+ 6*b^2*S^2 - 6*b*(R1 + R2)*S - 9*R1*x*S^2 - 9*R2*y*S^2 # = 0
+-S^6 - 2*b*S^4 + (8*R1 + 8*R2)*S^3 + 8*b^2*S^2 - 10*b*(R1 + R2)*S +
+	- 9*R1*x*S^2 - 9*R2*y*S^2 + 2*(R1+R2)^2 # = 0
+# (9*R1*x + 9*R2*y)*S^2 =
+	-S^6 - 2*b*S^4 + (8*R1 + 8*R2)*S^3 + 8*b^2*S^2 - 10*b*(R1 + R2)*S + 2*(R1+R2)^2
+
+
+### Diff =>
+x^3 - y^3 - b*(x - y) - R1 + R2 # = 0
+(x-y)*(x^2 + y^2 + x*y) - b*(x - y) - R1 + R2 # = 0
+d * (S^2 - x*y - b) - R1 + R2
+d * (3*S^3 - (S^3 + b*S - R1 - R2) - 3*b*S) - (3*R1 - 3*R2)*S
+d * (2*S^3 - 4*b*S + R1 + R2) - (3*R1 - 3*R2)*S
+
+
+### Diff(x*...) =>
+x^4 - y^4 - R1*x + R2*y # = 0
+(x-y)*S*(x^2 + y^2) - R1*x + R2*y
+d*S*(S^2 - 2*x*y) - R1*x + R2*y
+d*S*(S^3 - 2*b*S + 2*R1 + 2*R2) - 3*R1*x*S + 3*R2*y*S
+# =>
+d*S*(2*S^3 - 4*b*S + 4*R1 + 4*R2) - 6*R1*x*S + 6*R2*y*S
+(R1 - R2)*S^2 + (R1 + R2)*d*S - (2*R1*x - 2*R2*y)*S
+# (2*R1*x - 2*R2*y)*S =
+	(R1 - R2)*S^2 + (R1 + R2)*d*S
+### x =>
+# 36*R1*S^2*x # =
+-2*S^6 - 4*b*S^4 + (25*R1 + 7*R2)*S^3 + 9*(R1 + R2)*d*S^2 +
+	+ 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2
+-2*S^6 - 4*b*S^4 + (25*R1 + 7*R2)*S^3 + 27*(R1 + R2)*(R1 - R2)*S^3 / (2*S^3 - 4*b*S + R1 + R2) +
+	+ 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2
+### y =>
+# 36*R2*S^2*y # =
+-2*S^6 - 4*b*S^4 + (7*R1 + 25*R2)*S^3 - 9*(R1 + R2)*d*S^2 +
+	+ 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2
+-2*S^6 - 4*b*S^4 + (7*R1 + 25*R2)*S^3 - 27*(R1 + R2)*(R1 - R2)*S^3 / (2*S^3 - 4*b*S + R1 + R2) +
+	+ 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2
+
+### Sum x + y =>
+# 36*R1*R2*S^2*(x+y)*(2*S^3 - 4*b*S + R1 + R2) # =
+(2*S^3 - 4*b*S + R1 + R2)*(R1 + R2)*(-2*S^6 - 4*b*S^4 + 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2) +
+	+ (2*S^3 - 4*b*S + R1 + R2)*(50*R1*R2 + 7*R1^2 + 7*R2^2)*S^3 +
+	- 27*(R1 + R2)*(R1 - R2)^2*S^3
+(2*S^3 - 4*b*S + R1 + R2)*(R1 + R2)*(-2*S^6 - 4*b*S^4 + 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2) +
+	+ (2*S^3 - 4*b*S)*(50*R1*R2 + 7*R1^2 + 7*R2^2)*S^3 +
+	- (R1 + R2)*(20*R1^2 + 20*R2^2 - 104*R1*R2)*S^3
+# =>
+(2*S^3 - 4*b*S + R1 + R2)*(R1 + R2)*(-2*S^6 - 4*b*S^4 + 16*b^2*S^2 - 20*b*(R1 + R2)*S + 4*(R1+R2)^2) +
+	+ (2*S^3 - 4*b*S)*(50*R1*R2 + 7*R1^2 + 7*R2^2)*S^3 - 36*R1*R2*(2*S^3 - 4*b*S + R1 + R2)*S^3+
+	- (R1 + R2)*(20*R1^2 + 20*R2^2 - 104*R1*R2)*S^3 # = 0
+(2*S^3 - 4*b*S + R1 + R2)*(-S^6 - 2*b*S^4 + 8*b^2*S^2 - 10*b*(R1 + R2)*S + 2*(R1+R2)^2) +
+	+ 7*(S^3 - 2*b*S)*(R1 + R2)*S^3 +
+	- (10*R1^2 + 10*R2^2 - 34*R1*R2)*S^3 # = 0
+
+### Eq:
+S^9 - 3*(R1 + R2)*S^6 - 12*b^2*S^5 + 18*b*(R1 + R2)*S^4 +
+	+ (3*(R1 + R2)^2 - 27*R1*R2 + 16*b^3)*S^3 - 24*b^2*(R1 + R2)*S^2 +
+	+ 9*b*(R1 + R2)^2*S - (R1 + R2)^3
+
+#############
+
+### Solution:
+solve.asym.S2P3 = function(R, b) {
+	R12 = R[1] + R[2]
+	coeff = c(1, 0, 0, - 3*R12, - 12*b[1]^2, 18*b[1]*R12,
+		(3*R12^2 - 27*R[1]*R[2] + 16*b[1]^3), - 24*b[1]^2*R12,
+		9*b[1]*R12^2, - R12^3)
+	S = roots(coeff)
+	len = length(S)
+	#
+	div = (2*S^3 - 4*b[1]*S + R12);
+	### TODO: div == 0
+	# d = 3*(R[1] - R[2])*S / div;
+	if(round0(R12) == 0) {
+		isZero = round0(S == 0)
+		S = S[ ! isZero]
+		d = 3*(R[1] - R[2])*S / div[ ! isZero]
+		d0 = roots(c(1,0,-b[1], -R[1]))
+		d = c(d, 2*d0)
+		S = c(S, rep(0, length(d0)))
+	} else {
+		d = ifelse(round0(div) == 0,
+			NA, # TODO
+			3*(R[1] - R[2])*S / div)
+	}
+	x = (S + d)/2;
+	y = (S - d)/2;
+	return(cbind(x=x, y=y))
+}
+
+### Examples:
+R = c(1,2)
+b = 1
+sol = solve.asym.S2P3(R, b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+x^3 + b[1]*y
+y^3 + b[1]*x
+
+
+#########
+### Ex 2:
+# special case: R1 + R2 == 0
+# Note: can be decomposed more efficiently using a different approach;
+R = c(1, -1)
+b = 1
+sol = solve.asym.S2P3(R, b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+x^3 + b[1]*y
+y^3 + b[1]*x
+
+
+#########
+### Ex 3:
+# special case: b3 == 0 & R1 + R2 == 0
+R = c(4, -4)
+b = -3
+sol = solve.asym.S2P3(R, b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+x^3 + b[1]*y
+y^3 + b[1]*x
+
+
+#########
+### Ex 3:
+R = c(1, 2)
+b = -3
+sol = solve.asym.S2P3(R, b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+x^3 + b[1]*y
+y^3 + b[1]*x
+
+round0.p(poly.calc(x))
+x = sol[,1] # Note: x^1!
+err = -55 - 81*x + 3*x^3 - 3*x^6 + x^9
+round0(err)
+#
+round0.p(poly.calc(x + y))
+x = sol[,1] + sol[,2]
+err = -27 - 243*x - 648*x^2 - 459*x^3 - 162*x^4 - 108*x^5 - 9*x^6 + x^9
+round0(err)
+
+
+#########
+### Ex 4:
+R = c(-1, 2)
+b = -3
+sol = solve.asym.S2P3(R, b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+x^3 + b[1]*y
+y^3 + b[1]*x
+
+round0.p(poly.calc(x))
+x = sol[,1] # Note: x^1!
+err = -53 - 81*x + 3*x^3 + 3*x^6 + x^9
+round0(err)
+# special relationship between polynomials:
+round0.p(poly.calc(x + y))
+x = sol[,1] + sol[,2]
+err = -1 - 27*x - 216*x^2 - 375*x^3 - 54*x^4 - 108*x^5 - 3*x^6 + x^9
+round0(err)
+
+
+###########
+
+### Debug
+R = c(2,3)
+b = 3
+x = 0.9860634751
+y = 0.3470765347
+S = x+y; d = x - y; R1 = R[1]; R2 = R[2];
+
+### Test
+x^3 + b[1]*y
+y^3 + b[1]*x
+
