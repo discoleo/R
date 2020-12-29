5fbbbf23ff307ba335eaa058928769738e0a08cb
discoleo <leo.mada@syonic.eu>
2020-12-29 15:06:14 +0200

    P2 variant: Eq 2: x^2 + y^2 + z^2 = R2;

--- Math/Poly.System.Symmetric.S3.Multiple.R
+++ Math/Poly.System.Symmetric.S3.Multiple.R
@@ -7,7 +7,7 @@
 ### Symmetric S3:
 ### Multiple/Composite Monoms
 ###
-### draft v.0.1c
+### draft v.0.1d
 
 
 ### Heterogenous Symmetric
@@ -31,6 +31,9 @@
 ###############
 
 
+### draft v.0.1d:
+# - Order 2 variant:
+#   Eq 2: x^2 + y^2 + z^2 = R2;
 ### draft v.0.1c:
 # - basic examples of M1 & D1 extensions;
 ### draft v.0.1b-name:
@@ -183,6 +186,17 @@ x*y*z # - R[3] # = 0
 (2*R3+R1)*S - R2^2 # = 0
 S = R[2]^2 / (2*R[3] + R[1])
 
+### Example:
+R = c(1, 2, 1)
+S = R[2]^2 / (2*R[3] + R[1])
+sol = solve.S(S, R)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+### Test
+(x^2*y^2 + y^2*z^2 + z^2*x^2)/(x+y+z) # - R[1] # = 0
+x*y + y*z + z*x # - R[2] # = 0
+x*y*z # - R[3] # = 0
+
 
 ############
 ### Type M2:
@@ -277,7 +291,123 @@ poly.calc(x)
 err = 1 - 1.5*x^2 + 5*x^3 + 0.5*x^4 - 4*x^5 + 11*x^6 - 0.5*x^8 + x^9
 round0(err)
 
+################
+### Variants ###
 
+# (x*y)^2 + (x*z)^2 + (y*z)^2 = R1
+# x^2 + y^2 + z^2 = R2
+# x*y*z = R3
+
+### Solution:
+# [not run]
+
+### Eq 2 =>
+x^2 + y^2 + z^2 - R2 # = 0
+S^2 - 2*E2 - R2 # = 0
+# 2*E2 = S^2 - R2
+# =>
+4*E2^2 - 8*E3*S - 4*R1 # = 0
+(S^2 - R2)^2 - 8*R3*S - 4*R1 # = 0
+S^4 - 2*R2*S^2 - 8*R3*S + R2^2 - 4*R1 # = 0
+
+### Solution:
+solve.SymM2.S2MP2 = function(R, b=0) {
+	coeff = c(1, 0, - 2*R[2], - 8*R[3], R[2]^2 - 4*R[1])
+	# Extensions:
+	b1 = if(length(b) > 0) b[1] else 0;
+	b2 = if(length(b) > 1) b[2] else 0;
+	b3 = if(length(b) > 2) b[3] else 0;
+	if(b1 != 0) coeff = coeff + c(0, 0, 0, 4*b1, 0)
+	if(b2 != 0) coeff = coeff + c(0, 2*b2, b2^2, -2*b2*R[2], 0)
+	if(b3 != 0) coeff = coeff + c(0, 0, 8*b3, 0, 0)
+	S = roots(coeff)
+	#
+	E2 = (S^2 - R[2] + b2*S) / 2
+	E3 = R[3] - b3*S;
+	len = length(S);
+	x = sapply(seq(len), function(id) roots(c(1, -S[id], E2[id], -E3[id])))
+	S  = matrix(S, ncol=len, nrow=3, byrow=T)
+	E2 = matrix(E2, ncol=len, nrow=3, byrow=T)
+	E3 = matrix(E3, ncol=len, nrow=3, byrow=T)
+	yz.s = S - x;
+	yz = E2 - x*yz.s;
+	yz.d = sqrt(yz.s^2 - 4*yz) # TODO: add also negative values;
+	y = (yz.s + yz.d) / 2;
+	z = (yz.s - yz.d) / 2;
+	return(cbind(x=as.vector(x), y=as.vector(y), z=as.vector(z)))
+}
+
+### Examples:
+
+R = c(1,3,2)
+sol = solve.SymM2.S2MP2(R)
+x = sol[,1]; y = sol[,2]; z = sol[,3]
+
+### Test:
+(x*y)^2 + (x*z)^2 + (y*z)^2 # - R1
+x^2 + y^2 + z^2 # - R2
+x*y*z # - R3
+
+round0.p(poly.calc(x[1:3]^2))
+
+
+### Ex 2:
+R = c(0,2,1)
+sol = solve.SymM2.S2MP2(R)
+x = sol[,1]; y = sol[,2]; z = sol[,3]
+
+### Test:
+(x*y)^2 + (x*z)^2 + (y*z)^2 # - R1
+x^2 + y^2 + z^2 # - R2
+x*y*z # - R3
+
+
+### Extensions:
+# Classical Polynomial: P12
+
+### Ext A1:
+R = c(1,3,2)
+b = 1
+sol = solve.SymM2.S2MP2(R, b=b)
+x = sol[,1]; y = sol[,2]; z = sol[,3]
+
+### Test:
+(x*y)^2 + (x*z)^2 + (y*z)^2 + b[1]*(x+y+z) # - R1
+x^2 + y^2 + z^2 # - R2
+x*y*z # - R3
+
+round0.p(poly.calc(x))
+
+
+### Ext A2:
+R = c(1,3,2)
+b = c(1, -1)
+sol = solve.SymM2.S2MP2(R, b=b)
+x = sol[,1]; y = sol[,2]; z = sol[,3]
+
+### Test:
+(x*y)^2 + (x*z)^2 + (y*z)^2 + b[1]*(x+y+z) # - R1
+x^2 + y^2 + z^2 + b[2]*(x+y+z) # - R2
+x*y*z # - R3
+
+round0.p(poly.calc(x))
+
+
+### Ext A2:
+R = c(1,3,2)
+b = c(1, -1, -2)
+sol = solve.SymM2.S2MP2(R, b=b)
+x = sol[,1]; y = sol[,2]; z = sol[,3]
+
+### Test:
+(x*y)^2 + (x*z)^2 + (y*z)^2 + b[1]*(x+y+z) # - R1
+x^2 + y^2 + z^2 + b[2]*(x+y+z) # - R2
+x*y*z + b[3]*(x+y+z) # - R3
+
+round0.p(poly.calc(x))
+
+
+#######################
 #######################
 
 ##############
