d664262858b27e05ede8590187f372007586f280
discoleo <leo.mada@syonic.eu>
2021-01-01 10:59:28 +0200

    Class 2 Polynomials

---
+++ Math/Polynomials.Class2.Description.R
@@ -0,0 +1,45 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Class 2 Polynomials
+### Description
+###
+### draft v.0.1a
+
+
+### Class 2 Polynomials:
+
+### Roots of P[n]:
+# Unlike Class 1 polynomials:
+# - do NOT contain radicals of order n;
+# - do not contain roots of unity of order n;
+# - many can be formed using roots of unity of order [n+1];
+#   [see also Generalization]
+
+
+### Generalization:
+# For Class 2 polynomials of Order [2*n]:
+# - roots of unity of order [2*n+1] satisfy:
+#   m + 1/m = r[n],
+#   where r[n] is the root of a polynomial of order n;
+#   [the use of roots of unity is still very practical]
+# - more generally, Class 2 polynomials are
+#   entanglements of lower order polynomials;
+# - for P[2*n]: P[2] o P[n];
+# - for P[2*n+1]: P[1]*P[2*n];
+#  -- e.g. P[5] = P[1]*P[2]*P[2];
+
+
+### Formulas:
+
+### A.) Simple Class 2 Polynomials:
+# - see file: Polynomials.Class2.Formulas.R;
+
+### B.) Special types of entanglements: P[2] o P[n]
+# - see file: Poly.System.Hetero.Symmetric.R;
+
+### C.) Very Simple / Quasi-Symmetric Polynomials:
+# - see file: Polynomials.Derived.P6.Symmetric.R;
+
