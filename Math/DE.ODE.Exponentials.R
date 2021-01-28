ee302a5999ca2426b28b43ffa5ea49942dc53a91
discoleo <leo.mada@syonic.eu>
2021-01-28 12:39:55 +0200

    log(y+x)*log(y-x) = P(x); [very pre-release]

--- Math/DE.ODE.Exponentials.R
+++ Math/DE.ODE.Exponentials.R
@@ -7,7 +7,7 @@
 ### Differential Equations
 ### ODEs - Exponentials
 ###
-### draft v.0.1a-ex2
+### draft v.0.1b-pre
 
 
 ### ODEs Derived from Exponentials
@@ -135,4 +135,27 @@ sapply(c(3/5 + (1:3)*3/5), line.tan, dx=3, p=y, dp=dy, PFUN=y1.lst)
 # inverse-exp-like:
 curve(dy(x, PFUN=y1.lst), add=T, col="green")
 sapply(c(3/5 + (1:3)*3/5), line.tan, dx=3, p=dy, dp=d2y, PFUN=y1.lst, col="orange")
+
+
+######################
+######################
+
+### Section D: Product-Type
+### Non-Linear ODEs
+
+### log(y + x) * log(y - x) = P(x)
+
+### D =>
+(dy + 1)*(y - x)*log(y - x) + (dy - 1)*(y + x)*log(y + x) - (y^2 - x^2)*dp # = 0
+
+### D2 =>
+(d2y*(y - x) + dy^2 - 1)*log(y - x) + (d2y*(y + x) + dy^2 - 1)*log(y + x) +
+	+ 2*dy^2 - 2 - 2*(y*dy - 2*x)*dp - (y^2 - x^2)*d2p # = 0
+
+### Solve liniar system:
+log(y + x) = - ... /2 / (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 - x*dy + y)
+log(y - x) = ... /2 / (...)
+
+### ODE:
+# TODO
 
