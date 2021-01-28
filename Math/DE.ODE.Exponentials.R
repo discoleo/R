bf87c455b9f4928ca4f5f5488bd74a7b9d61e284
discoleo <leo.mada@syonic.eu>
2021-01-28 14:12:46 +0200

    ODEs derived from: log(y+x)*log(y-x) = x; [cont]

--- Math/DE.ODE.Exponentials.R
+++ Math/DE.ODE.Exponentials.R
@@ -7,7 +7,7 @@
 ### Differential Equations
 ### ODEs - Exponentials
 ###
-### draft v.0.1b-pre
+### draft v.0.1b-alpha
 
 
 ### ODEs Derived from Exponentials
@@ -153,9 +153,33 @@ sapply(c(3/5 + (1:3)*3/5), line.tan, dx=3, p=dy, dp=d2y, PFUN=y1.lst, col="orang
 	+ 2*dy^2 - 2 - 2*(y*dy - 2*x)*dp - (y^2 - x^2)*d2p # = 0
 
 ### Solve liniar system:
-log(y + x) = - ... /2 / (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 - x*dy + y)
-log(y - x) = ... /2 / (...)
+log(y + x) = - ... /2 / (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+log(y - x) = ... /2 / (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+
 
 ### ODE:
 # TODO
+
+### Example:
+### P(x) = x
+(dy + 1)*(y - x)*log(y - x) + (dy - 1)*(y + x)*log(y + x) - (y^2 - x^2) # = 0
+(d2y*(y - x) + dy^2 - 1)*log(y - x) + (d2y*(y + x) + dy^2 - 1)*log(y + x) +
+	+ 2*(dy^2 - y*dy + x - 1) # = 0
+# =>
+2*log(y + x) = - (y - x) * (2*(dy + 1)*(dy^2 - y*dy + x - 1) + (d2y*(y - x) + dy^2 - 1)*(y + x)) /
+	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+2*log(y - x) = - (y + x) * (2*(dy - 1)*(dy^2 - y*dy + x - 1) + (d2y*(y + x) + dy^2 - 1)*(y - x)) /
+	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+# =>
+2*log(y + x) = - (y - x)*(y^2*d2y - x^2*d2y + 2*dy^3 - y*dy^2 + x*dy^2 + 2*dy^2 - 2*y*dy + 2*x*dy - 2*dy - y + x - 2) /
+	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+2*log(y - x) = - (y + x)*(...) /
+	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+
+### ODE:
+(y^2 - x^2)*(...)*(...) +
+	- 4*x*(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)^2 # = 0
+
+### TODO: check!
+
 
