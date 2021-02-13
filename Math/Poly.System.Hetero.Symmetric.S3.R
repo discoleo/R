13c68f7ec40d180f18c93a69b62313684b1fb66e
discoleo <leo.mada@syonic.eu>
2021-02-13 21:31:05 +0200

    systems with Composite Leading Term: move to new file;

--- Math/Poly.System.Hetero.Symmetric.S3.R
+++ Math/Poly.System.Hetero.Symmetric.S3.R
@@ -7,7 +7,7 @@
 ### Polynomial Systems: S3
 ### Heterogenous Symmetric
 ###
-### draft v.0.3h-fix2
+### draft v.0.4a
 
 
 ### Hetero-Symmetric
@@ -24,6 +24,10 @@ z^n + P(z, x, y) = R
 ### History ###
 ###############
 
+### draft v.0.4a:
+# - moved to new file:
+#   systems with Composite Leading Term: e.g. x^m*y^n;
+# - file: Poly.System.Hetero.Symmetric.S3.Leading.R;
 ### draft v.0.3h - v.0.3h-fix:
 # - Order 3: (partially solved + partial bug fix)
 #   x^3 + a1*y^3 + a2*z^3 = R;
@@ -109,10 +113,11 @@ z^n + P(z, x, y) = R
 # - the simple cases are less rewarding;
 
 
-################
+####################
 
-################
-### Introduction
+####################
+### Introduction ###
+####################
 
 ### 3 Variables:
 # x^n + P(x, y, z) = R
@@ -1129,9 +1134,9 @@ z^3 + b[1]*x + b[2]*y
 #########################
 #########################
 
-##########################
-### 2 High-Power Terms ###
-##########################
+#######################
+### 2 Leading Terms ###
+#######################
 
 ### x[i]^2 + x[j]^2 + b*x[j]
 
@@ -1455,7 +1460,8 @@ x^2 + z^2 + b[1]*y
 ########################
 ########################
 
-### High-Power Terms: 3
+########################
+### Leading Terms: 3 ###
 
 # x^2 + a1*y^2 + a2*z^2 = R
 # y^2 + a1*z^2 + a2*x^2 = R
@@ -1635,8 +1641,8 @@ E2^2 + 6*E3*S + S^4 - 4*E2*S^2
 ### Order 3 ###
 ###############
 
-##########################
-### 3 High-Power Terms ###
+#######################
+### 3 Leading Terms ###
 
 # x^3 + a1*y^3 + a2*z^3 = R
 # y^3 + a1*z^3 + a2*x^3 = R
@@ -1718,7 +1724,7 @@ calc.E2 = function(S, R, a) {
 ### Examples:
 R = -1
 a = c(-2, 2)
-b.ext = c(3)
+b.ext = c(1)
 sol = solve.3HT.S3P3(R, a, b.ext=b.ext)
 x = sol[,1]; y = sol[,2]; z = sol[,3];
 
@@ -1730,8 +1736,8 @@ z^3 + a[1]*x^3 + a[2]*y^3 + ext # - R
 
 
 ### Debug
-R = 2
-a = c(-2, 3)
+R = -1
+a = c(-2, 2)
 m = complex(re=cos(2*pi/3), im=sin(2*pi/3))
 x = rootn(R / (a[1] + a[2] + 1), 3)
 y = x*m; z = x*m^2;
@@ -2003,239 +2009,11 @@ z^3 + b[1]*(x+y)
 (x^3 - b*x - R)^2
 
 
-#######################
-#######################
-
-######################
-### X*Y High Power ###
-######################
-
-### x*y + b*y = R
-
-# x*y + b1*y = R
-# y*z + b1*z = R
-# z*x + b1*x = R
-
-### Extensions:
-
-### Simple Extension: type A1
-# x*y + b1*y + be2*(x+y+z)^2 + be1*(x+y+z) = R
-### Structural Extension:
-# a*x*y*z + x*y + b1*y = R
-
-
-### Solution:
-
-### Note:
-# - simple system: has NO solutions x != y != z;
-# - type A extensions: have such solutions;
-
-### Sum =>
-E2 + b1*S - 3*R # = 0
-
-### Sum(z*...) =>
-3*E3 + b1*E2 - R*S # = 0
-# b1*E2 = R*S - 3*E3
-
-### Diff =>
-# y*(x-z) = -b1*(y-z)
-# z*(x-y) = -b1*(x-z)
-# x*(y-z) =  b1*(x-y)
-### Prod =>
-# E3 = b1^3
-
-### Eq:
-b1*E2 + b1^2*S - 3*b1*R # = 0
-R*S - 3*E3 + b1^2*S - 3*b1*R # = 0
-(R + b1^2)*S - 3*b1*(b1^2 + R) # = 0
-S = 3*b1; # is a FALSE solution;
-
-
-### Solver:
-
-solve.CHP.S3P1 = function(R, b, b.ext=0, a=0, debug=TRUE) {
-	be1 = b.ext[1];
-	be2 = if(length(b.ext) > 1) b.ext[2] else 0;
-	a1 = a[1];
-	a2 = if(length(a) > 1) a[2] else 0;
-	if(be1 == 0 && be2 == 0) {
-		stop("NO solutions: x != y != z")
-		S = 3*b[1];
-	} else {
-		# S = roots(c(-be2, 3*b[1]*be2 - be1, R + b[1]^2 + 3*b[1]*be1, -3*b[1]^3 - 3*b[1]*R))
-		# isWrong = round0(S - 3*b[1]) == 0
-		# S = S[ ! isWrong]
-		S = roots(c(be2, be1, -R[1] - b[1]^2 + a1*b[1]^3 + a2*b[1]^6))
-	}
-	if(debug) print(S);
-	E3 = b[1]^3 - 0*S;
-	R1 = R[1] - be1*S - be2*S^2 - a1*E3 - a2*E3^2;
-	E2 = 3*R1 - b[1]*S;
-	#
-	len = length(S)
-	x = sapply(seq(len), function(id) roots(c(1, -S[id], E2[id], -E3[id])))
-	### robust
-	if(len == 1) {
-		S = rep(S, 3); R1 = rep(R1, 3);
-	} else {
-		S  = matrix(S, ncol=len, nrow=3, byrow=T)
-		R1 = matrix(R1, ncol=len, nrow=3, byrow=T)
-		E3 = matrix(E3, ncol=len, nrow=3, byrow=T)
-	}
-	#
-	yz = E3 / x;
-	z = (R1 - yz) / b[1];
-	y = S - x - z;
-	# Note: 1 set of solutions is incorrect!
-	sol = cbind(x=as.vector(x), y=as.vector(y), z=as.vector(z))
-	return(sol)
-}
-test.CHP.S3P1 = function(sol, R, b, b.ext=0, a=0) {
-	if(length(b.ext) < 2) b.ext = c(b.ext, 0);
-	if(length(a) < 2) a = c(a, 0)
-	x = sol[,1]; y = sol[,2]; z = sol[,3];
-	xyz = x*y*z; a.ext = a[1]*xyz + a[2]*xyz^2;
-	s = (x+y+z); s.ext = b.ext[1]*s + b.ext[2]*s^2;
-	ext = a.ext + s.ext;
-	err1 = x*y + b[1]*y + ext # - R
-	err2 = y*z + b[1]*z + ext # - R
-	err3 = z*x + b[1]*x + ext # - R
-	round0(rbind(err1, err2, err3))
-}
-
-### Examples:
-R = -1
-b = 3
-b.ext = c(1, 0)
-sol = solve.CHP.S3P1(R, b, b.ext=b.ext)
-
-### Test
-test.CHP.S3P1(sol, R, b, b.ext)
-
-
-### Ex 2:
-R = -1
-b = 3
-b.ext = c(1, 3)
-sol = solve.CHP.S3P1(R, b, b.ext=b.ext)
-
-### Test
-test.CHP.S3P1(sol, R, b, b.ext)
-round0.p(poly.calc(sol[1:6, 1]))
-
-
-############
-### Str Ext: Ex 1
-R = -1
-b = 3
-b.ext = c(1, 3); a = 2
-sol = solve.CHP.S3P1(R, b, b.ext=b.ext, a=a)
-
-### Test
-test.CHP.S3P1(sol, R, b, b.ext, a=a)
-round0.p(poly.calc(sol[1:6, 1]))
-
-
-############
-### Str Ext: Ex 2
-R = -1
-b = 3
-b.ext = c(1, 3); a = c(-1, 1)
-sol = solve.CHP.S3P1(R, b, b.ext=b.ext, a=a)
-
-### Test
-test.CHP.S3P1(sol, R, b, b.ext, a=a)
-round0.p(poly.calc(sol[1:6, 1]))
-
-
-############
-### Str Ext: Ex 3
-R = -1
-b = 3
-b.ext = c(0, -1); a = c(2, -1)
-sol = solve.CHP.S3P1(R, b, b.ext=b.ext, a=a)
-
-### Test
-test.CHP.S3P1(sol, R, b, b.ext, a=a)
-round0.p(poly.calc(sol[1:6, 1]))
-
-
-
-#######################
-### Mixt-Order: 2+1 ###
-
-### x[i]^2*x[j] + b*Sum
-
-# x^2*y + b1*(x+y+z) = R
-# y^2*z + b1*(x+y+z) = R
-# z^2*x + b1*(x+y+z) = R
-
-### Solution:
-
-# - trivial P9 polynomial;
-
-### Diff =>
-# x^2 = y*z
-# y^2 = x*z
-# z^2 = x*y
-
-# y = x^2/z
-# => x^4/z^2 = x*z
-# => x^3 = z^3
-# => y^3 = z^3
-
-# Case x != y != z
-# y = x*m
-# z = x*m^2
-# => 1 + m + m^2 = 0!
-# x^3*m = R
-
-m3 = unity(3, all=FALSE)
-
-### Example 1:
-
-b = 3
-R = 1
-#
-x = (R/m3)^(1/3) * c(1, m3, m3^2)
-y = x * m3
-z = x * m3^2
-sol = cbind(x,y,z)
-sol
-
-### Test
-x^2*y + b[1]*(x+y+z)
-y^2*z + b[1]*(x+y+z)
-z^2*x + b[1]*(x+y+z)
-
-### Classical Polynomial
-x^9 - R^3
-
-
 ##########################
 ##########################
 
-### Shifted
-### x[i]^2 * (x[j] - shift) + b*Sum
-
-# x^2*(y - s) + b1*(x+y+z) = R
-# y^2*(z - s) + b1*(x+y+z) = R
-# z^2*(x - s) + b1*(x+y+z) = R
-
-### Solution
-
-# Diff =>
-# x^2*(y - s) = y^2*(z - s)
-# y^2*(z - s) = z^2*(x - s)
-# z^2*(x - s) = x^2*(y - s)
-# =>
-# x^4*(y-s)^2 = y^2*z^2*(x-s)*(z-s)
-
-### TODO
-
-
-
-##########################
+### TODO:
+# - move to appropriate section;
 
 ### Shifted
 ### x[i]^2 * (x[i] - shift) + b*Sum
@@ -2365,14 +2143,14 @@ m3 = unity(3, all=F)
 
 ### Diff =>
 # x^3 - y^3 + b2*x*y*z*(x-y) + b1*(x-y) = 0
-# (x-y)*(x^2 + y^2 + x*y + b2*x*y*z + b1) = 0
-# (y-z)*(y^2 + z^2 + y*z + b2*x*y*z + b1) = 0
-# (z-x)*(x^2 + z^2 + x*z + b2*x*y*z + b1) = 0
+(x-y)*(x^2 + y^2 + x*y + b2*x*y*z + b1) # = 0
+(y-z)*(y^2 + z^2 + y*z + b2*x*y*z + b1) # = 0
+(z-x)*(x^2 + z^2 + x*z + b2*x*y*z + b1) # = 0
 # Case: x != y != z =>
-# x^2 + y^2 + x*y + b2*x*y*z + b1 = 0
-# y^2 + z^2 + y*z + b2*x*y*z + b1 = 0
-# x^2 + z^2 + x*z + b2*x*y*z + b1 = 0
-# =>
+x^2 + y^2 + x*y + b2*x*y*z + b1 # = 0
+y^2 + z^2 + y*z + b2*x*y*z + b1 # = 0
+x^2 + z^2 + x*z + b2*x*y*z + b1 # = 0
+# => Diff =>
 # x + y + z = 0
 
 ### Sum =>
@@ -2410,58 +2188,6 @@ z^3 + b[2]*x*y*z^2 + b[1]*z
 # Trivial: P9 = (P3)^3
 
 ### TODO
-
-
-
-##########################
-
-### x^2*y*z + b*y
-
-# x^2*y*z + b1*y = R
-# x*y^2*z + b1*z = R
-# x*y*z^2 + b1*x = R
-
-### Solution
-
-### Diff =>
-# x*y*z*(x-y) = - b1*(y-z)
-# x*y*z*(y-z) = - b1*(z-x)
-# x*y*z*(z-x) = - b1*(x-y)
-### Prod =>
-# (x*y*z)^3 = -b1^3
-# E3 = -b1 * m3;
-
-### Sum =>
-# x*y*z*(x+y+z) + b1*(x+y+z) = 3*R
-# (E3 + b1)*S = 3*R
-# S = 3*R / (E3 + b1)
-
-### Sum(x[i] * ...) =>
-# x*y*z*(x^2 + y^2 + z^2) + b1*E2 = R*S
-# E3*(S^2 - 2*E2) + b1*E2 = R*S
-# (2*E3 - b1)*E2 = E3*S^2 - R*S
-# E2 = (E3*S^2 - R*S) / (2*E3 - b1)
-
-m3.all = unity(3, all=T)
-
-### Example:
-b = 1/3
-R = 1
-#
-E3 = - b[1] * m3.all [-1]; # real root has to be removed
-S = 3*R / (E3 + b[1]); # E3 != -b1
-E2 = (E3*S^2 - R*S) / (2*E3 - b[1])
-x = as.vector(sapply(1:2, function(id) roots(c(1, -S[id], E2[id], -E3[id]))))
-E3 = rep(E3, each=3)
-y = (R - x*E3) / b[1]
-z = (R - y*E3) / b[1]
-sol = cbind(x,y,z)
-sol
-
-### Test
-x^2*y*z + b[1]*y
-x*y^2*z + b[1]*z
-x*y*z^2 + b[1]*x
 
 
 #################################
@@ -2596,4 +2322,54 @@ b2^2*(x^4+y^4+z^4) - 2*b1*b2*(x^3+y^3+z^3) + (b1^2 - 2*b2*R)*(x^2+y^2+z^2) - 2*b
 
 ########################
 ########################
+
+### [Temporary]
+# TODO:
+# - clean & integrate where appropriate;
+
+# x^2 + b*x*z = R
+# y^2 + b*y*z = R
+# z^2 + b*x*y = R
+
+### Diff: (1) - (2) =>
+x^2 - y^2 + b*z*(x - y) # = 0
+(x - y)*(x + y + b*z) # = 0
+### Cases:
+# x == y *OR*
+# x + y + b*z = 0;
+x + y + b*z # = 0
+
+### Sum => [not used]
+x^2 + y^2 + z^2 + b*E2 - 3*R # = 0
+S^2 + (b - 2)*E2 - 3*R # = 0
+# (b - 2)*E2 = - (S^2 - 3*R);
+
+### Sum: (1) + (2) =>
+x^2 + y^2 + b*z*(x+y) - 2*R # = 0
+(x+y)^2 - 2*x*y + b*z*(-b*z) - 2*R # = 0
+(-b*z)^2 - 2*x*y + b*z*(-b*z) - 2*R # = 0
+x*y + R # = 0
+# =>
+# z^2 = R*(b + 1);
+# =>
+# x + y = - b*z;
+
+### Solution:
+R = 1
+b = 2
+#
+z = sqrt(R[1]*(b[1] + 1) + 0i)
+z = c(z, -z);
+xy.s = - b[1] * z;
+xy = -R[1];
+xy.d = sqrt(xy.s^2 - 4*xy)
+xy.d = c(xy.d, -xy.d)
+xy.s = rep(xy.s, 2); z = rep(z, 2)
+x = (xy.s + xy.d) / 2;
+y = (xy.s - xy.d) / 2;
+
+### Test
+x^2 + b[1]*x*z # - R
+y^2 + b[1]*y*z # - R
+z^2 + b[1]*x*y # - R
 
