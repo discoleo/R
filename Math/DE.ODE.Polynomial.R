6da250daaa34350a3e4ae8de116abc3f3d9bdb4f
discoleo <leo.mada@syonic.eu>
2021-02-28 22:00:02 +0200

    [fixed] y = x * I(dx / (x^n + k)) + F0(x);

--- Math/DE.ODE.Polynomial.R
+++ Math/DE.ODE.Polynomial.R
@@ -7,15 +7,15 @@
 ### Differential Equations
 ### ODEs: Polynomial types
 ###
-### draft v.0.4a
+### draft v.0.4a-fix
 
 
 ### History
 
 ### Order 1 Non-Liniar
-### draft v.0.4a:
+### draft v.0.4a - v.0.4a-fix:
 # - derived from:
-#   y = x * I(dx / (x^n + k)) + F0(x);
+#   y = x * I(dx / (x^n + k)) + F0(x); [fixed]
 ### draft v.0.3b:
 # - Base-solution with Radicals:
 #   2*x*y^2*d2y - (n-2)*y^2*dy - n*dy = 0;
@@ -1626,22 +1626,95 @@ x^2 / (x*dy - y + f0 - x*df0) - k
 
 ### D2(y)
 # x*d2y + dy =
-dy + 2*x / (x^n + k)) - n*x^(n + 1) / (x^n + k)^2 + x*d2f0;
+dy + 2*x / (x^n + k) - n*x^(n + 1) / (x^n + k)^2 + x*d2f0;
 # x^2*d2y =
 2*(x*dy - y + f0 - x*df0) - n*x^(n + 2) / (x^n + k)^2 + x^2*d2f0;
-2*(x*dy - y + f0 - x*df0) - n*x^(n - 2) / (x*dy - y + f0 - x*df0)^2 + x^2*d2f0;
+2*(x*dy - y + f0 - x*df0) - n*x^(n - 2)*(x*dy - y + f0 - x*df0)^2 + x^2*d2f0;
 
 ### ODE:
-x^2*(x*dy - y + f0 - x*df0)^2*d2y +
-	- 2*(x*dy - y + f0 - x*df0)^3 - x^2*d2f0*(x*dy - y + f0 - x*df0)^2 + n*x^(n - 2)
+x^2*d2y +
+	- 2*(x*dy - y + f0 - x*df0) + n*x^(n - 2)*(x*dy - y + f0 - x*df0)^2 - x^2*d2f0;
 
 ### Note:
 # - it is possible to leave n*x^(n + 1) / (x^n + k)^2 as such;
 # - it is possible to express x^n as a function of: dy, y;
 # - this latter case becomes interesting when n = sqrt(n0):
-x^2*(x*dy - y + f0 - x*df0)^2*d2y +
-	- 2*(x*dy - y + f0 - x*df0)^3 - x^2*d2f0*(x*dy - y + f0 - x*df0)^2 +
-	+ sqrt(n0) / (x*dy - y + f0 - x*df0) - k/x^2;
+x^4*d2y - n*k*(x*dy - y + f0 - x*df0)^2 +
+	+ (n - 2)*x^2*(x*dy - y + f0 - x*df0) - x^4*d2f0;
 
 ### TODO: check;
+
+### Solution:
+y.Int = function(x, n, k, lower=0) {
+	sapply(x, function(upper)
+		integrate(function(x) 1/(x^n + k), lower=lower, upper=upper)$value)
+}
+y = function(x, b0=1, n=2, k=1, lower=0, asList=FALSE) {
+	I.x = y.Int(x, n=n, k=k, lower=lower);
+	f0 = eval.pol(x, b0);
+	y.x = x*I.x + f0;
+	if(asList) return(list(y=y.x, f0=f0));
+	return(y.x)
+}
+dy = function(x, b0=1, n=2, k=1, lower=0, asList=FALSE) {
+	y.all = y(x, b0=b0, n=n, k=k, lower=lower, asList=TRUE)
+	y.x = y.all$y; f0 = y.all$f0;
+	df0 = deriv.pol(x, b0, dn=1);
+	div = x;
+	dp = (y.x - f0) + x^2 / (x^n + k) + x*df0
+	dp = ifelse(div != 0, dp / div,
+		dy(x + 1E-3, b0=b0, n=n, k=k, lower=lower)); # TODO: check
+	if(asList) return(list(y=y.x, dy=dp, f0=f0, df0=df0));
+	return(dp)
+}
+d2y = function(x, b0=1, n=2, k=1, lower=0) {
+	y.all = dy(x, b0=b0, n=n, k=k, lower=lower, asList=TRUE);
+	y.x = y.all$y; dy.x = y.all$dy;
+	f0 = y.all$f0; df0 = y.all$df0; d2f0 = deriv.pol(x, b0, dn=2);
+	Tx = (x*dy.x - y.x + f0 - x*df0);
+	dp = n*k*Tx^2 - (n - 2)*x^2*Tx + x^4*d2f0;
+	div = x^4;
+	dp = ifelse(div != 0, dp / div,
+		d2y(x + 1E-3, b0=b0, n=n, k=k, lower=lower)); # TODO
+	return(dp)
+}
+### Plot
+b0 = c(1, 1); n = 2; k = 1;
+px = c(0:3, 5) * 2/5 - 1/2;
+curve(y(x, b0=b0, n=n, k=k), from=-0.5, to=3, ylim=c(-0.5, 6.5))
+line.tan(px, dx=2.5, p=y, dp=dy, b0=b0, n=n, k=k)
+# D(y)
+curve(dy(x, b0=b0, n=n, k=k), add=T, col="green")
+line.tan(px, dx=2.5, p=dy, dp=d2y, b0=b0, n=n, k=k, col="orange")
+
+
+### Ex 2:
+b0 = c(1, -1); n = 2; k = 3;
+px = c(0:3, 5) * 2/5 - 1/2;
+curve(y(x, b0=b0, n=n, k=k), from=-0.5, to=3, ylim=c(-2, 2))
+line.tan(px, dx=2.5, p=y, dp=dy, b0=b0, n=n, k=k)
+# D(y)
+curve(dy(x, b0=b0, n=n, k=k), add=T, col="green")
+line.tan(px, dx=2.5, p=dy, dp=d2y, b0=b0, n=n, k=k, col="orange")
+
+
+### Ex 3:
+b0 = c(1, -1); n = 5^(1/5); k = 3;
+px = c(0:3, 5) * 2/5;
+curve(y(x, b0=b0, n=n, k=k), from=0, to=3, ylim=c(-1.2, 1.2))
+line.tan(px, dx=2.5, p=y, dp=dy, b0=b0, n=n, k=k)
+# D(y)
+curve(dy(x, b0=b0, n=n, k=k), add=T, col="green")
+line.tan(px, dx=2.5, p=dy, dp=d2y, b0=b0, n=n, k=k, col="orange")
+
+### [cont]
+# nice global minimum
+px = c(0.5, 1:4)*2 - 1/2;
+curve(y(x, b0=b0, n=n, k=k), from=0, to=10, ylim=c(-1.2, 2.5))
+line.tan(px, dx=2.5, p=y, dp=dy, b0=b0, n=n, k=k)
+# D(y)
+curve(dy(x, b0=b0, n=n, k=k), add=T, col="green")
+line.tan(px, dx=2.5, p=dy, dp=d2y, b0=b0, n=n, k=k, col="orange")
+
+
 
