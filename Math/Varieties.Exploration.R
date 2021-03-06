1db1eb74d110d27f61c3babdd10f30eb75ceecff
discoleo <leo.mada@syonic.eu>
2021-03-07 00:06:18 +0200

    more exploration of varieties;

--- Math/Varieties.Exploration.R
+++ Math/Varieties.Exploration.R
@@ -6,7 +6,7 @@
 ### Varieties:
 ### Basic Exploration
 ###
-### draft v.0.1a
+### draft v.0.1b
 
 ### Introduction
 
@@ -50,7 +50,7 @@ variety = function(x, sVar, R=1, b=c(10,60), type="min", tol=1E-5) {
         sol = sort(solve.Y2Y1.S3P3(R, b=c(b2, b1), debug=F)[,1])
         sol = round0(sol, tol=tol);
         sol = Re(sol[Im(sol) == 0]);
-		type = match(type, c("min", "max", "count"));
+		type = match(type, c("min", "max", "count", "min2", "max2"));
 		if(is.na(type)) stop("Type NOT supported!")
 		if(type == 1) {
 			sol = if(length(sol) >= 1) sol[1] else NA;
@@ -58,6 +58,10 @@ variety = function(x, sVar, R=1, b=c(10,60), type="min", tol=1E-5) {
 			sol = if(length(sol) >= 1) sol[length(sol)] else NA;
 		} else if(type == 3) {
 			sol = length(sol);
+		} else if(type == 4) {
+			sol = if(length(sol) >= 3) sol[2] else NA;
+		} else if(type == 5) {
+			sol = if(length(sol) >= 4) sol[length(sol) - 1] else NA;
 		}
         return(sol)
     }
@@ -71,12 +75,29 @@ variety = function(x, sVar, R=1, b=c(10,60), type="min", tol=1E-5) {
 	}
     return(sol)
 }
+toRGB = function(col, scale=1) {
+	col = col / scale;
+	col[col < 0] = 1 + col[col < 0];
+	rgb(col[1], col[2], col[3])
+}
 
 ### Examples:
 
 curve(variety(x, sVar="R", b=c(10,60)), from=-100, to=100, ylim=c(-10, 10))
 curve(variety(x, sVar="R", b=c(10,60), type="max"), col="green", add=T)
 curve(variety(x, sVar="R", b=c(10,60), type="count"), col="red", add=T)
-text(c(-50, -2), "Varieties") # "sort of"
+curve(variety(x, sVar="R", b=c(10,60), type="min2"), col="orange", add=T)
+curve(variety(x, sVar="R", b=c(10,60), type="max2"), col="orange", add=T)
+text(-50, -2, "Varieties") # "sort of"
+
+
+### Multiple
+curve.v = function(b, type="max") curve(variety(x, sVar="R", b=c(10,b), type=type),
+	col=toRGB(c(-b+20,60,0), 80), add=T);
+by = 2;
+curve(variety(x, sVar="R", b=c(10,60)), from=-100, to=100, ylim=c(-10, 10))
+r = sapply(seq(30, 60, by=by), curve.v)
+r = sapply(seq(30, 60, by=by), curve.v, type="min")
+text(0, -1.25, "Varieties") # "sort of"
 
 
