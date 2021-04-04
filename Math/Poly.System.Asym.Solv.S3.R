169c5a7485f7d57f31f21267236274918c24a885
discoleo <leo.mada@syonic.eu>
2021-04-04 09:29:05 +0300

    solved: x*y*z + B %*% c(x*y, x*z, y*z) = R;

--- Math/Poly.System.Asym.Solv.S3.R
+++ Math/Poly.System.Asym.Solv.S3.R
@@ -6,7 +6,7 @@
 ### Polynomial Systems: S3
 ### Asymmetric: solvable
 ###
-### draft v.0.1b
+### draft v.0.1c
 
 
 
@@ -16,6 +16,9 @@
 ### History ###
 ###############
 
+### draft v.0.1c:
+# - system:
+#   x*y*z + B %*% c(x*y, x*z, y*z) = R;
 ### draft v.0.1b:
 # - system:
 #   x*y*z + B %*% c(x^2, y^2, z^2) = R;
@@ -129,4 +132,59 @@ sol = solve.E3.S3P1(R, B);
 ### Test
 round0(rep(apply(sol, 2, prod), each=3) + B %*% sol^2)
 
+
+############################
+############################
+
+### x*y*z + B %*% c(x*y, x*z, y*z) = R
+
+x*y*z + b11*x*y + b12*x*z + b13*y*z = R1
+# ...
+
+### Solution:
+
+### Step 1: solve for: x*y, x*z, y*z;
+
+### Step 2:
+(r1 + E3)*(r2 + E3)*(r3 + E3) - a*E3^2 # = 0
+E3^3 + (r1+r2+r3 - a)*E3^2 + (r1*r2+r1*r3+r2*r3)*E3 + r1*r2*r3 # = 0
+
+### Step 3:
+# x = E3 / (y*z);
+
+### Solver
+solve.E3.S3P1 = function(R, B, debug=TRUE) {
+	m.coeff = solve(B, cbind(R, -1 * c(1,1,1)))
+	r = m.coeff[,1] / m.coeff[,2];
+	a = 1 / prod(m.coeff[,2]);
+	E3 = roots(c(1, sum(r) - a, (r[1]*r[2]+r[1]*r[3]+r[2]*r[3]), prod(r)));
+	if(debug) print(E3);
+	sol = sapply(E3, function(e3) m.coeff[,1] + m.coeff[,2]*e3)
+	E3 = rep(E3, each=3);
+	sol = E3 / sol[c(3,2,1),];
+	rownames(sol) = c("x","y","z")
+	return(sol)
+}
+E2.f = function(sol) {
+	matrix(c(sol[1,]*sol[2,], sol[1,]*sol[3,], sol[2,]*sol[3,]), ncol=3, byrow=TRUE)
+}
+
+### Examples:
+R = c(1,2,3)
+B = matrix(c(1,2,-1, 3,3,1, -1,2,-2), ncol=3, byrow=TRUE)
+
+sol = solve.E3.S3P1(R, B);
+
+### Test
+rep(apply(sol, 2, prod), each=3) + B %*% E2.f(sol)
+
+
+### Ex 2:
+R = c(0,-1,3)
+B = matrix(c(1,2,-1, 3,3,1, -1,2,-2), ncol=3, byrow=TRUE)
+
+sol = solve.E3.S3P1(R, B);
+
+### Test
+round0(rep(apply(sol, 2, prod), each=3) + B %*% E2.f(sol))
 
