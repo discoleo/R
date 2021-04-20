ab746244200bbd73d021bfaf7f8462972d9653b9
discoleo <leo.mada@syonic.eu>
2021-04-20 20:47:06 +0300

    Types of Coupled Processes: using binomial dist;

--- Stat/Probabilities.Sim.R
+++ Stat/Probabilities.Sim.R
@@ -1,7 +1,15 @@
+#######################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Coupled / Recursive Poisson Processes
+###
+### draft v.0.1c
+
 
 ### Coupled / Recursive Poisson Processes
-
-# some basic experiments in Probabilities;
+# - some basic experiments in Probabilities;
 
 
 ######################
@@ -13,18 +21,28 @@ rpois.heavy = function(n, lambda, rm.zero=FALSE) {
 	y = sapply(x, function(n) rpois(1, n))
 	invisible(y);
 }
-rpois.cor = function(n, lambda, dL, scale.by=0, rm.zero=TRUE) {
+rpois.cor = function(n, lambda, dL, scale.by=0, rm.zero=TRUE,
+		type=c("recursive", "binomial")) {
 	# correlated pseudo-Poisson processes
+	# rm.zero = remove the 0s in the initial process;
 	x = rpois(n, lambda1)
 	if(rm.zero) x = x[x != 0];
-	x1 = x - dL; x1[x1 < 0] = 0;
-	if(scale.by != 0) {
-		isGr = (x > lambda); isLs = (x < lambda);
-		x1[isGr] = x1[isGr] - scale.by;
-		x1[isLs] = x1[isLs] + scale.by;
-		x1[x1 < 0] = 0;
+	# Type
+	type = match.arg(type);
+	if(is.na(type)) stop("Type NOT supported!")
+	if(type == "recursive") {
+		x1 = x - dL; x1[x1 < 0] = 0;
+		if(scale.by != 0) {
+			isGr = (x > lambda); isLs = (x < lambda);
+			x1[isGr] = x1[isGr] - scale.by;
+			x1[isLs] = x1[isLs] + scale.by;
+			x1[x1 < 0] = 0;
+		}
+		y = sapply(x1, function(n) rpois(1, n));
+	} else if(type == "binomial") {
+		p = 0.5 - dL/lambda / 2;
+		y = sapply(x, function(n) rbinom(1, 2*n, p));
 	}
-	y = sapply(x1, function(n) rpois(1, n));
 	invisible(list(x=x, y=y));
 }
 simulate = function(n, FUN, FUN2, iter=100, ...) {
@@ -97,6 +115,33 @@ y1 = dpois(gs.seq, lambda);
 y2 = dpois(gs.seq, lambda + 0.5);
 lines(gs.seq, y1, col="red")
 lines(gs.seq, y2, col="green")
+
+### Heatmap
+cor(x, y)
+tbl = table(x, y)
+heatmap(tbl, Rowv=NA, Colv=NA)
+
+
+############
+
+### Test 2:
+### Correlated events
+n = 1000
+lambda1 = 3
+dL = 0.25
+#
+r = rpois.cor(n, lambda1, dL=dL, type="binomial")
+x = r$x; y = r$y;
+
+### Analysis
+length(x); mean(x); mean(y);
+table(x); table(y)
+table(y <= x)
+table(y - x)
+
+v = c(x, -y)
+hist(v, breaks=12)
+sum(v) / n;
 
 ### Heatmap
 cor(x, y)
