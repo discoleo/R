1d680bd26d7466872d3bb0a67976f289032f446d
discoleo <leo.mada@syonic.eu>
2021-05-05 08:24:20 +0300

    Mixed / Composite S3 Systems

---
+++ Math/Poly.System.Hetero.Symmetric.S3.Composite.R
@@ -0,0 +1,104 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Polynomial Systems:
+### Heterogeneous Symmetric S3:
+### Mixed Type: Composite
+###
+### draft v.0.1a
+
+
+### Heterogeneous Symmetric
+### Polynomial Systems: 3 Variables
+### Mixed/Composite: Hetero + Symmetric
+
+### Example:
+# x^p + b*y = Ru # where Ru = unknown
+# y^p + b*z = Ru
+# z^p + b*x = Ru
+# x*y + x*z + y*z = R2
+
+####################
+####################
+
+### helper functions
+
+library(polynom)
+library(pracma)
+
+# the functions are in the file:
+# Polynomials.Helper.R
+
+######################
+######################
+
+###################
+### Liniar Type ###
+###################
+
+###############
+### Order 2 ###
+###############
+
+x^2 + b*y = Ru # where Ru = unknown
+y^2 + b*z = Ru
+z^2 + b*x = Ru
+x*y + x*z + y*z = R
+
+### Solution
+
+### Sum =>
+x^2 + y^2 + z^2 + b1*(x+y+z) - 3*Ru # = 0
+S^2 - 2*E2 + b1*S - 3*Ru # = 0
+# 3*Ru = S^2 - 2*E2 + b1*S;
+
+### Sum(x[i]*...) =>
+6*E3 - S^3 - 2*b1*S^2 + 7*Ru*S + b1^2*S - 3*b1*Ru # = 0
+
+### Sum(x[i+1]^2*...) =>
+### Eq:
+(S^2 + 3*b1*S - 9*Ru)*(S^2 - b1*S - Ru + 2*b1^2) # = 0
+(S^2 - 3*E2)*(S^2 - 2*b1*S + E2 + 3*b1^2) # = 0
+
+
+### Solver:
+solve.CompLin.S3P2 = function(R, b, debug=TRUE) {
+	coeff = c(1, - 2*b[1], R[1] + 3*b[1]^2);
+	S  = roots(coeff);
+	S2 = roots(c(1, 0, -3*R[1]));
+	S = c(S, S2);
+	if(debug) print(S);
+	len = length(S);
+	E2 = rep(R[1], len);
+	Ru = (S^2 - 2*E2 + b[1]*S) / 3;
+	E3 = (S^3 + 2*b[1]*S^2 - 7*Ru*S - b[1]^2*S + 3*b[1]*Ru) / 6;
+	x = sapply(seq_along(S), function(id) roots(c(1, -S[id], E2[id], -E3[id])));
+	#
+	Ru = rep(Ru, each=3); S = rep(S, each=3);
+	y = (Ru - x^2) / b[1];
+	z = S - x - y;
+	sol = cbind(x=as.vector(x), y=as.vector(y), z=as.vector(z));
+	return(sol);
+}
+
+### Examples:
+
+R = -1
+b = 1
+sol = solve.CompLin.S3P2(R, b)
+x = sol[,1]; y = sol[,2]; z = sol[,3];
+
+cbind(
+x^2 + b*y,
+y^2 + b*z,
+z^2 + b*x)
+x*y + x*z + y*z # - R
+
+###
+round0.p(poly.calc(x[1:6]))
+x = x[1:6]
+err = 1 - x^2 + 2*x^3 - 2*x^5 + x^6
+round0(err)
+
