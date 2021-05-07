a82c59e3bb427d5bb0544a72ff206e038ffeef77
discoleo <leo.mada@syonic.eu>
2021-05-07 10:30:04 +0300

    Order 4 variant: x*y*(x+y) = R;

--- Math/Poly.System.Hetero.Symmetric.S2.Composite.R
+++ Math/Poly.System.Hetero.Symmetric.S2.Composite.R
@@ -7,7 +7,7 @@
 ### Heterogeneous Symmetric S2:
 ### Mixed Type: Composite
 ###
-### draft v.0.1b
+### draft v.0.1b-o4
 
 
 ### Heterogeneous Symmetric
@@ -34,9 +34,11 @@
 ###############
 
 
-### draft v.0.1b:
-# - Order 3 variant:
+### draft v.0.1b - v.0.1b-04:
+# - Order 3 & Order 4 variants:
 #   x*y*(x+y) = R;
+# - example P[6]:
+#   1 - 2*x + x^2 + 4*x^4 + 3*x^6 = 0;
 ### draft v.0.1a:
 # - solved: double Ht-Symmetric Composite;
 # - example P[6]:
@@ -68,6 +70,7 @@ S^3 - b2*S^2 - b1*S - R # = 0
 
 ### Solver:
 solve.HtComposite.S2P3 = function(b, debug=TRUE) {
+	if(length(b) < 2) b = c(b, 0);
 	coeff = c(1, -b[2], -b[1], -R[1]);
 	S = roots(coeff);
 	if(debug) print(S);
@@ -96,6 +99,77 @@ round0.p(poly.calc(x))
 err = 1 + 6*x - x^2 - 4*x^3 - 2*x^4 + 2*x^5 + x^6
 round0(err)
 
+
+######################
+
+################
+### Order: 4 ###
+################
+
+# x^4 + b2*y^2 + b1*y = Ru1
+# y^4 + b2*x^2 + b1*x = Ru1
+# x*y*(x+y) = R
+
+### Solution:
+
+### Case: x != y
+
+### Diff Eqs 1, 2 =>
+S^3 - 2*x*y*S - b2*S - b1 # = 0
+### =>
+S^3 - b2*S - b1 - 2*R # = 0
+
+### Solver:
+solve.HtComposite.S2P4 = function(b, debug=TRUE) {
+	if(length(b) < 2) b = c(b, 0);
+	coeff = c(1, 0, -b[2], -b[1] - 2*R[1]);
+	S = roots(coeff);
+	if(debug) print(S);
+	S = S[S != 0]; # exclude 0!
+	xy = R[1] / S;
+	xy.d = sqrt(S^2 - 4*xy + 0i);
+	x = (S + xy.d) / 2;
+	y = S - x;
+	sol = cbind(x=x, y=y);
+	sol = rbind(sol, sol[,2:1]);
+	return(sol);
+}
+
+### Examples:
+
+R = -1
+b = c(-1, -1)
+sol = solve.HtComposite.S2P4(b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+cbind(x^4 + b[2]*y^2 + b[1]*y, y^4 + b[2]*x^2 + b[1]*x)
+x*y*(x+y) # - R[1]
+
+###
+round0.p(poly.calc(x) * 3)
+err = 1 - 2*x + x^2 + 4*x^4 + 3*x^6
+round0(err)
+
+
+#########
+### Ex 2:
+R = -1
+b = c(-1, 2)
+sol = solve.HtComposite.S2P4(b)
+x = sol[,1]; y = sol[,2];
+
+### Test
+cbind(x^4 + b[2]*y^2 + b[1]*y, y^4 + b[2]*x^2 + b[1]*x)
+x*y*(x+y) # - R[1]
+
+###
+round0.p(poly.calc(x) * 3)
+err = 1 + 4*x + 4*x^2 - 8*x^4 + 3*x^6
+round0(err)
+
+### TODO:
+-R^3 + 2*b[2]*R^2*x - R*b[2]^2*x^2 + (...)*x^3 - (4)*b[2]*x^4 - (2*R-1)*x^6
 
 ######################
 
