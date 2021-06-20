eab4700737fa1a466d59227e2cdbacd7dbfac955
discoleo <leo.mada@syonic.eu>
2021-06-20 08:01:55 +0300

    S4P3 Simple: (partial) Eq for S;

--- Math/Poly.System.Hetero.Symmetric.S4.Derivation.R
+++ Math/Poly.System.Hetero.Symmetric.S4.Derivation.R
@@ -7,7 +7,7 @@
 ### Heterogeneous Symmetric
 ###  == Derivation ==
 ###
-### draft v.0.3a-clPoly-alpha
+### draft v.0.3b-part
 
 
 ####################
@@ -556,8 +556,14 @@ b^4*E4 - R^4 + R^3*(x1^3+x2^3+x3^3+x4^3) - R^2*E2_3 + R*E3_3 - E4^3 # = 0
 b^4*E4 + R^3*(S^3 - 3*E2*S + 3*E3) - R^2*(E2^3 + 3*E3^2 - 3*E3*E2*S + 3*E4*S^2 - 3*E2*E4) +
 	+ R*(E3^3 - 3*E4*E3*E2 + 3*E4^2*S) - E4^3 - R^4 # = 0
 
+
+### Eq:
+S^18 - 15*R*S^15 + 48*b^2*S^14 - 126*R*b*S^13 + 222*R^2*S^12 - 256*b^3*S^12 + 609*R*b^2*S^11 +
+	- (1764*R^2*b + 540*b^4)*S^10 + (2158*R^3 + 5061*R*b^3)*S^9 +
+	- (8433*R^2*b^2 + 960*b^5)*S^8 + ...
+
 ### TODO:
-# - solve;
+# - solve: substantial numerical instability for coeffs of S^[7-0];
 # - solve special Sub-Case;
 
 ### Special Sub-Case:
@@ -1246,7 +1252,7 @@ E4^4 - b^4*E4 + b^3*R*E3 - b^2*R^2*E2 + b*R^3*S - R^4 # = 0
 4*b*R*E4^2*S - 8*R^2*E4^2 - 4*b^3*R*E3 - b^4*E2^2 + 4*b^2*R^2*E2 + 2*b^4*E3*S +
 	- 4*b*R^3*S - b^5*S + b^2*R^2*S^2 - 4*b*R^3*S + 4*b^4*R + 8*R^4 # = 0
 ### Eq 3:
-E3^2 - 2*E2*E4 + 3*b*E3 - 3*b*E2*S + 2*R*E2 + b*S^3 - R*S^2 # = 0
+E3^2 + 3*b*E3 - 2*E2*E4 - 3*b*E2*S + 2*R*E2 + b*S^3 - R*S^2 # = 0
 ### Eq 4:
 2*E4^3 + 2*b*E4^2*S - 2*R*E4^2 + b^2*E2*E4 + b*R*E4*S - 2*R^2*E4 - b^2*E3^2 + 2*b^3*E3 - b^3*E2*S +
 	- b^2*R*E2 + 2*b^2*R*S^2 - 3*b*R^2*S + 2*R^3 # = 0
@@ -1286,9 +1292,20 @@ R*x3^2*x4^2 + b^2*x1*x3 - b*R*x3 + b*x1^2*x4^3 - R*x1^2*x4^2
 b^2*x1^4*x4^8 - 2*b*R*x1^4*x4^7 + R^2*x1^4*x4^6 + 2*b*R^2*x1^2*x4^5 - 2*b^2*R*x1^3*x4^5 - 2*R^3*x1^2*x4^4 +
 	+ 2*b*R^2*x1^3*x4^4 + R^4*x4^2 + b^2*R^2*x1^2*x4^2 - 2*b*R^3*x1*x4^2 - b^2*R^3 + b^5*x1^3 +
 	+ 3*b^3*R^2*x1 - 3*b^4*R*x1^2
+
+### Eq 1 =>
+(b^2*x1 - b*R)*x1^2*x2^2 - b*(b*x1^2*x4^3 - R*x1^2*x4^2 - b*R*x1 + R^2) - (b^2*x1 - b*R)*R # = 0
+(b*x1^3 - R*x1^2)*x2^2 - (b*x1^2*x4^3 - R*x1^2*x4^2) # = 0
+### Eq 4 =>
+b^2*x2^2 - (x4^2*x1^2 - R)^2 # = 0
+b^2*(b*x1^2*x4^3 - R*x1^2*x4^2) - (b*x1^3 - R*x1^2)*(x4^2*x1^2 - R)^2 # = 0
+R*x1^6*x4^4 - b*x1^7*x4^4 + b^3*x1^2*x4^3 - b^2*R*x1^2*x4^2 - 2*R^2*x1^4*x4^2 + 2*b*R*x1^5*x4^2 +
+	+ R^3*x1^2 - b*R^2*x1^3
 # TODO
 
+x3^2*(b*x3^3 - R*x3^2 + R*x1^2)^2 + b^3*x1^5 - b^2*R*x1^4 # = 0
 
+pX.gen = function(x1, x4, b=0, R=0, coeff=1) data.frame(x1=x1, x4=x4, b=b, R=R, coeff=coeff);
 p14 = data.frame(
 	x1 = c(2, 2, 1, 0),
 	x4 = c(3, 2, 0, 0),
@@ -1305,4 +1322,33 @@ p14r = sum.pm(p14sq, p1b);
 p14r = sort.pm(p14r, xn="x4");
 p14r = p14r[,c("b", "R", "x1", "x4", "coeff")]
 print.p(p14r, leading="x4")
+
+#
+p4a = data.frame(
+	x1 = 2, x4 = c(3, 2), b = c(3, 2), R = c(0, 1),
+	coeff = c(1,-1)
+)
+p4b = data.frame(x1=c(3, 2), b=c(1, 0), R=c(0,1), coeff=c(1,-1))
+p4c = data.frame(x1=c(2, 0), x4=c(2, 0), R=c(0, 1), coeff=c(1,-1))
+#
+p4 = mult.pm(pow.pm(p4c, 2), p4b);
+p4 = diff.pm(p4a, p4);
+p4 = sort.pm(p4, xn="x4");
+p4 = p4[,c("b", "R", "x1", "x4", "coeff")]
+print.p(p4, leading="x4")
+
+#
+p4r = diff.pm(mult.pm(p14r, diff.pm(pX.gen(2,0,R=1), pX.gen(3,0,b=1,))),
+	mult.pm(p4, pX.gen(x1=0, x4=4, b=2)))
+# p4r$coeff = - p4r$coeff;
+p4r$x1 = p4r$x1 - min(p4r$x1);
+p4r = sort.pm(p4r, c(4,2), xn="x4");
+p4r = p4r[,c("b", "R", "x1", "x4", "coeff")]
+print.p(p4r, leading="x4")
+
+2*b^2*R*x1^5*x4^7 - 2*b*R^2*x1^4*x4^7 - b^5*x4^7 - b*R^2*x1^5*x4^6 - 2*b^3*R*x1^3*x4^6 + R^3*x1^4*x4^6 +
+	+ 2*b^2*R^2*x1^2*x4^6 + b^4*R*x4^6 + 2*b^3*R*x1^4*x4^5 - 4*b^2*R^2*x1^3*x4^5 + 2*b*R^3*x1^2*x4^5 +
+	- 2*b^2*R^2*x1^4*x4^4 + 4*b*R^3*x1^3*x4^4 + b^3*R^2*x1*x4^4 - 2*R^4*x1^2*x4^4 - b^2*R^3*x4^4 +
+	- b^3*R^2*x1^3*x4^2 + 3*b^2*R^3*x1^2*x4^2 - 3*b*R^4*x1*x4^2 + R^5*x4^2 - b^6*x1^4 + 4*b^5*R*x1^3 +
+	- 6*b^4*R^2*x1^2 + 4*b^3*R^3*x1 - b^2*R^4
 
