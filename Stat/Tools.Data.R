28b2651fb86cb888f0b4f1ffd0a6444a7809fa79
discoleo <leo.mada@syonic.eu>
2021-08-19 04:39:31 +0300

    Extract variables from expression;

--- Stat/Tools.Data.R
+++ Stat/Tools.Data.R
@@ -5,7 +5,7 @@
 ###
 ### Data Tools
 ###
-### draft v.0.1b
+### draft v.0.1c
 
 
 ### Tools to Process/Transform Data
@@ -47,6 +47,46 @@ encrypt = function(x, offset=0, isRandom=TRUE, DEBUG=TRUE) {
 }
 
 
+##########################
+##########################
+
+### Formulas / Expressions
+
+extract.vars = function(e) {
+	signs = numeric(0);
+	vars  = character(0);
+	isNum = logical(0); # TODO
+	while(length(e) > 1) {
+		if(e[[1]] == "+") {
+			signs = c(signs, 1);
+		} else if(e[[1]] == "-") {
+			signs = c(signs, -1);
+		}
+		len = length(e);
+		if(len == 3) {
+			# TODO: if(is.numeric(e[[3]])) {...}
+			# as.character needed for vector vs list
+			vars = c(vars, as.character(e[[3]]));
+			e = e[[2]];
+		} else if(len == 2) {
+			vars = c(vars, as.character(e[[2]])); e = NULL; break;
+		} else {
+			warning("Not yet supported!"); break;
+		}
+	}
+	if( ! is.null(e)) {
+		signs = c(signs, 1);
+		vars = c(vars, as.character(e));
+	}
+	return(list(vars=rev(vars), signs=rev(signs)));
+}
 
 
+### Test
+e = parse(text="x+y+z+2+e")
+extract.vars(e[[1]])
+
+
+e = parse(text="-x+y-z+2+e")
+extract.vars(e[[1]])
 
