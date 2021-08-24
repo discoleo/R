18d95e46a15cc48a337e1968b04b2946cfaedfb5
discoleo <leo.mada@syonic.eu>
2021-08-24 04:32:42 +0300

    native toDate.R;

--- Stat/Tools.DateTime.R
+++ Stat/Tools.DateTime.R
@@ -27,6 +27,29 @@ is.LeapYear = function(year) {
 	return(year %% 4 == 0 && year %% 400 != 0);
 }
 
+toDate.R = function(s) {
+	sf = function(s) {
+		bdt = as.integer(charToRaw(s)[-c(5,8)]);
+		bdt = bdt - 48L;
+		bdt = bdt * c(1000L, 100L, 10L, 1L, 10L, 1L, 10L, 1L);
+		year = sum(bdt[1:4]);
+		month = bdt[5] + bdt[6];
+		day   = bdt[7] + bdt[8] + days[month];
+		return(c(year, day));
+	}
+	dt0 = s[1]; year0 = extract.YMDate(dt0)$year;
+	# assuming same year (or only years of same type: Leap *OR* non-Leap)!
+	days = as.integer(start.month(year0));
+	r = lapply(s, sf);
+	r = do.call(rbind, r);
+	scale.day = 86400L;
+	scale.year = 31556926L;
+	r[,2] = r[,2] * scale.day;
+	r[,1] = (r[,1] - 1970L) * scale.year + r[,2];
+	r = as.POSIXct(r[,1], origin="1970-01-01");
+	return(r);
+}
+
 
 ##############
 ### Months ###
@@ -64,4 +87,8 @@ shift.vdays = function(start.dt) {
 ### Test
 
 shift.vdays(as.Date(c("2019-03-24", "2021-06-19")))
+
+
+toDate.R(c("2019-03-24", "2021-06-19"))
+
 
