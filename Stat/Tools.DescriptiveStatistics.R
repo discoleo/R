a4ce5cca4152ebac04f2d7f7f5f2ae3daee6029a
discoleo <leo.mada@syonic.eu>
2021-10-03 20:19:11 +0300

    Preparation for redesign;

--- Stat/Tools.DescriptiveStatistics.R
+++ Stat/Tools.DescriptiveStatistics.R
@@ -5,7 +5,7 @@
 ###
 ### Tools: Descriptive Statistics
 ###
-### draft v.0.1i
+### draft v.0.1j
 
 
 ###############
@@ -13,6 +13,9 @@
 ###############
 
 
+### draft v.0.1j:
+# - preparation for redesign:
+#   moved *Hack* to bottom of file;
 ### draft v.0.1g - v.0.1i:
 # - some fixes & better example;
 # - improved example; [v.0.1g-ex]
@@ -85,49 +88,13 @@ split.stat = function(x, len=10, sep="<br/>", reg = "\\([0-9 ,.]++\\)", BLOCK="_
 	}
 	return(x);
 }
-render_as_html = function (data, ...) 
-{
-	# various results may contain characters that need escaping;
-	data <- gt:::build_data(data = data, context = "html")
-	# splitting the lines after the escaping;
-	data = split.stat(data, ...);
-	data <- gt:::add_css_styles(data = data)
-    caption_component <- gt:::create_caption_component_h(data = data)
-    heading_component <- gt:::create_heading_component_h(data = data)
-    columns_component <- gt:::create_columns_component_h(data = data)
-    body_component <- gt:::create_body_component_h(data = data)
-    source_notes_component <- gt:::create_source_notes_component_h(data = data)
-    footnotes_component <- gt:::create_footnotes_component_h(data = data)
-    table_defs <- gt:::get_table_defs(data = data);
-    html_tbl = as.character(htmltools::tags$table(class = "gt_table", 
-        style = table_defs$table_style, caption_component, table_defs$table_colgroups, 
-        heading_component, columns_component, body_component, 
-        source_notes_component, footnotes_component));
-	invisible(html_tbl);
-}
-as.tags.gt_tbl = function (x, len=10, ..., view = interactive()) 
-{
-    table_id <- gt:::dt_options_get_value(x, option = "table_id")
-    if (is.na(table_id)) {
-        id <- gt:::random_id()
-    }
-    else {
-        id <- table_id
-    }
-    html_table = render_as_html(data = x, len=len); # override original function;
-    css <- gt:::compile_scss(data = x, id = id)
-    container_overflow_x <- gt:::dt_options_get_value(x, option = "container_overflow_x")
-    container_overflow_y <- gt:::dt_options_get_value(x, option = "container_overflow_y")
-    container_width <- gt:::dt_options_get_value(x, option = "container_width")
-    container_height <- gt:::dt_options_get_value(x, option = "container_height")
-    html_tbl <- htmltools::tags$div(id = id, htmltools::tags$style(htmltools::HTML(css)), 
-        style = htmltools::css(`overflow-x` = container_overflow_x, 
-            `overflow-y` = container_overflow_y, width = container_width, 
-            height = container_height), htmltools::HTML(html_table));
-	if( ! is.null(view))
-		print(html_tbl, browse = view, ...);
-    invisible(html_tbl);
-}
+
+### Note:
+# - functions render_as_html() & as.tags.gt_tbl():
+#   moved to the bottom of this file;
+
+
+######################
 
 ### Add Abbreviations:
 
@@ -270,3 +237,56 @@ mtcars %>%
 		c("Displacement (in^3)", "Gross horsepower", "Rear axle ratio",
 		"Weight (1000 lbs)"));
 }
+
+
+#########################
+#########################
+
+
+### [will be: old]
+# Code from the old Hack
+
+render_as_html = function (data, ...) 
+{
+	# various results may contain characters that need escaping;
+	data <- gt:::build_data(data = data, context = "html")
+	# splitting the lines after the escaping;
+	data = split.stat(data, ...);
+	data <- gt:::add_css_styles(data = data)
+    caption_component <- gt:::create_caption_component_h(data = data)
+    heading_component <- gt:::create_heading_component_h(data = data)
+    columns_component <- gt:::create_columns_component_h(data = data)
+    body_component <- gt:::create_body_component_h(data = data)
+    source_notes_component <- gt:::create_source_notes_component_h(data = data)
+    footnotes_component <- gt:::create_footnotes_component_h(data = data)
+    table_defs <- gt:::get_table_defs(data = data);
+    html_tbl = as.character(htmltools::tags$table(class = "gt_table", 
+        style = table_defs$table_style, caption_component, table_defs$table_colgroups, 
+        heading_component, columns_component, body_component, 
+        source_notes_component, footnotes_component));
+	invisible(html_tbl);
+}
+as.tags.gt_tbl = function (x, len=10, ..., view = interactive()) 
+{
+    table_id <- gt:::dt_options_get_value(x, option = "table_id")
+    if (is.na(table_id)) {
+        id <- gt:::random_id()
+    }
+    else {
+        id <- table_id
+    }
+    html_table = render_as_html(data = x, len=len); # override original function;
+    css <- gt:::compile_scss(data = x, id = id)
+    container_overflow_x <- gt:::dt_options_get_value(x, option = "container_overflow_x")
+    container_overflow_y <- gt:::dt_options_get_value(x, option = "container_overflow_y")
+    container_width <- gt:::dt_options_get_value(x, option = "container_width")
+    container_height <- gt:::dt_options_get_value(x, option = "container_height")
+    html_tbl <- htmltools::tags$div(id = id, htmltools::tags$style(htmltools::HTML(css)), 
+        style = htmltools::css(`overflow-x` = container_overflow_x, 
+            `overflow-y` = container_overflow_y, width = container_width, 
+            height = container_height), htmltools::HTML(html_table));
+	if( ! is.null(view))
+		print(html_tbl, browse = view, ...);
+    invisible(html_tbl);
+}
+
