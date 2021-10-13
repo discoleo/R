50d32ff98aa0931b9d3da61a7e4feeb6d67b3689
discoleo <leo.mada@syonic.eu>
2021-10-14 01:06:09 +0300

    [solved] Simple Order z=4;

--- Math/Poly.System.Asymmetric.S2.Conjugated.R
+++ Math/Poly.System.Asymmetric.S2.Conjugated.R
@@ -7,7 +7,7 @@
 ### Asymmetric S2:
 ### Conjugated Types
 ###
-### draft v.0.1b-fix
+### draft v.0.1c
 
 
 ### Asymmetric Polynomial Systems: 2 Variables
@@ -19,9 +19,10 @@
 ###############
 
 
-### draft v.0.1a - v.0.1b:
+### draft v.0.1a - v.0.1c:
 # - [solved] Simple Order z=2;
 # - [solved] Simple Order z=3;
+# - [solved] Simple Order z=4;
 
 
 ####################
@@ -188,5 +189,96 @@ test.S2Conj3(sol, R, b)
 
 ### Debug:
 R1 = R[1]; R2 = R[2]; b1 = b[1]; b2 = b[2];
+
+
+###################
+
+###################
+### Order z = 4 ###
+###################
+
+### System:
+(x^3 - 3*x*y^2)*(x^2 + y^2) + b3*(x^2 - y^2)*(x^2 + y^2) + b2*x*(x^2 + y^2) - R1*(x^2 + y^2) + b1*x # = 0
+(y^3 - 3*x^2*y)*(x^2 + y^2) - b3*2*x*y*(x^2 + y^2) - b2*y*(x^2 + y^2) + R2*(x^2 + y^2) + b1*y # = 0
+
+### Solution:
+
+### Trivial solution:
+x = y = 0
+# TODO: explore more;
+
+### Non-Trivial solutions:
+
+### let:
+z = x + 1i*y; zbar = x - 1i*y;
+# =>
+(x^3 - 3*x*y^2)*z*zbar + b3*(x^2 - y^2)*z*zbar + b2*x*z*zbar - R1*z*zbar + b1*x # = 0
+-(y^3 - 3*x^2*y)*z*zbar + 2*b3*x*y*z*zbar + b2*y*z*zbar - R2*z*zbar - b1*y # = 0
+### Sum(Eq 1 + 1i * Eq 2) =>
+z^4*zbar + b3*z^3*zbar + b2*z^2*zbar - (R1+R2*1i)*z*zbar + b1*zbar # = 0
+# zbar != 0 =>
+z^4 + b3*z^3 + b2*z^2 - (R1+R2*1i)*z + b1 # = 0
+
+### x = z - 1i*y # =>
+(y^3 - 3*(z - 1i*y)^2*y)*z*(z - 2i*y) + 2*b3*z*y*(2*y^2 + 3i*z*y - z^2) + b2*z*y*(2i*y - z) - R2*z*(2i*y - z) + b1*y # = 0
+(8i*z*y^4 - 16*z^2*y^3 - 12i*z^3*y^2 + 3*z^4*y) +
+	- 4*b3*z*y^3 - (6i*b3*z^2 + 2i*b2*z)*y^2 + (2*b3*z^3 + b2*z^2 + 2i*R2*z - b1)*y - R2*z^2 # = 0
+# =>
+8i*z*y^4 - (16*z^2 + 4*b3*z)*y^3 - (12i*z^3 + 6i*b3*z^2 + 2i*b2*z)*y^2 +
+	+ (3*z^4 + 2*b3*z^3 + b2*z^2 + 2i*R2*z - b1)*y - R2*z^2 # = 0
+
+
+### Solver:
+solve.S2Conj4 = function(R, b) {
+	zb1 = (R[1] + 1i*R[2]);
+	coeff = c(1, b[3], b[2], - zb1, b[1]);
+	z = roots(coeff);
+	# Simple roots:
+	# x = Re(z); y = Im(z);
+	# sol1 = cbind(x=x, y=y);
+	# All solutions:
+	R1 = R[1]; R2 = R[2];
+	b1 = b[1]; b2 = b[2]; b3 = b[3];
+	y = sapply(z, function(z) {
+		coeff = c(8i*z, - (16*z^2 + 4*b3*z), - (12i*z^3 + 6i*b3*z^2 + 2i*b2*z),
+			(3*z^4 + 2*b3*z^3 + b2*z^2 + 2i*R2*z - b1), - R2*z^2);
+		roots(coeff);
+	})
+	z = rep(z, each=4);
+	x = z - 1i*y;
+	sol = cbind(x=as.vector(x), y=as.vector(y));
+	# sol = rbind(sol, sol1);
+	return(sol);
+}
+test.S2Conj4 = function(sol, R, b) {
+	x = sol[,1]; y = sol[,2];
+	R1 = R[1]; R2 = R[2]; b1 = b[1]; b2 = b[2]; b3 = b[3];
+	x2y2 = x^2 + y^2;
+	err1 = (x^3 - 3*x*y^2 +  b3*(x^2 - y^2) + b2*x - R1)*x2y2 + b1*x # = 0
+	err2 = (- y^3 + 3*x^2*y + 2*b3*x*y + b2*y - R2)*x2y2 - b1*y # = 0
+	err = round0(rbind(err1, err2));
+	return(err);
+}
+
+### Examples:
+R = c(1,2)
+b = c(-2, 3, 3);
+#
+sol = solve.S2Conj4(R, b);
+x = sol[,1]; y = sol[,2];
+
+### Test:
+test.S2Conj4(sol, R, b)
+
+### Debug:
+R1 = R[1]; R2 = R[2]; b1 = b[1]; b2 = b[2]; b3 = b[3];
+z = x + 1i*y; zbar = x - 1i*y;
+
+### Derivation:
+p1 = toPoly.pm("4*y^3 + 6i*z*y^2 - 3*z^2*y")
+p2 = toPoly.pm("2i*y - z")
+
+# TODO: fix print.p!
+print.p(mult.pm(p1, p2), "y")
 
 
