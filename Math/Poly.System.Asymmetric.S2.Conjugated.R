763d4396620403135538911140cf7f658015bb52
discoleo <leo.mada@syonic.eu>
2021-10-13 23:04:04 +0300

    Asymmetric S2: Conjugated Types;

---
+++ Math/Poly.System.Asymmetric.S2.Conjugated.R
@@ -0,0 +1,114 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Polynomial Systems:
+### Asymmetric S2:
+### Conjugated Types
+###
+### draft v.0.1a
+
+
+### Asymmetric Polynomial Systems: 2 Variables
+### Conjugated Types
+
+
+###############
+### History ###
+###############
+
+
+### draft v.0.1a:
+# ...
+
+
+####################
+####################
+
+### helper functions
+
+library(polynom)
+library(pracma)
+
+# the functions are in the file:
+# Polynomials.Helper.R;
+# e.g. round0(), round0.p;
+source("Polynomials.Helper.R")
+
+### Other
+# [...]
+
+
+##########################
+
+##########################
+### Polynomial Systems ###
+##########################
+
+###############
+### Order 2 ###
+###############
+
+# based on:
+# [Michael Penn] Something is hiding in this problem...
+# https://www.youtube.com/watch?v=zBWgNf4Jl_A
+
+### System:
+x*(x^2 + y^2) - R1*(x^2 + y^2) + b*x # = 0
+y*(x^2 + y^2) - R2*(x^2 + y^2) - b*y # = 0
+
+### Solution:
+
+### Trivial solution:
+x = y = 0
+
+### Non-Trivial solutions:
+
+### let:
+z = x + 1i*y; zbar = x - 1i*y;
+# =>
+x*z*zbar - R1*z*zbar + b*x # = 0
+y*z*zbar - R2*z*zbar - b*y # = 0
+### Sum(Eq 1 + 1i * Eq 2) =>
+z^2*zbar - (R1+R2*1i)*z*zbar + b*zbar # = 0
+# zbar != 0 =>
+z^2 - (R1+R2*1i)*z + b # = 0
+
+### x = z - 1i*y # =>
+y*z*(z - 2i*y) - R2*z*(z - 2i*y) - b*y # = 0
+- 2i*z*y^2 + (z^2 + 2i*R2*z - b)*y - R2*z^2 # = 0
+
+### Solver:
+solve.S2Conj2 = function(R, b) {
+	zb1 = (R[1] + 1i*R[2]);
+	det = sqrt(zb1^2 - 4*b[1]);
+	z = c((zb1 + det) / 2, (zb1 - det)/2);
+	# Simple roots:
+	# x = Re(z); y = Im(z);
+	# sol1 = cbind(x=x, y=y);
+	# All solutions:
+	y = sapply(z, function(z) {
+		coeff = c(- 2i*z, (z^2 + 2i*R2*z - b), - R2*z^2);
+		roots(coeff);
+	})
+	z = rep(z, each=2);
+	x = z - 1i*y;
+	sol = cbind(x=as.vector(x), y=as.vector(y));
+	# sol = rbind(sol, sol1);
+	return(sol);
+}
+
+### Examples:
+R = c(1,2)
+b = 3;
+#
+sol = solve.S2Conj2(R, b);
+x = sol[,1]; y = sol[,2];
+
+### Test:
+R1 = R[1]; R2 = R[2];
+err1 = x*(x^2 + y^2) - R1*(x^2 + y^2) + b*x
+err2 = y*(x^2 + y^2) - R2*(x^2 + y^2) - b*y
+round0(rbind(err1, err2))
+
