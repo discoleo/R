b4ac95793aa5d4e815a2ba7f70ddee214644a87a
discoleo <leo.mada@syonic.eu>
2021-10-30 06:05:51 +0300

    D( sin(log(T(x)) );

--- Math/Polynomials.Helper.D.R
+++ Math/Polynomials.Helper.D.R
@@ -60,7 +60,7 @@ dp.div.pm = function(p1, pdiv, xn="x") {
 }
 # D( p1*sin(T(x)) + p2*cos(T(x)) )
 dp.trig.pm = function(plst, pT, xn="x", trig.order="sin") {
-	if(is.data.frame(plst)) {
+	if(inherits(plst, "pm")) {
 		p1 = plst;
 		p2 = NULL;
 	} else if(is.list(plst)) {
@@ -71,9 +71,8 @@ dp.trig.pm = function(plst, pT, xn="x", trig.order="sin") {
 			pT = plst$Trig;
 			trig.order = attr(plst, "trig.order");
 		} else {
-			p1 = plst[[1]];
-			p2 = if(length(plst) >= 2) plst[[2]] else NULL;
-			if(length(plst) > 2) warning("Only the first 2 polynomials of plst processed!")
+			p  = extract2.pm(plst, "p list");
+			p1 = p[[1]]; p2 = p[[2]];
 		}
 	}
 	# Order: p1 * sin, then p2 * cos;
@@ -97,6 +96,67 @@ dp.trig.pm = function(plst, pT, xn="x", trig.order="sin") {
 	attr(rlst, "trig.order") = trig.order;
 	attr(rlst, "D") = "Trig";
 	return(rlst);
+}
+
+# D( p1*sin(T0(x) + log(T1(x))) + p2*cos(T0(x) + log(T1(x))) )
+dp.trigLog.pm = function(plst, pT, xn="x", trig.order="sin") {
+	if(inherits(plst, "pm")) {
+		p1 = plst;
+		p2 = NULL;
+		pT = extract2.pm(pT, "pT list");
+		pT1 = pT[[1]]; pT0 = pT[[2]];
+	} else if(is.list(plst)) {
+		isD = ! is.null(attr(plst, "D")); # TODO: type;
+		if(isD) {
+			p1 = plst$C1; p2 = plst$C2;
+			if( ! missing(pT)) warning("Using pT included in the list!");
+			pT  = plst$TrigLog;
+			pT0 = plst$Trig0;
+			trig.order = attr(plst, "trig.order");
+		} else {
+			p  = extract2.pm(plst, "p list");
+			p1 = p[[1]]; p2 = p[[2]];
+			pT = extract2.pm(pT, "pT list");
+			pT1 = pT[[1]]; pT0 = pT[[2]];
+		}
+	}
+	# Order: p1 * sin, then p2 * cos;
+	trig.order.id = match(trig.order, c("sin", "cos"));
+	if(is.null(trig.order.id)) stop("Invalid order: p[[1]]*sin(T) + p[[2]]*cos(T);");
+	# Result
+	dT = function(p, type) {
+		r = dp.pm(pT1, xn=xn);
+		if( ! is.null(pT0)) r = sum.pm(r, mult.pm(pT1, dp.pm(pT0, xn=xn)));
+		if(type == 2) r$coeff = - r$coeff;
+		r = mult.pm(r, p);
+		return(r);
+	}
+	next.type = function(id) if(id == 1) 2 else 1;
+	C1 = mult.pm(pT1, dp.pm(p1, xn=xn)); # T1 * D(P1)
+	C2 = dT(p1, type=trig.order.id); # P1*(T1*D(T0) + D(T1))
+	if( ! is.null(p2)) {
+		C1 = sum.pm(C1, dT(p2, type = next.type(trig.order.id)));
+		C2 = sum.pm(C2, mult.pm(pT1, dp.pm(p2, xn=xn)));
+	}
+	rlst = list(C1=C1, C2=C2, TrigLog=pT1, Trig0=pT0, Div=pT1);
+	attr(rlst, "trig.order") = trig.order;
+	attr(rlst, "D") = "TrigLog";
+	return(rlst);
+}
+extract2.pm = function(p, s.warn=NULL) {
+	check.f = function(p, sn)
+		if(length(p) > 2) warning(paste0("Only the first 2 polynomials of ", sn, " processed!"));
+	if(inherits(p, "pm")) {
+		return(list(p, NULL));
+	}
+	if(is.list(pT)) {
+		if(length(pT) >= 2) {
+			p1 = p[[1]]; p2 = p[[2]];
+			if( ! is.null(s.warn)) check.f(p, s.warn);
+			return(list(p1, p2));
+		} else return(list(pT[[1]], NULL));
+	}
+	# TODO: error;
 }
 
 
