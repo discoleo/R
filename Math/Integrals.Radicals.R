e4d72000465030b0cf914679a5e1a6116c8fe4e4
discoleo <leo.mada@syonic.eu>
2021-11-14 21:53:41 +0200

    Simplification: Case n = 3;

--- Math/Integrals.Radicals.R
+++ Math/Integrals.Radicals.R
@@ -6,7 +6,7 @@
 ### Infinite Sums: Fractions
 ### Radicals
 ###
-### draft v.0.1b
+### draft v.0.1c
 
 
 ###########################
@@ -31,10 +31,14 @@ rh1 = line_integral(function(x)   sqrt(2) * sqrt(cos(3*x/2 + 0i)) * exp(7i*x/4),
 rh2 = line_integral(function(x) - sqrt(2) * sqrt(cos(3*x/2) + 0i) * exp(-7i*x/4), c(0, 2*pi/3))
 - (rh1 + rh2) * 1i/3
 # use cos() instead of sin():
-line_integral(function(x)   2* sqrt(2) * sqrt(cos(3*x/2)) * sin(7*x/4), c(0, pi/3)) +
-line_integral(function(x) - 2* sqrt(2) * sqrt(-cos(3*x/2)) * cos(7*x/4), c(pi/3, 4*pi/7)) +
-line_integral(function(x) - 2* sqrt(2) * sqrt(-cos(3*x/2)) * cos(7*x/4), c(4*pi/7, 2*pi/3))
-- (rh1 + rh2) * 1i
+r = line_integral(function(x)  sqrt(cos(3*x/2)) * sin(7*x/4), c(0, pi/3)) +
+line_integral(function(x) - sqrt(-cos(3*x/2)) * cos(7*x/4), c(pi/3, 2*pi/3));
+r * 2*sqrt(2) / 3
+#
+r = line_integral(function(x)  sqrt(cos(6*x)) * sin(7*x), c(0, pi/12)) +
+line_integral(function(x) - sqrt(-cos(6*x)) * cos(7*x), c(pi/12, pi/6));
+r * sqrt(2) * 8/3
+- (rh1 + rh2) * 1i/3
 
 
 #######################
