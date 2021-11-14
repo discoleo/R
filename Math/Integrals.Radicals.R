9e1ced18b2eaca9a00034afd74b7617fb78e0256
discoleo <leo.mada@syonic.eu>
2021-11-14 12:16:51 +0200

    [fix] cos vs sin;

--- Math/Integrals.Radicals.R
+++ Math/Integrals.Radicals.R
@@ -6,7 +6,7 @@
 ### Infinite Sums: Fractions
 ### Radicals
 ###
-### draft v.0.1a
+### draft v.0.1a-fix
 
 
 ######################
@@ -25,8 +25,8 @@ integrate(function(x) sqrt(x^3 + 1), lower=0, upper=1)
 rh1 = line_integral(function(x)   sqrt(2) * sqrt(cos(3*x/2 + 0i)) * exp(7i*x/4), c(0, 2*pi/3))
 rh2 = line_integral(function(x) - sqrt(2) * sqrt(cos(3*x/2) + 0i) * exp(-7i*x/4), c(0, 2*pi/3))
 - (rh1 + rh2) * 1i/3
-# TODO: debug!
-line_integral(function(x)   2i* sqrt(2) * sqrt(cos(3*x/2)) * sin(7*x/4), c(0, pi/3)) +
-line_integral(function(x)   2i*sqrt(2) * sqrt(-cos(3*x/2)) * sin(7*x/4), c(pi/3, 4*pi/7)) +
-line_integral(function(x) - 2i*sqrt(2) * sqrt(-cos(3*x/2)) * sin(7*x/4), c(4*pi/7, 2*pi/3))
-
+# use cos() instead of sin():
+line_integral(function(x)   2* sqrt(2) * sqrt(cos(3*x/2)) * sin(7*x/4), c(0, pi/3)) +
+line_integral(function(x) - 2* sqrt(2) * sqrt(-cos(3*x/2)) * cos(7*x/4), c(pi/3, 4*pi/7)) +
+line_integral(function(x) - 2* sqrt(2) * sqrt(-cos(3*x/2)) * cos(7*x/4), c(4*pi/7, 2*pi/3))
+- (rh1 + rh2) * 1i
