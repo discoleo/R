fabbd4800533dd773be90753d84590f6c983b255
discoleo <leo.mada@syonic.eu>
2021-12-13 02:08:57 +0200

    some experiments with log(Trig());

---
+++ Math/Integrals.Trig.Other.R
@@ -0,0 +1,64 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Integrals:
+### Trigonometric: Other
+###
+### draft v.0.1a
+
+
+
+#####################
+#####################
+
+### I( atan(1/x0^n) )
+# on [0, Inf]
+
+
+### Ref:
+# Michael Penn: An inverse tangent integral.
+# https://www.youtube.com/watch?v=oh0qCeV7o9k
+# https://www.youtube.com/watch?v=7wiybMkEfbc
+
+
+# [work in progress]
+
+n = 3
+integrate(function(x) atan(1/x)^n, 0, Inf, subdivisions=1024)
+integrate(function(x) n*x^(n-1) / tan(x), 0, pi/2, subdivisions=1024)
+
+
+n = 5
+integrate(function(x) atan(1/x)^n, 0, Inf, subdivisions=1024)
+integrate(function(x) n*x^(n-1) / tan(x), 0, pi/2, subdivisions=1024)
+
+
+#######
+
+n = 3
+integrate(function(x) - x*log(cos(x)), 0, pi/2, subdivisions=1024)
+integrate(function(x) x*log(sin(x)), 0, pi/2, subdivisions=1024)$value + (pi/2)^2*log(2)
+# unfortunately I[cos] = - I[sin] + ...;
+
+integrate(function(x) 4 * x*log(sin(2*x)), 0, pi/2, subdivisions=1024)
+integrate(function(x) x*log(sin(x)), 0, pi, subdivisions=1024)
+- pi^2*log(2) / 2
+
+integrate(function(x) x*log(sin(x)), 0, pi/2, subdivisions=1024)$value + 
+	integrate(function(x) (x+pi/2)*log(cos(x)), 0, pi/2, subdivisions=1024)$value
+- pi^2*log(2) / 2
+# but: I[sin] + I[cos] = constant!
+
+
+integrate(function(x) x*log(sin(x)/cos(x)), 0, pi/2, subdivisions=1024)
+integrate(function(x) (x-pi/2)*log(sin(x)/cos(x)), 0, pi/2, subdivisions=1024)
+
+# Note: I == 0
+integrate(function(x) log(sin(x)/cos(x)), 0, pi/2);
+
+integrate(function(x) log(cos(x)/sin(x)), 0, pi/4)
+integrate(function(x) - log(x)/(1+x^2), 0, tan(pi/4))
+# TODO: How?
+
