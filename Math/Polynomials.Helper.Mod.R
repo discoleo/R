5e189fdde67b1368f555f263813e121a2f28b084
discoleo <leo.mada@syonic.eu>
2022-01-23 12:15:53 +0200

    Optimized P2 solver;

--- Math/Polynomials.Helper.Mod.R
+++ Math/Polynomials.Helper.Mod.R
@@ -6,7 +6,7 @@
 ### Multi-Variable Polynomials
 ### Modular Arithmetic
 ###
-### draft v.0.1d
+### draft v.0.1e
 
 
 # - minimal Modular Arithmetic;
@@ -24,6 +24,7 @@
 ### Inverse (mod p)
 inv.mod = function(x, mod) {
 	if(mod == 1 || mod == 0) stop("Invalid mod!");
+	if(x == 1) return(1);
 	f = function(x) {
 		id = which((x * seq(mod - 1) - 1) %% mod == 0);
 		if(length(id) == 0) NA else id;
@@ -76,7 +77,8 @@ solve.ModP1Base = function(r, mod) {
 ### Solve P2:
 solve.ModP2 = function(b, mod) {
 	b0 = b[1]; b1 = b[2]; b2 = b[3];
-	x.f = function(x) (b2*x^2 + b1*x + b0) %% mod;
+	# x.f = function(x) (b2*x^2 + b1*x + b0) %% mod;
+	x.f = function(x) (x^2 + b0) %% mod;
 	if(b0 == 0) {
 		if(b2 == 1) {
 			x = c(0, (mod - b1));
@@ -86,10 +88,31 @@ solve.ModP2 = function(b, mod) {
 			b2inv = inv.mod(b2, mod=mod);
 			x = c(0, (- b1*b2inv) %% mod);
 		}
+	} else if(b2 == 0) {
+		x = - b0 * inv.mod(b1, mod=mod);
+		x = x %% mod;
 	} else {
-		err = sapply(seq(mod), x.f);
-		x   = which(err == 0);
-		if(length(x) == 0) return(list(hasSol = FALSE, Mod = mod));
+		# Optimized version:
+		if(b1 == 0) {
+			sh = 0;
+		} else {
+			if(b2 != 1) {
+				b2inv = inv.mod(b2, mod=mod);
+				b1 = (b1 * b2inv) %% mod;
+				b0 = (b0 * b2inv) %% mod;
+			}
+			inv2 = inv2.mod(mod, pow=1);
+			sh = (- b1 * inv2) %% mod;
+			b0 = (b0 - sh*sh) %% mod;
+		}
+		if((b0 + 1) %% mod == 0) {
+			x = (1 + sh) %% mod;
+		} else {
+			err = sapply(seq(mod), x.f);
+			x   = which(err == 0);
+			if(length(x) == 0) return(list(hasSol = FALSE, Mod = mod));
+			x = (x + sh) %% mod;
+		}
 	}
 	return(list(hasSol = TRUE, Sol = x, Mod = mod));
 }
