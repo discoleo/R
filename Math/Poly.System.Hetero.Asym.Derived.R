cd85db3e940bd10935764704b69dd94cd3b90e74
discoleo <leo.mada@syonic.eu>
2022-02-16 10:21:54 +0200

    P[9]: Variant;

--- Math/Poly.System.Hetero.Asym.Derived.R
+++ Math/Poly.System.Hetero.Asym.Derived.R
@@ -7,7 +7,7 @@
 ### Quasi-Asymmetric S2:
 ### Derived Polynomials
 ###
-### draft v.0.1b-ex5
+### draft v.0.1c
 
 
 ####################
@@ -29,28 +29,42 @@ source("Polynomials.Helper.R")
 # - see also Polynomials.Derived.P6.R
 #   for another/similar approach (for P6);
 
-### P9:
+###############
+### Order 3 ###
+###############
+
+# - Exact solutions for the following P[9] polynomials;
+
+### P[9]:
 # b, k = parameters;
 (x^3 + 4*b1*x + 8*b0)*(x^3 + b1*x - b0)^2 + 27*k^2*x^3 # = 0
 x^9 + 6*b1*x^7 + 6*b0*x^6 + 9*b1^2*x^5 + 6*b0*b1*x^4 + (27*k^2 + 4*b1^3 - 15*b0^2)*x^3 - 12*b0^2*b1*x + 8*b0^3
 
+### P[9] Variant:
+S^9 - 2*k2^2*S^7 + 6*b1*S^7 + 6*b0*S^6 + (k2^2 - 3*b1)^2*S^5 - 2*b0*(k2^2 - 3*b1)*S^4 +
+	- 4*k1*k2^3*S^3 - k2^2*b1^2*S^3 + 18*k1*k2*b1*S^3 + 4*b1^3*S^3 + 27*k1^2*S^3 - 15*b0^2*S^3 +
+	+ (4*k2^2*b0^2 - 12*b1*b0^2)*S + 8*b0^3
 
-### P9
-solve.P9.S2sp = function(k, b, debug=TRUE) {
+
+### Soler:
+solve.P9.S2sp = function(k, b, sort=TRUE, debug=TRUE) {
 	len = length(b);
 	bL  = if(len == 2) c(1, 0) else if(len == 3) 1
 		else if(len == 1) c(1,0,0) else stop("Only P3 => P9 supported!");
 	b = c(bL, rev(b));
-	bE1 = b; bE1[4] = bE1[4] + k;
+	if(length(k) < 2) k = c(k, 0);
+	bE1 = b;
+	bE1[4] = bE1[4] + k[1]; bE1[2] = bE1[2] - k[2];
 	x = roots(bE1);
 	#
-	bE2 = b; bE2[4] = bE2[4] - k;
+	bE2 = b;
+	bE2[4] = bE2[4] - k[1]; bE2[2] = bE2[2] + k[2];
 	y = roots(bE2);
 	if(debug) {print(x); print(y); }
 	#
 	s = expand.grid(x, y);
 	S = apply(s, 1, sum);
-	S = sort.sol(matrix(S, ncol=1), ncol=1)[,1];
+	if(sort) S = sort.sol(matrix(S, ncol=1), ncol=1)[,1];
 	sol = list(x=x, y=y, S=S);
 	return(sol);
 }
@@ -58,7 +72,14 @@ test.P9.S2sp = function(S, b, k) {
 	if(length(b) != 2) warning("Not yet implemented!")
 	b0 = b[1]; b1 = b[2];
 	if(is.list(S)) S = S$S;
-	err = (S^3 + 4*b1*S + 8*b0)*(S^3 + b1*S - b0)^2 + 27*k^2*S^3
+	if(length(k) == 1) {
+		err = (S^3 + 4*b1*S + 8*b0)*(S^3 + b1*S - b0)^2 + 27*k^2*S^3;
+	} else {
+		k1 = k[1]; k2 = k[2];
+		err = S^9 - 2*k2^2*S^7 + 6*b1*S^7 + 6*b0*S^6 + (k2^4 - 6*k2^2*b1 + 9*b1^2)*S^5 - 2*b0*(k2^2 - 3*b1)*S^4 +
+			- 4*k1*k2^3*S^3 - k2^2*b1^2*S^3 + 18*k1*k2*b1*S^3 + 4*b1^3*S^3 + 27*k1^2*S^3 - 15*b0^2*S^3 +
+			+ (4*k2^2*b0^2 - 12*b1*b0^2)*S + 8*b0^3
+	}
 	err = round0(err);
 	return(err);
 }
@@ -120,4 +141,39 @@ sol = solve.P9.S2sp(k, b); S = sol$S;
 test.P9.S2sp(S, b, k)
 
 round0(poly.calc(S))
+x = S
+1 + x - x^4 + x^5 + 3*x^6 - 2*x^7 + x^9
+
+
+#########
+### Ex 6:
+b = c(-2, 3)
+k = c(2, 1)
+sol = solve.P9.S2sp(k, b); S = sol$S;
+
+### Test
+test.P9.S2sp(S, b, k)
+
+round0(poly.calc(S))
+x = S
+-64 - 128*x + 247*x^3 - 32*x^4 + 64*x^5 - 12*x^6 + 16*x^7 + x^9
+
+
+### Derivation:
+
+p1 = toPoly.pm("(S^3 - 3*xy*S + b1*S + 2*b0) * (S^2 - xy + b1) + k2*(2*k1 - k2*(S^2 - 2*xy))*S")
+p2 = toPoly.pm("(S^2 - 4*xy)*(S^2 - xy + b1)^2 - (k2*(S^2 - 2*xy) - 2*k1)^2")
+pDiv = toPoly.pm("(k2*S^2 + 2*k1 + 2*k2*b1)^2")
+pRez = solve.pm(p1, p2, "xy");
+p = pRez$Rez;
+pRez = div.pm(p, pDiv, by="k1");
+p = pRez$Rez;
+p = sort.pm(p, "S")
+top.pm(p, "S")
+
+# P[13] = P[9] * P[4]
+
+
+#####################
+#####################
 
