2b6514138111bd87162c51146d78eb6124848860
discoleo <leo.mada@syonic.eu>
2022-02-18 04:17:11 +0200

    Diophantine Eqs;

---
+++ Math/Poly.Sys.Diophantine.R
@@ -0,0 +1,33 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Polynomial Systems:
+### Diophantine Equations
+###
+### draft v.0.1a
+
+
+####################
+####################
+
+### Helper Functions
+
+# the functions are in the file:
+# Polynomials.Helper.R;
+# e.g. round0(), round0.p;
+
+source("Polynomials.Helper.R")
+
+
+##############################
+
+
+### x^3 + y^3 + z^3 = 3*x*y*z
+
+### Examples
+
+x = 16; y = 9; z = -25;
+x^3 + y^3 + z^3 - 3*x*y*z
+
