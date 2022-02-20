42179e8aa4415ea184c1f74b41e818f4fabe318c
discoleo <leo.mada@syonic.eu>
2022-02-20 13:14:56 +0200

    Elliptic Curves: basic Transformations;

--- Math/Poly.Sys.Diophantine.R
+++ Math/Poly.Sys.Diophantine.R
@@ -6,7 +6,7 @@
 ### Polynomial Systems:
 ### Diophantine Equations
 ###
-### draft v.0.1a
+### draft v.0.1b
 
 
 ####################
@@ -23,6 +23,37 @@ source("Polynomials.Helper.R")
 
 ##############################
 
+
+### Elliptic Curves
+
+# History of elliptic curves rank records
+# https://web.math.pmf.unizg.hr/~duje/tors/rankhist.html
+
+
+### Examples
+
+x0 = c(-8, -1, 49/4)
+y0 = c(12, 9, 231/8)
+
+### Base:
+x = x0; y = y0;
+y^2 - x^3 + 82*x
+
+###
+x = x0^2; y = y0^2;
+y^2 - x^3 + 2*82*x^2 - 82^2*x
+
+###
+x = x0^2 - 164/3; y = y0^2;
+y^2 - x^3 + 1/12*164^2*x - 1/4*(164/3)^3
+
+###
+x = 9*x0^2 - 3*164; y = 27*y0^2;
+y^2 - x^3 + 27*82^2*x - 54*82^3
+
+
+
+####################
 
 ### x^3 + y^3 + z^3 = 3*x*y*z
 
