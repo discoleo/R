1ab46eca470b6d578e475f9cc2b400c5b12f9aef
discoleo <leo.mada@syonic.eu>
2022-02-23 16:03:22 +0200

    [refactor] XML to new file;

--- TextMining/Pubmed/Pubmed.Examples.R
+++ TextMining/Pubmed/Pubmed.Examples.R
@@ -12,6 +12,11 @@
 
 source("Pubmed.R")
 
+# Pretty formatting
+# - src file: in R/Stat/Tools.CRAN.R;
+# - formatting functions may get moved to a different file;
+source("Tools.CRAN.R")
+
 
 ##################
 
@@ -30,16 +35,23 @@ length(ids);
 print(ids)
 
 
+###########
 ### Test 2:
 doc = search.entrez("serotonin syndrome")
 r = parse.entrez(doc)
 print(r)
 
+# Retrieve Abtracts
 nStart = 0; nMax = 30;
 doc2 = search.entrez.fetch(r, nStart, max=nMax)
 writeLines(doc2, con="_Pubmed_Test.xml")
 
+# Titles
+titles = extractTitles(doc2)
+scroll.txt(titles)
 
+
+###########
 ### Test 3:
 doc = search.entrez(c("serotonin syndrome", "MAO"))
 r = parse.entrez(doc)
---
+++ TextMining/Pubmed/Pubmed.XML.R
@@ -0,0 +1,127 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Pubmed
+### XML Tools
+###
+### draft v.0.1a
+
+
+### XML Tools
+# - Extract data from Pubmed XML files;
+
+
+#################
+
+###########
+### XML ###
+###########
+
+### Parse
+
+# Parse the Search Results:
+parse.entrez = function(x) {
+	xml = read_xml(x);
+	as.xml.numeric = function(xpath) {
+		as.numeric(xml_text(xml_find_all(xml, xpath)));
+	}
+	count = as.xml.numeric("/eSearchResult/Count");
+	len   = as.xml.numeric("/eSearchResult/RetMax");
+	npos  = as.xml.numeric("/eSearchResult/RetStart");
+	key   = xml_text(xml_find_first(xml, "/eSearchResult/QueryKey"));
+	key2  = xml_text(xml_find_first(xml, "/eSearchResult/WebEnv"));
+	r = list(Count=count, Len=len, nStart=npos, Key=key, Key2=key2);
+	class(r) = c("Entrez", class(r));
+	return(r)
+}
+# Parse fetched IDs:
+parse.entrez.ids = function(x, nStart=0) {
+	if(inherits(nStart, "Entrez")) {
+		nStart = nStart$nStart;
+	}
+	xml = read_xml(x);
+	ns  = xml_find_all(xml, "/IdList/Id");
+	sID = xml_text(ns);
+	if(nStart > 0) {
+		attr(sID, "nStart") = nStart;
+	}
+	return(sID);
+}
+# Count
+count.entrez.ids = function(x) {
+	isXML = inherits(x, "xml_document");
+	xml = if(isXML) x else read_xml(x);
+	n   = xml_find_num(xml, "count(/IdList/Id)");
+	return(n);
+}
+count.entrez.abstract = function(x) {
+	isXML = inherits(x, "xml_document");
+	xml = if(isXML) x else read_xml(x);
+	n   = xml_find_num(xml, "count(/PubmedArticleSet/PubmedArticle)");
+	return(n);
+}
+
+# Extract Titles & Year:
+extractTitles = function(x) {
+	isXML = inherits(x, "xml_document");
+	xml = if(isXML) x else read_xml(x);
+	# assumes: only 1 Title:
+	pred  = "count(./MedlineCitation/Article/ArticleDate/Year)";
+	as.XP = function(path)
+		paste0("/PubmedArticleSet/PubmedArticle[", pred, " > 0]/MedlineCitation/", path);
+	PMID   = xml_find_all(xml, as.XP("PMID"));
+	sTitle = xml_find_all(xml, as.XP("Article/ArticleTitle"));
+	year   = xml_find_all(xml, as.XP("Article/ArticleDate/Year[1]"));
+	r = data.frame(PMID = xml_text(PMID), Year = as.numeric(xml_text(year)), Title = xml_text(sTitle));
+	#
+	as.XP = function(path)
+		paste0("/PubmedArticleSet/PubmedArticle[", pred, " = 0]/MedlineCitation/", path);
+	PMID   = xml_find_all(xml, as.XP("PMID"));
+	sTitle = xml_find_all(xml, as.XP("Article/ArticleTitle"));
+	r2 = data.frame(PMID = xml_text(PMID), Year = NA, Title = xml_text(sTitle));
+	r  = rbind(r, r2);
+	return(r);
+}
+
+# DISASTER:
+extractTitles.slowBeyondAnyHope = function(x) {
+	# Note: libxml2 does NOT implement XPATH 2 !!!
+	# [while XPATH 1.0 has massive shortcomings!]
+	isXML = inherits(x, "xml_document");
+	xml = if(isXML) x else read_xml(x);
+	nodes = xml_find_all(xml, "/PubmedArticleSet/PubmedArticle");
+	# only first 100:
+	r = lapply(nodes[1:100], function(r) {
+		PMID   = xml_find_first(r, "./MedlineCitation/PMID");
+		PMID   = xml_text(PMID)[1];
+		sTitle = xml_find_all(r, "./MedlineCitation/Article/ArticleTitle");
+		sTitle = paste(xml_text(sTitle), collapse="\n");
+		year   = xml_find_first(r, "./MedlineCitation/Article/ArticleDate/Year");
+		year   = as.numeric(xml_text(year)[1]);
+		#
+		data.frame(PMID=PMID, Title=sTitle, Year=year);
+	});
+	r = do.call(rbind, r);
+	return(r);
+}
+
+# Extract Authors
+extractAuthors = function(x, max=3, collapse=";") {
+	isXML = inherits(x, "xml_document");
+	xml = if(isXML) x else read_xml(x);
+	#
+	base = "/PubmedArticleSet/PubmedArticle";
+	pred = "count(./MedlineCitation/Article/AuthorList/Author)";
+	PMID = xml_find_all(xml, paste0(base, "[", pred, "> 0]/MedlineCitation/PMID"));
+	# PMID = xml_find_all(xml, paste0(base, "/MedlineCitation/PMID"));
+	PMID  = xml_text(PMID);
+	nodes = xml_find_all(xml, paste0(base, "[", pred, "> 0]"));
+	nA    = sapply(nodes, function(nd) {
+		xml_find_num(nd, "count(/MedlineCitation/Article/AuthorList/Author)")
+	});
+	r = data.frame(PMID=PMID, nA=nA);
+	return(r)
+}
+
