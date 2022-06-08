9292f0b481d60ce312d7731703e8229e8efc0a85
discoleo <leo.mada@syonic.eu>
2022-06-08 23:42:37 +0300

    S3P3: Extension 3;

--- Math/Poly.System.Asymmetric.S3.UDerived.R
+++ Math/Poly.System.Asymmetric.S3.UDerived.R
@@ -7,7 +7,7 @@
 ### Asymmetric Derived from Symmetric
 ###   Based on Roots of Unity
 ###
-### draft v.0.1c
+### draft v.0.1c-ext
 
 
 #######################
@@ -153,6 +153,7 @@ x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z
 ### Extension:
 # x^3 + y^3 + z^3 + b1*(x+y+z) = R1
 # x*y + x*z + y*z + b2*(x+y+z) = R2
+# x*y*z + b3*(x+y+z) = R3
 
 
 ### Derived:
@@ -160,16 +161,17 @@ x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z
 # y => x + m*k*y + m^2*k^2*z
 # z => x + m^2*k*y + m*k^2*z
 # where m^3 = 1, k^3 = K, K = given parameter;
+# Note: K is a simple "scaling" for (y, z);
 
 ### Derived System
 x^3 + K*y^3 + K^2*z^3 + 6*K*x*y*z + b1*x # = R1 / 3
 x^2 - K*y*z + b2*x # = R2 / 3
-x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z # = R3
+x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z + 3*b3*x # = R3
 
 ### Variant:
-x^3 + K*y^3 + K^2*z^3 + b[1]/3 * x # = (R[1]/9 + 2/3*R[3])
-x^2 - K*y*z + b2*x # = R2 / 3
-9*K*x*y*z + b[1]*x # = (R[1]/3 - R[3])
+x^3 + K*y^3 + K^2*z^3 + (b[1]/3 + 2*b[3]) * x # = (R[1]/9 + 2/3*R[3])
+x^2 - K*y*z + b[2]*x # = R[2] / 3
+9*K*x*y*z + (b[1] - 3*b[3])*x # = (R[1]/3 - R[3])
 
 
 ### Solver:
@@ -178,14 +180,17 @@ solve.S3.AsDer.P3 = function(R, b=0, K=1, all=FALSE, debug=TRUE) {
 	if(all(b == 0)) {
 		S = roots(c(1, 0, -3*R[2], 3*R[3] - R[1]));
 		E2 = rep(R[2], 3);
+		E3 = rep(R[3], 3);
 	} else {
-		S = roots(c(1, 3*b[2], -3*R[2] + b[1], 3*R[3] - R[1]));
+		if(length(b) < 3) b = c(b, 0);
+		S = roots(c(1, 3*b[2], -3*R[2] + b[1] -3*b[3], 3*R[3] - R[1]));
 		E2 = R[2] - b[2]*S;
+		E3 = R[3] - b[3]*S;
 	}
 	if(debug) print(S);
 	# Step 2:
 	x3 = sapply(seq(3), function(id) {
-		roots(c(1, -S[id], E2[id], -R[3]));
+		roots(c(1, -S[id], E2[id], -E3[id]));
 	})
 	S = rep(S, each=3); E2 = rep(E2, each=3);
 	x3 = as.vector(x3);
@@ -204,18 +209,59 @@ solve.S3.AsDer.P3 = function(R, b=0, K=1, all=FALSE, debug=TRUE) {
 	sol = solve(mm, mms);
 	return(sol);
 }
+test.S3.AsDer.P3 = function(sol, R=NULL, b, K=1) {
+	if(length(b) < 3) b = c(b, 0);
+	err1 = 3*(x^3 + K*y^3 + K^2*z^3 + 6*K*x*y*z + b[1]*x) # = R[1]
+	err2 = 3*(x^2 - K*y*z + b[2]*x) # = R[2]
+	err3 = x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z + 3*b[3]*x # = R[3]
+	#
+	err = rbind(err1, err2, err3);
+	if( ! is.null(R)) {
+		err = err - R;
+	}
+	err = round0(err);
+	return(err);
+}
 
 ### Examples:
 R = c(-3,2,3)
 b = c(1, -2)
 K = 1;
-sol = solve.S3.AsDer.P3(R, b=b)
+sol = solve.S3.AsDer.P3(R, b=b, K=K)
 x = sol[1,]; y = sol[2,]; z = sol[3,];
 
 ### Test
-3*(x^3 + K*y^3 + K^2*z^3 + 6*K*x*y*z + b[1]*x) # = R[1]
-3*(x^2 - K*y*z + b[2]*x) # = R[2]
-x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z # = R[3]
+test.S3.AsDer.P3(sol, R=R, b=b, K=K)
+
+### Classic Poly
+round0.p(poly.calc(x) * 27 * 27)
+
+
+#########
+### Ex 2:
+R = c(-3,2,-2)
+b = c(1, -2, -1)
+K = 1;
+sol = solve.S3.AsDer.P3(R, b=b, K=K)
+x = sol[1,]; y = sol[2,]; z = sol[3,];
+
+### Test
+test.S3.AsDer.P3(sol, R=R, b=b, K=K)
+
+### Classic Poly
+round0.p(poly.calc(x) * 27 * 27)
+
+
+#########
+### Ex 3:
+R = c(-3,2,1)
+b = c(1, -2, -1)
+K = 2;
+sol = solve.S3.AsDer.P3(R, b=b, K=K)
+x = sol[1,]; y = sol[2,]; z = sol[3,];
+
+### Test
+test.S3.AsDer.P3(sol, R=R, b=b, K=K)
 
 ### Classic Poly
 round0.p(poly.calc(x) * 27 * 27)
