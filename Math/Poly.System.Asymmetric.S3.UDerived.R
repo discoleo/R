b351433967fd49a6957c7e5f5df9a030c12fd2f0
discoleo <leo.mada@syonic.eu>
2022-06-08 21:32:19 +0300

    S3P2: Extension to radical order 3;

--- Math/Poly.System.Asymmetric.S3.UDerived.R
+++ Math/Poly.System.Asymmetric.S3.UDerived.R
@@ -7,7 +7,7 @@
 ### Asymmetric Derived from Symmetric
 ###   Based on Roots of Unity
 ###
-### draft v.0.1a-ext2
+### draft v.0.1b
 
 
 #######################
@@ -30,19 +30,21 @@ source("Polynomials.Helper.R")
 
 
 ### Derived:
-# x => x + y + z
-# y => x + m*y + m^2*z
-# z => x + m^2*y + m*z
-# where m^3 = 1
+# x => x + k*y + k^2*z
+# y => x + m*k*y + m^2*k^2*z
+# z => x + m^2*k*y + m*k^2*z
+# where m^3 = 1, k^3 = K, K = given parameter;
 
 ### Derived System
-x^2 + 2*y*z + b1*x # = R1 / 3
-x^2 - y*z + b2*x # = R2 / 3
-x^3 + y^3 + z^3 - 3*x*y*z # = R3
+x^2 + 2*K*y*z + b1*x # = R1 / 3
+x^2 - K*y*z + b2*x # = R2 / 3
+x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z # = R3
+# Note:
+# - P[2] is still easy to solve for x (from eqs. 1 & 2);
 
 
 ### Solver:
-solve.S3.AsDer.P2 = function(R, b=0, debug=TRUE) {
+solve.S3.AsDer.P2 = function(R, b=0, K=1, all=FALSE, debug=TRUE) {
 	# Step 1:
 	if(all(b == 0)) {
 		S = sqrt(R[1] + 2*R[2] + 0i);
@@ -66,8 +68,11 @@ solve.S3.AsDer.P2 = function(R, b=0, debug=TRUE) {
 	z3 = (yz.s - d3)/2;
 	# Step 3:
 	m = unity(3, all=FALSE);
-	mm = matrix(c(1,1,1, 1,m,m^2, 1,m^2,m), ncol=3, nrow=3, byrow=TRUE)
+	k = rootn(K, 3); k2 = k*k;
+	mm = matrix(c(1,k,k2, 1,m*k,m^2*k2, 1,m^2*k,m*k2), ncol=3, nrow=3, byrow=TRUE)
 	mms = rbind(x3, y3, z3);
+	# all roots: for computing Classic Poly in (y) or in (z);
+	if(all) mms = cbind(mms, mms[c(1,3,2), ]);
 	sol = solve(mm, mms);
 	return(sol);
 }
@@ -88,28 +93,49 @@ x^3 + y^3 + z^3 - 3*x*y*z # = R[3]
 round0.p(poly.calc(x) * 27 * 27)
 
 
+#########
+### Ex 2:
+R = c(-3,2,3)
+b = c(1, -2)
+K = 2
+sol = solve.S3.AsDer.P2(R, b=b, K=K)
+m = unity(3, all=FALSE)
+x = sol[1,]; y = sol[2,]; z = sol[3,];
+
+### Test
+3*(x^2 + 2*K*y*z + b[1]*x) # = R[1]
+3*(x^2 - K*y*z + b[2]*x) # = R[2]
+x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z # = R[3]
+
+### Classic Poly
+round0.p(poly.calc(x) * 27 * 27)
+
 ### Derivation
 
 ### Eq 1:
-p1 = toPoly.pm("(x + y + z)^2 + (x + m*y + m^2*z)^2 + (x + m^2*y + m*z)^2")
+p1 = toPoly.pm("(x + k*y*k + k^2*z)^2 + (x + m*k*y + m^2*k^2*z)^2 + (x + m^2*k*y + m*k^2*z)^2")
+p1 = replace.pm(p1, "K", "k", pow=3)
 p1 = replace.pm(p1, 1, "m", pow=3)
 p1 = replace.pm(p1, toPoly.pm("-m-1"), "m", pow=2)
 
-3*(x^2 + 2*y*z)
+3*(x^2 + 2*K*y*z)
 
 ### Eq 2:
 p2 = toPoly.pm(paste0(
-	"(x + y + z)*(x + m*y + m^2*z) + (x + y + z)*(x + m^2*y + m*z) +",
-	"(x + m*y + m^2*z)*(x + m^2*y + m*z)"))
+	"(x + k*y + k^2*z)*(x + m*k*y + m^2*k^2*z) +",
+	"(x + k*y + k^2*z)*(x + m^2*k*y + m*k^2*z) +",
+	"(x + m*k*y + m^2*k^2*z)*(x + m^2*k*y + m*k^2*z)"))
+p2 = replace.pm(p2, "K", "k", pow=3)
 p2 = replace.pm(p2, 1, "m", pow=3)
 p2 = replace.pm(p2, toPoly.pm("-m-1"), "m", pow=2)
 
-3*(x^2 - y*z)
+3*(x^2 - K*y*z)
 
 ### Eq 3:
-p3 = toPoly.pm("(x + y + z)*(x + m*y + m^2*z)*(x + m^2*y + m*z)")
+p3 = toPoly.pm("(x + k*y + k^2*z)*(x + m*k*y + m^2*k^2*z)*(x + m^2*k*y + m*k^2*z)")
+p3 = replace.pm(p3, "K", "k", pow=3)
 p3 = replace.pm(p3, 1, "m", pow=3)
 p3 = replace.pm(p3, toPoly.pm("-m-1"), "m", pow=2)
 
-x^3 + y^3 + z^3 - 3*x*y*z
+x^3 + K*y^3 + K^2*z^3 - 3*K*x*y*z
 
