24ed3551908e528277ce21955afedf3bf92da41b
discoleo <leo.mada@syonic.eu>
2022-06-08 20:41:55 +0300

    S3P2: Extension 2;

--- Math/Poly.System.Asymmetric.S3.UDerived.R
+++ Math/Poly.System.Asymmetric.S3.UDerived.R
@@ -7,7 +7,7 @@
 ### Asymmetric Derived from Symmetric
 ###   Based on Roots of Unity
 ###
-### draft v.0.1a-ext1
+### draft v.0.1a-ext2
 
 
 #######################
@@ -26,6 +26,7 @@ source("Polynomials.Helper.R")
 
 ### Extension:
 # x^2 + y^2 + z^2 + b1*(x+y+z) = R1
+# x*y + x*z + y*z + b2*(x+y+z) = R2
 
 
 ### Derived:
@@ -36,28 +37,30 @@ source("Polynomials.Helper.R")
 
 ### Derived System
 x^2 + 2*y*z + b1*x # = R1 / 3
-x^2 - y*z # = R2 / 3
+x^2 - y*z + b2*x # = R2 / 3
 x^3 + y^3 + z^3 - 3*x*y*z # = R3
 
 
 ### Solver:
 solve.S3.AsDer.P2 = function(R, b=0, debug=TRUE) {
 	# Step 1:
-	if(b == 0) {
+	if(all(b == 0)) {
 		S = sqrt(R[1] + 2*R[2] + 0i);
 		S = c(S, -S);
+		E2 = c(R[2], R[2]);
 	} else {
-		S = roots(c(1, b[1], -2*R[2] - R[1]))
+		S = roots(c(1, b[1] + 2*b[2], -2*R[2] - R[1]))
+		E2 = R[2] - b[2]*S;
 	}
 	if(debug) print(S);
 	# Step 2:
 	x3 = sapply(seq(2), function(id) {
-		roots(c(1, -S[id], R[2], -R[3]));
+		roots(c(1, -S[id], E2[id], -R[3]));
 	})
-	S = rep(S, each=3);
+	S = rep(S, each=3); E2 = rep(E2, each=3);
 	x3 = as.vector(x3);
 	yz.s = S - x3;
-	yz = R[2] - x3*yz.s;
+	yz = E2 - x3*yz.s;
 	d3 = sqrt(yz.s^2 - 4*yz);
 	y3 = (yz.s + d3)/2;
 	z3 = (yz.s - d3)/2;
@@ -71,15 +74,15 @@ solve.S3.AsDer.P2 = function(R, b=0, debug=TRUE) {
 
 ### Examples:
 R = c(-3,2,3)
-b = -2
+b = c(1, -2)
 sol = solve.S3.AsDer.P2(R, b=b)
 m = unity(3, all=FALSE)
 x = sol[1,]; y = sol[2,]; z = sol[3,];
 
 ### Test
 3*(x^2 + 2*y*z + b[1]*x) # = R[1]
-(x + y + z)*(x + m*y + m^2*z) + (x + y + z)*(x + m^2*y + m*z) + (x + m*y + m^2*z)*(x + m^2*y + m*z)
-(x + y + z)*(x + m*y + m^2*z)*(x + m^2*y + m*z)
+3*(x^2 - y*z + b[2]*x) # = R[2]
+x^3 + y^3 + z^3 - 3*x*y*z # = R[3]
 
 ### Classic Poly
 round0.p(poly.calc(x) * 27 * 27)
