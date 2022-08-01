6a8b62ce95b73d89168ed2cdde8a3458f972b67b
discoleo <leo.mada@syonic.eu>
2022-08-01 23:52:55 +0300

    [refactor] new file with Helper Functions;

--- Math/Poly.System.S4..C2.R
+++ Math/Poly.System.S4..C2.R
@@ -6,29 +6,19 @@
 ### Polynomial Systems
 ### S4: C2-Hetero-Symmetric
 ###
-### draft v.0.1h-eq4
+### draft v.0.1i
 
 
 ####################
 
 ### Helper Functions
 
-source("Polynomials.Helper.R")
+
+source("Poly.System.S4.C2.Helper.R")
 
 
-# Test
-test.S4C2 = function(sol, n, R=NULL) {
-	if(length(n) == 2) n = c(n, n[2]);
-	#
-	err1 = apply(sol^n[1], 1, sum);
-	err4 = apply(sol, 1, prod);
-	x1 = sol[,1]; x2 = sol[,2]; y1 = sol[,3]; y2 = sol[,4];
-	err2 = x1^n[2]*y1^n[3] + x2^n[2]*y2^n[3];
-	err3 = x1*x2*(y1 + y2) + y1*y2*(x1 + x2);
-	err = cbind(err1, err2, err3, err4);
-	if( ! is.null(R)) err = round0(err - rep(R, each=4));
-	return(err)
-}
+# - moved to file:
+#   Poly.System.S4..C2.Helper.R;
 
 
 ####################
