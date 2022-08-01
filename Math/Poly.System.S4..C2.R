a734e617d41fc9759155ac8a2f80bb5879bf402c
discoleo <leo.mada@syonic.eu>
2022-08-01 06:40:35 +0300

    [Ht P21] Variants

--- Math/Poly.System.S4..C2.R
+++ Math/Poly.System.S4..C2.R
@@ -6,7 +6,7 @@
 ### Polynomial Systems
 ### S4: C2-Hetero-Symmetric
 ###
-### draft v.0.1g-special2
+### draft v.0.1h
 
 
 ####################
@@ -382,4 +382,74 @@ p = sapply(unique(pR$Rez$s1), function(id) {
 })
 cat(p, sep = rep("\n", length(p)))
 
+
+######################
+
+######################
+### Eq 4: Variants ###
+######################
+
+#################
+### Variant Eq 4:
+x1*x2 + y1*y2 - R4 # = 0
+
+### Transformed System:
+s1 + s2 - R1 # = 0
+R2*s2*(s1^2 - 2*p1) - p2*(s1^4 - 4*p1*s1^2) - p1^2*s2^2 - R2^2 # = 0
+p2*s1 + p1*s2 - R3 # = 0
+p1 + p2 - R4 # = 0
+
+# TODO: solve;
+
+
+#################
+### Variant Eq 4:
+x1*y1 + x2*y2 - R4 # = 0
+
+### Transformed System:
+s1 + s2 - R1 # = 0
+R2*s2*(s1^2 - 2*p1) - p2*(s1^4 - 4*p1*s1^2) - p1^2*s2^2 - R2^2 # = 0
+p2*s1 + p1*s2 - R3 # = 0
+R4*s1*s2 - p2*s1^2 - p1*s2^2 + 4*p1*p2 - R4^2 # = 0
+
+### Derivation:
+A = R4;
+B = x1*y2 + x2*y1;
+A + B - s1*s2 # = 0;
+A * B - p2*(s1^2 - 2*p1) - p1*(s2^2 - 2*p2) # = 0
+
+# TODO: solve;
+
+
+
+###########################
+
+###########################
+### Eq 1: Higher Powers ###
+###########################
+
+
+### Eq 1: Power 2 (part)
+# - simplified version: only x^2;
+
+### System:
+# x1^2 + x2^2 + y1 + y2 = R1
+# x1^n2*y1^n3 + x2^n2*y2^n3 = R2
+# x1*x2*y1 + x1*x2*y2 + x1*y1*y2 + x2*y1*y2 = R3
+# x1*x2*y1*y2 = R4
+
+
+### Solution:
+
+# Translate system to new variables:
+s1 = x1 + x2; s2 = y1 + y2;
+p1 = x1 * x2; p2 = y1 * y2;
+
+### Transformed System:
+s1^2 - 2*p1 + s2 - R1 # = 0
+R2*s2*(s1^2 - 2*p1) - p2*(s1^4 - 4*p1*s1^2) - p1^2*s2^2 - R2^2 # = 0
+p2*s1 + p1*s2 - R3 # = 0
+p1*p2 - R4 # = 0
+
+# TODO: solve;
 
