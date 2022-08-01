595ded3a3df7620410d9c5b466d9d85aff9b9726
discoleo <leo.mada@syonic.eu>
2022-08-02 01:17:58 +0300

    [S4C2] Symmetric Variants

--- Math/Poly.System.S4.C2.Helper.R
+++ Math/Poly.System.S4.C2.Helper.R
@@ -7,7 +7,11 @@
 ### S4: C2-Hetero-Symmetric
 ### Helper Functions
 ###
-### draft v.0.1a
+### draft v.0.1b
+
+
+# this file:
+# source("Poly.System.S4.C2.Helper.R")
 
 
 ####################
@@ -17,12 +21,31 @@
 source("Polynomials.Helper.R")
 
 
-# Test
+### Test
 test.S4C2 = function(sol, n, R=NULL) {
 	if(length(n) == 2) n = c(n, n[2]);
 	#
 	err1 = apply(sol^n[1], 1, sum);
 	err4 = apply(sol, 1, prod);
+	x1 = sol[,1]; x2 = sol[,2]; y1 = sol[,3]; y2 = sol[,4];
+	err2 = x1^n[2]*y1^n[3] + x2^n[2]*y2^n[3];
+	err3 = x1*x2*(y1 + y2) + y1*y2*(x1 + x2);
+	err = cbind(err1, err2, err3, err4);
+	if( ! is.null(R)) err = round0(err - rep(R, each=4));
+	return(err)
+}
+
+### Test Variants
+test.S4C2.Var = function(sol, n, type="x1x2", R=NULL) {
+	if(length(n) == 2) n = c(n, n[2]);
+	#
+	type = pmatch(type, c("x1x2", "x1y2"));
+	if(is.na(type)) stop("Invalid type!");
+	FUN = if(type == 1) function(x) { x[1]*x[2] + x[3]*x[4]; }
+		else if(type == 2) function(x) { x[1]*x[4] + x[2]*x[3]; }
+	#
+	err1 = apply(sol^n[1], 1, sum);
+	err4 = apply(sol, 1, FUN);
 	x1 = sol[,1]; x2 = sol[,2]; y1 = sol[,3]; y2 = sol[,4];
 	err2 = x1^n[2]*y1^n[3] + x2^n[2]*y2^n[3];
 	err3 = x1*x2*(y1 + y2) + y1*y2*(x1 + x2);
--- Math/Poly.System.S4.C2.Symmetric.R
+++ Math/Poly.System.S4.C2.Symmetric.R
@@ -7,7 +7,7 @@
 ### S4: C2-Hetero-Symmetric
 ### with Additional Symmetry
 ###
-### draft v.0.1a
+### draft v.0.1b
 
 
 ####################
@@ -153,4 +153,100 @@ n = c(1, 2)
 sol = solve.S4C2(R, n=n)
 
 test.S4C2(sol, n=n)
+
+
+########################
+########################
+
+################
+### Variants ###
+################
+
+### Variants Eq 4:
+# - break the decomposition into (x1*y1) & (x2*y2);
+# - but the original symmetry is still preserved;
+
+
+### System:
+# x1^n1 + x2^n1 + y1^n1 + y2^n1 = R1
+# (x1*y1)^n + (x2*y2)^n = R2
+# x1*x2*y1 + x1*x2*y2 + x1*y1*y2 + x2*y1*y2 = R3
+# x1*y2 + x2*y1 = R4
+
+
+### Case: n = 1
+x1*y1 + x2*y2 - R2 # = 0
+x1*x2*(y1 + y2) + y1*y2*(x1 + x2) - R3 # = 0
+x1*y2 + x2*y1 - R4 # = 0
+
+### Solution:
+
+# let:
+s1 = x1 + x2; s2 = y1 + y2;
+p1 = x1 * x2; p2 = y1 * y2;
+S  = s1 + s2;
+E4 = p1 * p2;
+
+### Eq 2 + Eq 3 =>
+s1*s2 - R2 - R4 # = 0
+
+### Eq 2 * Eq 3 =>
+p2*(s1^2 - 2*p1) + p1*(s2^2 - 2*p2) - R2*R4 # = 0
+p2*s1^2 + p1*s2^2 - 4*p1*p2 - R2*R4 # = 0
+
+### Transformed System:
+# Eq 1: n1 = 1 vs n1 > 1;
+s1 + s2 - R1 # = 0
+s1*s2 - R2 - R4 # = 0
+p2*s1 + p1*s2 - R3 # = 0
+p2*s1^2 + p1*s2^2 - 4*p1*p2 - R2*R4 # = 0
+
+
+### Solver:
+
+solve.S4C2.SymVar1 = function(R, debug=TRUE, all=FALSE) {
+	R1 = R[1]; R2 = R[2]; R3 = R[3]; R4 = R[4];
+	# Step 1:
+	s1 = roots(c(1, -R1, R2 + R4));
+	s2 = R1 - s1;
+	if(debug) print(s1);
+	len = length(s1);
+	# Step 2:
+	b1 = s2^2*s1 - s2*s1^2 - 4*R3;
+	b0 = R3*s1^2 - s1*R4*R2;
+	p1 = sapply(seq(len), function(id) {
+		roots(c(4*s2[id], b1[id], b0[id]));
+	});
+	p1 = as.vector(p1);
+	s1 = rep(s1, each = 2);
+	s2 = rep(s2, each = 2);
+	p2 = (R3 - p1*s2) / s1;
+	# Step 3:
+	solve2 = function(id, s, p) {
+		roots(c(1, -s[id], p[id]));
+	}
+	len = length(s1);
+	x12 = lapply(seq(len), solve2, s1, p1);
+	x12 = matrix(unlist(x12), nrow=2);
+	y12 = lapply(seq(len), solve2, s2, p2);
+	y12 = matrix(unlist(y12), nrow=2);
+	sol = rbind(x12, y12);
+	sol = t(sol);
+	if(all) sol = rbind(sol, sol[ , c(2,1,4,3)]);
+	colnames(sol) = c("x1", "x2", "y1", "y2");
+	return(sol);
+}
+
+### Examples
+
+###
+R = c(-4,3,-1,-2)
+sol = solve.S4C2.SymVar1(R, all=T)
+
+test.S4C2.Var(sol, n = c(1,1), type="x1y2")
+
+
+###############
+### Derivation:
+4*s2*p1^2 + (s2^2*s1 - s2*s1^2 - 4*R3)*p1 + R3*s1^2 - s1*R4*R2 # = 0
 
