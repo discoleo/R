c339ce18762a7a52b913f4f45202d15087f2cc9d
discoleo <leo.mada@syonic.eu>
2022-08-26 18:04:17 +0300

    Elementary Polynomials

--- Math/Poly.System.S4.C2.Formulas.R
+++ Math/Poly.System.S4.C2.Formulas.R
@@ -7,7 +7,7 @@
 ### S4: Hetero-Symmetric
 ### Useful Formulas
 ###
-### draft v.0.1a
+### draft v.0.1b
 
 
 ### Formulas:
@@ -33,11 +33,66 @@ source("Poly.System.S4.C2.Helper.R")
 ####################
 
 ### Notation:
+# x1 = x[1]; x2 = x[2]; x3 = x[3]; x4 = x[4];
 s1 = x1 + x3; s2 = x2 + x4;
 p1 = x1 * x3; p2 = x2 * x4;
 sp = p1 + p2; ps = s1 * s2;
 S  = s1 + s2;
 E4 = p1 * p2;
+
+E2 = x1*x2 + x1*x3 + x1*x4 + x2*x3 + x2*x4 + x3*x4;
+E3 = x1*x2*x3 + x1*x2*x4 + x1*x3*x4 + x2*x3*x4;
+
+
+###################
+###################
+
+###################
+### Elementary  ###
+### Polynomials ###
+###################
+
+##########
+### E2 ###
+##########
+
+### Eq:
+sp + ps - E2 # = 0
+
+### Derivation:
+E2 = x1*x2 + x1*x3 + x1*x4 + x2*x3 + x2*x4 + x3*x4;
+p1 + p2 + x1*x2 + x1*x4 + x2*x3 + x3*x4 - E2 # = 0
+p1 + p2 + s1*s2 - E2 # = 0
+# =>
+sp + ps - E2 # = 0
+
+
+##########
+### E3 ###
+##########
+
+### Eq:
+E3^2 - sp*S*E3 + ps*sp^2 + E4*S^2 - 4*ps*E4 # = 0
+
+### Derivation:
+E3 = x1*x2*x3 + x1*x2*x4 + x1*x3*x4 + x2*x3*x4;
+x1*x3*(x2 + x4) + x2*x4*(x1 + x3) - E3 # = 0
+p1*s2 + p2*s1 - E3 # = 0
+
+A = p1*s2 + p2*s1;
+B = p1*s1 + p2*s2;
+
+### A + B =>
+A + B - (p1 + p2)*(s1 + s2) # = 0
+A + B - sp*S # = 0
+
+### A * B =>
+A * B - s1*s2*(p1^2 + p2^2) - p1*p2*(s1^2 + s2^2) # = 0
+A * B - ps*(sp^2 - 2*E4) - E4*(S^2 - 2*ps) # = 0
+
+### =>
+A*(sp*S - A) - ps*(sp^2 - 2*E4) - E4*(S^2 - 2*ps) # = 0
+A^2 - sp*S*A + ps*sp^2 + E4*S^2 - 4*ps*E4 # = 0
 
 
 #################
