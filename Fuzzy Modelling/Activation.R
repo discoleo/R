5202aa464a5cdbd2a925045f539c95fa8e11e39e
discoleo <leo.mada@syonic.eu>
2022-09-09 18:00:54 +0300

    More questions for happy students

--- Fuzzy Modelling/Activation.R
+++ Fuzzy Modelling/Activation.R
@@ -1,8 +1,9 @@
 
+### Exam Questions for putative students
 
 ### Activation Functions
 
-# 1.) Explain the characteristics of the following (activation) functions:
+# Q1.) Explain the characteristics of the following (activation) functions:
 
 lim = c(-3, 3)
 curve((tanh(x) + 1)/2, lim[1], lim[2], ylim=c(-0.125,1.125))
@@ -11,4 +12,13 @@ curve((tanh(x-1) + tanh(x+1))^2/4, add=TRUE, col="blue")
 curve((tanh(x-2) + tanh(x+1))^2/4, add=TRUE, col="#D09632")
 
 abline(h=1, col="grey")
+
+
+### Probabilities
+
+# Q2.) Explain the concept of probability density function (PDF) and the normalization of the PDF. Can any of these functions represent a PDF?
+
+# Q3.) Explain the concept of cumulative distribution function (CDF). Can any of these functions represent a CDF?
+
+# Q4.) Create the functions G[i] = 1 - f[i](x), where f[i] represents the last 3 functions defined above ( i = 1 to 3). Compare the G[i] functions to a set of Gaussian distributions. Explain if these functions behave similarly to a Gaussian distribution or if there are relevant differences?
 
