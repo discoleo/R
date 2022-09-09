d0b131ffeab613777070d1d55d787c4d5297f677
discoleo <leo.mada@syonic.eu>
2022-09-09 18:03:15 +0300

    [formatted] More questions for happy students

--- Fuzzy Modelling/Activation.R
+++ Fuzzy Modelling/Activation.R
@@ -16,9 +16,14 @@ abline(h=1, col="grey")
 
 ### Probabilities
 
-# Q2.) Explain the concept of probability density function (PDF) and the normalization of the PDF. Can any of these functions represent a PDF?
+# Q2.) Explain the concept of probability density function (PDF) and the normalization of the PDF.
+#  Can any of these functions represent a PDF?
 
-# Q3.) Explain the concept of cumulative distribution function (CDF). Can any of these functions represent a CDF?
+# Q3.) Explain the concept of cumulative distribution function (CDF).
+#  Can any of these functions represent a CDF?
 
-# Q4.) Create the functions G[i] = 1 - f[i](x), where f[i] represents the last 3 functions defined above ( i = 1 to 3). Compare the G[i] functions to a set of Gaussian distributions. Explain if these functions behave similarly to a Gaussian distribution or if there are relevant differences?
+# Q4.) Create the functions G[i] = 1 - f[i](x), where f[i] represents
+#  the last 3 functions defined above ( i = 1 to 3). Compare the G[i] functions
+#  to a set of Gaussian distributions. Explain if these functions behave similarly
+#  to a Gaussian distribution or if there are relevant differences?
 
