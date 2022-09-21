aefde54df4cf33025c0e9b5f34bd54c99947fb2a
discoleo <leo.mada@syonic.eu>
2022-09-22 00:13:16 +0300

    [fix] Formulas E2, E3, E4;

--- Math/Poly.System.S5.Ht.Formulas.R
+++ Math/Poly.System.S5.Ht.Formulas.R
@@ -7,7 +7,7 @@
 ### S5: Hetero-Symmetric
 ### Useful Formulas
 ###
-### draft v.0.1a
+### draft v.0.1a-fix
 
 
 ### Formulas:
@@ -23,7 +23,7 @@
 
 ### Basic:
 # A.) E11a
-# B.) Higher Poers
+# B.) Higher Powers
 
 
 ####################
@@ -43,9 +43,15 @@ x1 = x[1]; x2 = x[2]; x3 = x[3]; x4 = x[4]; x5 = x[5];
 S  = sum(x);
 E5 = prod(x);
 
-E2 = x1*(S - x1) + x2*(x3 + x4 + x5) + x3*(x4 + x5) + x4*x5;
-E3 = x1*x2*(x3 + x4 + x5) + x3*x4*(x1 + x2 + x5) + x1*x4*x5 + x3*x5*(x1 + x2);
-E4 = x1*x2*x3*x4 + x1*x2*x3*x5 + x1*x3*x4*x5 + x2*x3*x4*x5;
+s1 = x1 + x3; s2 = x2 + x4;
+p1 = x1 * x3; p2 = x2 * x4;
+ps = s1 * s2; sp = p1 + p2;
+# E2 = x1*(S - x1) + x2*(x3 + x4 + x5) + x3*(x4 + x5) + x4*x5;
+E2 = sp + ps + x5*(S - x5);
+# E3 = x1*x2*(x3 + x4 + x5) + x3*x4*(x1 + x2 + x5) + (x1 + x2)*(x3 + x4)*x5;
+E3 = p1*s2 + p2*s1 + x5*(sp + ps);
+# E4 = x1*x2*x3*x4 + x1*x2*x3*x5 + x1*x2*x4*x5 + x1*x3*x4*x5 + x2*x3*x4*x5;
+E4 = p1*p2 + x5*(p1*s2 + p2*s1);
 
 E11a = x1*x2 + x2*x3 + x3*x4 + x4*x5 + x5*x1;
 E11b = x1*x3 + x2*x4 + x3*x5 + x4*x1 + x5*x2;
@@ -60,4 +66,29 @@ E11a + E11b - E2 # = 0
 #######################
 
 # TODO
+
+
+#######################
+
+### Disaster:
+pE1 = toPoly.pm("x1 + x2 + x3 + x4 + x5 - 1"); # S = 1; !!!
+pE2a = toPoly.pm("x1*x2 + x2*x3 + x3*x4 + x4*x5 + x5*x1"); # E11a = 0; !!!
+pE2b = toPoly.pm("x1*x3 + x2*x4 + x3*x5 + x4*x1 + x5*x2"); # E11b = 0; !!!
+# E3 = 0; E4 = 0; !
+pE3 = toPoly.pm("x1*x2*(x3 + x4 + x5) + x3*x4*(x1 + x2 + x5) + (x1 + x2)*x4*x5 + x3*x5*(x1 + x2)");
+pE4 = toPoly.pm("x1*x2*x3*x4 + x1*x2*x3*x5 + x1*x2*x4*x5 + x1*x3*x4*x5 + x2*x3*x4*x5");
+
+pR = solve.lpm(pE1, pE2a, pE2b, pE3, pE4, xn=c("x5", "x4", "x3", "x2"))
+
+
+### Overflows!
+pE1 = toPoly.pm("s1 + s2 + x5 - 1"); # S = 1; !!!
+pE2 = toPoly.pm("p1 + p2 + s1*s2 + x5*(1 - x5)"); # E2 = 0; S = 1;
+pE3 = toPoly.pm("p1*s2 + p2*s1 + x5*(s1*s2 + p1 + p2)");
+pE4 = toPoly.pm("p1*p2 + x5*(p1*s2 + p2*s1)");
+pE5 = toPoly.pm("p1*p2*x5 - 1"); # E5 = 1;
+
+pR = solve.lpm(pE1, pE5, pE2, pE3, pE4, xn=c("x5", "s2", "s1", "p2"))
+max(pR[[4]]$Rez$p1)
+
 
