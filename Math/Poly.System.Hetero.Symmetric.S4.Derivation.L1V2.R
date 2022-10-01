767a3ff697255b128261d4c5e4ef80e76cd74d21
discoleo <leo.mada@syonic.eu>
2022-10-01 23:19:53 +0300

    [refactor] moved V2 code to the Derivation.V2 file;

--- Math/Poly.System.Hetero.Symmetric.S4.Derivation.L1V2.R
+++ Math/Poly.System.Hetero.Symmetric.S4.Derivation.L1V2.R
@@ -29,6 +29,15 @@
 source("Polynomials.Helper.R")
 source("Polynomials.Helper.EP.R")
 
+### Solver:
+xi.f = function(x, R, b, n=2) {
+	(R - x^n) / b[1];
+}
+xip.f = function(x, R, b, n=2, p=1) {
+	(R - x^n) / b[1] / x^p;
+}
+
+### En:
 E2af = function(x, n=2) {
 	if(is.null(dim(x))) {
 		x1 = x[1]; x2 = x[2]; x3 = x[3]; x4 = x[4];
@@ -118,6 +127,14 @@ x4^2 + b*x4*x1 # - R
 
 ### Solution:
 
+### Eq 1: Sum(Prod(x2*x3*x4*...)) =>
+E4*S + b*E4*S - R*E3 # = 0
+(b+1)*E4*S - R*E3 # = 0
+
+### Eq 3:
+E4^2 - b^4*E4^2 + 2*R^2*E4 + 2*R*E2*E4 - R*E3^2 - 2*R^2*E3*S +
+	+ 2*R^3*E2 + R^2*E2^2 - R^3*S^2 + R^4 # = 0
+
 
 ### Helper Eqs:
 
@@ -169,6 +186,11 @@ b^3*E13a - 4*E4 + 4*E3*S + 2*E2^2 - 4*E2*S^2 + 2*(b^2 + 2)*R*E2 +
 	+ S^4 - (b^2 + 2)*R*S^2 + 4*R^2 # = 0
 
 
+#######
+### Eq:
+S^3 - R*(b^4 - 2*b^3 + 4*b^2 - 4*b + 4)*S
+
+
 ### Debug:
 R = 5; b = -2;
 x1 = -5.0222909065 + 0.0000000000i;
@@ -207,6 +229,320 @@ toCoeff(pR, "x1")
 
 ### Case: b = 1i
 x1^8 - 3*R*x1^6 + 4*R^2*x1^4 - 2*R^3*x1^2 + R^4 # = 0
+
+
+### [old]
+# - moved from file:
+#   Poly.System.Hetero.Symmetric.S4.Derivation.R;
+
+### Eq 1: Sum(Prod(x2*x3*x4*...)) =>
+E4*S + b*E4*S - R*E3 # = 0
+(b+1)*E4*S - R*E3 # = 0
+
+### Eq 2:
+### Sum =>
+S^2 - 2*E2 + b*E11a - 4*R # = 0
+P2a = E11a;
+
+### x1^2 = R - b*x1*x2 => Prod =>
+E4^2 - R^4 + b*R^3*P2a - b^2*R^2*(P3a + 2*E4) + b^3*R*E4*P2a - b^4*E4^2 # = 0
+# Eq 2:
+(b^4 - 1)*(b^2 + 2)*E4^2 - 2*b^2*(b^2 + 4)*R^2*E4 +
+	- 2*b^2*(b^2 + 2)*R*E4*E2 + b^2*(b^2 + 2)*R*E4*S^2 +
+	+ 4*R^2*E2^2 + 2*(b^2 + 6)*R^3*E2 - 4*R^2*E2*S^2 +
+	+ (b^2+10)*R^4 - (b^2+6)*R^3*S^2 + R^2*S^4 # = 0
+
+### Eq 3:
+### b*x1*x2 = R - x1^2 => Prod =>
+# [see Simple version: E4 => E4^2]
+E4^2 - b^4*E4^2 + 2*R^2*E4 + 2*R*E2*E4 - R*E3^2 - 2*R^2*E3*S +
+	+ 2*R^3*E2 + R^2*E2^2 - R^3*S^2 + R^4 # = 0
+
+### Eq 4:
+### Alternative 1:
+### Sum(x3^2*x4^2*...) =>
+(E3^2 - 2*E4*E2) + b*E4*P2a - R*(P2a^2 - 2*P3a - 4*E4) # = 0
+b^2*E3^2 - b^2*E4*S^2 + 8*b^2*R*E4 - R*(2*E2 - S^2 + 4*R)^2 + 2*b^2*R*P3a # = 0
+b^2*E3^2 - b^2*E4*S^2 + 8*b^2*R*E4 - 4*R*E2^2 + 4*R*E2*S^2 - 16*R^2*E2 +
+	- R*(S^4 - 8*R*S^2 + 16*R^2) + 2*b^2*R*P3a # = 0
+(b^2 + 2)*E3^2 - (b^2 + 2)*E4*S^2 + 8*(b^2 + 1)*R*E4 +
+	- 4*R*E2^2 + 4*R*E2*S^2 - 8*R^2*E2 +
+	- R*S^4 + 4*R^2*S^2 - 8*R^3 # = 0
+
+
+#######
+### Eq:
+S^3 - R*(b^4 - 2*b^3 + 4*b^2 - 4*b + 4)*S
+
+
+### Auxiliary Eqs:
+### Special Case: S = 0
+E3 = 0; E2 = -2*R; E4 = R^2 / (b^2 + 1);
+### Case: S != 0
+E2 = -b*R*(b^2 - b + 2);
+E3 = - (b+1) * R * S;
+E4 = R*E3 / ((b+1)*S); # - R^2;
+
+
+### [Eq 4] Alternative 2:
+### Sum(x4*...) =>
+(x1^2*x4 + x2^2*x1 + x3^2*x2 + x4^2*x3) + b*E3 - R*S # = 0
+### Sum(x3*x4*...) =>
+P3b = x1^2*x3*x4 + x2^2*x1*x4 + x3^2*x1*x2 + x4^2*x2*x3;
+P3b + 4*b*E4 - R*P2a # = 0
+# [...]
+
+### [old approach]
+### Workout:
+### Half-Elementary Polynomials
+P3a = x1*x2^2*x3 + x1^2*x2*x4 + x2*x3^2*x4 + x1*x3*x4^2
+p2a = perm.poly(4, c(1,1))[c(1,3,4,6),]
+pR = data.frame(x1=0, coeff=1);
+for(nr in seq(nrow(p2a))) {
+	p1 = p2a[nr,];
+	p1 = rbind(p1, 0); p1$coeff[2] = -1;
+	p1$R = c(0, 1);
+	pR = mult.pm(pR, p1);
+}
+pR = sort.pm(pR, c(4,2,3,1), xn="R")
+pR
+
+### P3a:
+### x1^2 = R - b*x1*x2 => Sum(Prod(2 eqs)) =>
+b^2*P3a - 2*b*R*P2a + 4*R^2 - (P2a^2 - 2*P3a - 4*E4) # = 0
+(b^2 + 2)*P3a - 2*R*(2*E2 - S^2 + 4*R) - P2a^2 + 4*E4 + 4*R^2
+b^2*(b^2 + 2)*P3a - 2*b^2*R*(2*E2 - S^2 + 4*R) - (2*E2 - S^2 + 4*R)^2 + 4*b^2*E4 + 4*b^2*R^2
+b^2*(b^2 + 2)*P3a - 4*E2^2 + 4*E2*S^2 - 4*(b^2+4)*R*E2 +
+	+ 4*b^2*E4 - S^4 + 2*(b^2+4)*R*S^2 - 4*(b^2+4)*R^2 # = 0
+
+
+### Debug:
+solve.S4P2V2a.classic = function(R, b, debug=FALSE) {
+	x1 = roots(coeff.S4P2V2a(R, b)); # see below for coeff.S4P2V2();
+	x2 = xip.f(x1, R, b, p=1);
+	x3 = xip.f(x2, R, b, p=1);
+	x4 = xip.f(x3, R, b, p=1);
+	sol = cbind(x1, x2, x3, x4);
+	return(sol);
+}
+
+### Classic solver:
+coeff.S4P2V2a = function(R, b) {
+	# P[12]
+	coeff = c(b^2 + 1, 0,
+		- R*(b^2+1)*(b^4 + 2*b^2 + 6), 0,
+		15*R^2 + 22*b^2*R^2 + 13*b^4*R^2 + 6*b^6*R^2 + b^8*R^2, 0,
+		- 20*R^3 - 28*b^2*R^3 - 18*b^4*R^3 - 10*b^6*R^3 - 3*b^8*R^3, 0,
+		15*R^4 + 17*b^2*R^4 + 9*b^4*R^4 + 5*b^6*R^4 + 2*b^8*R^4, 0,
+		- 6*R^5 - 4*b^2*R^5 - b^4*R^5 - b^6*R^5, 0,
+		R^6);
+	return(coeff);
+}
+coeff.S4P2V2a_P16 = function(R, b) {
+	# P[16]: complete version;
+	coeff = c(b^4 - 1, 0,
+		- b^8*R - 2*b^6*R - 5*b^4*R + 4*b^2*R + 8*R, 0,
+		b^10*R^2 + 5*b^8*R^2 + 5*b^6*R^2 + 3*b^4*R^2 - 24*b^2*R^2 - 28*R^2, 0,
+		- 3*b^10*R^3 - 5*b^8*R^3 + 5*b^6*R^3 + 19*b^4*R^3 + 60*b^2*R^3 + 56*R^3, 0,
+		2*b^10*R^4 - 4*b^8*R^4 - 22*b^6*R^4 - 41*b^4*R^4 - 80*b^2*R^4 - 70*R^4, 0,
+		6*b^8*R^5 + 20*b^6*R^5 + 33*b^4*R^5 + 60*b^2*R^5 + 56*R^5, 0,
+		- 2*b^8*R^6 - 7*b^6*R^6 - 11*b^4*R^6 - 24*b^2*R^6 - 28*R^6, 0,
+		b^6*R^7 + b^4*R^7 + 4*b^2*R^7 + 8*R^7, 0, - R^8)
+	return(coeff);
+}
+#
+R = -1; b = 2;
+sol = solve.S4P2V2.classic(R, b);
+x1 = sol[,1]; x2 = sol[,2]; x3 = sol[,3]; x4 = sol[,4];
+E = do.call(rbind, lapply(seq(nrow(sol)), function(id) debug.E(sol[id,])));
+E$S = round0(E$S);
+S = E$S; E2 = E$E2; E3 = E$E3; E4 = E$E4;
+E2 = -b*R*(b^2 - b + 2);
+
+### [old]
+# TODO: clean;
+
+pEq3 = data.frame(
+	E4 = c(2, 2, 1, 1, 0, 0, 0, 0, 0, 0),
+	E3 = c(0, 0, 0, 0, 2, 1, 0, 0, 0, 0),
+	E2 = c(0, 0, 0, 1, 0, 0, 1, 2, 0, 0),
+	S  = c(0, 0, 0, 0, 0, 1, 0, 0, 2, 0),
+	b  = c(0, 4, 0, 0, 0, 0, 0, 0, 0, 0),
+	R  = c(0, 0, 2, 1, 1, 2, 3, 2, 3, 4),
+	coeff = c(1,-1, 2, 2, -1, -2, 2, 1, -1, 1)
+)
+pEq1   = data.frame(E3=1, R=1, coeff=1); # E4 vs E3
+pEq1fr = data.frame(S=c(1,1), b=c(1,0), coeff=c(1,1));
+pDiv = data.frame(
+	S = c(2, 0, 0, 0, 0, 0),
+	b = c(0, 4, 3, 2, 1, 0),
+	R = c(0, 1, 1, 1, 1, 1),
+	coeff = c(1,-1, 2, -4, 4, -4)
+)
+pEq3Coeff = data.frame(b=5:0, coeff=c(1,-1,3,-1,1,3));
+#
+pEq3r = replace.fr.pm(pEq3, pEq1, pEq1fr, "E4", pow=1)
+lP3 = div.pm(pEq3r, pDiv, by="S")
+# lP3$Rem
+lP3 = div.pm(lP3$Rem, data.frame(b=c(1,0), coeff=c(1,1)), by="b")
+lP3 = lP3$Rez; lP3$R = lP3$R - 2; lP3$coeff = - lP3$coeff;
+id = order( - lP3$E3, - lP3$E2, - lP3$b); lP3 = lP3[id,];
+print.p(lP3, "E3")
+lP3
+
+# Debug:
+eval.pm(pEq3, c(E4[10], E3[10], E2[10], S[10], b, R))
+
+(3 + b - b^2 + 3*b^3 - b^4 + b^5)*E3^2 +
+	- 2*(E2 - 3*R - 2*R*b^3 + R*b^4 - R*b^5)*S*E3 +
+	- R*(b^5 - b^4 + 2*b^3 + 4)*(E2^2 + 2*R*E2) +
+	+ (b^9 - 3*b^8 + 8*b^7 - 12*b^6 + 15*b^5 - 7*b^4 - 2*b^3 + 16*b^2 - 16*b + 12)*R^3
+
+
+### Eq 4:
+(b^2 + 2)*E3^2 - (b^2 + 2)*E4*S^2 + 8*(b^2 + 1)*R*E4 +
+	- 4*R*E2^2 + 4*R*E2*S^2 - 8*R^2*E2 +
+	- R*S^4 + 4*R^2*S^2 - 8*R^3 # = 0
+
+pEq4 = data.frame(
+	E4 = c(0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0),
+	E3 = c(2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+	E2 = c(0, 0, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0),
+	S  = c(0, 0, 2, 2, 0, 0, 0, 2, 0, 4, 2, 0),
+	b  = c(2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0),
+	R  = c(0, 0, 0, 0, 1, 1, 1, 1, 2, 1, 2, 3),
+	coeff = c(1, 2,-1,-2, 8, 8,-4, 4,-8,-1, 4,-8)
+)
+pEq4Coeff = data.frame(S=1, b=c(3,2,1,0), coeff=c(1,1,2,2));
+#
+pEq4r = replace.fr.pm(pEq4, pEq1, pEq1fr, "E4", pow=1)
+lP4 = div.pm(pEq4r, pDiv, by="S")
+lP4 = lP4$Rem;
+id = order( - lP4$E3, - lP4$E2, -lP4$b); lP4 = lP4[id,];
+print.p(lP4, "E3")
+# lP4
+
+pR = diff.pm(mult.pm(lP3, pEq4Coeff), mult.pm(lP4, pEq3Coeff))
+lP2r = div.pm(pR, pDiv, by="S")
+lP2r = lP2r$Rem;
+id = order( - lP2r$E3, - lP2r$E2, -lP2r$b); lP2r = lP2r[id,];
+print.p(lP2r, "E3")
+
+
+# Debug:
+eval.pm(pEq4, c(E4[10], E3[10], E2[10], S[10], b, R))
+
+(b+1)*(b^2 + 2)*E3^2*S +
+	+ R^2 * (- b^6 + 2*b^5 - 6*b^4 + 8*b^3 - 4*b^2 + 8*b)*E3 +
+	- 4*R*(b+1)*E2^2*S +
+	+ 4*R^2 * (b+1)*(b^4 - 2*b^3 + 4*b^2 - 4*b + 2)*E2*S +
+	- R^3 * (b+1)*(b^8 - 4*b^7 + 12*b^6 - 24*b^5 + 36*b^4 - 40*b^3 + 32*b^2 - 16*b + 8)*S
+	# (b^9 - 3*b^8 + 8*b^7 - 12*b^6 + 12*b^5 - 4*b^4 - 8*b^3 + 16*b^2 - 8*b + 8)*S
+
+((- 2*b^7 + 2*b^6 - 8*b^5 + 4*b^4 - 8*b^3 - 8*b^2 - 16)*E2 +
+	+ R*(2*b^12 - 3*b^11 + 11*b^10 - 5*b^9 + 7*b^8 + 31*b^7 - 39*b^6 + 84*b^5 - 46*b^4 +
+		+ 44*b^3 + 28*b^2 - 24*b + 48))*E3 +
+	- (b^8 - b^6 + 2*b^5 - 6*b^4 + 4*b^2 - 8*b - 4)*E2^2*S +
+	- (4*b^10 - 8*b^9 + 26*b^8 - 24*b^7 + 30*b^6 + 20*b^5 - 32*b^4 + 72*b^3 - 8*b^2 + 40)*R*E2*S +
+	+ (b^14 - 4*b^13 + 15*b^12 - 32*b^11 + 59*b^10 - 68*b^9 + 56*b^8 + 12*b^7 - 71*b^6 + 134*b^5 - 86*b^4 +
+		+ 48*b^3 + 44*b^2 - 24*b + 48)*R^2*S
+
+### Eq 2:
+(b^4 - 1)*(b^2 + 2)*E4^2 - 2*b^2*(b^2 + 4)*R^2*E4 +
+	- 2*b^2*(b^2 + 2)*R*E4*E2 + b^2*(b^2 + 2)*R*E4*S^2 +
+	+ 4*R^2*E2^2 + 2*(b^2 + 6)*R^3*E2 - 4*R^2*E2*S^2 +
+	+ (b^2+10)*R^4 - (b^2+6)*R^3*S^2 + R^2*S^4 # = 0
+
+pEq2 = data.frame(
+	E4 = c(2, 2, 2, 2, 1, 1,   1, 1, 1, 1,   0, 0, 0, 0,   0, 0, 0, 0, 0),
+	E2 = c(0, 0, 0, 0, 0, 0,   1, 1, 0 ,0,   2, 1, 1, 1,   0, 0, 0, 0, 0),
+	S  = c(0 ,0, 0, 0, 0, 0,   0, 0, 2, 2,   0, 0, 0, 2,   0, 0, 2, 2, 4),
+	b  = c(6, 4, 2, 0, 4, 2,   4, 2, 4, 2,   0, 2, 0, 0,   2, 0, 2, 0, 0),
+	R  = c(0, 0, 0, 0, 2, 2,   1, 1, 1, 1,   2, 3, 3, 2,   4, 4, 3, 3, 2),
+	coeff = c(1, 2,-1,-2,-2,-8,  -2,-4,1, 2,  4, 2,12,-4,  1,10,-1,-6, 1)
+)
+bDiv4 = data.frame(b=0:9, coeff=c(8,16,12,12,10,4,5,3,1,1))
+bDiv3 = data.frame(b=0:8, coeff=c(8,8,4,8,2,2,3,0,1))
+
+pEq2r = replace.fr.pm(pEq2, pEq1, pEq1fr, "E4", pow=1)
+lP2 = div.pm(pEq2r, pDiv, by="S")
+id = order( - lP2$Rem$E3, - lP2$Rem$E2, -lP2$Rem$b); lP2$Rem = lP2$Rem[id,];
+print.p(lP2$Rem, "E3")
+# lP$Rem
+
+# lP2r: E3 vs E2
+ncE3 = match("E3", names(lP2r))
+isDivE3 = (lP2r[, ncE3] > 0);
+E3fr = lP2r[isDivE3, -ncE3];
+E3p  = lP2r[ ! isDivE3, -ncE3];
+E3p$coeff = - E3p$coeff;
+#
+lP2 = replace.fr.pm(lP2$Rem, E3p, E3fr, "E3", pow=1)
+lP2 = div.pm(lP2, pDiv, by="S")
+lP2 = lP2$Rem;
+lP2$S = lP2$S - min(lP2$S);
+lP2$R = lP2$R - min(lP2$R);
+lP2 = div.pm(lP2, bDiv3, by="b");
+lP2 = lP2$Rez;
+id = order( - lP2$E2, -lP2$b); lP2 = lP2[id,];
+rownames(lP2) = seq(nrow(lP2));
+lP2
+
+# lP2 & lP4b are similar: redundancy or simplification method?
+pEq4r = replace.fr.pm(lP4, E3p, E3fr, "E3", pow=1)
+lP4b = div.pm(pEq4r, pDiv, by="S")
+lP4b = lP4b$Rem;
+lP4b$S = lP4b$S - min(lP4b$S);
+lP4b$R = lP4b$R - min(lP4b$R);
+id = order( - lP4b$E2, -lP4b$b); lP4b = lP4b[id,];
+lP4b = div.pm(lP4b, bDiv3, by="b");
+lP4b = lP4b$Rez;
+id = order( - lP4b$E2, -lP4b$b); lP4b = lP4b[id,];
+rownames(lP4b) = seq(nrow(lP4b));
+lP4b
+# print.p(lP4, "E2")
+
+### simplification of E3:
+# -b*(b^2 - b + 2)
+pRepl = data.frame(b=c(3,2,1), coeff=c(-1,1,-2));
+pDiv = add.pm(mult.pm(E3fr[E3fr$E2 == 1, c("b", "coeff")], pRepl),
+	E3fr[E3fr$E2 == 0, c("b", "coeff")])
+print.p(pDiv, "b")
+
+pE3p = add.lpm(
+	list(mult.pm(E3p[E3p$E2 == 2, c("b", "coeff")], pow.pm(pRepl, 2)),
+	mult.pm(E3p[E3p$E2 == 1, c("b", "coeff")], pRepl),
+	E3p[E3p$E2 == 0, c("b", "coeff")]))
+pE3p$coeff = - pE3p$coeff; # use: - E3 !!!
+print.p(pE3p, "b")
+
+gcd.pm(pE3p, pDiv, by="b");
+# (b+1) !!!
+
+# Debug:
+eval.pm(pEq2, c(E4[10],E2[10],S[10],b,R))
+eval.pm(E3p, c(R,E2[10],S[10],b))
+
+gcd.pm(lP2[lP2$E2 == 4, c("b", "coeff")], lP4b[lP4b$E2 == 4, c("b", "coeff")], by="b", div.sc=5974.576)
+div.pm(lP2[lP2$E2 == 4, c("b", "coeff")], bDiv4, "b")
+div.pm(lP4b[lP4b$E2 == 4, c("b", "coeff")], bDiv4, "b")
+
+diff.pm(mult.pm(lP2, lP4b[lP4b$E2 == 4, c("b", "coeff")]), mult.pm(lP4b, lP2[lP2$E2 == 4, c("b", "coeff")]))
+
+### [old]
+getE3.old = function(S, E2, R, b) {
+	# only for Case: S != 0;
+	# E3 = 0 for S == 0; [this formula actually works as well]
+	pDiv = ((2*b^7 - 2*b^6 + 8*b^5 - 4*b^4 + 8*b^3 + 8*b^2 + 16)*E2 +
+		- R*(2*b^12 - 3*b^11 + 11*b^10 - 5*b^9 + 7*b^8 + 31*b^7 - 39*b^6 + 84*b^5 - 46*b^4 +
+			+ 44*b^3 + 28*b^2 - 24*b + 48));
+	#
+	pE3 = - (b^8 - b^6 + 2*b^5 - 6*b^4 + 4*b^2 - 8*b - 4)*E2^2 +
+		- (4*b^10 - 8*b^9 + 26*b^8 - 24*b^7 + 30*b^6 + 20*b^5 - 32*b^4 + 72*b^3 - 8*b^2 + 40)*R*E2 +
+		+ (b^14 - 4*b^13 + 15*b^12 - 32*b^11 + 59*b^10 - 68*b^9 + 56*b^8 + 12*b^7 - 71*b^6 + 134*b^5 - 86*b^4 +
+			+ 48*b^3 + 44*b^2 - 24*b + 48)*R^2;
+	return(pE3 * S / pDiv);
+}
 
 
 ####################
