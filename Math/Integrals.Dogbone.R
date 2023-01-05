6c8863de25df4579b6d05a78edcf95fa351efeef
discoleo <leo.mada@syonic.eu>
2023-01-05 04:02:49 +0200

    Integrals: Dogbone Contour;

---
+++ Math/Integrals.Dogbone.R
@@ -0,0 +1,55 @@
+
+
+### Dogbone Integration
+
+# 1. Complex Analysis: Dogbone Contour Example
+# https://www.youtube.com/watch?v=UDIKojCQ94U
+integrate(function(x) x^(3/4)*(3 - x)^(1/4) / (5 - x), lower=0, upper=3)
+
+### TODO: explore subsequent examples:
+
+# 2. Complex Analysis: Dogbone Contour Example #2
+# https://www.youtube.com/watch?v=q1BxM1MWAqA
+integrate(function(x) (1 - x)^(-2/3)*(1 + x)^(-1/3) / (4 + x^2), lower=-1, upper=1)
+
+# 3. Complex Analysis: Dogbone Contour Example #3
+# https://www.youtube.com/watch?v=-HWcFun7e4k
+integrate(function(x) (1 - x)^(1/2)*(1 + x)^(1/2) / (1 + x^2), lower=-1, upper=1)
+
+# 4. ...
+
+
+##############
+
+### Example 1:
+
+integrate(function(x) x^(3/4)*(3 - x)^(1/4) / (5 - x), lower=0, upper=3)
+pi*(17/4 - (5^3*2)^(1/4))*sqrt(2)
+
+
+### Generalizations:
+
+integrate(function(x) x^(3/4)*(3 - x)^(1/4) / (7 - x), lower=0, upper=3)
+pi*(7 - 3/4 - (7^3*4)^(1/4))*sqrt(2)
+
+### Gen 1:
+k = 11
+integrate(function(x) x^(3/4)*(3 - x)^(1/4) / (k - x), lower=0, upper=3)
+pi*(k - 3/4 - (k^3*(k - 3))^(1/4))*sqrt(2)
+
+### Gen 2:
+k = 11; b = 4;
+integrate(function(x) x^(3/4)*(b - x)^(1/4) / (k - x), lower=0, upper=b)
+pi*(k - b/4 - (k^3*(k - b))^(1/4))*sqrt(2)
+
+### Gen 3: Full
+# Note: variation/partitioning of powers NOT yet done;
+# - only the case: ^((p-1)/p) vs ^(1/p);
+k = 11; b = 4;
+p = 5;
+integrate(function(x) x^((p-1)/p)*(b - x)^(1/p) / (k - x), lower=0, upper=b)
+2i * pi*(k - b/p - (k^(p-1)*(k - b))^(1/p)) * exp(1i*pi/p) / (exp(2i*pi/p) - 1)
+
+# Arg - Inf from above: (p+1)/p * pi;
+# Arg - Inf from below: -(p-1)/p * pi;
+
