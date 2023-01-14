122df25f19e6858d8c3f4a5d28d94caa51c66d92
discoleo <leo.mada@syonic.eu>
2023-01-14 03:11:03 +0200

    [refactor] moved Definite Integrals to new file;

---
+++ Math/Integrals.Fractions.Unity.Definite.R
@@ -0,0 +1,186 @@
+########################
+###
+### Leonard Mada
+### [the one and only]
+###
+### Exact Integration
+### Polynomial Fractions: Unity
+### Definite Integrals
+###
+### draft v.0.1a
+
+
+
+### Definite Integrals
+
+# I( 1 / (x^n + 1) )
+# I( x^p / (x^n + 1) )
+
+### Intervals:
+# [0, Inf], [0, 1];
+
+# - contains various Shortcut Formulas;
+# - moved specific code to this file from file:
+#   Integrals.Fractions.Unity.R;
+
+
+######################
+
+### Helper Functions
+
+### I on [0, 1]
+intUnityI01 = function(n) {
+	if(n %% 2 == 0) {
+		return(intUnityI01Even(n));
+	}
+	n2 = n %/% 2;
+	sum(cos(2*seq(n2)*pi/n)*log(cos(seq(n2)*pi/n))) * 2 / n +
+	+ sum(seq(n2)*sin(2*seq(n2)*pi/n)) * 2*pi / n^2;
+}
+intUnityI01Even = function(n) {
+	if(n %% 2 == 1) return(intUnityI01(n));
+	isNotM4 = n %% 4 != 0;
+	id = seq(1, n, by=2);
+	if(isNotM4) id = id[ - (n+2)/4];
+	cs = cos(id*pi/n); sn = sin(id*pi/n);
+	# Base-Formula:
+	# fr = sum(2*cs*x + 2)/(x^2 + 2*cs*x + 1)) / n;
+	# r = sum(cs*log(x^2 + 2*cs*x + 1)) + sum(2*atan((x - cs)/sn));
+	r = sum(cs*log(cs + 1)) +
+		# + sum(2*sn*atan((1 - cs)/sn)) - sum(2*sn*atan((0 - cs)/sn));
+		+ sum(2*sn*atan((1 - cs)/sn)) - 2*pi*sum(id*sn)/n + pi*sum(sn);
+	if(isNotM4) r = r + pi/2;
+	return(r/n);
+}
+
+######################
+######################
+
+### Infinite Integrals
+# - shortcut formulas are available;
+
+n = 5
+integrate(function(x) 1/(x^n + 1), lower=0, upper=Inf)
+integrate(function(x) x^3/(x^n + 1), lower=0, upper=Inf)
+pi / sin(pi/n) / n
+#
+integrate(function(x) x/(x^n + 1), lower=0, upper=Inf)
+integrate(function(x) x^2/(x^n + 1), lower=0, upper=Inf)
+pi / sin(2*pi/n) / n
+
+###
+n = 7
+integrate(function(x) 1/(x^n + 1), lower=0, upper=Inf)
+integrate(function(x) x^5/(x^n + 1), lower=0, upper=Inf)
+pi / sin(pi/n) / n
+#
+integrate(function(x) x/(x^n + 1), lower=0, upper=Inf)
+integrate(function(x) x^4/(x^n + 1), lower=0, upper=Inf)
+pi / sin(2*pi/n) / n
+#
+integrate(function(x) x^2/(x^n + 1), lower=0, upper=Inf)
+integrate(function(x) x^3/(x^n + 1), lower=0, upper=Inf)
+pi / sin(3*pi/n) / n
+
+
+############################
+############################
+
+### Special Finite Integrals
+# - shortcut formulas are available;
+
+###
+n = 9
+integrate(function(x) 1/(x^n + 1), 0, 1)
+intUnityI01(n)
+
+###
+n = 15
+integrate(function(x) 1/(x^n + 1), 0, 1)
+intUnityI01(n)
+
+###
+n = 16
+integrate(function(x) 1/(x^n + 1), 0, 1)
+intUnityI01(n)
+
+
+### Specific Cases
+# Note: generalized formulas available;
+
+###
+n = 3
+integrate(function(x) 1/(x^3 + 1), 0, 1)
+2/n * cos(2*pi/3)*log(cos(pi/3)) +
+	+ 2*pi*sin(2*pi/3)/n^2
+
+# Fraction Decomposition:
+1/n/(x+1) + 2/n * sum( (cos(2*pi/3)*x + 1) / (x^2 + 2*cos(2*pi/3)*x + 1) )
+1/n/(x+1) + 1/n * cos(2*pi/3)*(2*x + 2*cos(2*pi/3)) / (x^2 + 2*cos(2*pi/3)*x + 1) +
+	+ 2/n * sin(2*pi/3)^2 / (x^2 + 2*cos(2*pi/3)*x + 1)
+# I on [0, 1] =>
+2/n * cos(2*pi/3)*log(cos(pi/3)) +
+	+ 2/n * sin(2*pi/3)*atan( (1 + cos(2*pi/3))/sin(2*pi/3) ) +
+	# - 2/n * sin(2*pi/3)*atan( (0 + cos(2*pi/3))/sin(2*pi/3) );
+	- 2/n * (pi/2 - 2*pi/3) * sin(2*pi/3);
+
+### n = 5
+# TODO: simplify
+n = 5
+integrate(function(x) 1/(x^n + 1), 0, 1)
+2*cos(2*pi/n)*log(cos(pi/n)) / n +
+	+ 2*cos(4*pi/n)*log(cos(2*pi/n)) / n +
+	+ 2*pi*(sin(2*pi/n) + 2*sin(4*pi/n)) / n^2;
+# useful ???
+- 4*log(2)*cos(2*pi/n)/n +
+	+ 4/n * (4*cos(pi/n)^4 - 5*cos(pi/n)^2 + 1)*log(cos(2*pi/n)) +
+	+ 2*pi*sin(2*pi/n)*(2*cos(pi/n)^2 + 3*cos(2*pi/n)) / n^2;
+
+
+###
+n = 7
+integrate(function(x) 1/(x^n + 1), 0, 1)
+2*cos(2*pi/n)*log(cos(pi/n)) / n +
+	+ 2*cos(4*pi/n)*log(cos(2*pi/n)) / n +
+	+ 2*cos(6*pi/n)*log(cos(3*pi/n)) / n +
+	+ 2*pi*(sin(2*pi/n) + 2*sin(4*pi/n) + 3*sin(6*pi/n)) / n^2;
+
+#
+2/n * cos(2*pi/n)*log(cos(pi/n)) +
+	+ 2/n * cos(4*pi/n)*log(cos(2*pi/n)) +
+	+ 2/n * cos(6*pi/n)*log(cos(3*pi/n)) +
+	+ 2/n*sin(2*pi/n)*atan( (1 + cos(2*pi/n))/sin(2*pi/n) ) +
+	+ 2/n*sin(4*pi/n)*atan( (1 + cos(4*pi/n))/sin(4*pi/n) ) +
+	+ 2/n*sin(6*pi/n)*atan( (1 + cos(6*pi/n))/sin(6*pi/n) ) +
+	- 2/n * (pi/2 - 2*pi/n) * sin(2*pi/n) +
+	- 2/n * (pi/2 - 4*pi/n) * sin(4*pi/n) +
+	- 2/n * (pi/2 - 6*pi/n) * sin(6*pi/n)
+
+
+
+
+###
+n = seq(3, 21, by=2)
+x = sapply(n, intUnityI01)
+# asymptotic to y = 1;
+plot(n, x)
+curve( (x + 1/3)/(x+1), add=T, col="orange")
+
+###
+n = seq(3, 101, by=2)
+x = sapply(n, intUnityI01)
+# asymptotic to y = 1;
+plot(n, x)
+curve( (x + 1/3)/(x+1), add=T, col="orange")
+
+
+### Special Fractional Powers
+n = 7
+integrate(function(x) 1/(x^(1 - 1/(n+1)) + 1), 0, 1)
+n + 1 - (n+1)*integrate(function(x) 1/(x^n + 1), 0, 1)$value;
+
+###
+n = 8
+integrate(function(x) 1/(x^(1 - 1/(n+1)) + 1), 0, 1)
+n + 1 - (n+1)*integrate(function(x) 1/(x^n + 1), 0, 1)$value;
+
--- Math/Integrals.Fractions.Unity.R
+++ Math/Integrals.Fractions.Unity.R
@@ -22,6 +22,9 @@
 
 ### version 1 [RC1] [draft]
 #
+### 2023-01-14:
+# - shortcut formulas moved to new file:
+#   Integrals.Fractions.Unity.Definite.R;
 ### 2023-01-09:
 # - shortcut formulas for I on [0, Inf];
 ### 2021-12-08:
@@ -112,6 +115,13 @@ x = 2 # any value;
 1/(x^n - 1) # ==
 sum((a*c1*x + b) / (x^2 - c1*x + 1)) - b/(x^2-1)
 
+###
+x = 3^(1/5)
+n = 3; # but for (4*n)!
+c1 = 2*cos(seq(1, 4*n, by=2)*pi/(4*n))
+1/(x^(4*n) + 1) # ==
+sum((c1*x + 2) / (x^2 + c1*x + 1)) / (4*n)
+
 
 ### Infinite Integrals
 # - shortcut formulas are available;
@@ -139,113 +149,10 @@ integrate(function(x) x^2/(x^n + 1), lower=0, upper=Inf)
 integrate(function(x) x^3/(x^n + 1), lower=0, upper=Inf)
 pi / sin(3*pi/n) / n
 
-
-### Other Shortcuts
-n = 3
-integrate(function(x) 1/(x^3 + 1), 0, 1)
-2/n * cos(2*pi/3)*log(cos(pi/3)) +
-	+ 2*pi*sin(2*pi/3)/n^2
-
-# Fraction Decomposition:
-1/n/(x+1) + 2/n * sum( (cos(2*pi/3)*x + 1) / (x^2 + 2*cos(2*pi/3)*x + 1) )
-1/n/(x+1) + 1/n * cos(2*pi/3)*(2*x + 2*cos(2*pi/3)) / (x^2 + 2*cos(2*pi/3)*x + 1) +
-	+ 2/n * sin(2*pi/3)^2 / (x^2 + 2*cos(2*pi/3)*x + 1)
-# I on [0, 1] =>
-2/n * cos(2*pi/3)*log(cos(pi/3)) +
-	+ 2/n * sin(2*pi/3)*atan( (1 + cos(2*pi/3))/sin(2*pi/3) ) +
-	# - 2/n * sin(2*pi/3)*atan( (0 + cos(2*pi/3))/sin(2*pi/3) );
-	- 2/n * (pi/2 - 2*pi/3) * sin(2*pi/3);
-
-### n = 5
-# TODO: simplify
-n = 5
-integrate(function(x) 1/(x^n + 1), 0, 1)
-2*cos(2*pi/n)*log(cos(pi/n)) / n +
-	+ 2*cos(4*pi/n)*log(cos(2*pi/n)) / n +
-	+ 2*pi*(sin(2*pi/n) + 2*sin(4*pi/n)) / n^2;
-# useful ???
-- 4*log(2)*cos(2*pi/n)/n +
-	+ 4/n * (4*cos(pi/n)^4 - 5*cos(pi/n)^2 + 1)*log(cos(2*pi/n)) +
-	+ 2*pi*sin(2*pi/n)*(2*cos(pi/n)^2 + 3*cos(2*pi/n)) / n^2;
-
-#
-n = 7
-integrate(function(x) 1/(x^n + 1), 0, 1)
-2*cos(2*pi/n)*log(cos(pi/n)) / n +
-	+ 2*cos(4*pi/n)*log(cos(2*pi/n)) / n +
-	+ 2*cos(6*pi/n)*log(cos(3*pi/n)) / n +
-	+ 2*pi*(sin(2*pi/n) + 2*sin(4*pi/n) + 3*sin(6*pi/n)) / n^2;
-
-#
-2/n * cos(2*pi/n)*log(cos(pi/n)) +
-	+ 2/n * cos(4*pi/n)*log(cos(2*pi/n)) +
-	+ 2/n * cos(6*pi/n)*log(cos(3*pi/n)) +
-	+ 2/n*sin(2*pi/n)*atan( (1 + cos(2*pi/n))/sin(2*pi/n) ) +
-	+ 2/n*sin(4*pi/n)*atan( (1 + cos(4*pi/n))/sin(4*pi/n) ) +
-	+ 2/n*sin(6*pi/n)*atan( (1 + cos(6*pi/n))/sin(6*pi/n) ) +
-	- 2/n * (pi/2 - 2*pi/n) * sin(2*pi/n) +
-	- 2/n * (pi/2 - 4*pi/n) * sin(4*pi/n) +
-	- 2/n * (pi/2 - 6*pi/n) * sin(6*pi/n)
-
-###
-intUnityI01 = function(n) {
-	if(n %% 2 == 0) {
-		return(intUnityI01Even(n));
-	}
-	n2 = n %/% 2;
-	sum(cos(2*seq(n2)*pi/n)*log(cos(seq(n2)*pi/n))) * 2 / n +
-	+ sum(seq(n2)*sin(2*seq(n2)*pi/n)) * 2*pi / n^2;
-}
-intUnityI01Even = function(n) {
-	if(n %% 2 == 1) return(intUnityI01(n));
-	isNotM4 = n %% 4 != 0;
-	id = seq(1, n, by=2);
-	if(isNotM4) id = id[ - (n+2)/4];
-	cs = cos(id*pi/n); sn = sin(id*pi/n);
-	# Base-Formula:
-	# fr = sum(2*cs*x + 2)/(x^2 + 2*cs*x + 1)) / n;
-	# r = sum(cs*log(x^2 + 2*cs*x + 1)) + sum(2*atan((x - cs)/sn));
-	r = sum(cs*log(cs + 1)) +
-		# + sum(2*sn*atan((1 - cs)/sn)) - sum(2*sn*atan((0 - cs)/sn));
-		+ sum(2*sn*atan((1 - cs)/sn)) - 2*pi*sum(id*sn)/n + pi*sum(sn);
-	if(isNotM4) r = r + pi/2;
-	return(r/n);
-}
-
-###
-n = 9
-integrate(function(x) 1/(x^n + 1), 0, 1)
-intUnityI01(n)
-
-###
-n = 15
-integrate(function(x) 1/(x^n + 1), 0, 1)
-intUnityI01(n)
-
-###
-n = seq(3, 21, by=2)
-x = sapply(n, intUnityI01)
-# asymptotic to y = 1;
-plot(n, x)
-curve( (x + 1/3)/(x+1), add=T, col="orange")
-
-###
-n = seq(3, 101, by=2)
-x = sapply(n, intUnityI01)
-# asymptotic to y = 1;
-plot(n, x)
-curve( (x + 1/3)/(x+1), add=T, col="orange")
-
-
-### Special Fractional Powers
-n = 7
-integrate(function(x) 1/(x^(1 - 1/(n+1)) + 1), 0, 1)
-n + 1 - (n+1)*integrate(function(x) 1/(x^n + 1), 0, 1)$value;
-
-###
-n = 8
-integrate(function(x) 1/(x^(1 - 1/(n+1)) + 1), 0, 1)
-n + 1 - (n+1)*integrate(function(x) 1/(x^n + 1), 0, 1)$value;
+### Note:
+# - the remaining shortcut formulas to specific Definite Integrals
+#   have been moved to file:
+#   Integrals.Fractions.Unity.Definite.R;
 
 
 ########################
@@ -358,6 +265,7 @@ decompose.fr = function(n, type=c("U+", "U-")) {
 
 
 # generate Roots of Unity
+# TODO: refactor & use directly trigonometric functions;
 roots1.conj = function(n, computeRotation=FALSE) {
 	m = complex(re=cos(2*pi/n), im=sin(2*pi/n))
 	n_1 = n - 1
