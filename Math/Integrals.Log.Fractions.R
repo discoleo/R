6fde8223140c69bcc1eb78cb2a1e42d94df69b2a
discoleo <leo.mada@syonic.eu>
2023-01-19 16:52:43 +0200

    [refactor] moved more code;

--- Math/Integrals.Log.Fractions.R
+++ Math/Integrals.Log.Fractions.R
@@ -6,13 +6,18 @@
 ### Integrals: Logarithms
 ### log-Fractions
 ###
-### draft v.0.1a
-
+### draft v.0.1b
 
 
 ##################
 ### Logarithms ###
 ##################
+
+# - definite integrals;
+# - various types of Logarithms combined with fractions;
+
+
+####################
 
 #################
 ### Fractions ###
@@ -206,4 +211,174 @@ pi*log(a)/(2*a) + pi*log(2)/(4*a) - Catalan/a;
 a = 7/5
 integrate(function(x) log(x+a) / (2*x^2 + 2*(a-1)*x + a^2 + 1), lower=-a, upper=1)
 pi*log(a+1)/(2*(a+1)) - pi*log(2)/(4*(a+1)) - Catalan/(a+1);
+
+
+#######################
+#######################
+
+### Composite Fractions
+
+# qncubed3: Complex Analysis: An Integral from @MichaelPennMath
+# https://www.youtube.com/watch?v=LH4i9XJsz_I
+# - Generalization of the 2nd (sub-) Integral;
+
+p = 1/5
+k = 3
+integrate(function(x) log(x)/x^p/(x+k)^2, lower=0, upper=Inf)
+pi*((p*log(k) - 1)*sin(pi*p) + pi*p*cos(pi*p)) / k^(p+1) / sin(pi*p)^2
+
+
+# Residue: x = - k;
+# x^(p-1)*(1 - p*log(x))
+2i*pi*k^(-p-1)*exp(-1i*pi*(p+1))*(1 - p*log(k) - 1i*pi*p)
+
+# Div:
+(1 - exp(-2i*pi*p))
+
+# Helper
+integrate(function(x) x^(-p)/(x+k)^2, lower=0, upper=Inf)
+pi*p/sin(pi*p)/k^(p+1)
+
+
+#####################
+#####################
+
+#####################
+### Log-Fractions ###
+### Higher Powers ###
+#####################
+
+
+# qncubed3: Complex Analysis: Is this solution way too overkill?
+# https://www.youtube.com/watch?v=vfa5wGE7fRI
+
+###
+integrate(function(x) log(x)^2/(x^2 + 1), lower=0, upper=Inf)
+pi^3/8
+
+### Gen 1:
+b = sqrt(5)
+integrate(function(x) log(x)^2/(x^2 + b^2), lower=0, upper=Inf)
+pi^3/(2^3*b) + pi/2 * log(b)^2/b
+
+
+### Gen: Power of log
+
+###
+b = sqrt(5)
+integrate(function(x) log(x)^3/(x^2 + b^2), lower=0, upper=Inf)
+pi*log(b)^3 / (2*b) + 3*pi^3*log(b)/(8*b)
+
+
+###
+integrate(function(x) log(x)^4/(x^2 + 1), lower=0, upper=Inf)
+5*pi^5/32
+
+#
+b = sqrt(5)
+integrate(function(x) log(x)^4/(x^2 + b^2), lower=0, upper=Inf)
+5*pi^5/(2^5*b) + pi/2 * log(b)^4/b + 6*pi^3*log(b)^2/(8*b)
+
+
+###
+integrate(function(x) log(x)^6/(x^2 + 1), lower=0, upper=Inf)
+61*pi^7/128
+
+#
+b = sqrt(5)
+integrate(function(x) log(x)^6/(x^2 + b^2), lower=0, upper=Inf)
+61*pi^7/(2^7*b) + pi/2 * log(b)^6/b + 75*pi^5*log(b)^2/(2^5*b) + 15*pi^3*log(b)^4/(8*b)
+
+
+###
+integrate(function(x) log(x)^8/(x^2 + 1), lower=0, upper=Inf)
+1385*pi^9/512
+
+#
+b = sqrt(5)
+integrate(function(x) log(x)^8/(x^2 + b^2), lower=0, upper=Inf)
+1385*pi^9/(2^9*b) + pi/2 * log(b)^8/b + 28*61*pi^7*log(b)^2/(2^7*b) +
+	+ 70*5*pi^5*log(b)^4/(2^5*b) + 28*pi^3*log(b)^6/(8*b)
+
+
+###
+integrate(function(x) log(x)^10/(x^2 + 1), lower=0, upper=Inf,
+	abs.tol=1E-8, rel.tol=1E-8, subdivisions=4096)
+50521*pi^11/2^11
+
+
+### Gen 2: Both Powers
+
+###
+integrate(function(x) log(x)^3/(x^3 + 1), lower=0, upper=Inf)
+pi^4*(25/3^4 - 1) / 12
+
+
+# Aux. Polynomial:
+x^4 - 4i*pi*x^3 - 4*pi^2*x^2
+
+# Derivation:
+- pi^4*(1 +
+	- (1/3^4 - 4/3^3 + 4/9)*m +
+	- (5^4/3^4 - 4*5^3/3^3 + 4*5^2/9)*m^5) / 12
+- pi^4*(1 - 25*m/3^4 - 25*m^5/3^4) / 12
+- pi^4*(1 - 25/3^4) / 12
+
+
+###
+integrate(function(x) log(x)^5/(x^5 + 1), lower=0, upper=Inf)
+pi^6*(2*4779*cos(pi/5) - 4*31311*cos(pi/5)^2 + 2*31311 - 3*5^6) / (6*5*5^6)
+
+# Aux. Polynomial:
+x^6 - 6i*pi*x^5 - 10*pi^2*x^4 - 8*pi^4*x^2
+b5 = - 3*k; b4 = 5/2*k^2; b3 = 0; b2 = -1/2*k^4;
+
+# Derivation:
+source("Polynomials.Helper.R")
+
+solve.polyInt = function(n, verbose=TRUE, subst=BULL) {
+	px = toPoly.pm(paste0("x^", seq(n-1), "*b", seq(n-1), collapse="+"));
+	px = sum.pm(px, toPoly.pm("x^n"));
+	pd = diff.pm(replace.pm(px, toPoly.pm("x+k"), "x"), px);
+	b = paste0("b", seq(n-1));
+	r = list();
+	for(i in seq(n-1, 1)) {
+		tmp = pd[pd$x == (i-1), , drop=FALSE];
+		tmp$x = NULL;
+		tmp = drop.pm(tmp);
+		tmp = simplify.pm.pow(tmp, do.gcd=TRUE);
+		if(verbose) print.pm(tmp);
+		if(nrow(tmp) == 1) {
+			r = c(r, list(0, 1));
+			pd = replace.pm(pd, 0, b[i]);
+			px = replace.pm(px, 0, b[i]);
+		} else {
+			tmp = solve.pm(tmp, pd, b[i], verbose=FALSE);
+			r = c(r, list(tmp$x0, tmp$div));
+			div = tmp$div;
+			if(is.null(div)) div = data.frame(coeff=1);
+			pd = replace.fr.pm(pd, tmp$x0, div, b[i], verbose=FALSE);
+			px = replace.fr.pm(px, tmp$x0, div, b[i], verbose=FALSE);
+		}
+	}
+	if(px$coeff[px$x == n] < 0) px$coeff = -px$coeff;
+	if( ! is.null(subst)) {
+		px = replace.pm(px, subst, "k");
+	}
+	px = reduce.coef.pm(px);
+	return(list(poly=px, Coeff=r));
+}
+
+n = 6
+px = solve.polyInt(n, subst=as.pm("2i*pi"))
+print.pm(px$poly, "x")
+# toCoeff(px$poly, "x")
+
+px = toPoly.pm("x^6 - 6i*pi*x^5 - 10*pi^2*x^4 - 8*pi^4*x^2")
+eval.pm(px, list(x=1i*pi/5, pi=pi)) / pi^6 * 5^6
+# =>
+- pi^6*(3*5^6/5 + 4779/(5*m^4) + 31311/(5*m^12) + 31311/(5*m^28) + 4779/(5*m^36)) / (6*5^6)
+- pi^6*(3*5^6 - 4779*m - 31311*m^3 + 31311*m^2 + 4779*m^4) / (6*5*5^6)
+- pi^6*(3*5^6 - 2*4779*cos(pi/5) + 2*31311*cos(2*pi/5)) / (6*5*5^6)
+pi^6*(2*4779*cos(pi/5) - 4*31311*cos(pi/5)^2 + 2*31311 - 3*5^6) / (6*5*5^6)
 
