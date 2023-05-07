5ba404433bec2b5f0e05b2736bf1c9c4da677b82
discoleo <leo.mada@syonic.eu>
2023-05-08 01:14:33 +0300

    I( x^p / (exp(x) - 1) ) on [0, Inf];

--- Math/Integrals.Gamma.Other.R
+++ Math/Integrals.Gamma.Other.R
@@ -1,12 +1,13 @@
 
 
-### I( x^p / (1 + exp(x)) )
+### I( x^p / (exp(x) + 1) )
 # Michael Penn: WHY are we finding pi HERE?
 # https://www.youtube.com/watch?v=amL2uBK2buU
 
 p = 1/3
 integrate(\(x) x^p / (1 + exp(x)), 0, Inf, rel.tol=1E-8)
 gamma(p) * pracma::zeta(p + 1) * (1 - 1/2^p) * p
+gamma(p + 1) * pracma::zeta(p + 1) * (1 - 1/2^p)
 
 ###
 p = sqrt(5)
@@ -18,4 +19,13 @@ gamma(p) * pracma::zeta(p + 1) * (1 - 1/2^p) * p
 p = sqrt(5)
 integrate(\(x) x^p / (1 + pi^x), 0, Inf, rel.tol=1E-8)
 gamma(p) * pracma::zeta(p + 1) * (1 - 1/2^p) * p / log(pi)^(p + 1)
+
+
+### I( x^p / (exp(x) - 1) )
+# Dr Peyam: A gamma zeta integral
+# https://www.youtube.com/watch?v=hqo_-AX4IXg
+
+p = sqrt(5)
+integrate(\(x) x^p / (exp(x) - 1), 0, Inf, rel.tol=1E-8)
+gamma(p + 1) * pracma::zeta(p + 1)
 
