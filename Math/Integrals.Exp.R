263b592718fcd1bd4c1ffea08dad53ed9ee86064
discoleo <leo.mada@syonic.eu>
2023-05-09 23:12:42 +0300

    I( F[EVEN] / (exp(1/x) + 1) ) on [-a, a];

--- Math/Integrals.Exp.R
+++ Math/Integrals.Exp.R
@@ -9,3 +9,45 @@ constEuler = 0.57721566490153286060651209008240243079;
 integrate(\(x) (1 - exp(-x) - exp(-1/x)) / x, 0, 1)
 constEuler
 
+
+#########################
+#########################
+
+### I( cos(x) / (exp(1/x) + 1) )
+# Michael Penn: is this integration trick TOO POWERFUL?
+# https://www.youtube.com/watch?v=PX2QXILRgsc
+
+###
+lim = 1
+integrate(\(x) cos(x) / (exp(1/x) + 1), -lim, lim)
+sin(lim)
+
+### Various Variants:
+lim = sqrt(pi)
+integrate(\(x) 1 / (exp(1/x) + 1), -lim, lim)
+lim
+
+###
+lim = 1
+integrate(\(x) cos(x)^2 / (exp(1/x) + 1), -lim, lim)
+sin(2*lim) / 4 + lim/2
+
+###
+lim = sqrt(pi)
+integrate(\(x) sin(x)^2 / (exp(1/x) + 1), -lim, lim)
+lim/2 - sin(2*lim) / 4
+
+
+### Variants: exp() - 1
+
+###
+lim = sqrt(pi)
+integrate(\(x) 1 / (exp(1/x) - 1), -lim, lim)
+- lim
+
+###
+lim = sqrt(pi)
+integrate(\(x) cos(x) / (exp(1/x) - 1), -lim, lim)
+- sin(lim)
+
+
