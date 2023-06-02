05c5ffe3921cd8bc8efccf2f3d4ff57ea76fc2a3
discoleo <leo.mada@syonic.eu>
2023-06-02 19:57:33 +0300

    sum( j / (j^2 + 1)^2 )

---
+++ Math/Sums.Fractions.Higher.R
@@ -0,0 +1,20 @@
+
+
+
+
+### sum( 1 / (j^2 + 1)^p )
+
+###
+j = seq(0, 10000)
+sum( 1 / (j^2 + 1)^2 )
+pi^2/4 / cosh(pi)^2 + pi/4 / tanh(pi) + 1/2
+# TODO
+
+
+###
+j = seq(0, 10000)
+sum( j / (j^2 + 1)^2 )
+(pracma::psi(1, 1 + 1i) - pracma::psi(1, 1 - 1i)) * 1i/4
+(pracma::psi(1, 1i) - pracma::psi(1, - 1i)) * 1i/4
+# TODO
+
