0e9914cfb943d54137b4ffa7c93f4c1753822193
discoleo <leo.mada@syonic.eu>
2023-06-02 22:50:32 +0300

    Derivation: sum( j / (j^2 + k^2)^2 );

--- Math/Sums.Fractions.Higher.R
+++ Math/Sums.Fractions.Higher.R
@@ -16,5 +16,21 @@ j = seq(0, 10000)
 sum( j / (j^2 + 1)^2 )
 (pracma::psi(1, 1 + 1i) - pracma::psi(1, 1 - 1i)) * 1i/4
 (pracma::psi(1, 1i) - pracma::psi(1, - 1i)) * 1i/4
-# TODO
+
+
+###
+k = sqrt(5)
+j = seq(0, 10000)
+sum( j / (j^2 + k^2)^2 )
+(pracma::psi(1, k*1i) - pracma::psi(1, - k*1i)) * 1i / (4*k)
+# TODO: simplify ?
+
+# Derivation
+k = sqrt(3)
+j = seq(0, 10000)
+sum( j / (j^2 + k^2)^2 )
+sum( (j + k*1i)^2 / (j^2 + k^2)^2 - (j - k*1i)^2 / (j^2 + k^2)^2) / 4i / k
+sum( 1 / (j - k*1i)^2 - 1 / (j + k*1i)^2) / 4i / k
+sum( 1/(j+1)^2 - 1 / (j + k*1i)^2 - (1/(j+1)^2 - 1 / (j - k*1i)^2) ) / 4i / k
+(pracma::psi(1, k*1i) - pracma::psi(1, - k*1i)) * 1i/ (4*k)
 
