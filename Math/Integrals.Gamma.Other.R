b4c691afc674aee9360814d2e5034ec436bcbe64
discoleo <leo.mada@syonic.eu>
2023-06-17 18:31:58 +0300

    I( exp(-x^p) / (x^p + 1) ) on [0, Inf];

--- Math/Integrals.Gamma.Other.R
+++ Math/Integrals.Gamma.Other.R
@@ -29,3 +29,30 @@ p = sqrt(5)
 integrate(\(x) x^p / (exp(x) - 1), 0, Inf, rel.tol=1E-8)
 gamma(p + 1) * pracma::zeta(p + 1)
 
+
+####################
+####################
+
+### I( exp(-x^p) / (x^p + 1) ) on [0, Inf]
+# Dr. Peyam: But I AM joking, Mr. Feynman!
+# https://www.youtube.com/watch?v=sdWpmwutSHE
+
+###
+integrate(\(x) exp(-x^2) / (x^2+1), 0, Inf)
+erf1 = integrate(\(x) exp(-x^2), 0, 1)$value;
+gamma(1/2) * (gamma(1/2)/2 - erf1) * exp(1)
+
+
+###
+integrate(\(x) exp(-x^3) / (x^3+1), 0, Inf)
+erf1 = integrate(\(x) x*exp(-x^3), 0, 1)$value;
+gamma(1/3) * (gamma(2/3)/3 - erf1) * exp(1)
+
+
+### Generalization:
+p = sqrt(5)
+integrate(\(x) exp(-x^p) / (x^p+1), 0, Inf)
+erf1 = integrate(\(x) x^(p-2)*exp(-x^p), 0, 1)$value;
+gamma(1/p) * (gamma(1-1/p)/p - erf1) * exp(1)
+
+
