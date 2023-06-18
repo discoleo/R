a03e0aea87bf8ab3cb31bb2da3070261d5782ef9
discoleo <leo.mada@syonic.eu>
2023-06-18 04:52:12 +0300

    various Log-Trig Integrals;

--- Math/Integrals.Log.Trig.R
+++ Math/Integrals.Log.Trig.R
@@ -6,14 +6,50 @@
 Catalan = 0.915965594177219015054603514;
 # Note:
 # Catalan = - I(log(x)/(x^2 + 1), lower=0, upper=1)
+# Catalan = (psi(1, 1/4) - psi(1, 3/4)) / 4^2
+
+
+###################
+###################
+
+### I( log(sin(x)) ) on various intervals
+
+# see e.g.:
+# Maths 505: see 2nd & 3rd intermediate integrals;
+# A MONSTER INTEGRAL!!! int from 0 to infty arctan(x)/(x(x+1)(x^2+1))
+# https://www.youtube.com/watch?v=u-FKjn_83l8
+
+
+###
+integrate(function(x) log(sin(x)), 0, pi/2)
+- pi/2*log(2)
+
+###
+integrate(function(x) log(sin(x)), 0, pi/4)
+- pi/4*log(2) - Catalan/2
+
+###
+integrate(function(x) log(cos(x)), 0, pi/4)
+- pi/4*log(2) + Catalan/2
+
+###
+integrate(function(x) log(cos(x) + sin(x)), 0, pi/2)
+- pi/4*log(2) + Catalan
+#
+integrate(function(x) log(cos(x) - sin(x)), 0, pi/4)
+- pi/8*log(2) - Catalan/2
+#
+integrate(function(x) log(cos(x) + sin(x)), 0, pi/4)
+- pi/8*log(2) + Catalan/2
+
 
 
 #################
 
 ### Michael Penn: Can you guess the trick for this integral?
 # https://www.youtube.com/watch?v=8R0MiRYmjbk
+
 # Intermediary:
-#
 integrate(function(x) log(tan(x)), 0, pi/4)
 # ==
 - Catalan
