3d38c333ebba24a10e0d2ac2ff19bd06a570a8fa
discoleo <leo.mada@syonic.eu>
2023-06-19 19:09:13 +0300

    [refactor] moved to file Exp.R;

--- Math/Integrals.Exp.R
+++ Math/Integrals.Exp.R
@@ -71,6 +71,45 @@ constEuler
 #########################
 #########################
 
+### I( x^p / (exp(x) + 1) )
+# Michael Penn: WHY are we finding pi HERE?
+# https://www.youtube.com/watch?v=amL2uBK2buU
+
+p = 1/3
+integrate(\(x) x^p / (1 + exp(x)), 0, Inf, rel.tol=1E-8)
+gamma(p) * pracma::zeta(p + 1) * (1 - 1/2^p) * p
+gamma(p + 1) * pracma::zeta(p + 1) * (1 - 1/2^p)
+
+###
+p = sqrt(5)
+integrate(\(x) x^p / (1 + exp(x)), 0, Inf, rel.tol=1E-8)
+gamma(p) * pracma::zeta(p + 1) * (1 - 1/2^p) * p
+
+###
+# - bringing pi back into the equation:
+p = sqrt(5)
+integrate(\(x) x^p / (1 + pi^x), 0, Inf, rel.tol=1E-8)
+gamma(p) * pracma::zeta(p + 1) * (1 - 1/2^p) * p / log(pi)^(p + 1)
+
+
+### I( x^p / (exp(x) - 1) )
+# Dr Peyam: A gamma zeta integral
+# https://www.youtube.com/watch?v=hqo_-AX4IXg
+
+p = sqrt(5)
+integrate(\(x) x^p / (exp(x) - 1), 0, Inf, rel.tol=1E-8)
+gamma(p + 1) * pracma::zeta(p + 1)
+
+
+### I( x^p * log((exp(x) - 1) / (exp(x) + 1)) )
+p = sqrt(5);
+# numerical issues: upper -> Inf;
+integrate(\(x) x^p * log((exp(x) - 1) / (exp(x) + 1)), 0, 200, rel.tol=1E-8)
+- gamma(p + 1) * pracma::zeta(p + 2) * (2 - 1/2^(p+1))
+
+
+#########################
+
 ### I( 1 / (exp(x^n) + 1) )
 # Maths 505: A RIDICULOUSLY AWESOME integral from the quantum realm
 # https://www.youtube.com/watch?v=oZWsDyN5ssI
