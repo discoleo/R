d6c29d25bced4e2af3b5bc227b18ca4736c38b16
discoleo <leo.mada@syonic.eu>
2023-06-19 22:45:30 +0300

    more basic stuff;

--- Math/Integrals.Exp.Log.R
+++ Math/Integrals.Exp.Log.R
@@ -14,6 +14,9 @@ Catalan = 0.915965594177219015054603514;
 # https://www.youtube.com/watch?v=wUY6TJxfFTk
 # 2.) Dr Peyam: A mathematically stunning formula
 # https://www.youtube.com/watch?v=hmYYYhQF1RM
+# 3.) Michael Penn: Euler's other constant
+# https://www.youtube.com/watch?v=UEqI9GKYozU
+# [the basic proof]
 
 
 ###
@@ -21,10 +24,16 @@ integrate(\(x) exp(-x) * log(x), 0, Inf)
 - Euler
 
 ###
+n = sqrt(3)
+integrate(\(x) exp(-n*x) * log(x), 0, Inf)
+- Euler/n - log(n)/n
+
+
+### Pow = 2
 integrate(\(x) exp(-x) * log(x)^2, 0, Inf)
 pi^2/6 + Euler^2
 
-###
+### Pow = 3
 integrate(\(x) exp(-x) * log(x)^3, 0, Inf)
 - 2 * pracma::zeta(3) - Euler*(pi^2/2 + Euler^2)
 # psi2(1)*gamma(1) + 2*psi1(1)*d(gamma)(1) + psi(1)*d2(gamma)(1)
