070d041b15c067c7105d1596dc422752cdf0dda5
discoleo <leo.mada@syonic.eu>
2023-06-26 22:29:20 +0300

    I( sin(x) / (sin(x)^6 + cos(x)^6) ) on [0, pi/4];

--- Math/Integrals.Fractions.Trig.R
+++ Math/Integrals.Fractions.Trig.R
@@ -52,3 +52,69 @@ integrate(function(x) (x^2 + 1)^(n/2 - 1) / (x^n + 1), lower=0, upper=1)
 pi*(1/tan(pi/n2) + 1/tan(9*pi/n2) + 4/tan(pi*3/n2) + 4/tan(pi*7/n2) +
 	+ 6/tan(pi*5/n2))/n2
 
+
+#################
+#################
+
+###
+integrate(\(x) sin(x) / (sin(x)^6 + cos(x)^6), 0, pi/4)
+integrate(\(x) sin(x) / (1 - 3*sin(x)^2*cos(x)^2), 0, pi/4)
+integrate(\(x) 1 / (1 - 3*x^2*(1-x^2)), sqrt(2)/2, 1)
+integrate(\(x) 1 / (3*x^4 - 3*x^2 + 1), sqrt(2)/2, 1)
+integrate(\(x) sqrt(3) / (x^4 - 3*x^2 + 3), sqrt(6)/2, sqrt(3))
+
+# alternative:
+integrate(\(x) sin(x) / (sin(x)^6 + cos(x)^6), 0, pi/4)
+integrate(\(x) x * (x^2 + 1)^(3/2) / (x^6 + 1), 0, 1)
+integrate(\(x) 1/2 * (x + 1)^(3/2) / (x^3 + 1), 0, 1)
+integrate(\(x) x^4 / ((x^2-1)^3 + 1), 1, sqrt(2))
+
+# =>
+integrate(\(x) sqrt(3) / (x^4 - 3*x^2 + 3), sqrt(6)/2, sqrt(3));
+1i*log( (1 - sqrt(r[1]/3)) * (sqrt(2)/2 + sqrt(r[1]/3))) / sqrt(r[1]) +
+	- 1i*log( (1 - sqrt(r[2]/3)) * (sqrt(2)/2 + sqrt(r[2]/3))) / sqrt(r[2]) +
+	- log(6) * sin(pi/12) / 3^(1/4) +
+	+ 2*pi * cos(pi/12) / 3^(5/4);
+
+
+# Derivation:
+r = (sqrt(3) + c(-1i,1i)) * sqrt(3)/2;
+rd = 1/2 - c(-1i,1i)*sqrt(3)/6;
+rr = - c(-1i,1i)*sqrt(3)/6;
+rfr = 1 + c(-1, 1) * sqrt(3)*1i;
+integrate(\(x) sqrt(3) / (x^4 - 3*x^2 + 3), sqrt(6)/2, sqrt(3));
+1i*log( (sqrt(3) - sqrt(r[1])) / (sqrt(3) + sqrt(r[1])) ) / (2*sqrt(r[1])) +
+	- 1i*log( (sqrt(3) - sqrt(r[2])) / (sqrt(3) + sqrt(r[2])) ) / (2*sqrt(r[2])) +
+	- 1i*log( (sqrt(6)/2 - sqrt(r[1])) / (sqrt(6)/2 + sqrt(r[1])) ) / (2*sqrt(r[1])) +
+	+ 1i*log( (sqrt(6)/2 - sqrt(r[2])) / (sqrt(6)/2 + sqrt(r[2])) ) / (2*sqrt(r[2]))
+1i*log( (1 - sqrt(r[1]/3))^2 / (1 - r[1]/3) ) / (2*sqrt(r[1])) +
+	- 1i*log( (1 - sqrt(r[2]/3))^2 / (1 - r[2]/3) ) / (2*sqrt(r[2])) +
+	- 1i*log( (sqrt(2)/2 - sqrt(r[1]/3))^2 / (1/2 - r[1]/3) ) / (2*sqrt(r[1])) +
+	+ 1i*log( (sqrt(2)/2 - sqrt(r[2]/3))^2 / (1/2 - r[2]/3) ) / (2*sqrt(r[2]));
+1i*log( (1 - sqrt(r[1]/3))^2 / rd[1] ) / (2*sqrt(r[1])) +
+	- 1i*log( (1 - sqrt(r[2]/3))^2 / rd[2] ) / (2*sqrt(r[2])) +
+	- 1i*log( (sqrt(2)/2 - sqrt(r[1]/3))^2 / rr[1] ) / (2*sqrt(r[1])) +
+	+ 1i*log( (sqrt(2)/2 - sqrt(r[2]/3))^2 / rr[2] ) / (2*sqrt(r[2]));
+1i*log( (1 - sqrt(r[1]/3))^2 / (sqrt(2)/2 - sqrt(r[1]/3))^2 *
+		1/rfr[1] ) / (2*sqrt(r[1])) +
+	- 1i*log( (1 - sqrt(r[2]/3))^2 / (sqrt(2)/2 - sqrt(r[2]/3))^2 *
+		1/rfr[2] ) / (2*sqrt(r[2]));
+1i*log( (1 - sqrt(r[1]/3)) / (sqrt(2)/2 - sqrt(r[1]/3))) / sqrt(r[1]) +
+	- 1i*log( (1 - sqrt(r[2]/3)) / (sqrt(2)/2 - sqrt(r[2]/3))) / sqrt(r[2]) +
+	- 1i * log(rfr[1]) * (1/(2*sqrt(r[1])) + 1/(2*sqrt(r[2]))) +
+	+ 1i * log(4) / (2*sqrt(r[2]));
+1i*log( (1 - sqrt(r[1]/3)) * (sqrt(2)/2 + sqrt(r[1]/3))) / sqrt(r[1]) +
+	- 1i*log( (1 - sqrt(r[2]/3)) * (sqrt(2)/2 + sqrt(r[2]/3))) / sqrt(r[2]) +
+	- 1i * log(6) * (sqrt(r[1]) - sqrt(r[2])) / (2*sqrt(3)) +
+	+ pi * (sqrt(r[1]) + sqrt(r[2])) / (3*sqrt(3));
+
+
+# Fraction Decomposition:
+r = (3 + c(-1,1)*sqrt(3)*1i)/2;
+r = (sqrt(3) + c(-1i,1i)) * sqrt(3)/2;
+x = 5^(1/3)
+1 / (x^4 - 3*x^2 + 3) # ==
+(1/(x^2 - r[1]) - 1/(x^2 - r[2])) / sqrt(3) * 1i
+(1i/(x - sqrt(r[1])) - 1i/(x + sqrt(r[1]))) / (2*sqrt(3*r[1])) +
+	- (1i/(x - sqrt(r[2])) - 1i/(x + sqrt(r[2]))) / (2*sqrt(3*r[2]))
+
