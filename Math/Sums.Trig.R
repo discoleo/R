a0eabcde02809264ab078a25077959afdf5f9947
discoleo <leo.mada@syonic.eu>
2023-07-06 01:23:56 +0300

    Sum( cos(n) / n )

--- Math/Sums.Trig.R
+++ Math/Sums.Trig.R
@@ -17,7 +17,8 @@ x = sqrt(3)
 sum((-1)^id * cos(2*x*id)/id)
 - log(2*abs(cos(x)))
 
-### Ex: 1 + 1/2 - 2/3 + ...
+### Ex: H(3*n) - H(n)
+# 1 + 1/2 - 2/3 + ...
 x = pi/3
 sum(cos(2*x*id)/id)
 - log(2*abs(sin(x)))
@@ -25,4 +26,18 @@ sum(cos(2*x*id)/id)
 id = seq(30000)
 sum(c(1,1,-2)/id)
 log(3)
+
+
+########################
+########################
+
+### sum(cos(n) / n)
+# Michael Penn: An infinite cosine sum.
+# https://www.youtube.com/watch?v=R_Uf78si8jk
+
+id = seq(60000)
+sum(cos(id) / id)
+- log(2 - 2*cos(1)) / 2
+- log(2*sin(1/2))
+
 
