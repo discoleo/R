0ed92bbc1c06dc38feee7dac99ff7e8310032668
discoleo <leo.mada@syonic.eu>
2023-07-06 01:33:38 +0300

    Finite sums: Sum( sin(pi * id/n) );

--- Math/Sums.Trig.R
+++ Math/Sums.Trig.R
@@ -1,5 +1,24 @@
 
 
+
+### Finite Sums
+
+### ODD:
+n = 9
+id = seq(1, floor(n/2));
+sn = sin(2*pi*id/n);
+#
+sum(sn)
+1/tan(pi/n/2) / 2
+#
+sum(id*sn)
+n/sin(pi/n) / 4
+
+
+#########################
+
+### Infinite Sums
+
 ### sum( cos(2*n*x) / n )
 # Maths 505:  My take on this on wonderful infinite series from @drpeyam
 # https://www.youtube.com/watch?v=mqPTvELJPM0
@@ -31,9 +50,10 @@ log(3)
 ########################
 ########################
 
-### sum(cos(n) / n)
+### sum( cos(n) / n )
 # Michael Penn: An infinite cosine sum.
 # https://www.youtube.com/watch?v=R_Uf78si8jk
+# Note: a special case of the previous sum;
 
 id = seq(60000)
 sum(cos(id) / id)
