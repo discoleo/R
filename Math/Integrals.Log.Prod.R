cecd56cd9aecdd73edef12c6145afd37ac92bdc1
discoleo <leo.mada@syonic.eu>
2023-07-15 20:09:23 +0300

    [refactor] Reordered sections;

--- Math/Integrals.Log.Prod.R
+++ Math/Integrals.Log.Prod.R
@@ -44,6 +44,7 @@ pracma::zeta(3) * 2/3
 
 
 ### Series: Pow = 2
+# P() / x^2
 
 ###
 integrate(\(x) log(x) * log(1 - x^2) / x^2, 0, 1)
@@ -62,6 +63,8 @@ integrate(\(x) log(x) * log(1 - x) / x^2 + log(x)/x + log(x)/2, 0, 1)
 pi^2/6 - 1.5
 
 
+### Base-Variants:
+
 ### Other: Simple
 integrate(function(x) log(x) * log(1 - x), 0, 1)
 2 - pi^2/6
@@ -72,37 +75,15 @@ integrate(function(x) log(1/2 - x) * log(1/2 + x), 0, 1/2)
 1 - pi^2/12
 
 
-### Higher Power
-
-# TODO: move below;
-
-###
-integrate(\(x) log(x) * log((1 - x) / (1 + x)) / x, 0, 1)
-7/4 * pracma::zeta(3)
-
-###
-integrate(\(x) log(x)^2 * log((1 - x) / (1 + x)) / x, 0, 1, rel.tol = 1E-8)
-- 15/4 * pracma::zeta(4)
-
-###
-integrate(\(x) log(x)^3 * log((1 - x) / (1 + x)) / x, 0, 1, rel.tol = 1E-8)
-93/8 * pracma::zeta(5)
-
-###
-integrate(\(x) log(x)^4 * log((1 - x) / (1 + x)) / x, 0, 1, rel.tol = 1E-8)
-- 189/4 * pracma::zeta(6)
-
-
-### Variants:
-# =>
+### Base: Pow = 1
 integrate(\(x) log(x) * log(1 - x) / x, 0, 1)
 pracma::zeta(3)
-#
+# =>
 integrate(\(x) log(x) * log(1 + x) / x, 0, 1)
 -3/4 * pracma::zeta(3)
 
 
-### Higher Powers
+### Higher Power
 
 # =>
 integrate(\(x) log(x)^3 * log(1 - x) / x, 0, 1)
@@ -123,7 +104,7 @@ integrate(\(x) abs(log(x))^p * log(1 - x^n) / x, 0, 1, rel.tol=1E-8)
 - gamma(p+1) * pracma::zeta(p+2) / n^(p+1)
 
 
-### I( |log(x)|^p * log(1 + x) / x )
+### I( |log(x)|^p * log(1 + x^n) / x )
 # workout of log(1 - x^2) =>
 p = sqrt(7)
 integrate(\(x) abs(log(x))^p * log(1 + x) / x, 0, 1, rel.tol=1E-8)
@@ -139,6 +120,26 @@ gamma(p+1) * pracma::zeta(p+2) * (1 - 1/3^(p+1))
 #
 integrate(\(x) abs(log(x))^p * log(1 - x + x^2) / x, 0, 1, rel.tol=1E-8)
 - gamma(p+1) * pracma::zeta(p+2) * (1 - 1/2^(p+1)) * (1 - 1/3^(p+1))
+
+
+### Variants:
+# I( log(x)^s * log((1-x)/(1+x) / x )
+
+###
+integrate(\(x) log(x) * log((1 - x) / (1 + x)) / x, 0, 1)
+7/4 * pracma::zeta(3)
+
+###
+integrate(\(x) log(x)^2 * log((1 - x) / (1 + x)) / x, 0, 1, rel.tol = 1E-8)
+- 15/4 * pracma::zeta(4)
+
+###
+integrate(\(x) log(x)^3 * log((1 - x) / (1 + x)) / x, 0, 1, rel.tol = 1E-8)
+93/8 * pracma::zeta(5)
+
+###
+integrate(\(x) log(x)^4 * log((1 - x) / (1 + x)) / x, 0, 1, rel.tol = 1E-8)
+- 189/4 * pracma::zeta(6)
 
 
 ### Simple:
