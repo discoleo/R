54f35dc15806416043d8f5c00f623796b88199a8
discoleo <leo.mada@syonic.eu>
2023-07-20 23:59:48 +0300

    I( x^p * log(x) * sin(x^n) ) on [0, Inf];

--- Math/Integrals.Trig.Fresnel.R
+++ Math/Integrals.Trig.Fresnel.R
@@ -18,7 +18,7 @@
 
 # I( sin(k*x^n) ) on [0, Inf]
 # much faster than naive pracma::integral;
-fresnel = function(n, iter=10000, k=1, doWeights = TRUE) {
+fresnel = function(n, iter=10000, k=1, p=0, doWeights = TRUE) {
 	id  = seq(1, iter);
 	id0 = c(0, id);
 	ninv = 1/n;
@@ -26,14 +26,21 @@ fresnel = function(n, iter=10000, k=1, doWeights = TRUE) {
 	limI = ((2*pi)*id0)^ninv;
 	int = sapply(id, function(iStart) {
 		lim = limI[c(iStart, iStart + 1)];
-		pracma::integral(function(x) sin(k*x^n), lim[1], lim[2]);
+		if(p == 0) {
+			pracma::integral(function(x) sin(k*x^n), lim[1], lim[2]);
+		} else {
+			pracma::integral(function(x) x^p * sin(k*x^n), lim[1], lim[2]);
+		}
 	})
-	if(doWeights) {
+	if(doWeights && p == 0) {
 		# w = (1 - id/iter^2);
 		# problematic for n > 2, but better for n = 2;
 		w = (1 - rev(1/limI[-1])/(n^3 - 13/2));
 		# w = (1 - 1/rev(limI[-1]*iter));
 		w = w / mean(w);
+		# Gauss: does not work (on 2*pi);
+		# xn = (head(limI, -1) + tail(limI, -1)) / 2;
+		# w = 2 / (n^2 * xn^(2*n-2) * (1 - xn^2) * (1 - int^2));
 		int = sum(w*int);
 		
 	} else int = sum(int);
@@ -128,4 +135,34 @@ k = 3
 # presumably correct:
 fresnel(n, k=k)
 1/n * sin(pi/(2*n)) * gamma(1/n) / k^(1/n)
+
+
+####################
+####################
+
+### I( x^p * sin(x^n) )
+n = 3
+p = - 3/4; # -n <= p <= 0;
+# very slow!
+# pracma::integral(\(x) x^p * sin(x^n), 0, 1000)
+fresnel(n=n, p=p, k=1)
+1/n * sin(pi*(p+1)/(2*n)) * gamma((p+1)/n)
+
+
+### I( x^p * log(x) * sin(x^n) )
+n = 3
+p = - 3/4; # -n <= p <= 0;
+# Upper = Inf: very slow!
+pracma::integral(\(x) x^p * log(x) * sin(x^n), 0, 100)
+gamma((p+1)/n)* (digamma((p+1)/n) * sin(pi*(p+1)/(2*n)) +
+	pi/2 * cos(pi*(p+1)/(2*n))) / n^2
+
+
+###
+n = 3
+p = - 2.5; # -n <= p <= 0;
+# Upper = Inf: very slow!
+pracma::integral(\(x) x^p * log(x) * sin(x^n), 0, 100)
+gamma((p+1)/n)* (digamma((p+1)/n) * sin(pi*(p+1)/(2*n)) +
+	pi/2 * cos(pi*(p+1)/(2*n))) / n^2
 
