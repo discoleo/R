7bbf5d4bfb5d3217c85d9891f9f80ef1abd0b702
discoleo <leo.mada@syonic.eu>
2023-07-22 08:45:28 +0300

    I( x^p * atan(x^n) * log(x) ) on [0, 1];

--- Math/Integrals.Trig.Tan.R
+++ Math/Integrals.Trig.Tan.R
@@ -29,3 +29,40 @@ integrate(\(x) x^3 * tan(x)^2, 0, pi/4)
 (pi/4)^3 - 1/4*(pi/4)^4 + 63/64 * zeta(3) +
 	+ 3/32 * pi^2 * log(2) - 3/4 * pi * Catalan;
 
+
+###
+integrate(\(x) tan(x) / x, 0, pi/4)
+# TODO: ???
+
+
+###
+integrate(\(x) x / atan(x), 0, 1)
+integrate(\(x) (tan(x) + tan(x)^3)/x, 0, pi/4)
+
+# TODO: ???
+
+
+#######################
+#######################
+
+### I( atan(x^n) )
+n = sqrt(5)
+integrate(\(x) atan(x^n), 0, 1)
+pi/4 - integrate(\(x) n*x^n / (x^(2*n) + 1), 0, 1)$value
+pi/4 - (digamma(((n+1)/(2*n) + 1)/2) - digamma((n+1)/(4*n))) / 4
+
+
+### I( x^p * atan(x^n) )
+n = sqrt(5); p = sqrt(7);
+integrate(\(x) x^p * atan(x^n), 0, 1)
+pi/(4*(p+1)) - integrate(\(x) n/(p+1) * x^(n+p) / (x^(2*n) + 1), 0, 1)$value
+pi/(4*(p+1)) - (digamma(((n+p+1)/(2*n) + 1)/2) - digamma((n+p+1)/(4*n))) / (4*(p+1))
+pi/(4*(p+1)) - (digamma(((p+1)/n + 3)/4) - digamma(((p+1)/n + 1)/4)) / (4*(p+1))
+
+
+### I( x^p * atan(x^n) * log(x) )
+n = sqrt(5); p = sqrt(3);
+integrate(\(x) x^p * atan(x^n) * log(x), 0, 1)
+- pi/(4*(p+1)^2) + (digamma(((p+1)/n + 3)/4) - digamma(((p+1)/n + 1)/4)) / (4*(p+1)^2) +
+	- (pracma::psi(1, ((p+1)/n + 3)/4) - pracma::psi(1, ((p+1)/n + 1)/4)) / (16*n*(p+1))
+
