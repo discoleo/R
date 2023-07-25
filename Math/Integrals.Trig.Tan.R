bf4d74ec63b1854e07ec0c719cf50c67a050647f
discoleo <leo.mada@syonic.eu>
2023-07-25 03:25:36 +0300

    "Ahmed's Integral" on [0, 1];

--- Math/Integrals.Trig.Tan.R
+++ Math/Integrals.Trig.Tan.R
@@ -68,7 +68,89 @@ Catalan / n;
 
 ### I( x^p * atan(x^n) * log(x) )
 n = sqrt(5); p = sqrt(3);
+# works only for n > 0;
 integrate(\(x) x^p * atan(x^n) * log(x), 0, 1)
 - pi/(4*(p+1)^2) + (digamma(((p+1)/n + 3)/4) - digamma(((p+1)/n + 1)/4)) / (4*(p+1)^2) +
 	- (pracma::psi(1, ((p+1)/n + 3)/4) - pracma::psi(1, ((p+1)/n + 1)/4)) / (16*n*(p+1))
+
+
+########################
+########################
+
+### ATAN-Fractions
+
+
+### I( atan(sqrt(x^2 + b^2)) / ((x^2 + 1) * sqrt(x^2 + b^2)) )
+# Maths 505: Ahmed's monster integral: solution using Feynman's technique
+# https://www.youtube.com/watch?v=Rq-bYzyoSwU
+
+
+###
+b = sqrt(5)
+integrate(\(x) (2*x^2 + b^2) * atan(sqrt(x^2 + b^2)) /
+	((x^2 + 1) * (x^2 + b^2-1) * sqrt(x^2 + b^2)), 0, 1)
+pi/4 * (3/2*pi + atan(sqrt(b^2 - 1)) - 2*atan(sqrt((b^2+1)/(b^2-1))) +
+	- 2*atan(sqrt((b^2+1)*(b^2-1)))) / sqrt(b^2 - 1)
+
+# Note: the I() collapses for b^2 - 1 == 1 to:
+integrate(\(x) atan(sqrt(x^2 + 2)) / ((x^2 + 1) * sqrt(x^2 + 2)), 0, 1)
+pi^2/8 * (7/4 - 4/3)
+
+
+###
+b = sqrt(5)
+integrate(\(x) atan(sqrt(x^2 + b^2)) / ((x^2 + 1) * sqrt(x^2 + b^2)), 0, 1)
+pi/4 * (3/2*pi + atan(sqrt(b^2 - 1)) - 2*atan(sqrt((b^2+1)/(b^2-1))) +
+	- 2*atan(sqrt((b^2+1)*(b^2-1)))) / sqrt(b^2 - 1) +
+	- integrate(\(x) atan(sqrt(x^2 + b^2)) / ((x^2 + b^2-1) * sqrt(x^2 + b^2)), 0, 1)$value
+
+### [redundant]
+integrate(\(x) atan(sqrt(x^2 + b^2)) / ((x^2 + b^2-1) * sqrt(x^2 + b^2)), 0, 1)
+pi/4*(3/2*pi + atan(sqrt(b^2-1)) - 2*atan(sqrt((b^2+1)*(b^2-1))) +
+	- 2*atan(sqrt((b^2+1)/(b^2-1)))) / sqrt(b^2 - 1) +
+	- integrate(\(x) atan(sqrt(x^2 + b^2)) / ((x^2 + 1) * sqrt(x^2 + b^2)), 0, 1)$value
+
+
+# D(I): 1st I
+# I( atan(t * sqrt(x^2 + b^2)) / ((x^2 + 1) * sqrt(x^2 + b^2)) ) on [0,1]
+pi/4 / ((b^2-1)*t^2 + 1) +
+	- t * atan(t / sqrt(b^2*t^2 + 1)) / sqrt(b^2*t^2 + 1) / ((b^2-1)*t^2 + 1)
+# t => 1/t; dt => - dt / t^2;
+# atan(1 / sqrt(t^2 + b^2)) / sqrt(t^2 + b^2) / (t^2 + (b^2-1))
+# (pi/2 - atan(sqrt(t^2 + b^2))) / sqrt(t^2 + b^2) / (t^2 + (b^2-1))
+
+# D(I): 2nd I
+# I( atan(t * sqrt(x^2 + b^2)) / ((x^2 + b^2-1) * sqrt(x^2 + b^2)) ) on [0,1]
+# I: 1 / ((x^2 + b^2-1) * (t^2*x^2 + t^2*b^2 + 1))
+# ( 1 / (x^2 + b^2-1) - t^2 / (t^2*x^2 + t^2*b^2 + 1) ) / (t^2 + 1)
+(pi/2 - atan(sqrt(b^2-1))) / ((t^2 + 1)*sqrt(b^2 - 1)) +
+	- t * atan(t / sqrt(b^2*t^2 + 1)) / sqrt(b^2*t^2 + 1) / (t^2 + 1)
+
+
+### Helper:
+b = 1/sqrt(5)
+b = sqrt(5)
+integrate(\(x) 1 / ((x^2+1) * sqrt(x^2 + b^2)), 0, 1)
+integrate(\(x) 1 / (x^2 + b^2 - 1), sqrt(b^2+1), Inf)
+# b^2 > 1
+pi/2/sqrt(b^2-1) - atan(sqrt((b^2+1)/(b^2-1)))/sqrt(b^2-1)
+# b^2 < 1
+(log(sqrt(b^2+1) + sqrt(1 - b^2)) - log(b) - log(2)/2) / sqrt(1 - b^2)
+
+#
+integrate(\(x) 1 / ((x^2 + b^2-1) * sqrt(x^2 + b^2)), 0, 1)
+integrate(\(x) x / (((b^2-1)*x^2 + 1) * sqrt(b^2*x^2 + 1)), 1, Inf)
+integrate(\(x) 1 / ((b^2-1)*x^2 + 1), sqrt(b^2+1), Inf)
+# b^2 > 1
+pi/2/sqrt(b^2-1) - atan(sqrt((b^2+1)*(b^2-1)))/sqrt(b^2-1)
+
+
+###
+b = sqrt(5)
+integrate(\(x) atan(sqrt(x^2 + b^2)) / ((x^2 + 1) * sqrt(x^2 + b^2)), 0, 1)
+
+# D(b)
+# I(  atan(sqrt(x^2 + b^2)) * sqrt(x^2 + b^2) / (x^2 + 1) )
+# b / ((x^2 + 1) * (x^2 + b^2+1)) + b * atan(sqrt(x^2 + b^2)) / sqrt(x^2 + b^2) / (x^2 + 1)
+# ???
 
