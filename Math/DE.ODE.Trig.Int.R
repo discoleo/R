fd521e24f43414d45febd88593bae6bfef1e90a3
discoleo <leo.mada@syonic.eu>
2023-08-05 07:27:02 +0300

    Proof of concept: from sin(k * x^3/2) / ...;

--- Math/DE.ODE.Trig.Int.R
+++ Math/DE.ODE.Trig.Int.R
@@ -113,3 +113,42 @@ plot(sol[, 1:2], type="l", col="green")
 y = sapply(x, \(k) Ipk(k, lim=lim))
 lines(x, y, col="red", lty=2)
 
+
+######################
+
+### y = I( sin(k*z^(3/2)) / (z^3 + 1) )
+# d2y = y + ???
+# TODO: solve vs proof of concept;
+
+Ipk = function(k, lim=pi/2) integrate(\(x) sin(k*x^(3/2)) / (x^3 + 1), 0, lim)$value;
+
+Ip = function(x, y, pars) {
+	int = integrate(\(z) sin(x*z^(3/2)), 0, pars$lim)$value;
+	d2y = y[1] - int;
+	list(c(y[2], d2y));
+}
+
+# TODO: complicated;
+# upper = Inf may work;
+
+
+###
+lim = 5/4 * pi;
+k.start = 0.1; k.end = 1;
+x = seq(k.start, k.end, by = 0.01)
+
+sol <- bvpshoot(
+	yini = c(Ipk(k.start, lim=lim), NA),
+	yend = c(Ipk(k.end, lim=lim), NA),
+	x = x, func = Ip, guess = 0, parms = list(lim=lim))
+
+### Test
+
+plot(sol)
+
+# perfect match
+par(mfrow = c(1, 1))
+plot(sol[, 1:2], type="l", col="green")
+y = sapply(x, \(k) Ipk(k, lim=lim))
+lines(x, y, col="red", lty=2)
+
