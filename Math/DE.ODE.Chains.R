cc7a440eceeb5a71d39a99be5fc7c3896c7adc58
discoleo <leo.mada@syonic.eu>
2023-08-17 16:25:44 +0300

    [idea] d2y = y + sum(b[i] * exp(k[i] * x));

--- Math/DE.ODE.Chains.R
+++ Math/DE.ODE.Chains.R
@@ -31,3 +31,22 @@
 # ODE: d2y = y + b * exp(x);
 
 # TODO: check & expand;
+
+
+#############
+
+### d2y = y + sum(b[i] * exp(k[i] * x))
+
+# y = y0 + b1/(k1^2 - 1) * exp(k1*x) + ...
+# where k[i] != 1;
+# y0 satisfies the fundamental solution;
+
+# =>
+# d2y = d2y0 + b1*k1^2/(k1^2 - 1) * exp(k1*x) + ...
+# d2y = y0 + b1/(k1^2 - 1) * exp(k1*x) + b1*exp(k1*x) + ...
+# ODE: d2y = y + b1*exp(k1*x) + ...
+
+### Examples
+
+# TODO
+
