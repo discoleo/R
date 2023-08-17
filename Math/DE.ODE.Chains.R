e8161e3d395cb6ef0f06fdc00bd031119ff437a3
discoleo <leo.mada@syonic.eu>
2023-08-17 18:51:50 +0300

    Chain: d2y = y + be * x^2 * exp(ke * x);

--- Math/DE.ODE.Chains.R
+++ Math/DE.ODE.Chains.R
@@ -86,3 +86,66 @@ plot(sol[, 1:2], type="l", col="green")
 y = sapply(x, \(x) Iyk(x, k=k, b=b))
 lines(x, y, col="red", lty=2)
 
+
+#############
+
+### d2y = y + be * x^2 * exp(ke * x)
+
+# y = y0 + be/(ke^2 - 1) * x^2 * exp(ke*x) +
+#     - 4*be*ke / (ke^2 - 1)^2 * x * exp(ke*x) +
+#     - 2*be*(1 / (ke^2 - 1)^2 - 4*ke^2 / (ke^2 - 1)^3) * exp(ke*x);
+# where ke[i] != 1;
+# y0 satisfies the fundamental solution;
+
+# =>
+# d2y # =
+d2y0 + be * x^2 * exp(ke*x) + be/(ke^2 - 1) * x^2 * exp(ke*x) +
+	- 4*be*ke / (ke^2 - 1)^2 * x * exp(ke*x) +
+	- 2*be*(1 / (ke^2 - 1) - 4*ke^2 / (ke^2 - 1)^2) * exp(ke*x);
+# d2y = y + be * x^2 * exp(ke*x);
+
+
+### Examples
+
+# F.S.: trivial y0 = 0;
+# - includes also previous example;
+
+Iyk = function(x, ke, be, k, b) {
+	div = (ke^2 - 1);
+	be/div * x^2 * exp(ke*x) +
+		- 4*be*ke / div^2 * x * exp(ke*x) +
+		- 2*be*(1 / div^2 - 4*ke^2 / div^3) * exp(ke*x) +
+	+ sum(b / (k^2 - 1) * exp(k*x));
+}
+
+Idny = function(x, y, parms) {
+	b = parms$b; k = parms$k;
+	be = parms$be; ke = parms$ke;
+	d2y = y[1] + be * x^2 * exp(ke*x) + sum(b * exp(k*x));
+	list(c(y[2], d2y));
+}
+
+
+###
+b = c(2, -3, -1/5, 1/7)
+k = c(1/2,1/3, 2, 2.5)
+ke = -1/2; be = 3/2;
+x.start = 0; x.end = 2.5;
+x = seq(x.start, x.end, by = 0.01)
+
+sol <- bvpshoot(
+	yini = c(Iyk(x.start, ke=ke, be=be, k=k, b=b), NA),
+	yend = c(Iyk(x.end,   ke=ke, be=be, k=k, b=b), NA),
+	x = x, func = Idny, guess = 0,
+	parms = list(ke=ke, be=be, k=k, b=b))
+
+### Test
+
+plot(sol)
+
+# perfect match
+par(mfrow = c(1, 1))
+plot(sol[, 1:2], type="l", col="green")
+y = sapply(x, \(x) Iyk(x, ke=ke, be=be, k=k, b=b))
+lines(x, y, col="red", lty=2)
+
