0ba7d1401181fb33746e43a3daf0da0c5e788d70
discoleo <leo.mada@syonic.eu>
2023-08-17 16:59:07 +0300

    [Example] d2y = y + sum(b[i] * exp(k[i] * x));

--- Math/DE.ODE.Chains.R
+++ Math/DE.ODE.Chains.R
@@ -4,6 +4,12 @@
 ### ODE: Chains ###
 ###################
 
+### Helper Functions
+
+library(bvpSolve)
+
+
+#########################
 
 ### Fundamental Solution:
 ### d2y = y
@@ -48,5 +54,35 @@
 
 ### Examples
 
-# TODO
+# F.S.: trivial y0 = 0;
+
+Iyk = function(x, k, b) sum(b / (k^2 - 1) * exp(k*x));
+
+Idny = function(x, y, parms) {
+	b = parms$b; k = parms$k;
+	d2y = y[1] + sum(b * exp(k*x));
+	list(c(y[2], d2y));
+}
+
+
+###
+b = c(2, -3, -1/5, 1/7)
+k = c(1/2,1/3, 2, 2.5)
+x.start = 0; x.end = 1.5;
+x = seq(x.start, x.end, by = 0.01)
+
+sol <- bvpshoot(
+	yini = c(Iyk(x.start, k=k, b=b), NA),
+	yend = c(Iyk(x.end,   k=k, b=b), NA),
+	x = x, func = Idny, guess = 0, parms = list(k=k, b=b))
+
+### Test
+
+plot(sol)
+
+# perfect match
+par(mfrow = c(1, 1))
+plot(sol[, 1:2], type="l", col="green")
+y = sapply(x, \(x) Iyk(x, k=k, b=b))
+lines(x, y, col="red", lty=2)
 
