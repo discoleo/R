bd41e9f29d496ffac34721235c40390f61b7534c
discoleo <leo.mada@syonic.eu>
2023-08-19 12:40:19 +0300

    [explore] I( 1 / (x - sin(x)) ) on [0, Inf];

--- Math/Integrals.Trig.Fractions.Mixed.R
+++ Math/Integrals.Trig.Fractions.Mixed.R
@@ -2,6 +2,11 @@
 
 ### Trig: Mixed Fractions
 
+int = function(FUN, upper, lower, rel.tol=1E-8, subdivisions=4097, ...) {
+	r = integrate(FUN, upper, lower, subdivisions=subdivisions, rel.tol=rel.tol, ...);
+	cat(paste0("Err = ", r$abs.error, "; subd = ", r$subdivisions, "\n"));
+	return(r$value);
+}
 
 ###############
 
@@ -9,7 +14,7 @@
 # Fails:
 integrate(\(x) 1 / (x - sin(x)) - 6/x^3 - 3/10/x, 0, 1)
 # pracma: did not finish for > 5 minutes
-# pracma::integral(\(x) 1 / (x - sin(x)) - 6/x^3 - 3/10/x, 0.1, 1)
+# pracma::integral(\(x) 1 / (x - sin(x)) - 6/x^3 - 3/10/x, 0, 1)
 
 # Interval split:
 mid = 0.0075; up = 1;
@@ -20,6 +25,26 @@ integrate(\(x) 1 / (x - sin(x)) - 6/x^3 - 3/10/x, mid, 10*mid, rel.tol=1E-7, sub
 # Note:
 # Base Int correction: - 3/x^2 + 3/10*log(x);
 # - Case: up = 1 => - 3;
+
+# TODO: ???
+
+
+###############
+
+### on [0, Inf]
+FUN = \(x) 1 / (x - sin(x)) - 6/x^3 - 3/10/x - 7/10/(x+1);
+mid = 0.01;
+int(FUN, mid, 1, rel.tol=1E-7, subdiv=8025) +
+int(FUN, 1, 1E+5, rel.tol=1E-7, subdiv=8025) +
+int(FUN, 1E+5, 1E+6, rel.tol=1E-6, subd=1025) +
+int(FUN, 1E+6, 5E+6, rel.tol=1E-6, subd=1025) +
++ 11/2800 * mid^2 + 17/(4*126000) * mid^4;
+
+# TODO: ???
+
+
+###
+integrate(\(x) 1 / (x - sin(pi/2*x)) - 1/(1-pi/2)/x - 1/(x-1), 0, 1)
 
 # TODO: ???
 
