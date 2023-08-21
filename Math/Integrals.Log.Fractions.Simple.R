6ad9d8b9e7a512827d7052e983f6baf9a0d77dd7
discoleo <leo.mada@syonic.eu>
2023-08-21 23:09:25 +0300

    [refactor] moved Log-Radicals to new file;

--- Math/Integrals.Log.Fractions.Simple.R
+++ Math/Integrals.Log.Fractions.Simple.R
@@ -6,8 +6,17 @@
 ### Integrals: Logarithms
 ### Simple Fractions
 ###
-### draft v.0.2e
+### draft v.0.2f
 
+
+
+### History
+
+# - [refactor] moved Radicals to new file:
+#   Integrals.Log.Fractions.Radicals.R;
+
+
+####################
 
 ### Helper Functions
 
@@ -47,7 +56,7 @@ log(2) - n * int.FrU01(n, p = n)
 ###
 n = sqrt(11);
 p = sqrt(3);
-integrate(function(x) x^p * log(x^n + 1), 0, 1)
+integrate(\(x) x^p * log(x^n + 1), 0, 1)
 - (digamma((p+1)/(2*n) + 1) - digamma((p+1)/n + 1)) / (p+1);
 
 
@@ -65,7 +74,7 @@ integrate(\(x) log(1 - x^n), 0, 1)
 ###
 n = sqrt(11);
 p = sqrt(3);
-integrate(function(x) x^p * log(1 - x^n), 0, 1)
+integrate(\(x) x^p * log(1 - x^n), 0, 1)
 - (digamma((n+p+1)/n) + Euler) / (p+1);
 
 
@@ -136,7 +145,7 @@ gamma(s+1) * pracma::zeta(s)
 
 
 ### x-Pow = 3
-s = 3; p = 3;
+s = 3;
 integrate(\(x) log(1 - x)^3 / x^3, 0, 1, rel.tol=1E-8)
 - gamma(4) * (pracma::zeta(3) + pracma::zeta(2)) / 2
 
@@ -148,6 +157,19 @@ gamma(5) * (pracma::zeta(4) + pracma::zeta(3)) / 2
 s = sqrt(11)
 integrate(\(x) abs(log(1 - x))^s / x^3, 0, 1, rel.tol=1E-8)
 gamma(s+1) * (pracma::zeta(s) + pracma::zeta(s-1)) / 2
+
+
+### x-Pow = 4
+s = sqrt(13)
+integrate(\(x) abs(log(1 - x))^s / x^4, 0, 1, rel.tol=1E-8)
+gamma(s+1) * (pracma::zeta(s) + 3/2*pracma::zeta(s-1) + pracma::zeta(s-2)/2) / 3
+
+# Derivation:
+integrate(\(x) s/3 * abs(log(1 - x))^(s-1) / x^3 / (1-x) +
+	- s/3 * abs(log(1 - x))^(s-1)/(1-x), 0, 1, rel.tol=1E-8)
+integrate(\(x) s/3 * abs(log(1 - x))^(s-1) * (1/x^3 + 1/x^2 + 1/x + 1/(1-x)) +
+	- s/3 * abs(log(1 - x))^(s-1)/(1-x), 0, 1, rel.tol=1E-8)
+integrate(\(x) s/3 * abs(log(1 - x))^(s-1) * (1/x^3 + 1/x^2 + 1/x), 0, 1, rel.tol=1E-8)
 
 
 ########################
@@ -378,90 +400,9 @@ integrate(function(x) log(x)/(x+1)^6, 0, 1)
 - log(2)/5 + (1/2 + 1/4/2 + 1/8/3 + 1/16/4 - 1 - 1/2 - 1/3 - 1/4)/5
 
 
-### I( log(x) * sqrt(x + 1) )
-integrate(\(x) log(x) * sqrt(x + 1), 0, 1)
-- 2/3 * (2*log(2) - 2*asinh(1) + 2*sqrt(2) - 2 + 2/3 * (2^(3/2) - 1))
-4/3 * (asinh(1) - log(2) - (1 + 2/3)*sqrt(2) + 1 + 1/3)
-
-# Derivation:
-integrate(\(x) (sqrt(x + 1) - 1)/x, 0, 1)
-2*log(2) - 2*asinh(1) + 2*sqrt(2) - 2
-#
-integrate(\(x) ((x+1)*sqrt(x + 1) - 1)/x, 0, 1)
-2*log(2) - 2*asinh(1) + 2*sqrt(2) - 2 + 2/3 * (2^(3/2) - 1)
-
-
-### I( log(x) / sqrt(x + 1) )
-integrate(\(x) log(x) / sqrt(x + 1), 0, 1)
-4 * (asinh(1) - log(2) - sqrt(2) + 1)
-
-
-### Power 1/3
-
-### I( log(x) * (x + 1)^(1/3) )
-integrate(\(x) log(x) * (x + 1)^(1/3), 0, 1)
--9/16*(2^(4/3) - 1 + 4*2^(1/3) - 4) + 9/8*log(2^(2/3) + 2^(1/3) + 1) - 9/8*log(3) +
-	+ 9/8 / sin(pi/3) * atan((2^(1/3) + cos(pi/3)) / sin(pi/3)) +
-	- 9/8 / sin(pi/3) * atan((1 + cos(pi/3)) / sin(pi/3));
-
-### I( log(x) * (x + 1)^(2/3) )
-integrate(\(x) log(x) * (x + 1)^(2/3), 0, 1)
-integrate(\(x) - 3/5 * ((x + 1)^(5/3) - 1) / x, 0, 1)
--9*(2^(5/3)/25 + 2^(2/3)/10) + 9*(1/25 + 1/10) +
-	+ 9/10 * log(2^(2/3) + 2^(1/3) + 1) - 9/10*log(3) +
-	- 9/10 / sin(pi/3) * atan((2^(1/3) + cos(pi/3)) / sin(pi/3)) +
-	+ 9/10 / sin(pi/3) * atan((1 + cos(pi/3)) / sin(pi/3));
-
-
-### I( log(x) / (x + 1)^(1/3) )
-integrate(\(x) log(x) / (x + 1)^(1/3), 0, 1)
-5/2 * integrate(\(x) log(x) * (x + 1)^(2/3), 0, 1)$value +
-	+ 3*2*2^(2/3)*(1/2 - 1/5) - 3*(1/2 - 1/5);
-# see above for I( log(x) * (x+1)^(2/3) );
-
-
-### I( log(x) / (x + 1)^(2/3) )
-integrate(\(x) log(x) / (x + 1)^(2/3), 0, 1)
-4 * integrate(\(x) log(x) * (x + 1)^(1/3), 0, 1)$value +
-	+ 3*2*2^(1/3)*(1 - 1/4) - 3*(1 - 1/4);
-# see above for I( log(x) * (x+1)^(1/3) );
-
-# Derivations:
-integrate(\(x) - 3/4 * ((x+1)^(4/3) - 1) / x, 0, 1)
-integrate(\(x) - 9/4 * (x^6 - x^2) / (x^3 - 1), 1, 2^(1/3))
--9/16*(2^(4/3) - 1) + integrate(\(x) - 9/4 * (x^3 - x^2) / (x^3 - 1), 1, 2^(1/3))$value
--9/16*(2^(4/3) - 1 + 4*2^(1/3) - 4) +
-	+ integrate(\(x) 9/4 * (x + 1) / (x^2 + x + 1), 1, 2^(1/3))$value
--9/16*(2^(4/3) - 1 + 4*2^(1/3) - 4) +
-	+ integrate(\(x) 9/4 * (1/2*(2*x+1) + 1/2) / (x^2 + x + 1), 1, 2^(1/3))$value;
-	# + 9/8 / sin(pi/3) * atan((x + cos(pi/3)) / sin(pi/3))
-	# + integrate(\(x) 9/8 / (x^2 + x + 1), 1, 2^(1/3))$value
-
-#
-integrate(\(x) - 3/5 * ((x + 1)^(5/3) - 1) / x, 0, 1)
-integrate(\(x) - 9/5 * (x^7 - x^2)/ (x^3 - 1), 1, 2^(1/3))
--9/25 * (2^(5/3) - 1) - 9/10*(2^(2/3) - 1) +
-	+ integrate(\(x) 9/5 * x / (x^2 + x + 1), 1, 2^(1/3))$value
--9*(2^(5/3)/25 + 2^(2/3)/10) + 9*(1/25 + 1/10) +
-	+ 9/10 * (log(2^(2/3) + 2^(1/3) + 1) - log(3)) +
-	- 9/10 / sin(pi/3) * atan((2^(1/3) + cos(pi/3)) / sin(pi/3)) +
-	+ 9/10 / sin(pi/3) * atan((1 + cos(pi/3)) / sin(pi/3));
-	# + integrate(\(x) 9/5 * x / (x^2 + x + 1), 1, 2^(1/3))$value
-
-
-# Derivation:
-integrate(\(x) ((x+1)^(1/3) - 1)/x, 0, 1)
-integrate(\(x) 3*(x^3 - x^2) / (x^3 - 1), 1, 2^(1/3))
-#
-integrate(\(x) ((x+1)^(4/3) - 1)/x, 0, 1)
-integrate(\(x) 3*(x^6 - x^2) / (x^3 - 1), 1, 2^(1/3))
-
-#
-integrate(\(x) ((x+1)^(2/3) - 1)/x, 0, 1)
-integrate(\(x) 3*(x^4 - x^2) / (x^3 - 1), 1, 2^(1/3))
-#
-integrate(\(x) ((x+1)^(5/3) - 1)/x, 0, 1)
-integrate(\(x) 3*(x^7 - x^2) / (x^3 - 1), 1, 2^(1/3))
+### Radicals
+# - have been moved to file:
+#   Integrals.Log.Fractions.Radicals.R
 
 
 ##################
