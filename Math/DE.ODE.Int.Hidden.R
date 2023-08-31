cfbff97aeb1a85cf98fd177dfcfdd8d207865a6d
discoleo <leo.mada@syonic.eu>
2023-09-01 00:20:16 +0300

    [checked] y = x^p * I( (z^n + 1)^k dz ) + F(x);

--- Math/DE.ODE.Int.Hidden.R
+++ Math/DE.ODE.Int.Hidden.R
@@ -108,7 +108,68 @@ x*dy - p*y - x^(p+1) * log(x^n + 1) / (x + 1) + p*f - x*df # = 0
 
 ### y = x^p * I( (z^n + 1)^k dz ) + F(x)
 
-# TODO
+### D =>
+# [not run]
+# dy = p*x^(p-1) * I(...) + x^p * (x^n + 1)^k + df;
+# dy = p*(y - f)/x + x^p * (x^n + 1)^k + df;
+x*dy - p*y - x^(p+1) * (x^n + 1)^k + p*f - x*df # = 0
+
+### D2 =>
+x*d2y - (p-1)*dy - (n*k*x^n/(x^n + 1) + p+1)*x^p * (x^n + 1)^k + (p-1)*df - x*d2f # = 0
+x^2*d2y - (p-1)*x*dy - (n*k*x^n/(x^n + 1) + p+1) * (x*dy - p*y + p*f - x*df) +
+	+ (p-1)*x*df - x^2*d2f # = 0
+### ODE:
+x^2*d2y - (n*k*x^n/(x^n + 1) + 2*p)*x*dy + p*(n*k*x^n/(x^n + 1) + p+1)*y +
+	- p*(n*k*x^n/(x^n + 1) + p+1)*f + (n*k*x^n/(x^n + 1) + 2*p)*x*df - x^2*d2f # = 0
+
+
+### Tests
+
+# F(x) = - x/3 - x^(1/2) + 1;
+
+Ipk = function(x, parms) {
+	n = parms$n; p = parms$p; k = parms$k; up = x;
+	- 1/3 * x - x^(1/2) + 1 +
+		x^p * integrate(\(x) (x^n + 1)^k, 0, up)$value;
+}
+dyf = function(x, parms) {
+	n = parms$n; p = parms$p; k = parms$k;
+	f = - 1/3 * x - x^(1/2) + 1; df = - 1/3 - 1/(2*x^(1/2));
+	y = Ipk(x, parms);
+	dy = p*y + x^(p+1) * (x^n + 1)^k - p*f + x*df;
+	return(dy);
+}
+
+Ip = function(x, y, parms) {
+	n = parms$n; p = parms$p; k = parms$k;
+	dy = y[2]; y = y[1];
+	f  = - 1/3 * x - x^(1/2) + 1; df = - 1/3 - 1/(2*x^(1/2)); d2f = 1/(4*x^(3/2));
+	d2y = - (n*k*x^n/(x^n + 1) + 2*p)*x*dy + p*(n*k*x^n/(x^n + 1) + p+1)*y +
+		- p*(n*k*x^n/(x^n + 1) + p+1)*f + (n*k*x^n/(x^n + 1) + 2*p)*x*df - x^2*d2f;
+	d2y = - d2y / x^2;
+	list(c(dy, d2y));
+}
+
+
+###
+parms = list(p = 1/4, k = -3/5, n = 4/3);
+x.start = 0.1; x.end = 14; # x.end = 4;
+x = seq(x.start, x.end, by = 0.01)
+
+sol <- bvpshoot(
+	yini = c(Ipk(x.start, parms), NA),
+	yend = c(Ipk(x.end, parms), NA),
+	x = x, func = Ip, guess = dyf(x.start, parms),
+	parms = parms)
+
+### Test
+
+plot(sol)
+
+# perfect match
+par(mfrow = c(1, 1))
+plot.ode(sol, Ipk, parms = parms)
+
 
 
 ######################
@@ -182,4 +243,18 @@ plot(sol)
 # perfect match
 par(mfrow = c(1, 1))
 plot.ode(sol, Ipk, parms = parms)
+
+
+######################
+
+### y = (x^n + 1)^k1 * I( (z^n + 1)^k2 dz ) + F(x)
+
+### D =>
+# [not run]
+# dy = n*k1*x^(n-1) * (x^n + 1)^(k1-1) * I(...) + (x^n + 1)^(k1 + k2) + df;
+# dy = n*k1*x^(n-1) * (y - f) / (x^n + 1) + (x^n + 1)^(k1 + k2) + df;
+(x^n + 1)*dy - n*k1*x^(n-1)*y - (x^n + 1)^(k1 + k2 + 1) +
+	+ n*k1*x^(n-1)*f - (x^n + 1)*df # = 0
+
+# TODO
 
