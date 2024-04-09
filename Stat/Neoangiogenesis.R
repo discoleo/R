5f50addc6254cb4a0647a6d16ffc1ea311fc0d20
discoleo <leo.mada@syonic.eu>
2024-04-09 14:33:52 +0300

    Neoangiogenesis: Models

---
+++ Stat/Neoangiogenesis.R
@@ -0,0 +1,18 @@
+
+
+### Biological Models
+
+# 1. Cancer Neo-Angiogenesis
+# 2. Syncytiotrophoblast in Pregnancy
+#    e.g. Eclampsia/Preeclampsia, other;
+
+# TODO
+
+
+##############
+
+### References
+
+# 1. Brandeis Math Bio Seminar - Helen Byrne (University of Oxford)
+#    Multiscale approaches for analyzing vascular networks
+#    https://www.youtube.com/watch?v=5HU8m3qoRB8
