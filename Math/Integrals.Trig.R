d0a8ca7d5bc34e41b208e4c1219827b2a29e6154
discoleo <leo.mada@syonic.eu>
2024-04-10 03:33:37 +0300

    Link to Fresnel integral;

--- Math/Integrals.Trig.R
+++ Math/Integrals.Trig.R
@@ -7,11 +7,15 @@
 # Maths 505: An absolutely wonderful integral!
 # https://www.youtube.com/watch?v=xBSxDLQHf3o
 
-# Note: see Mellin Transform
+# Note:
+# - see Mellin Transform;
+# - see also Integrals.Trig.Fresnel.R;
 
 p = 1/sqrt(3)
 # upper = Inf
 integrate(\(x) x^(p-1) * sin(x), 0, 240000, subdivisions = 20024)
+# Fresnel integral behaves very badly!
+integrate(\(x) sin(x^(1/p)) / p, 0, 1000, subdivisions = 20024)
 gamma(p) * sin(pi*p/2)
 
 # upper = Inf
@@ -27,4 +31,12 @@ gamma(p) * sin(pi*p/2) / k^p
 # upper = Inf
 integrate(\(x) x^(p-1) * cos(k*x), 0, 240000, subdivisions = 60024)
 gamma(p) * cos(pi*p/2) / k^p
+
+
+### Transforms:
+p = 1/sqrt(5); k = sqrt(3);
+# upper = Inf; drastic increase in subdivisions!
+integrate(\(x) x^(p-1) * sin(k*x) * log(x), 0, 240000, subdivisions = 60024)
+gamma(p) * sin(pi*p/2) * (digamma(p) - log(k)) / k^p +
+	+ pi/2 * gamma(p) * cos(pi*p/2) / k^p;
 
