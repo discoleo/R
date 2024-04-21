ecb7ebfb3e6add6eabdfa5497241a03383bbd496
discoleo <leo.mada@syonic.eu>
2024-04-21 03:19:14 +0300

    Tetrahedron: new Constructor;

--- Chemistry/Chem3D.R
+++ Chemistry/Chem3D.R
@@ -10,19 +10,72 @@ library(rgl)
 ### Helper
 
 dist.xyz = function(x, y, z) {
+	if(missing(y)) {
+		y = x[,2]; z = x[,3]; x = x[,1];
+	}
 	sqrt(diff(x)^2 + diff(y)^2 + diff(z)^2);
 }
 
 ###############
 
 ### Tetrahedron
+Th4 = function(r = 1, center = c(0,0,0), phi = 0) {
+	# Base Triangle:
+	pB = if(phi == 0) {
+			r2 = - r/2; r3 = - r2 * sqrt(3);
+			cbind(c(r,r2,r2), c(0,r3,-r3), 0);
+		} else {
+			phi3 = c(0, 2*pi/3, 4*pi/3) + phi;
+			x = r * cos(phi3) + center[1];
+			y = r * sin(phi3) + center[2];
+			cbind(x, y, center[3]);
+		}
+	pT = c(0, 0, r * sqrt(2)) + center; # Top
+	pV = rbind(pB, pT);
+	pC = c(0, 0, r/4 * sqrt(2)) + center; # Center
+	return(list(V = pV, C = pC, R = r));
+}
+
+plot.Th4 = function(v, lwd = 2, id.base = NULL,
+		fill.plane = "#A0A0A064", N = 20, alpha = 0.2) {
+	# Lines from Center to Vertices:
+	pV = v$V;
+	pC = v$C;
+	for(i in seq(4)) {
+		tmp = pV[i,];
+		lines3d(c(tmp[1], pC[1]), c(tmp[2], pC[2]), c(tmp[3], pC[3]), lwd=lwd);
+	}
+	# Draw Base-plane:
+	if( ! is.null(id.base)) {
+		# TODO:
+		id = c(1,2,3);
+		pB = as.data.frame(pV[id, ]);
+		names(pB) = c("x","y","z");
+		### Transparency: set with alpha!
+		polygon3d(pB, col = fill.plane, fill = TRUE, alpha = alpha);
+		# fake transparency:
+		trf = function(x, tt) {
+			x1 = tt * x[1] + (1 - tt) * x[2];
+			x2 = tt * x[1] + (1 - tt) * x[3];
+			x  = c(x1, x2);
+		}
+		# Hashlines in Base-Plane:
+		if(N > 0)
+		for(tt in seq(0, N - 1)/N) {
+			x = trf(pB$x, tt); y = trf(pB$y, tt);
+			z = trf(pB$z, tt);
+			lines3d(x, y, z, lwd = lwd, col = fill.plane);
+		}
+	}
+}
 
 ### Basic Tetrahedron
 # Note: transparency works only by specifying alpha!
 Th4.base = function(lwd = 2, fill.plane = "#A0A0A064", N = 20, alpha = 0.2) {
+	# Base Triangle:
 	pB = list(c(1,0,0), c(-1/2, sqrt(3)/2, 0), c(-1/2, -sqrt(3)/2, 0))
-	pT = c(0, 0, sqrt(2));
-	pC = c(0, 0, 1/4 * sqrt(2));
+	pT = c(0, 0, sqrt(2)); # Top
+	pC = c(0, 0, 1/4 * sqrt(2)); # Center
 	# Lines from Center to Vertices:
 	for(i in seq(3)) {
 		tmp = pB[[i]];
