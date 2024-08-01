028fff2b00908fad33c74b8af1138a6f134da4e7
discoleo <leo.mada@syonic.eu>
2024-08-01 20:18:40 +0300

    minor enhancement;

--- Stat/Tools.Files.R
+++ Stat/Tools.Files.R
@@ -3,11 +3,11 @@
 ### Compare 2 Directories
 # - file name & file size;
 # - TODO: option for hash;
-match.dir = function(path1, path2, pattern = NULL) {
+match.dir = function(path1, path2, pattern = NULL, verbose = TRUE) {
 	x1 = list.files(path1, pattern=pattern, no.. = TRUE);
-	cat("Finished Dir 1\n");
+	if(verbose) cat("Finished Dir 1\n");
 	x2 = list.files(path2, pattern=pattern, no.. = TRUE);
-	cat("Finished Dir 2\n");
+	if(verbose) cat("Finished Dir 2\n");
 	f1 = file.size(paste0(path1, "/", x1));
 	f2 = file.size(paste0(path2, "/", x2));
 	s1 = paste0(f1, "\t", x1);
@@ -23,9 +23,9 @@ match.dir = function(path1, path2, pattern = NULL) {
 
 # Difference between 2 dirs
 diff.dir = function(path1, path2, pattern = NULL, swap = FALSE,
-		copy = FALSE) {
+		copy = FALSE, verbose = TRUE) {
 	if(swap) { tmp = path1; path1 = path2; path2 = tmp; }
-	FILES = match.dir(path1, path2, pattern=pattern);
+	FILES = match.dir(path1, path2, pattern=pattern, verbose=verbose);
 	FILES = FILES[is.na(FILES$Match), ];
 	if(copy) {
 		# TODO
