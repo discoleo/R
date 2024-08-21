08e16de0e8a4ce29958634953b65a0b9b167e9f7
discoleo <leo.mada@syonic.eu>
2024-08-21 14:32:08 +0300

    Order 6: more Examples;

--- Math/Polynomials.Class3.R
+++ Math/Polynomials.Class3.R
@@ -1,6 +1,6 @@
 
 ## Polynomials: Class 3
-## All roots: Real
+## Roots: All Real
 
 
 ###################
@@ -39,6 +39,14 @@ x^6 - 8*x^5 + 5*x^4 + 20*x^3 - 20*x^2 + 2*x + 1
 x = r^4 - 2*r^2 + r; poly.calc0(x, digits = 5)
 x^6 - 8*x^5 + 5*x^4 + 46*x^3 + 19*x^2 - 11*x + 1
 
+#
+x = r^4 + r^3 - 2*r^2 - 2*r; poly.calc0(x, digits = 5)
+x^6 - 7*x^5 + 2*x^4 + 33*x^3 + 2*x^2 - 7*x + 1
+
+#
+x = r^4 + r^3 - 2*r^2 - r; poly.calc0(x, digits = 5)
+x^6 - 6*x^5 - 11*x^4 + 6*x^3 + 15*x^2 + 7*x + 1
+
 
 ### Based on cos(2*pi/21)
 id = c(1,2,4,5,8,10)
@@ -56,4 +64,10 @@ x^6 + x^5 - 13*x^4 - 6*x^3 + 15*x^2 + 8*x + 1
 
 x = r^3 + r^2 - r; poly.calc0(x)
 x^6 - 13*x^5 + 29*x^4 + 78*x^3 - 48*x^2 + x + 1
+
+x = r^4 - 2*r^2 + r; poly.calc0(x)
+x^6 - 16*x^5 + 74*x^4 - 57*x^3 - 180*x^2 + 10*x + 1
+
+x = r^4 - r^2 + r - 1; poly.calc0(x)
+x^6 - 23*x^5 + 158*x^4 - 225*x^3 - 579*x^2 - 214*x + 1
 
