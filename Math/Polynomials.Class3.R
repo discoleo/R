04541fafff0bf8912c1ba2fcdd16e67e3f0635c8
discoleo <leo.mada@syonic.eu>
2024-08-21 10:13:17 +0300

    Class 3 Polynomials: Examples

---
+++ Math/Polynomials.Class3.R
@@ -0,0 +1,59 @@
+
+## Polynomials: Class 3
+## All roots: Real
+
+
+###################
+
+### Helper Functions
+
+source("Polynomials.Helper.R")
+
+
+####################
+####################
+
+### Order 6
+# Examples
+
+### Based on cos(2*pi/13)
+r = 2*cos(2*pi*seq(6)/13)
+
+#
+x = r; poly.calc0(x)
+x^6 + x^5 - 5*x^4 - 4*x^3 + 6*x^2 + 3*x - 1
+
+#
+x = r^2 - r; poly.calc0(x)
+x^6 - 12*x^5 + 47*x^4 - 69*x^3 + 32*x^2 + 3*x - 1
+
+#
+x = r^3 - r^2 - r; poly.calc0(x)
+x^6 + 14*x^5 + 47*x^4 + 22*x^3 - 20*x^2 - 10*x - 1
+
+#
+x = r^3 + r^2 - r; poly.calc0(x)
+x^6 - 8*x^5 + 5*x^4 + 20*x^3 - 20*x^2 + 2*x + 1
+
+#
+x = r^4 - 2*r^2 + r; poly.calc0(x, digits = 5)
+x^6 - 8*x^5 + 5*x^4 + 46*x^3 + 19*x^2 - 11*x + 1
+
+
+### Based on cos(2*pi/21)
+id = c(1,2,4,5,8,10)
+r = 2*cos(2*pi*id/21)
+
+
+x = r; poly.calc0(x)
+x^6 - x^5 - 6*x^4 + 6*x^3 + 8*x^2 - 8*x + 1
+
+x = r^2 - r; poly.calc0(x)
+x^6 - 12*x^5 + 46*x^4 - 62*x^3 + 16*x^2 + 11*x + 1
+
+x = r^3 - 2*r; poly.calc0(x)
+x^6 + x^5 - 13*x^4 - 6*x^3 + 15*x^2 + 8*x + 1
+
+x = r^3 + r^2 - r; poly.calc0(x)
+x^6 - 13*x^5 + 29*x^4 + 78*x^3 - 48*x^2 + x + 1
+
