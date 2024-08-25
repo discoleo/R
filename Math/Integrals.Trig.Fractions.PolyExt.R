de239276d2a9d95b05b28ec00c1d554605f67680
discoleo <leo.mada@syonic.eu>
2024-08-26 01:01:51 +0300

    [edit] Std names;

--- Math/Integrals.Trig.Fractions.PolyExt.R
+++ Math/Integrals.Trig.Fractions.PolyExt.R
@@ -4,48 +4,69 @@
 ## Extended Variants
 
 
-### I( x / (k - sin(x)) )
-### I( x / (k + sin(x)) )
+### I( x / (b - sin(x)) )
+### I( x / (b + sin(x)) )
 # Maths 505: Thank you for this wonderful integral
 # https://www.youtube.com/watch?v=zaiA-0OBKa8
 
-k = sqrt(5) # k > 1;
-integrate(\(x) x / (k - sin(x)), 0, pi)
-pi / sqrt(k^2 - 1) * (pi - atan(sqrt(k^2 - 1)))
+b = sqrt(5) # b > 1;
+integrate(\(x) x / (b - sin(x)), 0, pi)
+pi / sqrt(b^2 - 1) * (pi - atan(sqrt(b^2 - 1)))
 
-integrate(\(x) x / (k + sin(x)), 0, pi)
-pi / sqrt(k^2 - 1) * atan(sqrt(k^2 - 1))
+integrate(\(x) x / (b + sin(x)), 0, pi)
+pi / sqrt(b^2 - 1) * atan(sqrt(b^2 - 1))
 
-integrate(\(x) x / (k - sin(x)), 0, 2*pi)
-pi / sqrt(k^2 - 1) * (pi + 2*atan(sqrt(k^2 - 1)))
+integrate(\(x) x / (b - sin(x)), 0, 2*pi)
+pi / sqrt(b^2 - 1) * (pi + 2*atan(sqrt(b^2 - 1)))
 
-integrate(\(x) x / (k + sin(x)), 0, 2*pi)
-pi / sqrt(k^2 - 1) * (3*pi - 2*atan(sqrt(k^2 - 1)))
+integrate(\(x) x / (b + sin(x)), 0, 2*pi)
+pi / sqrt(b^2 - 1) * (3*pi - 2*atan(sqrt(b^2 - 1)))
 
 ### Helper
-integrate(\(x) 1 / (k - sin(x)), 0, pi)
-integrate(\(x) 1 / (k + sin(x)), pi, 2*pi)
-2 / sqrt(k^2 - 1) * (pi - atan(sqrt(k^2 - 1)))
+integrate(\(x) 1 / (b - sin(x)), 0, pi)
+integrate(\(x) 1 / (b + sin(x)), pi, 2*pi)
+2 / sqrt(b^2 - 1) * (pi - atan(sqrt(b^2 - 1)))
 
-integrate(\(x) 1 / (k + sin(x)), 0, pi)
-integrate(\(x) 1 / (k - sin(x)), pi, 2*pi)
-2 / sqrt(k^2 - 1) * atan(sqrt(k^2 - 1))
+integrate(\(x) 1 / (b + sin(x)), 0, pi)
+integrate(\(x) 1 / (b - sin(x)), pi, 2*pi)
+2 / sqrt(b^2 - 1) * atan(sqrt(b^2 - 1))
 
 
 ### More Helper:
-k = sqrt(5)
-integrate(\(x) x / (k - sin(x)), pi, 2*pi)
-3*pi / sqrt(k^2 - 1) * atan(sqrt(k^2 - 1))
+b = sqrt(5)
+integrate(\(x) x / (b - sin(x)), pi, 2*pi)
+3*pi / sqrt(b^2 - 1) * atan(sqrt(b^2 - 1))
 
-integrate(\(x) x / (k + sin(x)), pi, 2*pi)
-3*pi / sqrt(k^2 - 1) * (pi - atan(sqrt(k^2 - 1)))
+integrate(\(x) x / (b + sin(x)), pi, 2*pi)
+3*pi / sqrt(b^2 - 1) * (pi - atan(sqrt(b^2 - 1)))
 
 
 ### Gen 1:
-k = sqrt(5)
-integrate(\(x) x / (k - sin(x)), 2*pi, 3*pi)
-5*pi / sqrt(k^2 - 1) * (pi - atan(sqrt(k^2 - 1)))
+b = sqrt(5)
+integrate(\(x) x / (b - sin(x)), 2*pi, 3*pi)
+5*pi / sqrt(b^2 - 1) * (pi - atan(sqrt(b^2 - 1)))
 
-integrate(\(x) x / (k + sin(x)), 2*pi, 3*pi)
-5*pi / sqrt(k^2 - 1) * atan(sqrt(k^2 - 1))
+integrate(\(x) x / (b + sin(x)), 2*pi, 3*pi)
+5*pi / sqrt(b^2 - 1) * atan(sqrt(b^2 - 1))
+
+
+### Gen 2:
+b = sqrt(7)
+integrate(\(x) 1 / (b - cos(x)), 0, pi)
+integrate(\(x) 1 / (b + cos(x)), 0, pi)
+pi / sqrt(b^2 - 1)
+
+# TODO
+
+
+#
+b = sqrt(5)
+integrate(\(x) x / (b^2 - cos(x)^2), 0, pi)
+pi^2 / (2*b*sqrt(b^2 - 1))
+
+integrate(\(x) x / (4 - cos(x)^2), 0, pi)
+pi^2 / (4*sqrt(3))
+
+integrate(\(x) x / (9 - cos(x)^2), 0, pi)
+pi^2 / (6*sqrt(8))
 
