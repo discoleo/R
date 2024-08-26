3c24b398915461513bf9bfcd265e1ecb0554a390
discoleo <leo.mada@syonic.eu>
2024-08-26 20:11:13 +0300

    Example I( atan(...) / x );

--- Math/Integrals.Trig.Formulas.R
+++ Math/Integrals.Trig.Formulas.R
@@ -1,6 +1,20 @@
 
 ### Trig: Formulas
 
+# Various Trig Identities
+
+
+####################
+
+### Helper Functions
+
+Euler   = 0.57721566490153286060651209008240243079;
+Catalan = 0.915965594177219015054603514;
+
+
+##################
+##################
+
 ### Trig(3*x)
 x = pi/7
 cos(3*x) - 4*cos(x)^3 + 3*cos(x)
@@ -22,6 +36,7 @@ atan((1 - 3*x^2) / (3*x - x^3))
 x = sqrt(5)
 atan((2*x^3 - 3*x + 1) / (2*x^3 - 6*x^2 + 3*x))
 - 3 * atan(1 - 1/x)
+3 * atan(x/(x-1)) - 3*pi/2
 #
 atan((2*x^3 - 3*x - 1) / (2*x^3 + 6*x^2 + 3*x))
 3 * atan(x/(x+1)) - pi/2;
@@ -49,4 +64,11 @@ integrate(\(x) -(1 - 4*x^2) / (4*x^3 - 3*x), lower = cos(x), 1)
 integrate(\(x) -(x^3 - 3*x) / ((x^2 + 1) * (1 - 3*x^2)), 0, up = tan(x))
 integrate(\(x) tan(3*x), 0, up=x)
 - log(cos(3*x)) / 3;
+
+
+### Ex 2:
+integrate(\(x) (atan((2*x^3 - 3*x + 1) / (2*x^3 - 6*x^2 + 3*x)) - pi/2) / x, 0, 1/2)
+integrate(\(x) (-3*atan(1 - 1/x) - 3*pi/2) / x, 0, 1/2)
+integrate(\(x) 3 * (atan(x - 1) - pi/2) / x, 2, Inf)
+3/8 * pi*log(2) - 3*Catalan
 
