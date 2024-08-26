ec4011a31cb13746d7de2b6722f39dcd7b48f72d
discoleo <leo.mada@syonic.eu>
2024-08-26 11:38:22 +0300

    Identities: 1/3 Angle;

--- Math/Integrals.Trig.Formulas.R
+++ Math/Integrals.Trig.Formulas.R
@@ -9,6 +9,26 @@ sin(3*x) + 4*sin(x)^3 - 3*sin(x)
 tan(3*x) + (4*sin(x)^3 - 3*sin(x)) / (4*cos(x)^3 - 3*cos(x))
 tan(3*x) + (tan(x)^3 - 3*tan(x)) / (1 - 3*tan(x)^2)
 
+### ATAN
+x = sqrt(5)
+# Note: force result between [-pi/2, pi/2];
+atan((3*x - x^3) / (1 - 3*x^2))
+3 * atan(x) - pi
+#
+atan((1 - 3*x^2) / (3*x - x^3))
+3/2*pi - 3 * atan(x)
+
+### 1/3 Angle:
+x = pi/7
+cos(x/3) # ==
+((cos(x) + 1i*sin(x))^(1/3) + (cos(x) - 1i*sin(x))^(1/3)) / 2
+
+### Note:
+# D(cos(3*x)) => - 3*sin(3*x), but masked;
+x = pi/7
+- 12*sin(x)*cos(x)^2 + 3*sin(x)
+- 3*sin(3*x)
+
 
 ### Examples:
 x = pi/7
