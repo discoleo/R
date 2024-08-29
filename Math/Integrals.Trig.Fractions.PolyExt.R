aad8039ec374df05ee554008ce59d67c4f940bcf
discoleo <leo.mada@syonic.eu>
2024-08-29 22:56:04 +0300

    I( x / (b^2 - cos(x)^2) ) on [pi, 2*pi];

--- Math/Integrals.Trig.Fractions.PolyExt.R
+++ Math/Integrals.Trig.Fractions.PolyExt.R
@@ -8,6 +8,8 @@
 ### I( x / (b + sin(x)) )
 # Maths 505: Thank you for this wonderful integral
 # https://www.youtube.com/watch?v=zaiA-0OBKa8
+# Maths 505: SUPREME GOLDEN INTEGRAL
+# https://www.youtube.com/watch?v=uXHBzyypGOw
 
 b = sqrt(5) # b > 1;
 integrate(\(x) x / (b - sin(x)), 0, pi)
@@ -50,20 +52,40 @@ integrate(\(x) x / (b + sin(x)), 2*pi, 3*pi)
 5*pi / sqrt(b^2 - 1) * atan(sqrt(b^2 - 1))
 
 
-### Gen 2:
+### Gen 2: cos-Variants
 b = sqrt(7)
 integrate(\(x) 1 / (b - cos(x)), 0, pi)
 integrate(\(x) 1 / (b + cos(x)), 0, pi)
 pi / sqrt(b^2 - 1)
 
-# TODO
-
-
 #
+b = sqrt(7)
+integrate(\(x) 1 / (b^2 - cos(x)^2), 0, pi)
+pi / (b * sqrt(b^2 - 1))
+#
+integrate(\(x) 1 / (b^2 + cos(x)^2), 0, pi)
+pi / (b * sqrt(b^2 + 1))
+# Note: Power can be extended;
+
+
+# I( x / (b^2 - cos(x)^2) )
 b = sqrt(5)
 integrate(\(x) x / (b^2 - cos(x)^2), 0, pi)
 pi^2 / (2*b*sqrt(b^2 - 1))
 
+# I( x / (b^2 + cos(x)^2) )
+integrate(\(x) x / (b^2 + cos(x)^2), 0, pi)
+pi^2 / (2*b*sqrt(b^2 + 1))
+
+# on [pi, 2*pi]
+integrate(\(x) x / (b^2 - cos(x)^2), pi, 2*pi)
+3*pi^2 / (2*b * sqrt(b^2 - 1))
+#
+integrate(\(x) x / (b^2 + cos(x)^2), pi, 2*pi)
+3*pi^2 / (2*b * sqrt(b^2 + 1))
+
+
+# Cases:
 integrate(\(x) x / (4 - cos(x)^2), 0, pi)
 pi^2 / (4*sqrt(3))
 
@@ -79,11 +101,15 @@ integrate(\(x) x * cos(x) / (b^2 - cos(x)^2), 0, pi)
 # TODO
 
 
-###
+### Simple Variants:
 b = sqrt(5); up = pi / 5
 integrate(\(x) cos(x) / (b^2 - cos(x)^2), 0, up)
 integrate(\(x) 1 / (x^2 + b^2 - 1), 0, sin(up))
 1/sqrt(b^2 - 1) * atan(sin(up) / sqrt(b^2 - 1))
+#
+integrate(\(x) cos(x) / (b^2 + cos(x)^2), 0, up)
+1/(2*sqrt(b^2 + 1)) * log((sqrt(b^2 + 1) + sin(up)) / (sqrt(b^2 + 1) - sin(up)))
+
 
 ###
 b = sqrt(7)
