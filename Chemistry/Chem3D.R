476bfb8323fcb16cf019ff2e6efaa8563b0d3926
discoleo <leo.mada@syonic.eu>
2024-08-31 04:15:13 +0300

    Minimal dist between 2 lines;

--- Chemistry/Chem3D.R
+++ Chemistry/Chem3D.R
@@ -125,6 +125,29 @@ rotate.xy = function(alpha = pi/6) {
 
 ### Orthogonal Projection/Rotation
 
+### Projection
+# p = point which will be projected on (x,y,z) line;
+proj.line3d = function(p, x, y, z) {
+	if(missing(y)) {
+		y = x[,2]; z = x[,3]; x = x[,1];
+	}
+	dx = x[2] - x[1]; dx0 = p[1] - x[1];
+	dy = y[2] - y[1]; dy0 = p[2] - y[1]; # p[2] = yP;
+	dz = z[2] - z[1]; dz0 = p[3] - z[1];
+	tt = (dx*dx0 + dy*dy0 + dz*dz0);
+	tt = tt / (dx^2 + dy^2 + dz^2);
+	t1 = 1 - tt;
+	px = t1*x[1] + tt*x[2];
+	py = t1*y[1] + tt*y[2];
+	pz = t1*z[1] + tt*z[2];
+	# TODO: compute d or not?
+	# d = sqrt(sum((p - c(px, py, pz))^2));
+	lst = list(P = c(px, py, pz), t = tt);
+	return(lst);
+}
+
+### Decompositions into Normals
+
 eigen.lineN2 = function(p, x, y = NULL, z = NULL, normalize = TRUE) {
 	N = eigen.xy3D(p, x=x, y=y, z=z, normalize=normalize);
 	N = list(N1 = N$Nx, N2 = N$Ny, P = N$P);
@@ -151,12 +174,15 @@ eigen.lineAnyN2 = function(x, y = NULL, z = NULL, normalize = TRUE) {
 }
 
 
-# p = point which will be rotated by pi/2;
-# (x,y,z) = coordinates of 2 points defining line;
+### Generate 2 Normals
+# (x,y,z) = coordinates of 3 points defining plane;
+# p = 1st point in (x,y,z);
+#     will be rotated by pi/2 around the remaining line;
 ### Out:
-# T1,T2 = rotated point (2 solutions);
-# P = projected point on line;
-
+# N = normal to plane (x,y,z);
+# p[out] = projected point on line;
+# Note:
+# - T1,T2 = rotated point (2 valid solutions);
 eigen.plane = function(x, y = NULL, z = NULL, normalize = TRUE) {
 	if(is.null(y)) {
 		p = x[1,]; x = x[-1,];
@@ -201,6 +227,9 @@ eigen.plane = function(x, y = NULL, z = NULL, normalize = TRUE) {
 	lst = list(N = N, P = c(px,py,pz));
 	return(lst);
 }
+### Project point p on line (x,y,z)
+# Returns:
+# - Normals & Projection Point;
 eigen.xy3D = function(p, x, y = NULL, z = NULL, normalize = TRUE) {
 	if(is.null(y)) {
 		xyz = x;
@@ -217,8 +246,12 @@ eigen.xy3D = function(p, x, y = NULL, z = NULL, normalize = TRUE) {
 	lst = list(Ny = N$N, Nx = Nx, P = pP);
 	return(lst);
 }
+
 # Projection Point: given
-# - p0 projects on pP;
+# - pP = Projection point (known);
+# - p0 = point which projects on pP;
+# - x, y, z = forms with pP a line on which p0 is projected;
+# Note: avoids recomputing pP if it is already known;
 eigen.projPoint = function(p0, pP, x, y = NULL, z = NULL,
 		normalize = TRUE, verbose = TRUE) {
 	if(is.null(y)) {
@@ -235,7 +268,7 @@ eigen.projPoint = function(p0, pP, x, y = NULL, z = NULL,
 	dyz = dy0*dz1 - dy1*dz0;
 	div = sqrt(dxy^2 + dxz^2 + dyz^2);
 	# Note: both roots of "R" are valid;
-	# TODO: check if dyz = 0; (already solved ?)
+	# TODO: check if dyz == 0; (already solved ?)
 	NN = R / div;
 	dxT =   dyz * NN;
 	dyT = - dxz * NN;
@@ -253,6 +286,7 @@ eigen.projPoint = function(p0, pP, x, y = NULL, z = NULL,
 }
 
 # Any Normal
+# x, y, z = given line;
 eigen.lineAny = function(x, y = NULL, z = NULL, normalize = TRUE) {
 	if(is.null(y)) {
 		y = x[,2]; z = x[,3]; x = x[,1];
@@ -385,6 +419,93 @@ test.eigen.lineAny = function(p, d = 1, both = TRUE, rev = FALSE,
 	invisible(N);
 }
 
+
+### Intersections
+
+dist.lines3d = function(xyz, xyz0, tol = 1E-6, verbose = TRUE) {
+	p1 = proj.line3d(xyz[1,], xyz0);
+	d1 = sqrt(sum((xyz[1,] - p1$P)^2));
+	if(d1 <= tol) {
+		# Lines intersect
+		if(verbose) print("Lines intersect at P1.");
+		return(0);
+	}
+	p2 = proj.line3d(xyz[2,], xyz0);
+	d2 = sqrt(sum((xyz[2,] - p2$P)^2));
+	if(d2 <= tol) {
+		# Lines intersect
+		if(verbose) print("Lines intersect at P2.");
+		return(0);
+	}
+	# Special Cases:
+	d = sqrt(sum((p1$P - p2$P)^2));
+	# Orthogonal lines:
+	if(d <= tol) {
+		# Both points project on same point;
+		# Project point back:
+		pO = proj.line3d(p1$P, xyz);
+		dO = sqrt(sum((p1$P - pO$P)^2));
+		if(dO <= tol) {
+			# Lines intersect
+			if(verbose) print("Lines intersect midway.");
+			return(0);
+		}
+		if(verbose) print("Lines are orthogonal.");
+		return(dO);
+	}
+	# Equidistant Projections
+	if(abs(d1 - d2) <= tol) {
+		pM = (p1$P + p2$P) / 2;
+		pO = proj.line3d(pM, xyz);
+		dO = sqrt(sum((pM - pO$P)^2));
+		if(dO <= tol) {
+			# Lines intersect
+			if(verbose) print("Lines intersect midway.");
+			return(0);
+		}
+		# Note: lines may be parallel,
+		# but is not essential for distance;
+		if(verbose) print("Special Lines but NO intersection.");
+		return(dO);
+	}
+	# General Case: Minimize d
+	dd = xyz[2,] - p2$P;
+	dy = xyz[1,] - p1$P;
+	ds = dy - dd;
+	div = sum(ds*ds);
+	tt  = - sum(dd * ds) / div;
+	tt1 = 1 - tt;
+	pL1 = tt * xyz[1,] + tt1 * xyz[2,];
+	pL2 = tt * p1$P + tt1 * p2$P;
+	d = sqrt(sum((pL1 - pL2)^2));
+	return(d);
+}
+
+# Shortest Distance between 2 lines
+# including points delimiting that line segment;
+proj.lines3d = function(xyz, xyz0, tol = 1E-6) {
+	p1 = proj.line3d(xyz[1,], xyz0);
+	d = sqrt(sum((xyz[1,] - p$P)^2));
+	if(d <= tol) {
+		# Lines intersect
+		lst = list(P1 = xyz[1,], p2 = p$P, d = 0,
+			doIntersect = TRUE, isParallel = FALSE);
+		return(lst);
+	}
+	stop("TODO");
+	# TODO
+}
+
+### Test Functions
+
+# Minimal Rectangle
+test.rect = function(L, d = 1,
+		col = c("#000000", "#64D032", "#D032D0")) {
+	lines3d(L, col = col[[1]]);
+	lines3d(L1 + c(0,0,d,d,0,0), col = col[[2]]);
+	lines3d(L1 + c(0,0,d,d,d,d), col = col[[2]]);
+	lines3d(L1 + c(0,0,0,0,d,d), col = col[[3]]);
+}
 
 ################
 
