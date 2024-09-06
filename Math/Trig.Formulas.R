e28b4da280a34eaf4f77d3445103ea88481f82a3
discoleo <leo.mada@syonic.eu>
2024-09-06 23:54:42 +0300

    [exact] acos(5/3);

---
+++ Math/Trig.Formulas.R
@@ -0,0 +1,15 @@
+#################
+##
+## Trig: Formulas
+## ATAN & ACOS
+
+
+###
+Im(acos(3/2 + 0i))
+2 * log((1+sqrt(5))/2)
+
+
+###
+Im(acos(5/3 + 0i))
+(2*digamma(1/2) - digamma(1/2 - 1/3) - digamma(1/2 + 1/3)) / 3
+
