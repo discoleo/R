191f74ae244f8cc2077c1f92953470bb2fbd49ae
discoleo <leo.mada@syonic.eu>
2024-09-07 02:49:09 +0300

    Gen: Im(acos(n));

--- Math/Trig.Formulas.R
+++ Math/Trig.Formulas.R
@@ -6,10 +6,37 @@
 
 ###
 Im(acos(3/2 + 0i))
+Re(log((sqrt(5) + 3) * 1i/2))
 2 * log((1+sqrt(5))/2)
 
 
 ###
+Im(acos(4/3 + 0i))
+log((sqrt(7) + 4)/3)
+
+###
 Im(acos(5/3 + 0i))
+log(3)
+# varia:
 (2*digamma(1/2) - digamma(1/2 - 1/3) - digamma(1/2 + 1/3)) / 3
+
+
+###
+Im(acos(2 + 0i))
+log(sqrt(3) + 2)
+
+
+###
+Im(acos(3 + 0i))
+2 * log(sqrt(2) + 1)
+
+###
+Im(acos(4 + 0i))
+log((sqrt(15) + 4))
+
+
+### Gen:
+n = sqrt(7)
+Im(acos(n + 0i))
+log((sqrt(n^2 - 1) + n))
 
