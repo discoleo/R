a6cdf9ab3035e2fcffd82fcab7e70dcbab56bad3
discoleo <leo.mada@syonic.eu>
2024-09-12 02:02:27 +0300

    Sum( (-1) gamma(n + 3/8) / gamma(n + 13/8) )

--- Math/Sums.Gamma.R
+++ Math/Sums.Gamma.R
@@ -28,6 +28,7 @@ pi^2/18
 
 # Maths 505: An awesome calculus result I cooked up
 # https://www.youtube.com/watch?v=WoxHJyOOma4
+# Note: Series expansion of 1 / (x^n + 1);
 
 
 ### Sum( (-1) gamma(n + 1/4) / gamma(n + 7/4) )
@@ -48,14 +49,26 @@ pi/4
 ### Gen 1:
 
 ### Sum( (-1) gamma(n/2 + 1/8) / gamma(n/2 + 11/8) )
-id = mpfr(seq(0, 401), 240)
+id = mpfr(seq(0, 601), 240)
 sum(rep(c(1,-1), length(id)/2) * gamma(id/2 + 1/8) / gamma(id/2 + 11/8))
 sqrt(2) * beta(5/8, 1/4) / gamma(5/4);
 4*sqrt(2) * gamma(5/8) / gamma(7/8);
 
 # Based on:
+# - for more details, see file:
+#   Integrals.Fractions.Unity.Radicals.R;
 integrate(\(x) (1 - x^8)^(1/4) / (x^4 + 1), 0, 1)
 integrate(\(x) (1/tan(x)^2 - tan(x)^2)^(1/4) / 2, 0, pi/4)
 integrate(\(x) sqrt(2)/4 * (cos(x) / sin(x)^2)^(1/4), 0, pi/2)
 sqrt(2)/8 * beta(5/8, 1/4)
+
+
+### Sum( (-1) gamma(n + 3/8) / gamma(n + 13/8) )
+id = mpfr(seq(0, 601), 240)
+sum(rep(c(1,-1), length(id)/2) * gamma(id + 3/8) / gamma(id + 13/8))
+2^(1/4)/2 * beta(5/8, 3/8) / gamma(5/4);
+
+# Based on:
+integrate(\(x) x^2 * (1 - x^8)^(1/4) / (x^8 + 1), 0, 1)
+2^(1/4)/16 * beta(5/8, 3/8)
 
