0e3da15d2612be597eb444371933bde18638b968
discoleo <leo.mada@syonic.eu>
2024-09-12 00:53:47 +0300

    Sum( (-1) gamma(n + 1/4) / gamma(n + 7/4) )
    + higher variant;

--- Math/Sums.Gamma.R
+++ Math/Sums.Gamma.R
@@ -22,3 +22,40 @@ sum( gamma(id)^2 / gamma(2*id+1) )
 pracma::psi(1,1)/3
 pi^2/18
 
+
+###################
+###################
+
+# Maths 505: An awesome calculus result I cooked up
+# https://www.youtube.com/watch?v=WoxHJyOOma4
+
+
+### Sum( (-1) gamma(n + 1/4) / gamma(n + 7/4) )
+id = seq(0, 80)
+sum((-1)^id * gamma(id + 1/4) / gamma(id + 7/4))
+2*sqrt(pi)
+#
+id = mpfr(seq(0, 81), 240)
+sum(rep(c(1,-1), length(id)/2) * gamma(id + 1/4) / gamma(id + 7/4))
+2*sqrt(pi)
+
+# Based on:
+integrate(\(x) sqrt(1 - x^4) / (x^4 + 1), 0, 1)
+integrate(\(x) sqrt(1/tan(x) - tan(x)) / 2, 0, pi/4)
+pi/4
+
+
+### Gen 1:
+
+### Sum( (-1) gamma(n/2 + 1/8) / gamma(n/2 + 11/8) )
+id = mpfr(seq(0, 401), 240)
+sum(rep(c(1,-1), length(id)/2) * gamma(id/2 + 1/8) / gamma(id/2 + 11/8))
+sqrt(2) * beta(5/8, 1/4) / gamma(5/4);
+4*sqrt(2) * gamma(5/8) / gamma(7/8);
+
+# Based on:
+integrate(\(x) (1 - x^8)^(1/4) / (x^4 + 1), 0, 1)
+integrate(\(x) (1/tan(x)^2 - tan(x)^2)^(1/4) / 2, 0, pi/4)
+integrate(\(x) sqrt(2)/4 * (cos(x) / sin(x)^2)^(1/4), 0, pi/2)
+sqrt(2)/8 * beta(5/8, 1/4)
+
