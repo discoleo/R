8167b808d97459b2666f36a1838d801d58b41b9e
discoleo <leo.mada@syonic.eu>
2024-10-27 03:52:05 +0200

    I( log(x+1) / (x^3+1) ): more work;

--- Math/Integrals.Log.Fractions.Other.R
+++ Math/Integrals.Log.Fractions.Other.R
@@ -140,6 +140,18 @@ integrate(\(x) log(1-x) / (x^3 + 1), 0, 1)
 - pi^2/18 + log(2)^2/6 +
 	- pracma::psi(1, 1/3) / 18 + pracma::psi(1, 2/3) / 9;
 
+
+###
+integrate(\(x) log(1+x) / (x^3 + 1), 0, 1)
+integrate(\(x) (log(2) - log(x+1)) / (x^3 - 1), 0, 1)$value +
+(pracma::psi(1, 2/3) - pracma::psi(1, 1/6)) / 36 +
+	+ pracma::psi(1, 1/3) / 9 +
+	- (digamma(5/6) - digamma(1/3)) *
+		(digamma(2/3) - digamma(1/3)) / 18 +
+	+ (digamma(2/3) - digamma(1/6)) *
+		(digamma(1) - digamma(1/3)) / 18;
+# TODO
+
 ###
 integrate(\(x) x * log(1+x) / (x^3 + 1), 0, 1)
 integrate(\(x) (log(1+x) - log(2)) / (x^3 - 1), 0, 1)$value +
@@ -148,6 +160,16 @@ integrate(\(x) (log(1+x) - log(2)) / (x^3 - 1), 0, 1)$value +
 	- (digamma(2/3) - digamma(1/3)) * log(2) / 9 +
 	+ (digamma(5/6) - digamma(1/3)) *
 		(digamma(1) - digamma(1/3)) / 18;
+# TODO
+
+###
+integrate(\(x) x^2 * log(1+x) / (x^3 + 1), 0, 1)
+integrate(\(x) (log(2) - x^2*log(x+1)) / (x^3 - 1), 0, 1)$value +
+(pracma::psi(1, 1) - pracma::psi(1, 1/2)) / 36 +
+	+ pracma::psi(1, 1) / 9 +
+	+ (digamma(1) - digamma(1/3)) * log(2) / 9 +
+	+ (digamma(2/3) - digamma(1/6)) *
+		(digamma(2/3) - digamma(1/3)) / 18
 # TODO
 
 ###
@@ -169,6 +191,15 @@ integrate(\(x) log(1+x) * (x^2 + 2) / (1-x^3) - log(2)/(1-x), 0, 1)
 (pracma::psi(1, 1/3) - pracma::psi(1, 2/3)) / 36 +
 	- pi^2/12 + log(2)^2/2;
 
+
+# from log(x + b) / (x^3 + 1)
+b = sqrt(5)
+integrate(\(x) 1 / (x+b) / (x^3+1), 0, 1)
+integrate(\(x) ((x^2-b*x+b^2)/(x^3+1) - 1/(x+b)) / (b^3-1), 0, 1)
+(log(b) - log(b+1) + log(2) / 3 +
+	- (digamma(5/6) - digamma(1/3)) * b / 6 +
+	+ (digamma(2/3) - digamma(1/6)) * b^2 / 6) / (b^3 - 1);
+
 # from x * log(x + b) / (x^3 + 1)
 b = sqrt(5)
 integrate(\(x) x / (x+b) / (x^3+1), 0, 1)
@@ -177,6 +208,15 @@ integrate(\(x) (b/(x+b) - (b*x^2-b^2*x+1)/(x^3+1)) / (b^3-1), 0, 1)
 (b*log(b+1) - b*log(b) - b*log(2) / 3 +
 	+ (digamma(5/6) - digamma(1/3)) * b^2 / 6 +
 	- (digamma(2/3) - digamma(1/6)) / 6) / (b^3 - 1);
+
+# from x^2 * log(x + b) / (x^3 + 1)
+b = sqrt(5)
+integrate(\(x) x^2 / (x+b) / (x^3+1), 0, 1)
+integrate(\(x) x^2 * ((x^2-b*x+b^2)/(x^3+1) - 1/(x+b)) / (b^3-1), 0, 1)
+integrate(\(x) ((b^2*x^2-x+b)/(x^3+1) - b^2/(x+b)) / (b^3-1), 0, 1)
+(b^2*log(b) - b^2*log(b+1) + b^2*log(2) / 3 +
+	- (digamma(5/6) - digamma(1/3)) / 6 +
+	+ (digamma(2/3) - digamma(1/6)) * b / 6) / (b^3 - 1);
 
 
 ### Pow = 4
