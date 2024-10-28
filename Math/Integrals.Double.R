31528e27cdec24b74cd1ffaad299f63fa2a50a88
discoleo <leo.mada@syonic.eu>
2024-10-28 15:24:40 +0200

    I( atan(1 - x*y) ) on [0,1]x[0,1];

--- Math/Integrals.Double.R
+++ Math/Integrals.Double.R
@@ -5,7 +5,7 @@
 ##
 ## Integrals: Double Integrals
 ##
-## v.0.1.
+## v.0.1b
 
 ### Double Integrals
 
@@ -25,8 +25,21 @@ Catalan = 0.915965594177219015054603514;
 ### Simple
 
 ### I( 1 / (1 - x*y) )
-integrate(\(x) sapply(x, \(y) integrate(\(x) 1 / (1-x*y), 0, 1)$value), 0, 1);
+integrate(\(x) sapply(x, \(y) integrate(\(x) 1 / (1-x*y), 0, 1)$value), 0, 1)
 pi^2 / 6
+
+### I( 1 / (1 + x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) 1 / (1+x*y), 0, 1)$value), 0, 1)
+pi^2/12
+
+
+### I( log(1 - x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(1-x*y), 0, 1)$value), 0, 1)
+2 - pi^2/6
+
+### I( log(1 + x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(1+x*y), 0, 1)$value), 0, 1)
+pi^2/12 + 2*log(2) - 2
 
 
 ### I( (x+y)*log(x+y) / (x*y + 1) )
@@ -34,4 +47,21 @@ integrate(\(x) sapply(x, \(y) {
 	integrate(\(x) (x+y)*log(x+y) / (1+x*y), 0, 1, rel.tol=1E-12)$value
 	}), 0, 1, rel.tol=1E-12)
 - pi^2/2 + 2*log(2)^2 + 4;
+
+
+### ATAN
+
+### I( atan(x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) atan(x*y), 0, 1)$value), 0, 1)
+- pi^2 / 48 + pi/4 - log(2)/2
+
+
+### I( atan(1 - x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) atan(1 - x*y), 0, 1)$value), 0, 1)
+- (5/96*pi^2 + pi*log(2)/8 - log(2)^2 / 8 + log(2)/2 - pi/4 - Catalan)
+
+
+### I( atan(1 + x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) atan(1 + x*y), 0, 1)$value), 0, 1)
+# TODO
 
