b16e67c359429109e0704ce6591b54352ef99a7a
discoleo <leo.mada@syonic.eu>
2024-10-30 00:54:32 +0200

    Gen: I( atan(k * x^2) / (x^4 + 1) ) on [0, Inf];

--- Math/Integrals.Trig.Tan.Atan.R
+++ Math/Integrals.Trig.Tan.Atan.R
@@ -53,8 +53,30 @@ pi^2 / 8
 
 ### I( atan(x^2) / (x^4 + 1) )
 integrate(\(x) atan(x^2) / (x^4 + 1), 0, Inf, rel.tol=1E-9)
+pi^2 / sin(pi/4) / 8 - pi * (digamma(1/2) - digamma(1/4)) / sin(pi/4) / 8
 pi*(pi - 2*log(2)) * sqrt(2) / 16
 
+
+### Gen: I( atan(k * x^2) / (x^4 + 1) )
+k = 5^(1/3)
+integrate(\(x) atan(k*x^2) / (x^4 + 1), 0, Inf, rel.tol=1E-9)
+x = 1/k^(1/2);
+pi^2 / sin(pi/4) / 8 +
+	+ (log(x^2 + 1) - 2*log(x + 1) - 2*atan(x)) * pi / sin(pi/4) / 8;
+
+# Solution: Explicit formula
+id = c(2,4) * pi/4; x = 1/k^(1/2);
+cs = cos(id); cs1 = cs[1]; csp = cos(pi);
+sn = sin(id); sn1 = sn[1]; snp = sin(pi);
+pi^2 / sin(pi/4) / 8 +
+- (sum(csp*log(x^2 - 2*cs1*x + 1) +
+	- 2*snp * (atan((x - cs1)/sn1) + atan(cs1/sn1))) +
+- (sum(cs*log(x^2 - 2*cs*x + 1) +
+	- 2*sn * (atan((x - cs)/sn) + atan(cs/sn))))) * pi / sin(pi/4) / 8;
+
+
+
+### ATAN-Pow: 4
 
 ### I( atan(x^4) / (x^4 + 1) )
 integrate(\(x) atan(x^4) / (x^4 + 1), 0, Inf, rel.tol=1E-9)
@@ -115,13 +137,20 @@ pi^2 / sin(pi/4) / 8 - pi*(
 
 # Derivation:
 
-# from atan(x^4) / (x^4 + 1)
+# from atan(x^2 / b^2) / (x^4 + 1)
+b = 5^(1/8) # does NOT include factor * 2*b
+integrate(\(x) x^2/(x^4 + b^4) / (x^4 + 1), 0, Inf, rel.tol=1E-9)
+integrate(\(x) x^2 * (1/(x^4+1) - 1/(x^4 + b^4)) / (b^4 - 1), 0, Inf)
+pi * (1/sin(3*pi/4) - 1/sin(pi/4) / b) / 4 / (b^4 - 1)
+
+
+# from atan(x^4 / b^4) / (x^4 + 1)
 b = 5^(1/8) # does NOT include factor * 4*b^3
 integrate(\(x) x^4/(x^8 + b^8) / (x^4 + 1), 0, Inf, rel.tol=1E-9)
 integrate(\(x) ((x^4+b^8)/(x^8 + b^8) - 1/(x^4+1)) / (b^8 + 1), 0, Inf)
 pi * (1/sin(pi/8) * b + 1/sin(5*pi/8) / b^3 - 2/sin(pi/4)) / 8 / (b^8+1)
 
-# from x^2 * atan(x^4) / (x^4 + 1)
+# from x^2 * atan(x^4 / b^4) / (x^4 + 1)
 b = 5^(1/8) # does NOT include factor * 4*b^3
 integrate(\(x) x^6/(x^8 + b^8) / (x^4 + 1), 0, Inf, rel.tol=1E-9)
 integrate(\(x) x^2 * ((x^4+b^8)/(x^8 + b^8) - 1/(x^4+1)) / (b^8 + 1), 0, Inf)
