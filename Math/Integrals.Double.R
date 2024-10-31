297f29ec61cbb95ce58c5808ffaab1ba50b88953
discoleo <leo.mada@syonic.eu>
2024-10-31 04:34:34 +0200

    I( log(exp(x) + exp(y)) ) on [0,1]^2

--- Math/Integrals.Double.R
+++ Math/Integrals.Double.R
@@ -42,11 +42,36 @@ integrate(\(x) sapply(x, \(y) integrate(\(x) log(1+x*y), 0, 1)$value), 0, 1)
 pi^2/12 + 2*log(2) - 2
 
 
-### I( (x+y)*log(x+y) / (x*y + 1) )
+### I( log(1-x*y) / (x+y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(1-x*y) / (x+y),
+	0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+
+
+### I( log(1+x*y) / (x+y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(1+x*y) / (x+y),
+	0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+
+
+### I( log(x+y) / (1 - x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(x+y) / (1 - x*y), 0, 1)$value), 0, 1)
+
+
+### I( log(x+y) / (1 + x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(x+y) / (1 + x*y), 0, 1)$value), 0, 1)
+
+
+### I( (x+y)*log(x+y) / (1 + x*y) )
 integrate(\(x) sapply(x, \(y) {
 	integrate(\(x) (x+y)*log(x+y) / (1+x*y), 0, 1, rel.tol=1E-12)$value
 	}), 0, 1, rel.tol=1E-12)
 - pi^2/2 + 2*log(2)^2 + 4;
+
+
+### I( (x+y)*log(x+y) / (1 - x*y) )
+integrate(\(x) sapply(x, \(y) {
+	integrate(\(x) (x+y)*log(x+y) / (1-x*y), 0, 1, rel.tol=1E-12)$value
+	}), 0, 1, rel.tol=1E-12)
+# TODO
 
 
 ### ATAN
@@ -63,5 +88,34 @@ integrate(\(x) sapply(x, \(y) integrate(\(x) atan(1 - x*y), 0, 1)$value), 0, 1)
 
 ### I( atan(1 + x*y) )
 integrate(\(x) sapply(x, \(y) integrate(\(x) atan(1 + x*y), 0, 1)$value), 0, 1)
+# TODO
+
+### I( atan(x*y) / (x+y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) atan(x*y) / (x+y), 0, 1)$value), 0, 1)
+# TODO
+
+### I( atan(x+y) / (1 - x*y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x) atan(x+y) / (1-x*y), 0, 1)$value), 0, 1)
+# TODO
+
+
+###########
+### Exp ###
+
+### I( log(exp(x) + exp(y)) )
+# Maths 505: An extremely captivating double integral
+# https://www.youtube.com/watch?v=etR1AI3anpU
+# - for polylog2, see file: Integrals.Trig.Tan.R;
+
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(exp(x) + exp(y)), 0, 1)$value), 0, 1)
+- 3/2*pracma::zeta(3) - pi^2/6 - 2*polylog2(-exp(1), 3) + 1/3;
+
+
+###
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(exp(1) - exp(x*y)), 0, 1)$value), 0, 1)
+# TODO
+
+###
+integrate(\(x) sapply(x, \(y) integrate(\(x) log(exp(x*y) - 1), 0, 1)$value), 0, 1)
 # TODO
 
