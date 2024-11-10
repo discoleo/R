84422a78c6f0d8c75d04bc99778fc80af3ecb305
discoleo <leo.mada@syonic.eu>
2024-11-11 00:30:24 +0200

    [refactor] moved unusual types to Varia;

--- Math/Integrals.Exp.Trig.Varia.R
+++ Math/Integrals.Exp.Trig.Varia.R
@@ -1,5 +1,53 @@
+#########################
+##
+## Integrals: Trig( EXP )
+##
+## Leonard Mada
 
 
+### Unusual Types
+
+
+#####################
+
+### I( exp(2*cos(x)) ) on [0, pi]
+# Maths 505: A surprisingly interesting integral
+# https://www.youtube.com/watch?v=81qExKEYzo0
+# Note: Series expansion + Contour on half of Unit Circle;
+# Pole of order k: Res = 1/k!;
+
+integrate(\(x) exp(2*cos(x)), 0, pi)
+integrate(\(x) exp(2*cos(x)) / 2, -pi, pi)
+pi * sum(1 / factorial(seq(0, 15))^2)
+
+# TODO: closed formula?
+
+
+###
+integrate(\(x) Re(exp(2*exp(x*1i))), 0, pi)
+pi
+
+integrate(\(x) Im(exp(2*exp(x*1i))), 0, pi)
+# TODO: ?
+
+integrate(\(x) {
+	x = mpfr(x, 240);
+	y = exp(2*cos(x)) * sin(2*sin(x));
+	as.numeric(y); }, 0, pi, rel.tol=1E-8)
+
+
+######################
+
+### I( exp(sin(2*x)/2) * cos(cos(x)^2) )
+# Maths 505: A (literally) complex integral
+# https://www.youtube.com/watch?v=4I6UgSwWkIA
+# (intermediate transformation)
+
+integrate(\(x) exp(sin(2*x)/2) * cos(cos(x)^2), 0, pi)
+pi * cos(1/2)
+
+
+############
 
 ###
 # Maths 505: This wacky integral has a beautiful result
