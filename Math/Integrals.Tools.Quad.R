1de51b19e57fe4234c59123569222c520c183353
discoleo <leo.mada@syonic.eu>
2024-11-24 16:37:08 +0200

    Gauss-Cardano Quadrature;

--- Math/Integrals.Tools.Quad.R
+++ Math/Integrals.Tools.Quad.R
@@ -45,3 +45,57 @@ intw = function (lower, upper, n = 81, prec = 128) {
 	return(list(x = x, w = w, ww = ww))
 }
 
+# Weights based on Cardano-Polynomials
+# Note: may fail for certain n; TODO: investigate;
+intw.cardano = function(lower, upper, n = 81) {
+	if(missing(upper)) {
+		if(length(lower) == 2) {
+			upper = lower[2]; lower = lower[1];
+		} else stop("Missing upper Limit!");
+	}
+	# Roots of Unity:
+	id = 2*pi*seq(n-1)/n;
+	m = cos(id) + 1i*sin(id);
+	# Roots Cardano:
+	cc = 1; d = 1/2; # nice properties
+	dd = sqrt(d^2 - cc^n + 0i);
+	p = (d + dd); q = (d - dd);
+	r = sort(Re(p*c(1,m) + q*c(1, rev(m))));
+	# Scaling
+	dd = upper - lower;
+	x = (lower+upper)/2 + r*dd/4;
+	w = c(lower, x, upper);
+	w = abs(diff(diff(w)));
+	w = w / sum(w) * dd;
+	return(list(x = x, w = w));
+}
+
+# TODO: a lot more work
+int.cardano.mpfr = function(FUN, lower, upper, prec = 128, n = 401) {
+	if(missing(upper)) {
+		if(length(lower) == 2) {
+			upper = lower[2]; lower = lower[1];
+		} else stop("Missing upper Limit!");
+	}
+	if(inherits(lower, "mpfr")) {
+		if(is.null(prec)) prec = getPrec(lower);
+	} else {
+		if(is.null(prec)) stop("Precision must be specified!");
+		lower = mpfr(lower, prec);
+		upper = mpfr(upper, prec);
+	}
+	# Roots Unity:
+	pi2 = Rmpfr::Const("pi", prec);
+	id = (pi2*seq(n-1)) / n;
+	cs = cos(id); sn = sin(id);
+	# Roots Cardano:
+	c1 = Rmpfr::mpfr(1, prec);
+	d  = c1 / 2; # c = c1;
+	dd = d^2 - c1^n;
+	isEven = (n %% 2 == 0);
+	isC = (dd < 0);
+	if( ! isC) { dd = sqrt(dd); }
+	else dd = sqrt( - dd);
+	# TODO
+}
+
--- Math/Integrals.Tools.Quad.Tests.R
+++ Math/Integrals.Tools.Quad.Tests.R
@@ -31,3 +31,25 @@ print(integrate(\(x) { x = mpfr(x, 240); as.numeric(f(x)); },
 sum(tmp$w * f(tmp$x))
 tmp = intw(lim, n=281); sum(tmp$w * f(tmp$x))
 
+
+### Test 2:
+# - based on Cardano/Chebychev polynomials;
+
+f = \(x) sin(x) / (x + sin(3*x)^3 + cos(5*x)^5)
+
+n = 221;
+# n = 442; # n = 4452;
+# Precision does NOT really increase above:
+# - need for mpfr;
+lim = c(0,1);
+tmp = intw.cardano(lim, n=n)
+print(integrate(f, lim[1], lim[2]), 12)
+print(integrate(f, lim[1], lim[2], rel.tol=1E-12), 12)
+print(sum(f(tmp$x) * tmp$w), 12)
+
+#
+lim = c(1,4);
+tmp = intw.cardano(lim, n=n)
+print(integrate(f, lim[1], lim[2]), 12)
+print(integrate(f, lim[1], lim[2], rel.tol=1E-12), 12)
+print(sum(f(tmp$x) * tmp$w), 12)
