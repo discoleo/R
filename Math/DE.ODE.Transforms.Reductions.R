987994970aa81d15ba151f1fbfe1db5f1d89c41b
discoleo <leo.mada@syonic.eu>
2024-12-03 20:42:55 +0200

    Gen Reduction: d2y = b * x^n * y + f0;

--- Math/DE.ODE.Transforms.Reductions.R
+++ Math/DE.ODE.Transforms.Reductions.R
@@ -6,7 +6,7 @@
 ## Integrals: Transforms
 ## Order Reduction
 ##
-## v.0.1b
+## v.0.1c
 
 
 ### Order Reduction of ODEs
@@ -46,12 +46,30 @@
 # d2y = 4*k^2*x^2*y + f0
 
 
-### Transform: Exp(x^2)
-# y = z * exp(x^2)
+### Transform: Exp(k*x^2)
+# y = z * exp(k*x^2)
 # dy = (dz + 2*k*x*z) * exp(k*x^2)
 # d2y = (d2z + 4*k*x*dz + 4*k^2*x^2*z) * exp(k*x^2)
 
 ### Order Reduction
 # =>
 # d2z + 4*k*x*dz = f0 * exp(-k*x^2)
+
+
+###################
+
+### Gen: x^n * y
+
+### Base ODE:
+# d2y = (n*k)^2 * x^(2*n-2)*y + f0
+
+
+### Transform: Exp(k*x^n)
+# y = z * exp(k*x^n)
+# dy = (dz + n*k*x^(n-1)*z) * exp(k*x^n)
+# d2y = (d2z + 2*n*k*x^(n-1)*dz + n^2*k^2*x^(2*n-2)*z) * exp(k*x^n)
+
+### Order Reduction
+# =>
+# d2z + 2*n*k*x^(n-1)*dz = f0 * exp(-k*x^n)
 
