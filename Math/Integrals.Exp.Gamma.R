4b096851d68c2bfcea163b60fbe3332b79baacf5
discoleo <leo.mada@syonic.eu>
2024-12-14 18:40:18 +0200

    I( W(1/x^n) ) on [0, Inf];

--- Math/Integrals.Exp.Gamma.R
+++ Math/Integrals.Exp.Gamma.R
@@ -38,3 +38,17 @@ integrate(\(x) exp(-x^n/n), 0, 1)
 id = seq(1, 31, by=n)
 sum(1/cumprod(id)) / exp(1/n)
 
+
+####################
+####################
+
+### Lambert W
+
+### I( W(1/x^n) )
+# Dr Peyam: Destroying the Lambert integral with a triple u-substitution
+# https://www.youtube.com/watch?v=aMsNS5xz7U0
+
+n = sqrt(5)
+integrate(\(x) pracma::lambertWp(1/x^n), 0, Inf)
+gamma(1 - 1/n) * n^(1-1/n)
+
