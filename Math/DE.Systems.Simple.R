528dced2c910d782ffdc7e2438957ea8af468663
discoleo <leo.mada@syonic.eu>
2025-02-25 23:20:00 +0200

    DE: Simple Coupled System
    Case: b, c = f(x);

--- Math/DE.Systems.Simple.R
+++ Math/DE.Systems.Simple.R
@@ -20,3 +20,14 @@ d2y2 - (b1 + c2)*dy2 + (b1*c2 - b2*c1)*y2 - c1*f1 + b1*f2 - df2 # = 0
 # => Solve ODE of Order 2;
 
 # TODO: check;
+
+### Case: b, c = f(x)
+d2y2 - c1*dy1 - dc1*y1 - c2*dy2 - dc2*y2 - df2 # = 0
+d2y2 - (b1 + c2)*dy2 - dc1*y1 +
+	+ (b1*c2 - b2*c1 - dc2)*y2 - c1*f1 + b1*f2 - df2 # = 0
+c1*d2y2 - c1*(b1 + c2)*dy2 - dc1*dy2 +
+	+ c1*(b1*c2 - b2*c1 - dc2)*y2 + dc1*c2*y2 +
+	- c1^2*f1 + b1*c1*f2 - c1*df2 + dc1*f2 # = 0
+
+# TODO: check;
+
