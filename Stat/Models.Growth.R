026c27dead033ccda74a458964ef012e4f1cf47a
discoleo <leo.mada@syonic.eu>
2025-02-28 20:37:50 +0200

    Enhanced colours + lwd = 2;

--- Stat/Models.Growth.R
+++ Stat/Models.Growth.R
@@ -4,7 +4,7 @@
 ##
 ## Leonard Mada
 ##
-## draft v.0.1a
+## draft v.0.1b
 
 
 ### Models:
@@ -39,13 +39,13 @@ eval.Exp = function(x, nm, e, params) {
 }
 
 ### Plot:
-curve.ref = function(col = "#F02424E2", params = NULL, Vmax = 2,
+curve.ref = function(col = "#F02424E2", lwd = 2, params = NULL, Vmax = 2,
 		col.h = "#FF3224B0") {
 	ylim = c(0, Vmax + 0.1);
 	if(is.null(params)) params = list(Vmax = Vmax, b = 1, n = 1);
-	curve(eval.Exp(x, "t", MM.eq, params), col = col,
+	curve(eval.Exp(x, "t", MM.eq, params), col = col, lwd=lwd,
 		xlim=xlim, ylim=ylim, ylab = "Growth");
-	abline(h = Vmax, col = col.h);
+	abline(h = Vmax, col = col.h, lwd=lwd);
 }
 plot.curve = function() {
 	# TODO
@@ -104,49 +104,66 @@ plot.curve = function() {
 ### NLS Types ###
 #################
 
+col.blue    = c("#0000FFC0", "#72A2FAA0", "#A888FAA0", "#88A8FAA0");
+col.magenta = c("#F000FFC0", "#F8A2A2A0", "#F864B4A0", "#F8B4FFA0");
+col.green   = c("#00FF00A0", "#32FC90A0", "#64FC90A0", "#90FE64A0");
+col.brown   = c("#F8B464A0");
+
+
 ### Michaelis-Menten type:
 # V = Vmax * t^n / (b + t^n)
+
+MM.eq = expression(Vmax * t^n / (b + t^n));
+
+curve.MM = function(col, lwd = 2, legend = TRUE, Vmax = 2) {
+	params = list(Vmax = Vmax, b = 1, n = 1)
+	# curve(eval.Exp(x, "t", MM.eq, params), col = col[1], xlim=xlim, ylim=ylim)
+	curve.ref(params = params, col = col[1], lwd=lwd);
+	params = list(Vmax = Vmax, b = 2/5, n = 1)
+	curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[2], lwd=lwd)
+	params = list(Vmax = Vmax, b = 2, n = 1)
+	curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[3], lwd=lwd)
+	params = list(Vmax = Vmax, b = 3, n = 1)
+	curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[4], lwd=lwd)
+	if(legend) {
+		legend(12, 0.5, legend = paste0("MM", 1:4), fill = col);
+	}
+}
 
 xlim = c(0, 15); ylim = c(0, 2.1);
 # xlim = c(0, 100);  ylim = c(0, 2.1);
 # xlim = c(0, 1000); ylim = c(0, 2.1);
 #
-MM.eq = expression(Vmax * t^n / (b + t^n))
-col = c("#0000FFC0", "#72A2FAA0", "#A888FAA0", "#88A8FAA0")
-params = list(Vmax = 2, b = 1, n = 1)
-# curve(eval.Exp(x, "t", MM.eq, params), col = col[1], xlim=xlim, ylim=ylim)
-curve.ref(params = params, col = col[1]);
-params = list(Vmax = 2, b = 2/5, n = 1)
-curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[2])
-params = list(Vmax = 2, b = 2, n = 1)
-curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[3])
-params = list(Vmax = 2, b = 3, n = 1)
-curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[4])
-legend(12, 0.5, legend = paste0("MM", 1:4), fill = col)
+lwd = 2;
+
+curve.MM(col.blue, lwd=lwd)
 #
-col = c("#0000FFC0", "#F8A2A2A0", "#F864B4A0", "#F8B464A0")
+col = col.magenta
 params = list(Vmax = 2, b = 1/2, n = 1/2)
-curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[2])
+curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[2], lwd=lwd)
 params = list(Vmax = 2, b = 1, n = 1/2)
-curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[3])
+curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[3], lwd=lwd)
 params = list(Vmax = 2, b = 2, n = 1/2)
-curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[4])
+curve(eval.Exp(x, "t", MM.eq, params), add = T, col = col[4], lwd=lwd)
 
 
 ### Saturated Exponential type:
 # V = Vmax * (1 - exp(-b*t^n)^k)
 
 exps.eq = expression(Vmax * (1 - exp(-b*t^n))^k)
-col = c("#FF0000A0", "#FC2490A0", "#FC9064A0", "#FE8090A0")
+
+curve.MM(col.blue, lwd=lwd)
 #
+col = col.green;
 params = list(Vmax = 2, b = 1, n = 1, k = 1)
-curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[1])
+curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[1], lwd=lwd)
 params = list(Vmax = 2, b = 1, n = 1/2, k = 1)
-curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[2])
+curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[2], lwd=lwd)
+# Param b:
 params = list(Vmax = 2, b = 1/2, n = 1, k = 1)
-curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[3])
+curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[3], lwd=lwd)
 params = list(Vmax = 2, b = 1/5, n = 1, k = 1)
-curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[4])
+curve(eval.Exp(x, "t", exps.eq, params), add = T, col = col[4], lwd=lwd)
 
 
 #################
