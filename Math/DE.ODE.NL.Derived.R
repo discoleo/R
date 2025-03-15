ff54577d7a051b9bf2a3a590be591aff6bb53fa7
discoleo <leo.mada@syonic.eu>
2025-03-15 04:34:25 +0200

    d2y - 2*b^2*x^2*y^3 - b*y^2 - 2*b*x*y*fx = dfx;
    Order 1 => Order 2;

---
+++ Math/DE.ODE.NL.Derived.R
@@ -0,0 +1,17 @@
+
+
+
+######################
+
+### Order 1 => Order 2
+
+# dy = b*x*y^2 + fx
+
+# D =>
+d2y - 2*b*x*y*dy - b*y^2 - dfx # = 0
+# Subst =>
+d2y - 2*b*x*y*(b*x*y^2 + fx) - b*y^2 - dfx # = 0
+
+### ODE:
+d2y - 2*b^2*x^2*y^3 - b*y^2 - 2*b*x*y*fx - dfx # = 0
+
