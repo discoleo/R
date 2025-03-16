8eea6f9b3ba320f3e9a40b7f6d1f925c6f560ae0
discoleo <leo.mada@syonic.eu>
2025-03-16 17:51:34 +0200

    ODE NL: more examples;

--- Math/DE.ODE.NL.Derived.R
+++ Math/DE.ODE.NL.Derived.R
@@ -69,6 +69,7 @@ d2y - 2*x^2 * y^3 - (6*x^2 + 9*x + 1) * y^2 +
 # Initial ODE:
 dy - x*y^2 - 2*x*y - 3*y - fx # = 0
 
+
 ### Solve:
 ord1.f = function(t, y, parms, ...) {
 	x  = t;
@@ -89,20 +90,69 @@ ord2.f = function(t, y, parms, ...) {
 params = list(
 	FUN  = \(x) - x^2/3 - 3*x - 1,
 	DFUN = \(x) - 2/3*x - 3);
+# Numerical instabilities in 2nd order ODE:
 x  = seq(0, 4, by = 1/4096 / 4);
-y  = c(y = -1/2); # y = c(y = -2/7);
+y  = c(-1/2); # y = c(-2/7);
 y2 = c(y = y, dy = 3*y + params$FUN(0))
 
 sol1 = ode(y,  x, func = ord1.f, parms = params)
-sol2 = ode(y2, x, func = ord2.f, parms = params, type="rk4")
-col = "#FF6432A0"
-matplot(sol1[,1], sol1[,-1], type = "l", lwd=2)
-matplot(sol2[,1], sol2[, 2], type = "l", lwd=3, col=col, add=T)
+sol2 = ode(y2, x, func = ord2.f, parms = params)
+col = c("#2472E6A0", "#FF643290")
+matplot(sol1[,1], sol1[,-1], type = "l", lwd=2, col=col[1])
+matplot(sol2[,1], sol2[, 2], type = "l", lwd=3, col=col[2], lty=2, add=T)
 
 
-# TODO:
-# Check what happens for x > 2?
-# Just numerical instabilities?
+# sol = cbind(sol1, sol2[, 2]);
+# matplot(sol[,1], sol[,-1], type = "l", lwd=2, col=col)
+# Note: FAILS when 2nd ODE only partially solved;
+
+# Note: 2nd Order ODE
+# Numerical instabilities for x > 2!
 
 
+##############
+### Example 2:
+
+### Example:
+b = c(-1,1,3)
+names(b) = paste0("b", 2:0)
+
+### ODE:
+d2y - 2*x^2 * y^3 + (3*x^2 + 9*x + 1) * y^2 +
+	- (x^2 - 2*x*fx + 6*x + 10) * y - (x+3)*fx - dfx # = 0
+
+# Initial ODE:
+dy + x*y^2 - x*y - 3*y - fx # = 0
+
+
+### Solve:
+ord1.f = function(t, y, parms, ...) {
+	x  = t;
+	fx = parms$FUN(x);
+	dy = - x*y^2 + x*y + 3*y + fx;
+	return(list(dy));
+}
+ord2.f = function(t, y, parms, ...) {
+	x  = t; dy = y[2]; y = y[1];
+	fx = parms$FUN(x); dfx = params$DFUN(x);
+	d2y = -2*x^2 * y^3 + (3*x^2 + 9*x + 1) * y^2 +
+		- (x^2 - 2*x*fx + 6*x + 10) * y - (x+3)*fx - dfx;
+	d2y = - d2y;
+	return(list(c(dy, d2y)));
+}
+
+### Test:
+params = list(
+	FUN  = \(x) 1/4*x^2 - 2*x + 1/2,
+	DFUN = \(x) 2/4*x - 2);
+# Less/NO Numerical instabilities:
+x  = seq(0, 4, by = 1/64);
+y  = c(1/2); # y = c(-1/5); # y = c(6/7);
+y2 = c(y = y, dy = 3*y + params$FUN(0))
+
+sol1 = ode(y,  x, func = ord1.f, parms = params)
+sol2 = ode(y2, x, func = ord2.f, parms = params)
+col = c("#2472E6A0", "#FF643290")
+matplot(sol1[,1], sol1[,-1], type = "l", lwd=2, col=col[1])
+matplot(sol2[,1], sol2[, 2], type = "l", lwd=3, col=col[2], lty=2, add=T)
 
