512853e4a2e20ebf740180e991f7734762caa72c
discoleo <leo.mada@syonic.eu>
2025-03-24 21:02:34 +0200

    ODE: from y = x * exp(x + k*sqrt(x + b0));
    Special Case:
    x^2*(4*x - 3)*d2y - 2*x*(4*x^2 - 3) * dy + (4*x^3 - 6) * y = 0;

--- Math/DE.ODE.Exponentials.Radicals.R
+++ Math/DE.ODE.Exponentials.Radicals.R
@@ -57,6 +57,49 @@ params = list(x=x, k=k, b0=b0);
 4*(x + b0)*d2y - (8*x + 8*b0 - 2)*dy + 4*x*y # = 0
 
 
+### Variant:
+### y = x * exp(x + k*sqrt(x + b0))
+
+x = sqrt(3); k = -1/5; b0 = 2/3; params = list(x=x, k=k, b0=b0);
+e = expression(x * exp(x + k*sqrt(x + b0)))[[1]];
+#
+y   = x * exp(x + k*sqrt(x + b0));
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+
+# D =>
+2*x*dy - (2 + 2*x + k*x/sqrt(x + b0)) * y # = 0
+
+# D2 =>
+4*x*d2y - (2 + k/sqrt(x + b0)) * (2 + 2*x + k*x/sqrt(x + b0)) * y +
+	- k*(1 + b0/(x + b0))/sqrt(x + b0) * y - 4*y # = 0
+4*x*d2y - k*(4*x + 3 + b0/(x + b0)) / sqrt(x + b0) * y +
+	- (k^2 + 8 + 4*x - k^2*b0/(x + b0)) * y # = 0
+4*x*(x + b0)*d2y - k*((4*x + 3)*(x + b0) + b0) / sqrt(x + b0) * y +
+	- ((k^2 + 8 + 4*x)*(x + b0) - k^2*b0) * y # = 0
+
+### ODE:
+4*x^2*(x + b0)*d2y - 2*x*(4*x^2 + (4*b0+3)*x + 4*b0) * dy +
+	+ (4*x^3 + (4*b0 + 6 - k^2)*x^2 + 8*b0*x + 6*x + 8*b0) * y # = 0
+
+
+### Special Cases:
+
+### k = sqrt(4*b0 + 6)
+x = sqrt(3); b0 = 2/3; k = sqrt(4*b0 + 6);
+params = list(x=x, k=k, b0=b0);
+# Re-run assignments from above;
+4*x^2*(x + b0)*d2y - 2*x*(4*x^2 + (4*b0+3)*x + 4*b0) * dy +
+	+ (4*x^3 + 8*b0*x + 6*x + 8*b0) * y # = 0
+
+### & b0 = -3/4
+x = sqrt(5); b0 = -3/4; k = sqrt(4*b0 + 6); # k = sqrt(3);
+params = list(x=x, k=k, b0=b0);
+# Re-run assignments from above;
+x^2*(4*x - 3)*d2y - 2*x*(4*x^2 - 3) * dy + (4*x^3 - 6) * y # = 0
+
+
 ##############################
 
 ### y = exp(x + sqrt(x^2 + k))
