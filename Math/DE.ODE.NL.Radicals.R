245e9df59f53e1c340708883a24151d7ba0ce71d
discoleo <leo.mada@syonic.eu>
2025-03-26 02:44:52 +0200

    NL ODE: 8*x^3*y*dy*d2y - 8*x^3*dy^3 + 8*x^2*y*dy^2 - 9*k^2*y^3 = 0;

--- Math/DE.ODE.NL.Radicals.R
+++ Math/DE.ODE.NL.Radicals.R
@@ -34,6 +34,29 @@ x^2*y^2*d2y + x*y^2*dy - x^2*y*dy^2 + 2*x^3*dy^3 # = 0
 x*y^2*d2y + 2*x^2*dy^3 - x*y*dy^2 + y^2*dy # = 0
 
 
+#########################
+
+### y = exp(k * sqrt(log(x))^3)
+
+x = sqrt(5); k = sqrt(2);
+params = list(x=x, k=k);
+e = expression(exp(k * sqrt(log(x))^3))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+# D =>
+2*x*dy - 3*k*sqrt(log(x)) * y # = 0
+
+# D2 =>
+4*x^2*d2y + 4*x*dy - 9*k^2*log(x) * y +
+	- 3*k / sqrt(log(x)) * y # = 0
+
+### ODE:
+8*x^3*y*dy*d2y - 8*x^3*dy^3 + 8*x^2*y*dy^2 - 9*k^2*y^3 # = 0
+
+
 #############################
 
 ### y = exp(x * sqrt(log(x)))
