775e587c174d182ae6c81fd8a766b4f9ae11b142
discoleo <leo.mada@syonic.eu>
2025-04-17 01:13:01 +0300

    [fix] + Solved Linear System;

--- Math/DE.ODE.NL.LogY.R
+++ Math/DE.ODE.NL.LogY.R
@@ -7,22 +7,59 @@
 ## Log( F(Y) )-Types
 
 
+####################
+
+### Helper Functions
+
+source("Polynomials.Helper.R")
+source("DE.ODE.Helper.R")
+
+
+###################
+
 ###################
 ### Log-Product ###
 ###################
 
 ### log(y + x) * log(y - x) = P(x)
 
+### Check:
+ye = expression((x+b0)^(4/3))[[1]]
+pe = expression(log((x+b0)^(4/3) + x) * log((x+b0)^(4/3) - x))[[1]]
+x = sqrt(3); b0 = sqrt(2);
+params = list(x=x, b0=b0);
+#
+y = eval(ye, params); dy = eval(D(ye, "x"), params); 
+p = eval(pe, params); dp = eval(D(pe, "x"), params);
+d2y = eval(D(D(ye, "x"), "x"), params);
+d2p = eval(D(D(pe, "x"), "x"), params);
+
 ### D =>
 (dy + 1)*(y - x)*log(y - x) + (dy - 1)*(y + x)*log(y + x) - (y^2 - x^2)*dp # = 0
 
 ### D2 =>
 (d2y*(y - x) + dy^2 - 1)*log(y - x) + (d2y*(y + x) + dy^2 - 1)*log(y + x) +
-	+ 2*dy^2 - 2 - 2*(y*dy - 2*x)*dp - (y^2 - x^2)*d2p # = 0
+	+ 2*dy^2 - 2 - 2*(y*dy - x)*dp - (y^2 - x^2)*d2p # = 0
 
 ### Solve linear system:
-log(y + x) = - ... /2 / (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
-log(y - x) = ... /2 / (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+div = 2*y^2*d2y - 2*x^2*d2y - 2*x*dy^3 + 2*y*dy^2 + 2*x*dy - 2*y;
+# log(y + x)
+(- (dp*y^3 - x*dp*y^2 - x^2*dp*y + x^3*dp)*d2y +
+	- 2*(y-x)*dy^3 + (dp*y^2 - 2*x*dp*y - 2*y + x^2*dp + 2*x)*dy^2 +
+	+ (d2p*y^3 - d2p*x*y^2 + 2*dp*y^2 - d2p*x^2*y - 4*dp*x*y + 2*y +
+		+ d2p*x^3 + 2*dp*x^2 - 2*x)*dy +
+	+ d2p*y^3 - d2p*x*y^2 + dp*y^2 - d2p*x^2*y - 2*dp*x*y + 2*y +
+	+ x^3*d2p + x^2*dp - 2*x) / div;
+
+# log(y - x)
+((dp*y^3 + x*dp*y^2 - x^2*dp*y - x^3*dp)*d2y +
+	+ 2*(y+x)*dy^3 - (dp*y^2 + 2*x*dp*y + 2*y + x^2*dp + 2*x)*dy^2 +
+	- (d2p*y^3 + d2p*x*y^2 - 2*dp*y^2 - d2p*x^2*y +
+		- 4*x*dp*y + 2*y - x^3*d2p - 2*x^2*dp + 2*x)*dy +
+	+ d2p*y^3 + d2p*x*y^2 - dp*y^2 - d2p*x^2*y - 2*dp*x*y + 2*y +
+	- x^3*d2p - x^2*dp + 2*x) / div;
+
+# TODO: simplify / group;
 
 
 ### ODE:
