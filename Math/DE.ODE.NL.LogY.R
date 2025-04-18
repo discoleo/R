a19cfbded93c7974d3f98e5603cc4c8e3d337491
discoleo <leo.mada@syonic.eu>
2025-04-18 05:01:37 +0300

    [minor] Code to compute ODE;
    & update Example;

--- Math/DE.ODE.NL.LogY.R
+++ Math/DE.ODE.NL.LogY.R
@@ -68,21 +68,35 @@ div = 2*y^2*d2y - 2*x^2*d2y - 2*x*dy^3 + 2*y*dy^2 + 2*x*dy - 2*y;
 # TODO: huge;
 
 
-### Example:
+# Derivation:
+pDiv = as.pm("2*(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)");
+# pLp, pLn = expressions for the 2 logs (without the division by div);
+pR = as.pm("pLp() * pLn() - p*pDiv()^2");
+pR = sort.dpm(pR, "y", "x");
+print.pm(pR, lead = NA); # 101 monomials;
+
+
+### Examples:
+
 ### P(x) = x
 (dy + 1)*(y - x)*log(y - x) + (dy - 1)*(y + x)*log(y + x) - (y^2 - x^2) # = 0
+# =>
 (d2y*(y - x) + dy^2 - 1)*log(y - x) + (d2y*(y + x) + dy^2 - 1)*log(y + x) +
 	+ 2*(dy^2 - y*dy + x - 1) # = 0
 # =>
-2*log(y + x) = - (y - x) * (2*(dy + 1)*(dy^2 - y*dy + x - 1) + (d2y*(y - x) + dy^2 - 1)*(y + x)) /
-	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
-2*log(y - x) = - (y + x) * (2*(dy - 1)*(dy^2 - y*dy + x - 1) + (d2y*(y + x) + dy^2 - 1)*(y - x)) /
-	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+div = (y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y);
+2*log(y + x) # =
+- (y - x) * (2*(dy + 1)*(dy^2 - y*dy + x - 1) + (d2y*(y - x) + dy^2 - 1)*(y + x)) / div;
+2*log(y - x) # =
++ (y + x) * (2*(dy - 1)*(dy^2 - y*dy + x - 1) + (d2y*(y + x) + dy^2 - 1)*(y - x)) / div;
 # =>
-2*log(y + x) = - (y - x)*(y^2*d2y - x^2*d2y + 2*dy^3 - y*dy^2 + x*dy^2 + 2*dy^2 - 2*y*dy + 2*x*dy - 2*dy - y + x - 2) /
-	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
-2*log(y - x) = - (y + x)*(...) /
-	(y^2*d2y - x^2*d2y - x*dy^3 + y*dy^2 + x*dy - y)
+2*log(y + x) # =
+- (y - x)*(y^2*d2y - x^2*d2y + 2*dy^3 - y*dy^2 + x*dy^2 + 2*dy^2 +
+	- 2*y*dy + 2*x*dy - 2*dy - y + x - 2) / div;
+2*log(y - x) # =
++ (y + x)*(y^2*d2y - x^2*d2y + 2*dy^3 - y*dy^2 - x*dy^2 - 2*dy^2 +
+	+ 2*y*dy + 2*x*dy - 2*dy - y - x + 2) / div;
+
 
 ### ODE:
 (y^2 - x^2)*(...)*(...) +
