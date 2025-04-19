de0a70086d5e1796318a0fddef2aa4646d4b4526
discoleo <leo.mada@syonic.eu>
2025-04-19 22:07:31 +0300

    ODE from: y = x * sin(x^n) * exp(-k*x^n);

--- Math/DE.ODE.Mixed.Exp.Trig.R
+++ Math/DE.ODE.Mixed.Exp.Trig.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## Mixed Exp & Trig
 ##
-## draft v.0.1a
+## draft v.0.1b
 
 
 ### History:
@@ -104,4 +104,47 @@ sapply(x.px, line.tan, dx=2, p=y, dp=dy, n=n)
 # wave
 curve(dy(x, n=n), add=T, col="green")
 sapply(x.px, line.tan, dx=1.25, p=dy, dp=d2y, n=n, col="orange")
+
+
+####################
+
+### y = x * sin(x^n) * exp(-k*x^n)
+
+### Check:
+ye = expression(x * sin(x^n) * exp(-k*x^n))[[1]]
+x = sqrt(3); n = sqrt(2); k = 1/sqrt(5);
+params = list(x=x, n=n, k=k);
+#
+y = eval(ye, params); dy = eval(D(ye, "x"), params);
+d2y = eval(D(D(ye, "x"), "x"), params);
+
+
+### ODE:
+x^2*d2y + x*(2*k*n*x^n - (n+1))*dy +
+	+ ((k^2+1)*n^2*x^(2*n) - 2*k*n*x^n + n + 1)*y # = 0
+
+
+### Derivation:
+
+### D(y):
+x*dy - y - n*x^(n+1)*cos(x^n)*exp(-k*x^n) + k*n*x^n*y # = 0
+
+### D2(y):
+x*d2y +
+	- n*(n+1)*x^n*cos(x^n)*exp(-k*x^n) +
+	+ n^2*x^(2*n)*sin(x^n)*exp(-k*x^n) +
+	+ k*n^2*x^(2*n)*cos(x^n)*exp(-k*x^n) +
+	+ k*n*x^n*dy + k*n^2*x^(n-1)*y # = 0
+x^2*d2y + x*(2*k*n*x^n - (n+1))*dy +
+	+ ((k^2+1)*n^2*x^(2*n) - 2*k*n*x^n + n + 1)*y # = 0
+
+
+### Special Cases:
+
+### k = 1
+x^2*d2y + x*(2*n*x^n - (n+1))*dy +
+	+ (2*n^2*x^(2*n) - 2*n*x^n + n + 1)*y # = 0
+
+### n = -1
+x^4*d2y - 2*k*x^2*dy + (2*k*x + k^2 + 1)*y # = 0
 
