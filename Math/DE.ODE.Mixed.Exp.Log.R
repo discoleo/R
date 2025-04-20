b702e20a2c0cf669ba03c3935160651c4cae0ee1
discoleo <leo.mada@syonic.eu>
2025-04-20 15:17:05 +0300

    ODE from y = log(x + b0) * exp(k/x);

--- Math/DE.ODE.Mixed.Exp.Log.R
+++ Math/DE.ODE.Mixed.Exp.Log.R
@@ -6,7 +6,11 @@
 ## Differential Equations
 ## Mixed Exp & Log
 ##
-## draft v.0.1a
+## draft v.0.1b
+
+
+### Linear ODE of Order 2
+#   with Polynomial coefficients;
 
 
 ### Derived from:
@@ -102,4 +106,45 @@ line.tan(px, dx=3, p=y, dp=dy, b=b)
 # global minimum:
 curve(dy(x, b=b), add=T, col="green")
 line.tan(px, dx=3, p=dy, dp=d2y, b=b, col="orange")
+
+
+###################
+###################
+
+### y = log(x + b0) * exp(k/x)
+
+### Check:
+ye = expression(log(x + b0) * exp(k/x))[[1]]
+x  = sqrt(3); b0 = sqrt(2); k = -1/sqrt(5);
+params = list(x=x, k=k, b0=b0);
+#
+y = eval(ye, params); dy = eval(D(ye, "x"), params);
+d2y = eval(D(D(ye, "x"), "x"), params);
+
+### ODE:
+x^4*(x + b0)*d2y + x^2*(x^2 + 2*k*x + 2*k*b0)*dy +
+	- k*(x^2 + (2*b0 - k)*x - k*b0)*y # = 0
+
+
+### Derivation:
+
+# D =>
+x^2*(x + b0)*dy + k*(x + b0)*y - x^2*exp(k/x) # = 0
+
+# D2 =>
+x^2*(x + b0)*d2y + (3*x^2 + 2*b0*x + k*x + k*b0)*dy + k*y +
+	- (2*x - k)*exp(k/x) # = 0
+x^4*(x + b0)*d2y + x^2*(3*x^2 + 2*b0*x + k*x + k*b0)*dy + k*x^2*y +
+	- (2*x - k)*(x^2*(x + b0)*dy + k*(x + b0)*y) # = 0
+x^4*(x + b0)*d2y + x^2*(x^2 + 2*k*x + 2*k*b0)*dy +
+	- k*(x^2 + (2*b0 - k)*x - k*b0)*y # = 0
+
+
+### Special Cases:
+
+### k = 2*b0
+b0 = 1/sqrt(5); k = 2*b0;
+params = list(x=x, k=k, b0=b0);
+#
+x^4*(x + b0)*d2y + x^2*(x + 2*b0)^2*dy - 2*b0*(x^2 - 2*b0^2)*y # = 0
 
