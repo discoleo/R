2fdddd385cd348ce381948b41918648af5558b67
discoleo <leo.mada@syonic.eu>
2025-05-02 18:40:42 +0300

    [refactor] moved Combinations ATAN * LOG to new file;

--- Math/Integrals.Trig.Tan.R
+++ Math/Integrals.Trig.Tan.R
@@ -220,6 +220,8 @@ integrate(\(x) - log(tan(x) + 1), 0, pi/2)$value +
 # moved to file: Integrals.Trig.Tan.Atan.Trig.R;
 ### ATAN: Fractions of type ATAN() / Poly
 # moved to file: Integrals.Trig.Tan.Atan.Fractions.R;
+### ATAN * LOG
+# moved to file: Integrals.Trig.Tan.Atan.LogCombi.R;
 
 
 ### I( atan(x^n) )
@@ -249,105 +251,6 @@ Catalan / n;
 
 
 ###################
-
-### I( x^p * log(x) * atan(x) )
-# Maths 505: A cool result: int (0,1) arctan(x)ln(x)
-# https://www.youtube.com/watch?v=zL169U85SZc
-# Note: uses series expansion of atan,
-#   but method based on D(digamma) is more powerful;
-
-
-###
-p = sqrt(3)
-integrate(\(x) x^p * log(x) * atan(x), 0, 1)
-(pracma::psi(1, (p+2)/4) - pracma::psi(1, p/4 + 1)) / (16*(p+1)) +
-	- (digamma((p+2)/4) - digamma(p/4 + 1) + pi) / (4*(p+1)^2);
-
-### Example:
-integrate(\(x) log(x) * atan(x), 0, 1)
-pi^2 / 48 - pi/4 + log(2)/2
-
-
-### Varia:
-p = sqrt(3)
-integrate(\(x) x^p * log(x) / (x^2 + 1), 0, 1)
-(pracma::psi(1, (p+1)/4 + 1/2) - pracma::psi(1, (p+1)/4)) / 16
-
-###
-p = sqrt(3)
-integrate(\(x) x^p * log(x)^2 / (x^2 + 1), 0, 1)
-(pracma::psi(2, (p+1)/4 + 1/2) - pracma::psi(2, (p+1)/4)) / 64
-
-###
-p = sqrt(3)
-integrate(\(x) x^p / (x^2 + 1)^2, 0, 1)
-(digamma((p+1)/4) - digamma((p-1)/4)) * (p-1)/8 - 1/4
-
-
-### Full:
-
-### I( x^p * atan(x^n) * log(x) )
-n = sqrt(5); p = sqrt(3);
-# works only for n > 0;
-integrate(\(x) x^p * atan(x^n) * log(x), 0, 1)
-- (pracma::psi(1, ((p+1)/n + 3)/4) - pracma::psi(1, ((p+1)/n + 1)/4)) / (16*n*(p+1)) +
-	+ (digamma(((p+1)/n + 3)/4) - digamma(((p+1)/n + 1)/4)) / (4*(p+1)^2) +
-	- pi/(4*(p+1)^2);
-
-
-### Special Case:
-integrate(\(x) log(x) * atan(x), 0, 1)
-pi^2 / 48 - pi/4 + log(2)/2
-
-
-integrate(\(x) log(x) * atan(x) / x, 0, 1)
-- pi^3 / 32
-
-
-####################
-
-### Atan * Log
-
-### I( atan(x) * log(x) )
-integrate(\(x) atan(x) * log(x), 0, 1)
-- pi^2 / 48 + pi/4 - log(2)/2
-
-### I( atan(x) * log(1-x) )
-integrate(\(x) atan(x) * log(1-x), 0, 1)
-integrate(\(x) ((1-x)*log(1-x) + x) / (x^2+1) - pi/4, 0, 1)
-integrate(\(x) (1-x)*log(1-x) / (x^2+1) - pi/4 + log(2)/2, 0, 1)
-5/96 * pi^2 - log(2)^2 / 8 + pi*log(2)/8 - Catalan - pi/4 + log(2)/2;
-
-### I( atan(x) / (1-x) )
-integrate(\(x) atan(x) / (1-x) - pi/4/(1-x), 0, 1)
-pi*log(2)/8 - Catalan
-
-### I( log(x^2+1) * log(1-x) )
-integrate(\(x) log(x^2+1) * log(1-x), 0, 1)
-integrate(\(x) (1-x)*log(1-x) * 2*x/(x^2+1) + 2 - pi/2 - log(2), 0, 1)
-integrate(\(x) 2*(x+1)*log(1-x) / (x^2+1) + 4 - pi/2 - log(2), 0, 1)
--5/48 * pi^2 + log(2)^2 / 4 + pi*log(2)/4 - 2*Catalan - pi/2 - log(2) + 4;
-
-# Complex:
-integrate(\(x) Re(log(x + 1i)) * log(1-x), 0, 1)
--5/96 * pi^2 + log(2)^2 / 8 + pi*log(2)/8 - Catalan - pi/4 - log(2)/2 + 2;
-#
-integrate(\(x) Im(log(x + 1i)) * log(1-x), 0, 1)
-- (5/96 * pi^2 - log(2)^2 / 8 + pi*log(2)/8 - Catalan + pi/4 + log(2)/2);
-
-
-###
-integrate(\(x) atan(x)^2 * log(x), 0, 1)
-integrate(\(x) - 2*x * atan(x) * log(x) / (x^2+1), 0, 1)$value +
-	- (pi/4)^2 + Catalan - 1/4 * pi * log(2)
-
-# TODO
-
-###
-integrate(\(x) atan(x)^2 * log(1 - x), 0, 1)
-# TODO
-
-
 ###################
 ### ATAN: Fractions
 
