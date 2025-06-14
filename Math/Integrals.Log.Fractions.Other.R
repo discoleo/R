c92d72ff39fb905850d6511b1f86c212c38cd2fd
discoleo <leo.mada@syonic.eu>
2025-06-14 04:47:54 +0300

    some Li2 examples;

--- Math/Integrals.Log.Fractions.Other.R
+++ Math/Integrals.Log.Fractions.Other.R
@@ -305,6 +305,7 @@ integrate(\(x) - 1/2 * log(1-x^3) / (x+1), 0, 1)$value +
 # TODO
 
 # - using Li2:
+#   DiffLi2f3 = 0.67524635646487196;
 (pracma::psi(1, 1/3) + 2*pracma::psi(1, 2/3)) / 36 +
 	- (pracma::polylog(1/3, 2) - pracma::polylog(-1/3, 2)) / 2;
 
@@ -541,6 +542,45 @@ integrate(\(x) Im(log(x+1) / (x + 1/2+1i*sqrt(3)/2)), 0, 1)
 
 ##################
 ##################
+
+### Log( DIV )
+
+### I( log((1-x)/(x+1)) / (1 - 4*x^2) )
+# on [0, Inf]
+integrate(\(x) log((1-x)/(x+1)) / (1 - 4*x^2) + log(3)/(2-4*x), 0, 1/2)$value +
+integrate(\(x) log((1-x)/(x+1)) / (1 - 4*x^2) + log(3)/(2-4*x), 1/2, 1)$value +
+integrate(\(x) log((x-1)/(x+1)) / (1 - 4*x^2), 1, Inf)$value
+pi^2/24 - pracma::polylog(-1/2, 2)/2 - (log(3/2)^2 - log(3)^2)/4;
+
+### on [1, Inf]
+integrate(\(x) log((x-1)/(x+1)) / (1 - 4*x^2), 1, Inf)
+pi^2/24 - (2*log(3/2)^2 - log(3)^2) / 8 +
+- (pracma::polylog(-1/2, 2) + pracma::polylog(1/3, 2) - pracma::polylog(-1/3, 2)) / 2;
+
+### on [0, 1]
+integrate(\(x) log((1-x)/(x+1)) / (1 - 4*x^2) + log(3)/(2-4*x), 0, 1/2)$value +
+integrate(\(x) log((1-x)/(x+1)) / (1 - 4*x^2) + log(3)/(2-4*x), 1/2, 1)$value
+log(3)^2 / 8 + (pracma::polylog(1/3, 2) - pracma::polylog(-1/3, 2)) / 2;
+pi^2/12 - (pracma::polylog(1/3, 2) + log(3)^2 / 4) / 2;
+
+### on [0, 1/2]
+integrate(\(x) log((1-x)/(x+1)) / (1 - 4*x^2) + log(3)/(2-4*x), 0, 1/2)
+integrate(\(x) log((x-1)/(x+1)) / (1 - 4*x^2), 1, Inf)$value - log(2) * log(3) / 4;
+pi^2/24 - (2*log(3/2)^2 - log(3)^2 + 2*log(2) * log(3)) / 8 +
+- (pracma::polylog(-1/2, 2) + pracma::polylog(1/3, 2) - pracma::polylog(-1/3, 2)) / 2;
+
+# Helper:
+integrate(\(x) log((1-x)) / (x^2 - 4), 0, 1)
+pi^2/48 + pracma::polylog(1/3, 2)/4;
+
+#
+integrate(\(x) log((1+x)) / (x^2 - 4), 0, 1)
+-3/4 * pracma::polylog(1/3, 2) + pi^2/16 - log(3)^2 * 3/8;
+
+# Li2(-1/3)
+pracma::polylog(-1/3, 2)
+2*pracma::polylog(1/3, 2) - pi^2/6 + log(3)^2 / 2;
+
 
 ### I( log((b - x)/(b + x)) / (x * sqrt(1 - x^2)) ) on [0, 1]
 # Maths 505: DESTROYING a MONSTER integral using Feynman's technique
