692742f098f1f57e912a304530a7898bab6af9a1
discoleo <leo.mada@syonic.eu>
2025-07-02 02:28:24 +0300

    Gen: I( sin(k*x)^2 * exp(a1*x) / (exp(a2*x) + 1) ) on [0, Inf];
    - full series;

--- Math/Integrals.Exp.Trig.R
+++ Math/Integrals.Exp.Trig.R
@@ -317,6 +317,13 @@ integrate(\(x) sin(k*x)^2 * exp(a1*x) / (exp(a2*x) - 1), 0, Inf)
 (Re(pracma::psi(0, 1 - (a1+k*2i)/a2)) - digamma(1 - a1/a2)) / (2*a2);
 
 
+### Gen: I( sin(k*x)^2 * exp(a1*x) / (exp(a2*x) + 1) )
+k = sqrt(3); a1 = -1/sqrt(2); a2 = sqrt(5);
+integrate(\(x) sin(k*x)^2 * exp(a1*x) / (exp(a2*x) + 1), 0, Inf)
+(Re(pracma::psi(0, 1 - (a1+k*2i)/a2) - pracma::psi(0, 1 - (a1+k*2i)/(2*a2))) +
+	+ digamma(1 - a1/(2*a2)) - digamma(1 - a1/a2)) / (2*a2);
+
+
 ###########
 ### Pow = 3
 
