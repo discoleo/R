123e4426a8b85637ac836f3e5cace38b8f009313
discoleo <leo.mada@syonic.eu>
2025-07-17 03:47:55 +0300

    I( (1 + x^5)^(3/5) / (1-x^10)^(4/5) / x ) on any;

--- Math/Integrals.Fractions.Unity.Radicals.TanB.R
+++ Math/Integrals.Fractions.Unity.Radicals.TanB.R
@@ -39,3 +39,20 @@ integrate(\(x) x^2 / (x^5 + 1), tan(lim)^(2/5), Inf)
 integrate(\(x) x * (1-x^5)^(-2/5), 0, 1/2)
 integrate(\(x) 2/5 * tan(x)^(1/5), acos(1/2^(5/2)), pi/2)
 
+
+### Diff-Type
+
+### I( (1 + x^5)^(3/5) / (1-x^10)^(4/5) / x )
+lim = 1/2;
+integrate(\(x) (1 + x^5)^(3/5) / (1-x^10)^(4/5) / x, lim, 1)
+integrate(\(x) 1 / (1 - x^5), 0, tan(acos(lim^5)/2)^(2/5))
+
+
+# Derivation:
+lim = 1/3
+integrate(\(x) 1 / (1 - x^5), 0, lim)
+integrate(\(x) 2/5 * tan(x)^(-3/5) / (cos(x)^2 - sin(x)^2), 0, atan(lim^(5/2)))
+integrate(\(x) 2^(8/5)/5 * cos(x)^(6/5) / sin(2*x)^(3/5) / cos(2*x), 0, atan(lim^(5/2)))
+integrate(\(x) 1/5 * (cos(x) + 1)^(3/5) / sin(x)^(3/5) / cos(x), 0, 2*atan(lim^(5/2)))
+integrate(\(x) (1 + x^5)^(3/5) / (1-x^10)^(4/5) / x, cos(2*atan(lim^(5/2)))^(1/5), 1)
+
