4ad464d6d39e8346425c7414f2ecb67d02262de5
discoleo <leo.mada@syonic.eu>
2025-07-21 02:41:41 +0300

    I( atan(k * sqrt(1-x^2)) / (1-x^2) ) on [0,1];

--- Math/Integrals.Trig.Tan.Atan.FrOther.R
+++ Math/Integrals.Trig.Tan.Atan.FrOther.R
@@ -5,6 +5,7 @@
 ## Integrals: ATAN
 ## Fractions: Other Types
 
+
 ### I( atan(sqrt(x^2 + b^2)) / ((x^2 + 1) * sqrt(x^2 + b^2)) )
 # Maths 505: Ahmed's monster integral: solution using Feynman's technique
 # https://www.youtube.com/watch?v=Rq-bYzyoSwU
@@ -99,4 +100,43 @@ integrate(\(x) atan(sqrt(x^2 + b^2)) / (x^2 + b^2)^(3/2), lim[1], lim[2])
 x = lim
 diff(x * atan(sqrt(x^2 + b^2)) / sqrt(x^2 + b^2) + b*atan(x/b) +
 	- sqrt(b^2 + 1) * atan(x/sqrt(b^2 + 1))) / b^2
+
+
+###################
+###################
+
+### Various SQRT
+
+### I( atan(k * sqrt(1-x^2)) / (1-x^2) )
+# Hmath: Интеграл с арктангенсом и корнем. Трюк Фейнмана
+# [in Russian]
+# Trig-Subst & Feynman trick;
+
+integrate(\(x) atan(sqrt(1-x^2)) / (1-x^2), 0, 1)
+((digamma(5/8) - digamma(1/8))^2 - (digamma(7/8) - digamma(3/8))^2) / 32
+pi/2 * log(sqrt(2) + 1);
+
+### Gen:
+k = 1/3
+integrate(\(x) atan(k * sqrt(1-x^2)) / (1-x^2), 0, 1)
+integrate(\(x) pi/2 / sqrt(x^2 + 1), 0, k)
+pi/2 * log(sqrt(k^2+1) + k);
+
+
+### I( atan(sqrt(1-x^2)) / x )
+integrate(\(x) atan(sqrt(1-x^2)) / x - pi/4/x, 0, 1, rel.tol=1E-12)
+pi * log(2) / 8 - Catalan / 2;
+
+### Var: atan(1/2 * sqrt(1-x^2)) / x
+integrate(\(x) atan(sqrt(1-x^2) / 2) / x - atan(1/2)/x, 0, 1, rel.tol=1E-12)
+integrate(\(x) atan(cos(x) / 2) / tan(x) - atan(1/2)/x, 0, pi/2)$value +
+	+ log(pi/2) * atan(1/2);
+integrate(\(x) - x / tan(x), 0, atan(1/2))$value +
+	+ atan(1/2) * log(2);
+# TODO
+
+
+### I( atan(sqrt(1-x^2)) / x^2 )
+integrate(\(x) atan(sqrt(1-x^2)) / x^2 - pi/4/x^2, 0, 1, rel.tol=1E-12)
+pi * (1 - sqrt(2)) / 4;
 
