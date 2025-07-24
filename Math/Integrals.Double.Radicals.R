9e300feb24b134c50207bd351bdc41d4183e6095
discoleo <leo.mada@syonic.eu>
2025-07-24 18:28:41 +0300

    Radicals: I( 1 / sqrt( (1-x^2) * (1-y^2) * (1 - x^2*y^2) ) ) on [0,1]^2;

---
+++ Math/Integrals.Double.Radicals.R
@@ -0,0 +1,68 @@
+#####################
+##
+## Leonard Mada
+## (the one and only)
+##
+## Integrals: Double Integrals
+## Radicals of Polynomials
+##
+## v.0.1a
+
+
+### Double Integrals:
+# Radicals of Polynomials
+
+### Examples:
+# I( 1 / sqrt( (1-x^2) * (1-y^2) * (1 - x^2*y^2) ) )
+
+
+####################
+
+### Helper Constants
+Euler   = 0.577215664901532860606512090;
+Catalan = 0.915965594177219015054603514;
+
+
+########################
+
+### SQRT:
+
+### I( 1 / sqrt( (1-x^2) * (1-y^2) * (1 - x^2*y^2) ) )
+# - Series started as Trig-Radicals in:
+#   Integrals.Double.Trig.R;
+
+### Base:
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	1 / sqrt((1-x^2) * (1-y^2) * (1 - x^2*y^2)), 0, 1)$value), 0, 1)
+gamma(1/4)^3 / gamma(3/4) * sqrt(2) / 16;
+
+
+### I( 1 / sqrt((1-x^2) * (1 - x^2*y^2)) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	1 / sqrt((1-x^2) * (1 - x^2*y^2)), 0, 1)$value), 0, 1)
+2*Catalan;
+
+
+### I( 1 / sqrt(1 - x^2*y^2) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	1 / sqrt(1 - x^2*y^2), 0, 1)$value), 0, 1)
+pi*log(2)/2;
+
+
+### I( x / sqrt((1-x^2) * (1-y^2) * (1 - x^2*y^2)) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x / sqrt((1-x^2) * (1-y^2) * (1 - x^2*y^2)), 0, 1)$value), 0, 1)
+pi^2 / 4;
+
+
+### I( x / sqrt( (1-y^2) * (1 - x^2*y^2)) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x / sqrt((1-y^2) * (1 - x^2*y^2)), 0, 1)$value), 0, 1)
+1;
+
+
+### I( x / sqrt((1-x^2) * (1 - x^2*y^2)) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x / sqrt((1-x^2) * (1 - x^2*y^2)), 0, 1)$value), 0, 1)
+pi^2/8;
+
