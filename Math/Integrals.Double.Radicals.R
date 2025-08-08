6358672228548bab2109db6a5ea238e8bf1dd7ff
discoleo <lmada@umft.ro>
2025-08-08 18:41:01 +0100

    [refactor] moved Diff-Type to new file;

--- Math/Integrals.Double.Radicals.R
+++ Math/Integrals.Double.Radicals.R
@@ -6,7 +6,7 @@
 ## Double Integrals:
 ## Radicals of Polynomials
 ##
-## v.0.1a
+## v.0.2a
 
 
 ### Double Integrals:
@@ -15,6 +15,11 @@
 ### Examples:
 # I( 1 / sqrt( (1-x^2) * (1-y^2) * (1 - x^2*y^2) ) )
 
+
+### History:
+
+# - [refactor] moved Diff-Type to file:
+#   sIntegrals.Double.Radicals.Diff.R;
 
 ####################
 
@@ -88,44 +93,6 @@ integrate(\(x) sapply(x, \(y) integrate(\(x)
 integrate(\(x) sapply(x, \(y) integrate(\(x)
 	sqrt( (1-x)/(1-y) / (1 - x*y) ), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
 (2*Catalan - 1) * 2;
-
-
-### Diff: (x-y)
-
-### I( sqrt( abs(x-y) / (1 - x*y) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y) / (1 - x*y) ), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
-- (pi + 2*log(2) - 5) * 4/3;
-
-
-### I( sqrt( x*y * abs(x-y) / (1 - x*y) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( x*y * abs(x-y) / (1 - x*y) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-- gamma(3/4)^3 / gamma(9/4) * sqrt(2) * 3/2 + 3 + 11/15;
-
-
-### I( sqrt( abs(x-y) / ((1-x)*(1-y)*(1 - x*y)) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y) / ((1-x)*(1-y)*(1 - x*y)) ), 0, 1, rel.tol=1E-9)$value), 0, 1, rel.tol=3E-10)
-4*pi - gamma(3/4)^2 / gamma(1/2) * sqrt(2) * 8;
-
-
-### I( sqrt( abs(x-y) * (1-x)*(1-y) / (1 - x*y) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y) * (1-x)*(1-y) / (1 - x*y) ), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=3E-12)
-(5*pi - gamma(3/4)^2 / gamma(1/2) * sqrt(2) * 64/5) * 2/3;
-
-
-### I( sqrt( abs(x-y) * (1 - x*y) * (1-x)/(1-y) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y) * (1 - x*y) * (1-x)/(1-y) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-(pi*13/2 - gamma(3/4)^2 / gamma(1/2) * sqrt(2) * 64/5) * 2/15;
-
-
-### I( sqrt( abs(x-y) / (1 - x*y) * (1-x)/(1-y) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y) / (1 - x*y) * (1-x)/(1-y) ), 0, 1, rel.tol=5E-12)$value), 0, 1, rel.tol=1E-12)
-pi * 7/3 - gamma(3/4)^2 / gamma(1/2) * sqrt(2) * 16/3;
 
 
 ###########
@@ -403,44 +370,6 @@ integrate(\(x) sapply(x, \(y) integrate(\(x)
 	sqrt( (1 - x*y^2) / (1 - x^2*y) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
 # TODO
 
-################
-
-### Diff-Type
-
-### I( sqrt( abs(x^2-y^2) * (1 - x^2*y^2) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x^2-y^2) * (1 - x^2*y^2) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-16/15 - gamma(3/4)^3 / gamma(9/4) * sqrt(2) / 4;
-16/15 - gamma(3/4)^4 / gamma(1/2)^2 * 4/5;
-
-
-### I( sqrt( abs(x^2-y^2) / (1 - x^2*y^2) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x^2-y^2) / (1 - x^2*y^2) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-2 - 2*gamma(3/4)^4 / gamma(1/2)^2;
-
-### I( sqrt( x*y * abs(x^2-y^2) / (1 - x^2*y^2) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( x*y * abs(x^2-y^2) / (1 - x^2*y^2) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-8/3 - gamma(3/4)^2 / gamma(1/2) * sqrt(2) * 2;
-
-
-### I( x^2*y^2 * sqrt( abs(x^2-y^2) / (1 - x^2*y^2) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	x^2*y^2 * sqrt( abs(x^2-y^2) / (1 - x^2*y^2) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-14/15 - gamma(3/4)^4 / gamma(1/2)^2 * 6/5;
-
-
-### I( sqrt( abs(x-y)/(x+y) / (1 - x^2*y^2) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y)/(x+y) / (1 - x^2*y^2) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-2*Catalan - pi^2/8;
-
-
-### I( sqrt( abs(x-y) / (1 - x^2*y^2) ) )
-integrate(\(x) sapply(x, \(y) integrate(\(x)
-	sqrt( abs(x-y) / (1 - x^2*y^2) ), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
-# TODO
 
 ################
 ################
