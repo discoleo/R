4f94747a167f55577eaa499b3c5dd66b7589d715
discoleo <lmada@umft.ro>
2025-08-10 20:12:24 +0100

    Gen: I( x^p / (1 - x^n*y^n)^(1/n) ) on [0,1]^2;

--- Math/Integrals.Double.Radicals.R
+++ Math/Integrals.Double.Radicals.R
@@ -404,6 +404,13 @@ integrate(\(x) sapply(x, \(y) integrate(\(x)
 (beta(1/n, 1/n) * beta(2/n, 1 - 1/n) - beta(1/n, 1 - 1/n)) / n^3;
 
 
+### Gen: I( x^p / (1 - x^n*y^n)^(1/n) )
+n = sqrt(13) + 1/3; p = sqrt(2);
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x^p / (1 - x^n*y^n)^(1/n), 0, 1, rel.tol=1E-13)$value), 0, 1, rel.tol=1E-13)
+(pi / sin(pi/n) - beta((p+1)/n, 1-1/n)) / (p*n);
+
+
 ### Gen: I( ((1 - x^n) / (1 - x^n*y^n))^(1/n) )
 n = sqrt(5)
 integrate(\(x) sapply(x, \(y) integrate(\(x)
