22b2a9868d9a67244844af2d874494a33958dc2d
discoleo <lmada@umft.ro>
2025-10-01 19:52:06 +0100

    II Radicals: more variants;

--- Math/Integrals.Double.Radicals.R
+++ Math/Integrals.Double.Radicals.R
@@ -712,12 +712,28 @@ integrate(\(x) sapply(x, \(y) integrate(\(x)
 	y^p * ((1 - x*y) / (1 - x))^(1/n), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-13)
 (digamma(p+1) - digamma(-1/n) + (p+1)*n + 1) * n / (((p+1)*n+1)*(p*n+1));
 
+# Special Case: p = -1/n;
+n = sqrt(15);
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	y^(-1/n) * ((1 - x*y) / (1 - x))^(1/n), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-13)
+pracma::psi(1, 1-1/n) / n + 1;
+
 
 ### Gen: I( x*y^p * ((1 - x) / (1 - x*y^n))^(1/n) )
 n = sqrt(14); p = sqrt(5);
 integrate(\(x) sapply(x, \(y) integrate(\(x)
 	x*y^p * ((1 - x) / (1 - x*y^n))^(1/n), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
 (digamma((p+1)/n) - digamma(1/n) - p/2*(n-p-2)/(n-p-1)) * (n-p-1) / (p*(n-p)*(2*n-p));
+
+# Var: I( x*y^p * ((1 - x) / (1 - x*y))^(1/n) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x*y^p * ((1 - x) / (1 - x*y))^(1/n), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+((digamma(1/n) - digamma(p+1))*p/n + (p+1-1/n)*(p+1/n)/2) / ((p-1/n)*(p+1-1/n)*(p-1-1/n));
+
+# Var: I( x*y^p * ((1 - x*y) / (1 - x))^(1/n) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x*y^p * ((1 - x*y) / (1 - x))^(1/n), 0, 1, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+((digamma(p+1) - digamma(-1/n))*p/n + (p+1+1/n)*(p-1/n)/2) / ((p+1/n)*(p+1+1/n)*(p-1+1/n));
 
 
 ### Gen: I( x^2*y^p * ((1 - x) / (1 - x*y^n))^(1/n) )
