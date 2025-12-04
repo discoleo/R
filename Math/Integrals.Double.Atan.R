237f58151fb8f16937af429f2f6bff5eee2a2e33
discoleo <lmada@umft.ro>
2025-12-04 04:20:50 +0200

    I( x*y * (atan(x/(1-x)) - atan(y/(1-y))) / (x-y) ) on [0,1]^2;

--- Math/Integrals.Double.Atan.R
+++ Math/Integrals.Double.Atan.R
@@ -340,10 +340,26 @@ integrate(\(x) sapply(x, \(y) integrate(\(x)
 	+ (digamma(1/4) - digamma(3/4)) / sin(pi/4) * pi/8;
 
 
+### I( (atan(x/(1-x)) - atan(y/(1-y))) / (x-y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	(atan(x/(1-x)) - atan(y/(1-y))) / (x-y), 0, y, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+- pi^2 / 16 + pi/4 * log(2) + Catalan;
+
+### I( x * (atan(x/(1-x)) - atan(y/(1-y))) / (x-y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x * (atan(x/(1-x)) - atan(y/(1-y))) / (x-y), 0, y, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+(Catalan - pi^2 / 16 + pi*log(2)/4 - pi/4 + 1/2) / 2;
+
+### I( x*y * (atan(x/(1-x)) - atan(y/(1-y))) / (x-y) )
+integrate(\(x) sapply(x, \(y) integrate(\(x)
+	x*y * (atan(x/(1-x)) - atan(y/(1-y))) / (x-y), 0, y, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
+(2*Catalan - pi^2 / 16 + pi*log(2)/2 - pi/2 + 1/2) / 6;
+
+
 ### I( atan((x-y)/x) / (x-y) )
 integrate(\(x) sapply(x, \(y) integrate(\(x)
 	atan((x-y)/x) / (x-y), 0, y, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
-pi*log(2)/4 + Catalan;
+Catalan + pi*log(2)/4;
 
 ### I( x * atan((x-y)/x) / (x-y) )
 integrate(\(x) sapply(x, \(y) integrate(\(x)
@@ -353,7 +369,7 @@ Catalan / 2 + pi*log(2)/8 - pi/8;
 ### I( y * atan((x-y)/x) / (x-y) )
 integrate(\(x) sapply(x, \(y) integrate(\(x)
 	y * atan((x-y)/x) / (x-y), 0, y, rel.tol=1E-12)$value), 0, 1, rel.tol=1E-12)
-pi*log(2)/8 + Catalan/2;
+Catalan / 2 + pi*log(2)/8;
 
 ### I( x*y * atan((x-y)/x) / (x-y) )
 integrate(\(x) sapply(x, \(y) integrate(\(x)
