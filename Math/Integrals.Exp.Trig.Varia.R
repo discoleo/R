fb473b7ec643ed250a0c58b903dca882aef9033e
discoleo <lmada@umft.ro>
2025-12-29 00:11:59 +0200

    Gen: I( sin(x) * besselJ2(k*sin(x)) ) on [0, pi/2];

--- Math/Integrals.Exp.Trig.Varia.R
+++ Math/Integrals.Exp.Trig.Varia.R
@@ -237,6 +237,16 @@ integrate(\(x) sin(x) * besselJ(k*sin(x), 1), 0, pi/2, rel.tol=1E-13)
 pi/2 * besselJ(k/2, 0) * besselJ(k/2, 1);
 
 
+### I( sin(x) * besselJ2(sin(x)) 
+integrate(\(x) sin(x) * besselJ(sin(x), 2), 0, pi/2, rel.tol=1E-13)
+2*(1-cos(1)) - sin(1);
+
+### Gen: I( sin(x) * besselJ2(k*sin(x)) )
+k = log(pi+1);
+integrate(\(x) sin(x) * besselJ(k*sin(x), 2), 0, pi/2, rel.tol=1E-13)
+(2 - 2*cos(k) - k*sin(k)) / k^2;
+
+
 ### I( sin(x) * besselY0(sin(x)) )
 integrate(\(x) sin(x) * besselY(sin(x), 0), 0, pi/2, rel.tol=1E-13)
 (pracma::Ci(1) * sin(1) - pracma::Si(1) * cos(1)) * 2/pi;
