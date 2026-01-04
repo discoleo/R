33945120f0f2c68b308c0af3465002ebe1fe1403
discoleo <lmada@umft.ro>
2026-01-04 16:03:24 +0200

    ODE: x^2*d2y - (2*p-1)*x*dy + p^2*y - 2*x^p - p^2*c0 = 0;

--- Math/DE.ODE.Prod.R
+++ Math/DE.ODE.Prod.R
@@ -9,6 +9,10 @@
 ## draft v.0.1a
 
 
+### Examples:
+
+# x^2*d2y - (2*p-1)*x*dy + p^2*y - 2*x^p - p^2*c0 # = 0
+
 
 ####################
 
@@ -23,9 +27,8 @@ source("DE.ODE.Helper.R")
 #######################
 #######################
 
-### y = B(x) * Log(P(x))^2
-
-### Simple: LOG()^2
+### Type: LOG()^2
+# y = B(x) * Log(P(x))^2 + P(x)
 
 ### y = x * log(x)^2
 
@@ -75,6 +78,31 @@ dy - log(x+b0)^2 - 2*log(x+b0) # = 0
 # D2 =>
 (x+b0)*d2y - 2*log(x+b0) - 2 # = 0
 (x+b0)^2*d2y - ((x+b0)*dy - y + p0) - 2*(x+b0) # = 0
+
+############
+
+### y = x^p * log(x)^2
+# - Simple with exponent;
+
+# Check:
+# for Homogenous: p0 = 0;
+x = sqrt(3); p = 2/3; c0 = -1/2; params = list(x=x, p=p, c0=c0);
+e = expression(x^p * log(x)^2 + c0)[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x^2*d2y - (2*p-1)*x*dy + p^2*y - 2*x^p - p^2*c0 # = 0
+
+
+# D =>
+x*dy - p*y - 2*x^p*log(x) + p*c0 # = 0
+
+# D2 =>
+x*d2y - (p-1)*dy - 2*p*x^(p-1)*log(x) - 2*x^(p-1) # = 0
+x^2*d2y - (p-1)*x*dy - p*(x*dy - p*y + p*c0) - 2*x^p # = 0
 
 
 ###############
