4d1e9c2c17c38f5b473b2e432548d4c20bc84fb6
discoleo <lmada@umft.ro>
2026-01-05 00:01:06 +0200

    ODE from y = x * atan(sqrt(x+b0))^2 + c0;

--- Math/DE.ODE.Prod.R
+++ Math/DE.ODE.Prod.R
@@ -6,12 +6,13 @@
 ## Differential Equations
 ## Linear ODEs - Mixed / Products
 ##
-## draft v.0.1b
+## draft v.0.1c
 
 
 ### Examples:
 
 # x^2*d2y - (2*p-1)*x*dy + p^2*y - 2*x^p - p^2*c0 # = 0
+# 2*x^2*(x-1)*d2y - x*(x-2)*dy + (x-2)*y - (c0+1)*x + 2*c0 # = 0
 
 
 ####################
@@ -143,7 +144,7 @@ x^2*(x+b0)*d2y - x*(x+2*b0)*dy + (x+2*b0)*y +
 ### y = x * atan(x)^2
 
 # Check:
-# for Homogenous: c0 = 0;
+# for Quasi-Homogenous: c0 = 0;
 x = sqrt(3); c0 = -1/2; params = list(x=x, c0=c0);
 e = expression(x * atan(x)^2 + c0)[[1]];
 #
@@ -186,4 +187,51 @@ d2y = eval(D(D(e, "x"), "x"), params);
 # D2 =>
 (x^2+1)*d2y - 2*y - 4*x*atan(x) + 2*c0 - 2 # = 0
 (x^2+1)^2*d2y - 2*x*(x^2+1)*dy + 2*(x^2-1)*y - 2*(c0 + 1)*x^2 + 2*(c0 - 1) # = 0
+
+
+#####################
+
+### y = x * atan(sqrt(x+b0))^2
+
+# Check:
+# for Quasi-Homogenous: c0 = 0;
+x = sqrt(3); c0 = -1/2;
+b0 = 1/3; # b0 = -1;
+params = list(x=x, b0=b0, c0=c0);
+e = expression(x * atan(sqrt(x+b0))^2 + c0)[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+# see below;
+
+# D =>
+x*dy - y - x^2 * atan(sqrt(x+b0)) / sqrt(x+b0) / (x+b0+1) + c0 # = 0
+x*(x+b0+1)*dy - (x+b0+1)*y - x^2 * atan(sqrt(x+b0)) / sqrt(x+b0) + c0*(x+b0+1) # = 0
+
+# D2 =>
+x*(x+b0+1)*d2y + x*dy - y +
+	- 2*x * atan(sqrt(x+b0)) / sqrt(x+b0) +
+	- 1/2 * x^2 / (x+b0) / (x+b0+1) +
+	+ 1/2 * x^2 * atan(sqrt(x+b0)) / sqrt(x+b0)^3 + c0 # = 0
+2*x^2*(x+b0+1)*d2y + 2*x^2*dy - 2*x*y +
+	- 4*(x*(x+b0+1)*dy - (x+b0+1)*y + c0*(x+b0+1)) +
+	- x^3 / (x+b0) / (x+b0+1) +
+	+ x / (x+b0) * (x*(x+b0+1)*dy - (x+b0+1)*y + c0*(x+b0+1)) + 2*c0*x # = 0
+2*x^2*(x+b0)*(x+b0+1)*d2y +
+	- x * (x^2 + 5*b0*x + 3*x + 4*b0 + 4*b0^2) * dy +
+	+ (x^2 + 3*x + 5*b0*x + 4*b0 + 4*b0^2) * y +
+	- x^3 / (x+b0+1) - c0*(x^2 + 3*x + 5*b0*x + 4*b0 + 4*b0^2) # = 0
+2*x^2*(x+b0)*(x+b0+1)^2*d2y +
+	- x*(x+b0+1) * (x^2 + 5*b0*x + 3*x + 4*b0 + 4*b0^2) * dy +
+	+ (x+b0+1) * (x^2 + 3*x + 5*b0*x + 4*b0 + 4*b0^2) * y +
+	- c0*(x+b0+1) * (x^2 + 3*x + 5*b0*x + 4*b0 + 4*b0^2) - x^3 # = 0
+
+
+# Special Cases:
+
+# Case: b0 = -1
+2*x^2*(x-1)*d2y - x*(x-2)*dy + (x-2)*y - (c0+1)*x + 2*c0 # = 0
 
