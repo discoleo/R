ac9bb49c542d259dd0d01a5da5b4c1f38d94728f
discoleo <lmada@umft.ro>
2026-01-04 16:16:35 +0200

    ODE from: y = x * atan(x)^2 + c0;

--- Math/DE.ODE.Prod.R
+++ Math/DE.ODE.Prod.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## Linear ODEs - Mixed / Products
 ##
-## draft v.0.1a
+## draft v.0.1b
 
 
 ### Examples:
@@ -28,7 +28,7 @@ source("DE.ODE.Helper.R")
 #######################
 
 ### Type: LOG()^2
-# y = B(x) * Log(P(x))^2 + P(x)
+# y = B(x) * Log(P(x))^2 + P0(x)
 
 ### y = x * log(x)^2
 
@@ -132,4 +132,35 @@ x^2*(x+b0)*d2y + x^2*dy - x*y - 2*(x*(x+b0)*dy - (x+b0)*y + p0*(x+b0)) +
 	- 2*x^3/(x+b0) + p0*x # = 0
 x^2*(x+b0)*d2y - x*(x+2*b0)*dy + (x+2*b0)*y +
 	- 2*x^3/(x+b0) - p0*(x+2*b0) # = 0
+
+
+#######################
+#######################
+
+### Type: ATAN()^2
+# y = B(x) * Atan(P(x))^2 + P0(x)
+
+### y = x * atan(x)^2
+
+# Check:
+# for Homogenous: p0 = 0;
+x = sqrt(3); c0 = -1/2; params = list(x=x, c0=c0);
+e = expression(x * atan(x)^2 + c0)[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x^2*(x^2+1)^2*d2y - 2*x*(x^2+1)*dy + 2*(x^2+1)*y - 2*x^3 - 2*c0*(x^2+1) # = 0
+
+
+# D =>
+x*dy - y - 2*x^2*atan(x) / (x^2+1) + c0 # = 0
+x*(x^2+1)*dy - (x^2+1)*y - 2*x^2*atan(x) + c0*(x^2+1) # = 0
+
+# D2 =>
+x*(x^2+1)*d2y + 2*x^2*dy - 2*x*y +
+	- 4*x*atan(x) - 2*x^2/(x^2+1) + 2*c0*x # = 0
+x^2*(x^2+1)*d2y - 2*x*dy + 2*y - 2*x^3/(x^2+1) - 2*c0 # = 0
 
