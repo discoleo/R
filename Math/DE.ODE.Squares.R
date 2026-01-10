9c627a86667c37ee64113b1c0982e5053ef9221e
discoleo <lmada@umft.ro>
2026-01-10 20:43:14 +0200

    ODE: 2*x^2*(x-1)*d2y + x^2*dy - x*(y - c0) = x + 2;

--- Math/DE.ODE.Squares.R
+++ Math/DE.ODE.Squares.R
@@ -6,13 +6,14 @@
 ## Differential Equations
 ## Linear ODEs - Mixed / Products
 ##
-## draft v.0.1h
+## draft v.0.1i
 
 
 ### Examples:
 
 # x^2*d2y - (2*p-1)*x*dy + p^2*y - 2*x^p - p^2*c0 # = 0
 # 2*x^2*(x-1)*d2y - x*(x-2)*dy + (x-2)*y - (c0+1)*x + 2*c0 # = 0
+# 2*x^2*(x-1)*d2y + x^2*dy - x*(y - c0) - (x + 2) # = 0
 
 
 ####################
@@ -177,6 +178,8 @@ x^2*(x+b0)*d2y + x^2*dy - x*y - 2*(x*(x+b0)*dy - (x+b0)*y + p0*(x+b0)) +
 x^2*(x+b0)*d2y - x*(x+2*b0)*dy + (x+2*b0)*y +
 	- 2*x^3/(x+b0) - p0*(x+2*b0) # = 0
 
+
+#################
 
 #################
 ### Components: 2
@@ -477,4 +480,57 @@ x*(x+b0+1)*d2y + x*dy - y +
 
 # Case: b0 = -1
 2*x^2*(x-1)*d2y - x*(x-2)*dy + (x-2)*y - (c0+1)*x + 2*c0 # = 0
+
+#################
+
+#################
+### Components: 2
+
+### y = x * atan(sqrt(x+b0))^2 + c1 * sqrt(x+b0) * atan(sqrt(x+b0))
+
+# Check:
+# for Quasi-Homogenous: c0 = 0;
+x = sqrt(3); c1 = 2/5; c0 = -1/2; # c1 = 2;
+b0 = 1/3; # b0 = -1; c1 = 2;
+params = list(x=x, b0=b0, c0=c0);
+e = expression(x * atan(sqrt(x+b0))^2 + c1 * sqrt(x+b0) * atan(sqrt(x+b0)) + c0)[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+# see below;
+
+# D =>
+(x+b0)*(x+b0+1)*dy - (x+b0)*(x+b0+1)*atan(sqrt(x+b0))^2 - x*sqrt(x+b0)*atan(sqrt(x+b0)) +
+	- c1/2 * (x+b0+1) * sqrt(x+b0) * atan(sqrt(x+b0)) - c1/2 * (x+b0) # = 0
+2*x*(x+b0)*(x+b0+1)*dy - 2*(x+b0)*(x+b0+1)*(y - c0) +
+	- (2*x^2 - c1*(x+2*b0)*(x+b0+1)) * sqrt(x+b0) * atan(sqrt(x+b0)) - c1*x*(x+b0) # = 0
+
+# D2 =>
+4*x*(x+b0)^2*(x+b0+1) * d2y + 4*x*(x+b0)*(2*x+2*b0+1) * dy +
+	- 4*(x+b0)*(2*x+2*b0+1) * (y - c0) +
+	- (2*x*(5*x+4*b0) - c1*x*(5*x+7*b0+3) - c1*b0*(6*x+8*b0+4)) * sqrt(x+b0) * atan(sqrt(x+b0)) +
+	- 2*x^2 * (x+b0) / (x+b0 + 1) - 3*c1*x*(x+b0) # = 0
+# Substitution:
+2*x*(x+b0)^2*(x+b0+1)*(2*x^2 - c1*(x+2*b0)*(x+b0+1)) * d2y +
+	+ x*(x+b0) * ((c1-2)*x^3 + 2*b0*c1*x^2 - 10*b0*x^2 + 2*c1*x^2 - 6*x^2 +
+		- 8*b0*x - 8*b0^2*x + c1*x + 2*b0*c1*x + b0^2*c1*x) * dy +
+	- (x+b0) * ((c1-2)*x^3 + (2*b0*c1 - 10*b0 + 2*c1 - 6)*x^2 +
+		+ b0^2*c1*x + 2*b0*c1*x - 8*b0^2*x - 8*b0*x + c1*x) * (y - c0) +
+	- 2*x^4 * (x+b0) / (x+b0 + 1) +
+	- c1*x * (x+b0) * ((c1 - 3)*x^2 + 2*b0*(c1-3)*x + b0*c1*(b0-1)) # = 0
+
+
+### Special Cases:
+
+### Case: c1 = 2
+2*(x+b0)^2*(x+b0+1)^2 * ((3*b0+1)*x + 2*b0^2 + 2*b0) * d2y +
+	+ x*(x+b0)*(x+b0+1) * ((3*b0 + 1)*x + 3*b0^2 + 2*b0 - 1) * dy +
+	- (x+b0)*(x+b0+1) * ((3*b0 + 1)*x + 3*b0^2 + 2*b0 - 1) * (y - c0) +
+	- (x+b0) * ((3*b0 + 1)*x^2 + 4*b0*x - 2*b0^3 + 2*b0) # = 0
+
+### Case: c1 = 2; b1 = -1;
+2*x^2*(x-1)*d2y + x^2*dy - x*(y - c0) - (x + 2) # = 0
 
