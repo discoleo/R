8c6db655c0f03128a033e19b108439ba841feff4
discoleo <lmada@umft.ro>
2026-01-11 18:50:14 +0200

    ODE: x*(x^2+b0)^2 * d2y - b0*(x^2+b0) * dy + x^3 * (y - c0) = 0;

--- Math/DE.ODE.Radicals.R
+++ Math/DE.ODE.Radicals.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## Linear ODEs - Radicals
 ##
-## draft v.0.1c
+## draft v.0.1d
 
 
 ### Theory
@@ -16,6 +16,11 @@
 # - the Radical in the first term entangles with the free radical;
 # y = B1(x) * R(x)^(1/n) * SIN(P(x)) + B2(x) * R(x)^(1/n) * COS(P(x));
 # - the coefficients of SIN & COS entangle with each-other;
+
+
+### Examples:
+
+# x*(x^2+b0)^2 * d2y - b0*(x^2+b0) * dy + x^3 * (y - c0) = 0;
 
 
 ####################
@@ -137,4 +142,38 @@ d2y = eval(D(D(e, "x"), "x"), params);
 # D2 =>
 9*x^2*(x^2+b0)^2*d2y - 3*x*(x^2+b0)*(x^2-3*b0)*dy +
 	+ (9*k^2*(x^2+b0)^2 + 4*x^4 - 12*b0*x^2) * (y - c0) # = 0
+
+
+#################
+
+### y = (x^2+b0)^p * sin(k*log(x^2+b0));
+
+# Check:
+x = sqrt(3); p = 2/3; k = 2/5; b0 = sqrt(2); c0 = -1/3;
+# p = 1/4; k = sqrt(3)/4;
+params = list(x=x, k=k, b0=b0, c0=c0);
+e = expression((x^2+b0)^p * sin(k*log(x^2+b0)) + c0)[[1]];
+#
+y   = eval(e, params); dy = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x*(x^2+b0)^2 * d2y - ((4*p-1)*x^2 + b0)*(x^2+b0) * dy +
+	+ 4*(k^2 + p^2)*x^3 * (y - c0) # = 0
+
+# D =>
+(x^2+b0)*dy - 2*p*x * (y - c0) - 2*k*x * (x^2+b0)^p * cos(k*log(x^2+b0)) # = 0
+
+# D2 =>
+(x^2+b0)^2 * d2y - 2*(p-1)*x*(x^2+b0) * dy +
+	+ (4*k^2*x^2 - 2*p*(x^2+b0)) * (y - c0) +
+	- 2*k*(2*p*x^2 + x^2+b0) * (x^2+b0)^p * cos(k*log(x^2+b0)) # = 0
+x*(x^2+b0)^2 * d2y - ((4*p-1)*x^2 + b0)*(x^2+b0) * dy +
+	+ 4*(k^2 + p^2)*x^3 * (y - c0) # = 0
+
+
+### Special Cases:
+
+### Case: p = 1/4; k = sqrt(3)/4;
+x*(x^2+b0)^2 * d2y - b0*(x^2+b0) * dy + x^3 * (y - c0) # = 0
 
