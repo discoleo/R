667c46932a945e8400c397fe17eeddecc2e8a84b
discoleo <lmada@umft.ro>
2026-01-16 23:03:01 +0200

    ODE: x^2*d2y - (2*p-1)*x*dy + p^2*(y - c0) = 2*x^p;
    & Generic variant;

--- Math/DE.ODE.Squares.R
+++ Math/DE.ODE.Squares.R
@@ -4,9 +4,22 @@
 ## [the one and only]
 ##
 ## Differential Equations
-## Linear ODEs - Mixed / Products
+## Linear ODEs - Squares
 ##
-## draft v.0.1i
+## draft v.0.1j
+
+
+### Base-Solution:
+# Simple: 1 Component
+# y = B(x) * Log(P(x))^2 + F0(x);
+# y = B(x) * Atan(P(x))^2 + F0(x);
+# w. Radicals:
+# y = B(x) * Log(sqrt(P(x)^2 + b0) - P(x))^2 + F0(x);
+# y = B(x) * Atan(sqrt(P(x)))^2 + F0(x);
+
+# Full: 2 Components
+# y = B1(x) * Log(P(x))^2 + B2(x) * Log(P(x)) + F0(x);
+# y = B1(x) * Atan(P(x))^2 + B2(x) * Atan(P(x)) + F0(x);
 
 
 ### Examples:
@@ -73,7 +86,7 @@ y   = eval(e, params);
 dy  = eval(D(e, "x"), params);
 d2y = eval(D(D(e, "x"), "x"), params);
 
-### IDE:
+### ODE:
 x^2*d2y - x*dy + y - c2*x^2 - 2*x - c0 # = 0
 
 
@@ -249,6 +262,79 @@ x^2*(x+1)*d2y - x*(3*x+2)*dy + 2*(2*x+1)*(y - c0) +
 	+ x^3 + 3*x^2 + x # = 0
 
 
+############
+### Geenric:
+
+### y = B2(x) * log(P(x))^2 + B1(x)*log(P(x))
+
+# Check:
+# for Quasi-Homogenous: c0 = 0;
+x = sqrt(3); c0 = -1/2; params = list(x=x, c0=c0);
+eb2 = expression(x^2 + x/3 + 1/5)[[1]];
+eb1 = expression(x^2 + x/5 + 2/3)[[1]];
+# eb1 = eb2; # Case: b1 == b2;
+epx = expression(x^2 + x/2 + 3/5)[[1]];
+# eb1 = eb2; epx = expression(x)[[1]];
+# eb1 = eb2 = expression(sqrt(x))[[1]]; epx = expression(x)[[1]];
+# eb1 = eb2 = expression(x^(1/3))[[1]]; epx = expression(x)[[1]]; p = 1/3;
+e = expression(b2 * log(px)^2 + b1 * log(px) + c0)[[1]];
+e[[2]][[2]][[3]][[2]][[2]] = epx;
+e[[2]][[3]][[3]][[2]] = epx;
+e[[2]][[2]][[2]] = eb2;
+e[[2]][[3]][[2]] = eb1;
+
+#
+y  = eval(e, params); dy = eval(D(e, "x"), params);
+px = eval(epx, params); dp  = eval(D(epx, "x"), params);
+b1 = eval(eb1, params); db1 = eval(D(eb1, "x"), params);
+b2 = eval(eb2, params); db2 = eval(D(eb2, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+d2p = eval(D(D(epx, "x"), "x"), params);
+d2b1 = eval(D(D(eb1, "x"), "x"), params);
+d2b2 = eval(D(D(eb2, "x"), "x"), params);
+
+### ODE:
+# - see formula below;
+
+
+# D =>
+px*dy - db2*px * log(px)^2 - (2*b2*dp + db1*px) * log(px) - b1*dp # = 0
+b2*px * dy - db2*px * (y - c0) +
+	- (b2*(2*b2*dp + db1*px) - b1*db2*px) * log(px) - b1*b2*dp # = 0
+
+# D2 =>
+b2*px * d2y + b2*dp * dy - (db2*dp + d2b2*px) * (y - c0) +
+	+ (d2b2*b1*px - d2b1*b2*px + db2*(b1-4*b2)*dp - db1*b2*dp - 2*b2^2*d2p) * log(px) +
+	- b1*b2*d2p - 2*db1*b2*dp - 2*b2^2*dp^2 / px # = 0
+b2*px^2*((b2*db1 - b1*db2)*px + 2*b2^2*dp) * d2y +
+	+ b2*px * ((b1*d2b2 - b2*d2b1)*px^2 - 4*b2*db2*px*dp + 2*b2^2*dp^2 - 2*b2^2*px*d2p) * dy +
+	- px*(b2*(db1*d2b2 - db2*d2b1)*px^2 - 4*b2*db2^2*px*dp + 2*b2^2*d2b2*px*dp +
+		+ 2*b2^2*db2*dp^2 - 2*b2^2*db2*px*d2p) * (y - c0) +
+	- (2*db1*b2*(b2*db1 - b1*db2) + b1*b2*(b1 - b2)*d2b2) * px^2*dp +
+	- b2*(b1 - 6*b2)*(b1*db2 - b2*db1) * px*dp^2 - 4*b2^4*dp^3 +
+	- b1*b2*(b2*db1 - b1*db2) * px^2*d2p # = 0
+
+# TODO:
+# - gain insight from formula;
+
+### Special Cases:
+
+### Case: b1 = b2
+b2^2*px^2*dp * d2y +
+	- b2*px * (2*db2*px*dp - b2*dp^2 + b2*px*d2p) * dy +
+	+ px * ((2*db2^2 - b2*d2b2)*px*dp - b2*db2*dp^2 + b2*db2*px*d2p) * (y - c0) +
+	- 2*b2^3*dp^3 # = 0
+
+### Case: b1 = b2; px = x;
+b2^2*x^2 * d2y - b2*x * (2*db2*x - b2) * dy +
+	+ x*((2*db2^2 - b2*d2b2)*x - b2*db2) * (y - c0) - 2*b2^3 # = 0
+
+### Case: b1 = b2 = sqrt(x); px = x;
+4*x^2*d2y + (y - c0) - 8*sqrt(x) # = 0
+### Case: b1 = b2 = x^p; px = x;
+x^2*d2y - (2*p-1)*x*dy + p^2*(y - c0) - 2*x^p # = 0
+
+
 #######################
 #######################
 
@@ -301,6 +387,38 @@ d2y = eval(D(D(e, "x"), "x"), params);
 # D2 =>
 (x^2+1)*d2y - 2*y - 4*x*atan(x) + 2*c0 - 2 # = 0
 (x^2+1)^2*d2y - 2*x*(x^2+1)*dy + 2*(x^2-1)*y - 2*(c0 + 1)*x^2 + 2*(c0 - 1) # = 0
+
+
+#####################
+#####################
+
+### Simple Radical & Function
+
+# y = B1(x) * R(x)^p * log(P(x))^2
+# Linear ODE: Order 3 OR w. Radical;
+
+### Simple Example
+# y = sqrt(x^2 + b0) * log(x^2 + b0)^2
+
+# Check:
+# for Quasi-Homogenous: c0 = 0;
+x = sqrt(3); b0 = 1/3; c0 = -1/2; params = list(x=x, c0=c0);
+e = expression(sqrt(x^2+b0) * log(x^2+b0)^2 + c0)[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+# see below;
+
+
+# D =>
+(x^2+b0)*dy - x*(y - c0) - 4*x * sqrt(x^2+b0) * log(x^2+b0) # = 0
+
+# D2 =>
+x*(x^2+b0)^2*d2y - (x^2+b0)^2*dy + x^3 * (y - c0) +
+	- 8*x^3 * sqrt(x^2+b0) # = 0
 
 
 #####################
