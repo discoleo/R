caad0e57f412d3f0584d2f22160f8194214cc694
discoleo <lmada@umft.ro>
2026-01-21 02:35:20 +0200

    Exploration of Bessel-Like functions;

--- Math/DE.ODE.Bessel.R
+++ Math/DE.ODE.Bessel.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## ODEs: Bessel Functions
 ##
-## draft v.0.1c
+## draft v.0.1d
 
 
 ### Bessel Functions
@@ -62,6 +62,8 @@ y = tmp$y; dy = tmp$dy; d2y = tmp$d2y;
 ### ODE:
 x^2*d2y + x*dy + (x^2 - n^2)*y # = 0
 
+
+##################
 
 ##################
 ### Transforms ###
@@ -151,4 +153,149 @@ d2y = exp(x^2 / 4) * (tmp$d2y + 1/2*x*tmp$dy) + x*dy/2 + y/2;
 
 ### ODE:
 4*x^2*d2y - 4*x*(x^2 - 1)*dy + (x^4 - 4*n^2)*y # = 0
+
+
+##################
+##################
+
+### Bessel-Like
+
+# Exploration of Bessel-Like Functions
+
+### y = x^p * sin(k*x^n)
+# Check:
+x = sqrt(3); k = 1/5; p = sqrt(2); n = sqrt(3); c0 = -1/3;
+# k = 1/n; p = - n/2;
+params = list(x=x, p=p, n=n, c0=c0);
+e = expression(x^p * sin(k*x^n) + c0)[[1]];
+#
+y   = eval(e, params); dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x^2*d2y - (2*p+n-1)*x*dy +
+	+ (k^2*n^2*x^(2*n) + p*(p+n)) * (y - c0) # = 0
+
+
+# D =>
+x*dy - p*(y - c0) - k*n*x^(p+n) * cos(k*x^n) # = 0
+
+# D2 =>
+x^2*d2y - (p-1)*x*dy +
+	- k*n*(p+n)*x^(p+n) * cos(k*x^n) +
+	+ k^2*n^2*x^(p+2*n) * sin(k*x^n) # = 0
+x^2*d2y - (2*p+n-1)*x*dy +
+	+ (k^2*n^2*x^(2*n) + p*(p+n)) * (y - c0) # = 0
+
+
+### Special Cases:
+
+### Case: p = - n/2; k = 1/n;
+m = n/2;
+x^2*d2y + x*dy + (x^(4*m) - m^2) * (y - c0) # = 0
+
+
+###########
+### y = x^p * sin(k*log(x))
+# Check:
+x = sqrt(3); k = 1/5; p = sqrt(2); n = sqrt(3); c0 = -1/3;
+# k = 1/n; p = - n/2;
+params = list(x=x, k=k, p=p, c0=c0);
+e = expression(x^p * sin(k*log(x)) + c0)[[1]];
+#
+y   = eval(e, params); dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x^2*d2y - (2*p-1)*x*dy + (k^2 + p^2) * (y - c0) # = 0
+
+
+# D =>
+x*dy - p*(y - c0) - k*x^p*cos(k*log(x)) # = 0
+
+# D2 =>
+x^2*d2y - (p-1)*x*dy - k*p*x^p*cos(k*log(x)) +
+	+ k^2*x^p * sin(k*log(x)) # = 0
+x^2*d2y - (2*p-1)*x*dy + (k^2 + p^2) * (y - c0) # = 0
+
+
+##############
+### y = exp(k1*x) * sin(k2*log(x))
+
+# Check:
+x = sqrt(3); k1 = sqrt(2/3); k2 = 1/5; c0 = -1/3;
+params = list(x=x, k1=k1, k2=k2, c0=c0);
+e = expression(exp(k1*x) * sin(k2*log(x)) + c0)[[1]];
+#
+y   = eval(e, params); dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x^2*d2y - x*(2*k1*x-1)*dy + (k1^2*x^2 - k1*x + k2^2) * (y - c0) # = 0
+
+
+# D =>
+x*dy - k1*x*(y - c0) - k2*exp(k1*x)*cos(k2*log(x)) # = 0
+
+# D2 =>
+x^2*d2y - x*(k1*x-1)*dy - k1*x*(y - c0) +
+	- k1*k2*x*exp(k1*x)*cos(k2*log(x)) +
+	+ k2^2*exp(k1*x)*sin(k2*log(x)) # = 0
+x^2*d2y - x*(2*k1*x-1)*dy + (k1^2*x^2 - k1*x + k2^2) * (y - c0) # = 0
+
+
+###############
+### y = sqrt(x) * exp(k*sqrt(x))
+
+# Check:
+x = sqrt(3); k = 1/5; c0 = -1/3;
+params = list(x=x, k=k, c0=c0);
+e = expression(sqrt(x) * exp(k*sqrt(x)) + c0)[[1]];
+#
+y   = eval(e, params); dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+4*x^2*d2y - 2*x*dy - (k^2*x - 2) * (y - c0) # = 0
+
+
+# D =>
+2*x*dy - (y - c0) - k * x * exp(k*sqrt(x)) # = 0
+
+# D2 =>
+4*x*d2y + 2*dy - 2*k * exp(k*sqrt(x)) +
+	- k^2 * sqrt(x) * exp(k*sqrt(x)) # = 0
+4*x^2*d2y - 2*x*dy - (k^2*x - 2) * (y - c0) # = 0
+
+########
+### Gen:
+# y = x^n * sqrt(x) * exp(k*sqrt(x))
+
+# Check:
+x = sqrt(3); n = 1/3; k = 1/5; c0 = -1/3;
+# n = 1/4;
+params = list(x=x, k=k, c0=c0);
+e = expression(x^n * sqrt(x) * exp(2*k*sqrt(x)) + c0)[[1]];
+#
+y   = eval(e, params); dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+### ODE:
+x^2*d2y - (2*n+1/2)*x*dy +
+	- (k^2*x - (n+1)*(n+1/2)) * (y - c0) # = 0
+
+# D =>
+2*x*dy - (2*n+1)*(y - c0) - 2*k * x^(n+1) * exp(2*k*sqrt(x)) # = 0
+
+# D2 =>
+4*x*d2y - 2*(2*n-1)*dy - 4*k*(n+1) * x^n * exp(2*k*sqrt(x)) +
+	- 4*k^2 * x^n * sqrt(x) * exp(2*k*sqrt(x)) # = 0
+x^2*d2y - (2*n+1/2)*x*dy +
+	- (k^2*x - (n+1)*(n+1/2)) * (y - c0) # = 0
+
+
+### Special Cases:
+
+### Case: n = 1/4
+x^2*d2y - x*dy - (k^2*x - 15/16) * (y - c0) # = 0
 
