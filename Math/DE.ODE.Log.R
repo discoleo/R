3a944617153c64b6d77d3f3fc4211f1e8fedf522
discoleo <lmada@umft.ro>
2026-06-17 01:25:49 +0300

    [refactor] ODE: moved Composite Log to new file;

--- Math/DE.ODE.Log.R
+++ Math/DE.ODE.Log.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## ODEs - Logarithms
 ##
-## draft v.0.3p
+## draft v.0.3o
 
 
 ### ODEs Derived from Logarithms
@@ -21,13 +21,15 @@
 ### History ###
 ###############
 
-### draft v.0.3m - v.0.3p:
+### draft v.0.3m - v.0.3o:
 # - [refactor] moved ODEs based on Hidden Log
 #   to new file: DE.ODE.NL.Log.Other.R;
 # - [refactor] Prod( LOG ) moved to new file:
 #   DE.ODE.NL.Log.Prod.R;
 # - [refactor] Int( LOG ) moved to new file:
 #   DE.ODE.NL.Log.Int.R;
+# - [refactor] LOG( OTHER(F(x)) ) moved to new file:
+#   DE.ODE.NL.Log.Composite.R;
 ### draft v.0.3i - v.0.3j:
 # - Mixed variants: Sum(LOG, EXP)
 #   y = B1(x)*log(P1(x)) + B2(x)*exp(P2(x)) + F0(x);
@@ -807,62 +809,6 @@ line.tan(px, dx=3, p=dy, dp=d2y, k=k, col="orange")
 
 ###########################
 ###########################
-
-###########################
-### Compositions of Log ###
-###########################
-
-### y = log(exp(P1(x)) + P2(x)) + F0(x)
-
-### y = log(exp(x^n) + k) + f
-
-### D =>
-(exp(x^n)+k)*dy - n*x^(n-1)*exp(x^n) - df*(exp(x^n)+k) # = 0
-# exp(x^n) = - k*(dy - df) / (dy - n*x^(n-1) - df);
-
-### D2 =>
-(exp(x^n)+k)*d2y + n*x^(n-1)*exp(x^n)*dy +
-	- n*(n-1)*x^(n-2)*exp(x^n) - n^2*x^(2*n-2)*exp(x^n) +
-	- d2f*(exp(x^n)+k) - n*x^(n-1)*df*exp(x^n) # = 0
-(d2y + n*x^(n-1)*dy +
-		- n*(n-1)*x^(n-2) - n^2*x^(2*n-2) - n*x^(n-1)*df - d2f) * exp(x^n) +
-	+ k*d2y - d2f*k # = 0
-# Subst =>
-k*(d2y + n*x^(n-1)*dy +
-		- n*(n-1)*x^(n-2) - n^2*x^(2*n-2) - n*x^(n-1)*df - d2f) * (dy - df) +
-	- (k*d2y - k*d2f)*(dy - n*x^(n-1) - df) # = 0
-k*n*x^(n-1)*d2y + k*n*x^(n-1)*dy^2 - 2*k*n*x^(n-1)*df*dy +
-	- k*(n*(n-1)*x^(n-2) + n^2*x^(2*n-2)) * (dy - df) +
-	+ k*n*x^(n-1)*df^2 - k*n*x^(n-1)*d2f # = 0
-### ODE:
-x*d2y + x*dy^2 - (2*x*df + n*x^n + (n-1))*dy +
-	+ (n*x^n + (n-1))*df + x*df^2 - x*d2f # = 0
-
-# TODO: check!
-
-
-#######################
-#######################
-
-### y = log(log(P(x))) + F0(x)
-
-### y = log(log(x^2 + k)) + f
-
-### D =>
-(x^2+k)*log(x^2 + k)*dy - (x^2+k)*df*log(x^2 + k) - 2*x # = 0
-# (x^2+k)*log(x^2 + k) = 2*x / (dy - df);
-
-### D2 =>
-(x^2+k)*log(x^2 + k)*d2y + 2*x*dy + 2*x*log(x^2 + k)*dy +
-	- 2*x*(x^2+k)*df - 2*x*df*log(x^2 + k) - (x^2+k)*d2f*log(x^2 + k) - 2 # = 0
-2*x*(x^2+k)/(dy - df) * d2y + 2*x*(x^2+k)*dy + 4*x^2/(dy - df) * dy +
-	- 2*x*(x^2+k)^2*df - 4*x^2*df/(dy - df) - 2*x*(x^2+k)*d2f/(dy - df) - 2*(x^2+k) # = 0
-
-# TODO
-
-
-#######################
-#######################
 
 ### y = log(P(x)) * log(log(P(x))) + F0(x)
 
---
+++ Math/DE.ODE.NL.Log.Composite.R
@@ -0,0 +1,64 @@
+
+
+###########################
+### Compositions of Log ###
+###########################
+
+### y = log(exp(P1(x)) + P2(x)) + F0(x)
+
+### y = log(exp(x^n) + b0) + f
+
+# Note: b0 has NO impact on ODE;
+x = sqrt(3); n = -1/5; b0 = 4/5; bf = sqrt(2);
+params = list(x=x, n=n, b0=b0, bf=bf);
+e = expression(log(exp(x^n) + b0) + bf*x)[[1]];
+f = bf*x; df = bf; d2f = 0;
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+
+### D =>
+(exp(x^n)+b0)*dy - n*x^(n-1)*exp(x^n) - df*(exp(x^n)+b0) # = 0
+# exp(x^n) = - b0*(dy - df) / (dy - n*x^(n-1) - df);
+
+### D2 =>
+(exp(x^n)+b0)*d2y + n*x^(n-1)*exp(x^n)*dy +
+	- n*(n-1)*x^(n-2)*exp(x^n) - n^2*x^(2*n-2)*exp(x^n) +
+	- d2f*(exp(x^n)+b0) - n*x^(n-1)*df*exp(x^n) # = 0
+(d2y + n*x^(n-1)*dy +
+		- n*(n-1)*x^(n-2) - n^2*x^(2*n-2) - n*x^(n-1)*df - d2f) * exp(x^n) +
+	+ b0*d2y - d2f*b0 # = 0
+# Subst =>
+b0*(d2y + n*x^(n-1)*dy +
+		- n*(n-1)*x^(n-2) - n^2*x^(2*n-2) - n*x^(n-1)*df - d2f) * (dy - df) +
+	- (b0*d2y - b0*d2f)*(dy - n*x^(n-1) - df) # = 0
+b0*n*x^(n-1)*d2y + b0*n*x^(n-1)*dy^2 - 2*b0*n*x^(n-1)*df*dy +
+	- b0*(n*(n-1)*x^(n-2) + n^2*x^(2*n-2)) * (dy - df) +
+	+ b0*n*x^(n-1)*df^2 - b0*n*x^(n-1)*d2f # = 0
+
+### ODE:
+x*d2y + x*dy^2 - (2*x*df + n*x^n + (n-1))*dy +
+	+ (n*x^n + (n-1))*df + x*df^2 - x*d2f # = 0
+
+
+#######################
+#######################
+
+### y = log(log(P(x))) + F0(x)
+
+### y = log(log(x^2 + k)) + f
+
+### D =>
+(x^2+k)*log(x^2 + k)*dy - (x^2+k)*df*log(x^2 + k) - 2*x # = 0
+# (x^2+k)*log(x^2 + k) = 2*x / (dy - df);
+
+### D2 =>
+(x^2+k)*log(x^2 + k)*d2y + 2*x*dy + 2*x*log(x^2 + k)*dy +
+	- 2*x*(x^2+k)*df - 2*x*df*log(x^2 + k) - (x^2+k)*d2f*log(x^2 + k) - 2 # = 0
+2*x*(x^2+k)/(dy - df) * d2y + 2*x*(x^2+k)*dy + 4*x^2/(dy - df) * dy +
+	- 2*x*(x^2+k)^2*df - 4*x^2*df/(dy - df) - 2*x*(x^2+k)*d2f/(dy - df) - 2*(x^2+k) # = 0
+
+# TODO
+
