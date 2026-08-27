202dfd2e213c09e5c575ae1c0d87134bf5f96693
discoleo <lmada@umft.ro>
2026-08-28 02:05:06 +0300

    ODE: Explored Sin/Cos-swap;

--- Math/DE.ODE.Trig.Radicals.R
+++ Math/DE.ODE.Trig.Radicals.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## Linear ODEs - Trig w. Radicals
 ##
-## draft v.0.1a
+## draft v.0.1b
 
 ### Linear ODE: Trig w. Radicals
 # y = B1(x) * sin(SQRT(R(x))) + B2(x) * SQRT(R(x)) * cos(R(x));
@@ -28,9 +28,6 @@ source("DE.ODE.Helper.R")
 
 #######################
 #######################
-
-
-#####################
 
 ### y = x * sin(k*SQRT(x)) + c1 * SQRT(x) * cos(k*SQRT(x));
 
@@ -70,5 +67,36 @@ d2y = eval(D(D(e, "x"), "x"), params);
 ### Special Cases:
 
 # Case: c1*k = 1;
+4*x^3 * d2y - 6*x^2 * dy + x*(k^2*x + 4) * y # = 0
+
+
+#####################
+
+### y = x * cos(k*SQRT(x)) + c1 * SQRT(x) * sin(k*SQRT(x));
+# Basically: k => -k;
+
+# Check:
+k = sqrt(2); c1 = -1/3;
+# k = sqrt(2); c1 = -1/k; # k = 1i * sqrt(3); c1 = -1/k;
+x = sqrt(3);
+params = list(x=x, k=k, c1=c1);
+e = expression(x * cos(k*sqrt(x)) + c1*sqrt(x)*sin(k*sqrt(x)))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+# D =>
+2*x*dy - (c1*k+2) * x * cos(k*sqrt(x)) +
+	+ (k*x - c1) * sqrt(x) * sin(k*sqrt(x)) # = 0
+
+### ODE:
+4*x^2*(k*x + c1*(c1*k+1)) * d2y +
+	- 2*x * (3*k*x + c1*(c1*k+1)) * dy +
+	+ (k^3*x^2 + k*(c1^2*k^2 + 3*c1*k + 6)*x + 2*c1*(c1*k + 1)) * y # = 0
+
+### Special Cases:
+
+# Case: c1*k = -1;
 4*x^3 * d2y - 6*x^2 * dy + x*(k^2*x + 4) * y # = 0
 
