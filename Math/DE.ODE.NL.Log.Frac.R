1b215bbd3ac56505c5cb018e2b8df9475b9d2f86
discoleo <lmada@umft.ro>
2026-08-29 01:27:06 +0300

    NL ODE: Another example;

--- Math/DE.ODE.NL.Log.Frac.R
+++ Math/DE.ODE.NL.Log.Frac.R
@@ -45,3 +45,33 @@ x*d2y + 2*(x+k)*y*dy + y^2 + dy # = 0
 ### ODE:
 x*(x+k) * d2y + 2*(x+k)^2 * y*dy + k*dy # = 0
 
+#######################
+
+### Partial Generalization
+# y = x^p / (x + k*log(x))
+
+# Check:
+p = 1/3; # p = -1;
+k = sqrt(2);
+x = sqrt(3);
+params = list(x=x, k=k, p=p);
+e = expression(x^p / (x + k*log(x)))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+# D =>
+x^(p+1) * dy - p*x^p * y + (x+k)*y^2 # = 0
+
+# D2 & Substitution =>
+
+### ODE:
+x^(p+1)*(x+k) * d2y + 2*(x+k)^2 * y*dy + k*x^p * dy +
+	- (p^2-p)*x^p * y - k*p^2*x^(p-1) * y # = 0
+
+### Special Cases:
+
+# Case: p = -1;
+x^2*(x+k) * d2y + 2*x^2(x+k)^2 * y*dy + k*x * dy - (2*x + k) * y # = 0
+
