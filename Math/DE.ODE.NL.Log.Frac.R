c79ea6fdbcd6e58572f6b69a45c2c5d9160382e1
discoleo <lmada@umft.ro>
2026-09-05 05:52:10 +0300

    ODE: x^3 * y*d2y + x^3 * dy^2 + 2*x^2 * y*dy + k = 0;

--- Math/DE.ODE.NL.Log.Frac.R
+++ Math/DE.ODE.NL.Log.Frac.R
@@ -6,7 +6,7 @@
 ## Differential Equations
 ## NL ODEs - Log in Fractions
 ##
-## draft v.0.1c
+## draft v.0.1d
 
 
 ### Note:
@@ -214,4 +214,43 @@ x^2*(x^2+1)^2 * d2y +
 # Case: n = 0; p = 2;
 x*(x^2+1)^2 * d2y + 4*k*(x^2+1)^2 * y*dy +
 	+ (x^4-1) * dy + 4*x * y # = 0
+
+
+##################
+##################
+
+### Radicals
+
+### y = x^p ^ SQRT(x + k*log(x))
+
+# Check:
+p = 1/3; # p = -1; # p = -1/2;
+k = sqrt(2);
+x = sqrt(3);
+params = list(x=x, k=k, p=p);
+e = expression(x^p * sqrt(x + k*log(x)))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+# D =>
+2*x*dy - 2*p*y - x^(2*p) * (x+k) / y # = 0
+2*x*y*dy - 2*p*y^2 - x^(2*p) * (x+k) # = 0
+
+# D2 =>
+2*x*y*d2y + 2*x*dy^2 - (4*p-2) * y*dy +
+	- 2*p*x^(2*p-1) * (x+k) - x^(2*p) # = 0
+# Entanglement:
+2*x^2*y*d2y + 2*x^2*dy^2 - 2*(2*p-1)*x * y*dy +
+	- 2*(2*p+1) * (x*y*dy - p*y^2) + k*x^(2*p) # = 0
+
+### ODE:
+x^2 * y*d2y + x^2 * dy^2 - 4*p*x * y*dy +
+	+ p*(2*p+1) * y^2 + k/2 * x^(2*p) # = 0
+
+### Special Cases:
+
+### p = -1/2;
+x^3 * y*d2y + x^3 * dy^2 + 2*x^2 * y*dy + k/2 # = 0
 
