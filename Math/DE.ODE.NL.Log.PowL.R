d9232296317500f9b348852dbd383680dfe3933a
discoleo <lmada@umft.ro>
2026-09-20 06:04:21 +0300

    added some checks;

--- Math/DE.ODE.NL.Log.PowL.R
+++ Math/DE.ODE.NL.Log.PowL.R
@@ -27,18 +27,29 @@ source("DE.ODE.Helper.R")
 
 ### y = x^log(x)
 
-### D(y)
-2*log(x)/x * x^log(x)
-2*log(x)/x * y
-
-### D2(y)
-2*log(x)/x * dy + 2*(1 - log(x))/x^2 * y
-### x^2 * D2(y)
-2*x*log(x) * dy + 2*(1 - log(x)) * y
-x^2/y * dy^2 - x*dy + 2*y
+# Check:
+x = sqrt(3); params = list(x=x);
+e = expression(x^log(x))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
 
 ### ODE:
 x^2*y*d2y - x^2*dy^2 + x*y*dy - 2*y^2 # = 0
+
+
+### D(y)
+dy - 2*log(x)/x * x^log(x) # = 0
+dy - 2*log(x)/x * y # = 0
+
+### D2(y)
+d2y # ==
+2*log(x)/x * dy + 2*(1 - log(x))/x^2 * y
+### x^2 * D2(y)
+x^2 * d2y # ==
+2*x*log(x) * dy + 2*(1 - log(x)) * y
+x^2/y * dy^2 - x*dy + 2*y
 
 
 ### Extension
