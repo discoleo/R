9ff6cc89590b52a7d4e8d4a2ab2863f8fbecae2d
discoleo <lmada@umft.ro>
2026-09-25 02:26:29 +0300

    NL ODE: y*d2y - k*dy - 4*k^2 * y^2 + k^2 = 0;

---
+++ Math/DE.ODE.NL.Atan.CoupledExp.R
@@ -0,0 +1,62 @@
+########################
+##
+## Leonard Mada
+## [the one and only]
+##
+## Differential Equations
+## ODEs - ATAN w. Coupled Exp
+##
+## draft v.0.1a
+
+
+### Examples:
+
+# y*d2y - k*dy - 4*k^2 * y^2 + k^2 = 0;
+
+
+####################
+
+### Helper Functions
+
+source("Polynomials.Helper.ODE.R")
+source("DE.ODE.Helper.R")
+
+
+#########################
+#########################
+
+### y = cosh(k*x) * atan(exp(k*x))
+# - Simple example;
+
+# Check:
+k = sqrt(3); # k = 1;
+x = 3^(3/5);
+params = list(x=x, k=k);
+e = expression(cosh(k*x) * atan(exp(k*x)))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+# D =>
+2*dy - 2*k*sinh(k*x)* atan(exp(k*x)) - k # = 0
+
+# Linear System:
+# [actually NOT needed]
+exp(k*x) * atan(exp(k*x)) # ==
+(2*dy - k + 2*k*y) / (2*k);
+#
+exp(-k*x) * atan(exp(k*x)) # ==
+- (2*dy - k - 2*k*y) / (2*k);
+
+# D2 =>
+2*d2y - 2*k^2 * y - k^2*sinh(k*x) / cosh(k*x) # = 0
+4*d2y - 4*k^2 * y - k*(2*dy - k) / y # = 0
+
+### ODE:
+4*y*d2y - 2*k*dy - 4*k^2 * y^2 + k^2 # = 0
+
+# Variant:
+kd = k / 2;
+y*d2y - kd*dy - 4*kd^2 * y^2 + kd^2 # = 0
+
