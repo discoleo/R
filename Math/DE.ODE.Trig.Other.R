a2cc622a730870cd885ba41edca8a1844d16d344
discoleo <lmada@umft.ro>
2026-09-25 22:01:06 +0300

    Exploration of Sin & Cos;

--- Math/DE.ODE.Trig.Other.R
+++ Math/DE.ODE.Trig.Other.R
@@ -83,3 +83,35 @@ x*d2y - (p-1)*dy +
 ### Case: p = 1/2;
 x^2*d2y + (k^2+1/4)*y # = 0
 # k => sqrt(k - 1/4);
+x^2*d2y + k*y # = 0
+
+
+###########################
+
+### y = x^p * (sin(k*log(x)) + c1*cos(k*log(x)))
+# Same Polynomial coefficient for Sin & Cos;
+# Note: only c1 differs, but c1 has NO impact on ODE;
+
+# Check:
+x = 3^(3/5); c1 = 1/sqrt(2);
+k = sqrt(3); # k = 1;
+p = sqrt(2/5); # p = 1/2;
+params = list(x=x, k=k, p=p);
+e = expression(x^p * (sin(k*log(x)) + c1*cos(k*log(x))))[[1]];
+# e = expression(x^p * sin(sqrt(k-1/4)*log(x)))[[1]];
+#
+y   = eval(e, params);
+dy  = eval(D(e, "x"), params);
+d2y = eval(D(D(e, "x"), "x"), params);
+
+# D =>
+x*dy - p*y - k*x^p * (cos(k*log(x)) - c1*sin(k*log(x))) # = 0
+
+# D2 =>
+x^2*d2y - (p-1)*x*dy +
+	+ k^2*x^p * (sin(k*log(x)) + c1*cos(k*log(x))) +
+	- p*(x*dy - p*y) # = 0
+
+### ODE:
+x^2*d2y - (2*p-1)*x*dy + (p^2+k^2)*y # = 0
+
